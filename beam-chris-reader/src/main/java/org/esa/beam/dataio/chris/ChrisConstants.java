249d11637a24e1426df6170dc5f7c2c4c3e7ae9f
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-06-28 16:06:56 +0000

    - added attribute names for noise reduction
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@122 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-chris-reader/src/main/java/org/esa/beam/dataio/chris/ChrisConstants.java
+++ beam-chris-reader/src/main/java/org/esa/beam/dataio/chris/ChrisConstants.java
@@ -52,6 +52,7 @@ public class ChrisConstants {
 
     public static final String ATTR_NAME_SLIT_CORRECTION_APPLIED = "Slit Correction Applied";
     public static final String ATTR_NAME_NOISE_REDUCTION_APPLIED = "Noise Reduction Applied";
+    public static final String ATTR_NAME_ANGLES_USED_FOR_DESTRIPING = "Angles Used for Destriping";
 
     // Scientific data sets
     static final String SDS_NAME_RCI_IMAGE = "RCI Image";
