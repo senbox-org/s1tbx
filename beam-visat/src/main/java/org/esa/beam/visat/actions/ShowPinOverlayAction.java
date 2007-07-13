d07d460852e423ccb23c6a4dae3c585388fe52c2
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-07-13 11:46:09 +0000

    allow PinOverlay also if product has no GeoCoding
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@314 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-visat/src/main/java/org/esa/beam/visat/actions/ShowPinOverlayAction.java
+++ beam-visat/src/main/java/org/esa/beam/visat/actions/ShowPinOverlayAction.java
@@ -1,10 +1,8 @@
 package org.esa.beam.visat.actions;
 
-import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.framework.ui.command.ExecCommand;
 import org.esa.beam.framework.ui.product.ProductSceneView;
-import org.esa.beam.util.ProductUtils;
 import org.esa.beam.visat.VisatApp;
 
 public class ShowPinOverlayAction extends ExecCommand {
@@ -15,11 +13,8 @@ public class ShowPinOverlayAction extends ExecCommand {
         if (productSceneView == null) {
             return;
         }
-        Product product = productSceneView.getProduct();
-        if (ProductUtils.canGetPixelPos(product)) {
-            productSceneView.setPinOverlayEnabled(isSelected());
-            VisatApp.getApp().updateState();
-        }
+        productSceneView.setPinOverlayEnabled(isSelected());
+        VisatApp.getApp().updateState();
     }
 
     @Override
@@ -28,10 +23,7 @@ public class ShowPinOverlayAction extends ExecCommand {
         boolean enabled = false;
         if (productSceneView != null) {
             setSelected(productSceneView.isPinOverlayEnabled());
-            Product product = productSceneView.getProduct();
-            if (ProductUtils.canGetPixelPos(product)) {
-                enabled = true;
-            }
+            enabled = true;
         }
         setEnabled(enabled);
     }
