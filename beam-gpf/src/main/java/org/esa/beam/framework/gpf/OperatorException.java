53e5a070475d44ce83549381dd4ce2837ef26e59
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-08-08 10:24:32 +0000

    API doc
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@516 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf/src/main/java/org/esa/beam/framework/gpf/OperatorException.java
+++ beam-gpf/src/main/java/org/esa/beam/framework/gpf/OperatorException.java
@@ -8,14 +8,32 @@ package org.esa.beam.framework.gpf;
  */
 public class OperatorException extends Exception {
 
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public OperatorException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause   the cause
+     */
     public OperatorException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a new exception by delegating to
+     * {@link #OperatorException(String, Throwable) this(cause.getMessage(), cause)}.
+     *
+     * @param cause the cause
+     */
     public OperatorException(Throwable cause) {
         this(cause.getMessage(), cause);
     }
