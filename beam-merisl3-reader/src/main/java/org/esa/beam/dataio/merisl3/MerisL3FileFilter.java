ff32dce5618903ff3e4631a4b54019cd6dc5d313
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-08-08 14:37:21 +0000

    Directories not displayed when trying to open an Meris L3 Product
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@525 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-merisl3-reader/src/main/java/org/esa/beam/dataio/merisl3/MerisL3FileFilter.java
+++ beam-merisl3-reader/src/main/java/org/esa/beam/dataio/merisl3/MerisL3FileFilter.java
@@ -4,22 +4,19 @@ import org.esa.beam.util.io.BeamFileFilter;
 
 import java.io.File;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Norman
- * Date: 15.11.2006
- * Time: 09:39:11
- * To change this template use File | Settings | File Templates.
- */
 public class MerisL3FileFilter extends BeamFileFilter {
+
     public MerisL3FileFilter() {
         super(MerisL3ProductReaderPlugIn.FORMAT_NAME,
-                MerisL3ProductReaderPlugIn.FILE_EXTENSION,
-                MerisL3ProductReaderPlugIn.FORMAT_DESCRIPTION);
+              MerisL3ProductReaderPlugIn.FILE_EXTENSION,
+              MerisL3ProductReaderPlugIn.FORMAT_DESCRIPTION);
     }
 
     @Override
     public boolean accept(File file) {
+        if (file.isDirectory()) {
+            return true;
+        }
         if (super.accept(file)) {
             return isMerisBinnedL3Name(file.getName());
         }
@@ -27,7 +24,7 @@ public class MerisL3FileFilter extends BeamFileFilter {
     }
 
     /**
-     * Cecks if the given file name is valid.
+     * Checks if the given file name is valid.
      *
      * @param name the file name
      *
@@ -35,7 +32,7 @@ public class MerisL3FileFilter extends BeamFileFilter {
      */
     public static boolean isMerisBinnedL3Name(String name) {
         return name.startsWith("L3_ENV_MER_")
-              && name.indexOf("_GLOB_SI_") != -1
+               && name.indexOf("_GLOB_SI_") != -1
                && name.endsWith(MerisL3ProductReaderPlugIn.FILE_EXTENSION);
     }
 
