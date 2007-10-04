174454aa7a86112050892b7c4bd2bd4a44a13bcb
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-10-04 12:03:58 +0000

    - unmixing help
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@818 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-unmix/src/main/resources/doc/help/unmixing.hs
+++ beam-unmix/src/main/resources/doc/help/unmixing.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>BEAM Spectral Unmixing Tool Help</title>
+    <title>Spectral Unmixing Tool Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.jhm"/>
