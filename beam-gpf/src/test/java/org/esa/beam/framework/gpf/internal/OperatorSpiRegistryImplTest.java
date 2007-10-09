8b5bd1ce60067b3d7dc3dd17bdb7f64b57734fa2
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-10-09 10:50:42 +0000

    NodeContext now directly uses OperatorContext
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@872 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-gpf/src/test/java/org/esa/beam/framework/gpf/internal/OperatorSpiRegistryImplTest.java
@@ -0,0 +1,38 @@
+package org.esa.beam.framework.gpf.internal;
+
+import junit.framework.TestCase;
+import org.esa.beam.framework.gpf.AbstractOperatorSpi;
+import org.esa.beam.framework.gpf.GPF;
+import org.esa.beam.framework.gpf.TestOps;
+import org.esa.beam.framework.gpf.annotations.OperatorAlias;
+
+/**
+ * A registry for operator SPI instances.
+ *
+ * @author Marco Zuehlke
+ * @version $Revision: 1.1 $ $Date: 2007/03/27 12:51:05 $
+ * @since 4.1
+ */
+public class OperatorSpiRegistryImplTest extends TestCase {
+
+    public void testAlias() {
+        DummyOp.Spi spi = new DummyOp.Spi();
+        GPF.getDefaultInstance().getOperatorSpiRegistry().addOperatorSpi(spi);
+        assertSame(spi, GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi(DummyOp.Spi.class.getName()));
+        assertSame(spi, GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi("Heino"));
+        assertSame(spi, GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi("Willi"));
+        GPF.getDefaultInstance().getOperatorSpiRegistry().removeOperatorSpi(spi);
+        assertNull(GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi(DummyOp.Spi.class.getName()));
+        assertNull(GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi("Heino"));
+        assertNull(GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi("Willi"));
+    }
+
+    public static class DummyOp extends TestOps.Op1 {
+        @OperatorAlias("Heino")
+        public static class Spi extends AbstractOperatorSpi {
+            public Spi() {
+                super(DummyOp.class, "Willi");
+            }
+        }
+    }
+}
