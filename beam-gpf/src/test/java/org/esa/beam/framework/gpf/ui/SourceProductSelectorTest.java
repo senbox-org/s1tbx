38469affb0f97eeb58825ce256373ddba9c4bb81
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-10-31 11:38:23 +0000

    fix test
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@1170 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf/src/test/java/org/esa/beam/framework/gpf/ui/SourceProductSelectorTest.java
+++ beam-gpf/src/test/java/org/esa/beam/framework/gpf/ui/SourceProductSelectorTest.java
@@ -2,8 +2,13 @@ package org.esa.beam.framework.gpf.ui;
 
 import junit.framework.TestCase;
 import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.ui.AppContext;
+import org.esa.beam.util.PropertyMap;
 
+import java.awt.Window;
 import java.io.File;
+
+import javax.swing.JOptionPane;
 
 /**
  * @author Ralf Quast
@@ -12,6 +17,7 @@ import java.io.File;
 public class SourceProductSelectorTest extends TestCase {
 
     private Product[] defaultProducts;
+    private AppContext appContext;
 
 
     @Override
@@ -20,26 +26,29 @@ public class SourceProductSelectorTest extends TestCase {
         for (int i = 0; i < defaultProducts.length; i++) {
             defaultProducts[i] = new Product("P" + i, "T" + i, 10, 10);
         }
+        appContext = new MockAppContext();
 
     }
 
     public void testCreatedUIComponentsNotNull() {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source:");
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
+        selector.initProductList();
         assertNotNull(selector.getProductNameLabel());
         assertNotNull(selector.getProductNameComboBox());
         assertNotNull(selector.getProductFileChooserButton());
     }
 
     public void testCreatedUIComponentsAreSame() {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source:");
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
+        selector.initProductList();
         assertSame(selector.getProductNameLabel(), selector.getProductNameLabel());
         assertSame(selector.getProductNameComboBox(), selector.getProductNameComboBox());
         assertSame(selector.getProductFileChooserButton(), selector.getProductFileChooserButton());
     }
 
     public void testSetSelectedProduct() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source");
-
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+        selector.initProductList();
         Product selectedProduct = selector.getSelectedProduct();
         assertNull(selectedProduct);
 
@@ -61,8 +70,9 @@ public class SourceProductSelectorTest extends TestCase {
     }
 
     public void testSetSelectedProductThrowsException() {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source", "T.");
-
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source", "T.");
+        selector.initProductList();
+        
         Product newProduct = new Product("new", "T1", 0, 0);
         try {
             selector.setSelectedProduct(newProduct);
@@ -87,7 +97,8 @@ public class SourceProductSelectorTest extends TestCase {
     }
 
     public void testDispose() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source");
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+        selector.initProductList();
         try {
             selector.dispose();
         } catch (Throwable e) {
@@ -95,8 +106,9 @@ public class SourceProductSelectorTest extends TestCase {
         }
 
 
-        selector = new SourceProductSelector(defaultProducts, "Source");
-
+        selector = new SourceProductSelector(appContext, "Source");
+        selector.initProductList();
+        
         Product newProduct = new Product("new", "T1", 0, 0);
         newProduct.setFileLocation(new File(""));
         selector.setSelectedProduct(newProduct);
@@ -121,10 +133,43 @@ public class SourceProductSelectorTest extends TestCase {
     }
 
     public void testSetSelectedIndex() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(defaultProducts, "Source", "T.");
+        SourceProductSelector selector = new SourceProductSelector(appContext, "Source", "T.");
+        selector.initProductList();
         assertNull(selector.getSelectedProduct());
 
         selector.setSelectedIndex(0);
         assertSame(defaultProducts[0], selector.getSelectedProduct());
+    }
+    
+    private class MockAppContext implements AppContext {
+        private PropertyMap preferences = new PropertyMap();
+
+        public void addProduct(Product product) {
+            System.out.println("product added: " + product);
+        }
+
+        public Product[] getProducts() {
+            return defaultProducts;
+        }
+
+        public Product getSelectedProduct() {
+            return defaultProducts[0];
+        }
+
+        public Window getApplicationWindow() {
+            return null;
+        }
+
+        public String getApplicationName() {
+            return "Killer App";
+        }
+
+        public void handleError(Throwable e) {
+            JOptionPane.showMessageDialog(getApplicationWindow(), e.getMessage());
+        }
+
+        public PropertyMap getPreferences() {
+            return preferences;
+        }
     }
 }
