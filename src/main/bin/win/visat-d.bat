e13870ef3fdeeec1d15330f45c4478c8c615818e
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-10-31 09:00:35 +0000

    fix
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@1156 d90d2d48-2b33-0410-a973-e72f85796a02

--- src/main/bin/win/visat-d.bat
+++ src/main/bin/win/visat-d.bat
@@ -5,7 +5,7 @@ set BEAM4_HOME=${installer:sys.installationDir}
 "%BEAM4_HOME%\jre\bin\java.exe" ^
     -Xmx1024M ^
     -Dceres.context=beam ^
-    -Dceres.debug=true ^
+    -Dbeam.debug=true ^
     "-Dbeam.home=%BEAM4_HOME%" ^
     -jar "%BEAM4_HOME%\bin\ceres-launcher.jar" -d %*
 
