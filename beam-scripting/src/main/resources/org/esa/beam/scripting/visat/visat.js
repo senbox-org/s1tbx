15b2824c037ba1f2956b7b3811d30ed575868b8d
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-12-18 16:18:41 +0000

    scripting console now has some standard includes (visat.js)
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@1595 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-scripting/src/main/resources/org/esa/beam/scripting/visat/visat.js
+++ beam-scripting/src/main/resources/org/esa/beam/scripting/visat/visat.js
@@ -1,6 +1,8 @@
 importPackage(org.esa.beam.framework.datamodel);
 importPackage(org.esa.beam.framework.dataio);
 importPackage(org.esa.beam.framework.gpf);
+importPackage(org.esa.beam.framework.gpf.operators.common);
+importPackage(org.esa.beam.framework.gpf.operators.meris);
 importPackage(org.esa.beam.visat);
 
 visat = VisatApp.getApp();
