20a9bdbb6560a0832cdb8986839b2279c7a16624
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-06-11 15:29:21 +0000

    [BEAM-826] Support for user defined layout
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@2173 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/layout/SaveAsUserLayoutAction.java
@@ -0,0 +1,20 @@
+package org.esa.beam.visat.actions.layout;
+
+import com.jidesoft.swing.LayoutPersistence;
+import org.esa.beam.visat.VisatApp;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.beam.framework.ui.command.CommandEvent;
+
+/**
+ * User: Marco Peters
+* Date: 11.06.2008
+*/
+public class SaveAsUserLayoutAction extends AbstractVisatAction {
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+        final LayoutPersistence layoutPersistence = VisatApp.getApp().getMainFrame().getLayoutPersistence();
+        layoutPersistence.saveLayoutDataAs("user");
+    }
+
+}
