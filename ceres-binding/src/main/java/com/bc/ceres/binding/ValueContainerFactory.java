19d3959d4147f56b596b747a5d826640c274f292
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-06-12 15:00:59 +0000

    reverted
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@2201 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-binding/src/main/java/com/bc/ceres/binding/ValueContainerFactory.java
+++ ceres-binding/src/main/java/com/bc/ceres/binding/ValueContainerFactory.java
@@ -75,32 +75,18 @@ public class ValueContainerFactory {
     }
 
     /**
-     * Creates a value container for the given object used to store the values.
+     * Creates a value container for the given object.
      * The factory method will not modify the object, thus not setting any default values.
      *
      * @param object the backing object
      * @return the value container
      */
     public ValueContainer createObjectBackedValueContainer(Object object) {
-        return createObjectBackedValueContainer(object.getClass(), object);
+        return create(object.getClass(), new ObjectBackedValueAccessorFactory(object), false);
     }
 
     /**
-     * Creates a value container for for the given template type and object used to store the values.
-     * The object's class does not necessarily be the same as the template type, but is shall
-     * contain all the fields contained in the template type.
-     * The factory method will not modify the object, thus not setting any default values.
-     *
-     * @param templateType the template type
-     * @param object       the backing object
-     * @return the value container
-     */
-    public ValueContainer createObjectBackedValueContainer(Class<?> templateType, Object object) {
-        return create(templateType, new ObjectBackedValueAccessorFactory(object), false);
-    }
-
-    /**
-     * Creates a value container for the given template type and a map used to store the values.
+     * Creates a value container for the given template type and map backing the values.
      * The factory method will not modify the given map, thus not setting any default values.
      *
      * @param templateType the template type
