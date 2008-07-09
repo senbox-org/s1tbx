4b9c9067e00c13da76cac72ee7c5a360bd0dd59e
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-07-09 12:10:01 +0000

    imports
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@2535 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-collocation/src/main/java/org/esa/beam/collocation/visat/CollocationAction.java
+++ beam-collocation/src/main/java/org/esa/beam/collocation/visat/CollocationAction.java
@@ -1,6 +1,5 @@
 package org.esa.beam.collocation.visat;
 
-import org.esa.beam.framework.ui.ModalDialog;
 import org.esa.beam.framework.ui.ModelessDialog;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.visat.actions.AbstractVisatAction;
