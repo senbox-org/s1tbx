c77ca97cb7758bf91188a3e50fd4a6b58a878256
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-07-18 11:40:26 +0000

    test
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@2772 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-glayer/src/test/java/com/bc/ceres/glayer/BufferedImageRenderingTest.java
+++ ceres-glayer/src/test/java/com/bc/ceres/glayer/BufferedImageRenderingTest.java
@@ -3,18 +3,22 @@ package com.bc.ceres.glayer;
 import junit.framework.TestCase;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 
 public class BufferedImageRenderingTest extends TestCase {
     public void testViewIsAnImage() {
-        final Rendering rendering = new BufferedImageRendering(150, 150);
+        final BufferedImageRendering rendering = new BufferedImageRendering(3, 3);
 
         ShapeLayer sl = new ShapeLayer(new Shape[]{
-                new Rectangle(0, 0, 100, 100),
-                new Rectangle(50, 50, 100, 100),
+                new Rectangle2D.Double(0, 0, 2, 2),
         });
 
         sl.render(rendering);
+
+        assertEquals(Color.BLACK.getRGB(), rendering.getImage().getRGB(0, 0));
+        assertEquals(Color.WHITE.getRGB(), rendering.getImage().getRGB(1, 1));
+        assertEquals(Color.BLACK.getRGB(), rendering.getImage().getRGB(2, 2));
     }
 
 }
