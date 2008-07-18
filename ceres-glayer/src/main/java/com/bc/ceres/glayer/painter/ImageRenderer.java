a6dc60007cdd5f87edd62de39b3f453458d9bf55
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-07-18 11:36:31 +0000

    major refactoring
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@2771 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-glayer/src/main/java/com/bc/ceres/glayer/painter/ImageRenderer.java
+++ ceres-glayer/src/main/java/com/bc/ceres/glayer/painter/ImageRenderer.java
@@ -7,23 +7,22 @@ import com.bc.ceres.glayer.level.LevelImage;
 import java.awt.Graphics2D;
 
 /**
- * An {@code ImagePainter} is used to paint multi-resolution {@link LevelImage}s
+ * An {@code ImageRenderer} is used to render multi-resolution {@link LevelImage}s
  * at a certain level.
  */
 public interface ImageRenderer {
     /**
-     * Paints a {@link LevelImage} at the given resolution level
-     * on the given {@link Graphics2D} context using the given {@link Viewport}.
+     * Renders a {@link LevelImage} at the specified level onto the given {@link Rendering}.
      *
-     * @param rendering
+     * @param rendering  The rendering.
      * @param levelImage The current level image.
      * @param level      the current resolution level.
      */
     void renderImage(Rendering rendering, LevelImage levelImage, int level);
 
     /**
-     * Releases any allocated resources for {@code ImagePainter}s which maintain a state during calls
-     * to {@link #renderImage}.
+     * Releases any allocated resources for {@code ImageRenderer}s, which maintain a state in between
+     * calls to {@link #renderImage}.
      */
     void dispose();
 }
