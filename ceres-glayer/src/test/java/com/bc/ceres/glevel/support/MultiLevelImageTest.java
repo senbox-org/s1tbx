1ce4ae992c63331b06b056cd7223f03a80479702
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-09-05 11:46:03 +0000

    new tests
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@2978 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-glayer/src/test/java/com/bc/ceres/glevel/support/MultiLevelImageTest.java
+++ ceres-glayer/src/test/java/com/bc/ceres/glevel/support/MultiLevelImageTest.java
@@ -1,55 +1,32 @@
 package com.bc.ceres.glevel.support;
 
-import com.bc.ceres.glevel.MultiLevelSource;
 import junit.framework.TestCase;
 
 import javax.media.jai.PlanarImage;
-import javax.media.jai.RenderedOp;
-import javax.media.jai.operator.FormatDescriptor;
-import javax.media.jai.operator.MultiplyConstDescriptor;
-import java.awt.Rectangle;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.RenderedImage;
 
-
+/**
+ * TODO - Apidoc
+ *
+ * @author Norman Fomferra
+ * @version $revision$ $date$
+ */
 public class MultiLevelImageTest extends TestCase {
+    public void testAllProperties() {
+        final PlanarImage sourceImage = createSourceImage(2, 2);
+        final DefaultMultiLevelSource mls = new DefaultMultiLevelSource(sourceImage);
+        final MultiLevelImage mli = new MultiLevelImage(mls);
 
-    public void testIt() {
-        final PlanarImage src = createSourceImage(16, 16);
+        assertSame(mls, mli.getMultiLevelSource());
+        assertSame(sourceImage, mli.getWrappedImage());
+        assertSame(mls.getLevelImage(0), mli.getWrappedImage());
 
-        MultiLevelImage mri = createGeophysicalSourceImage(src);
-        assertEquals(3, mri.getModel().getLevelCount());
-        assertSame(mri.getWrappedImage(), mri.getLevelImage(0));
-
-        final RenderedImage l0 = mri.getLevelImage(0);
-        assertSame(l0, mri.getLevelImage(0));
-        assertEquals(DataBuffer.TYPE_DOUBLE, l0.getSampleModel().getDataType());
-        assertEquals(16, l0.getWidth());
-        assertEquals(16, l0.getHeight());
-
-        final RenderedImage l1 = mri.getLevelImage(1);
-        assertSame(l1, mri.getLevelImage(1));
-        assertEquals(DataBuffer.TYPE_DOUBLE, l1.getSampleModel().getDataType());
-        assertEquals(8, l1.getWidth());
-        assertEquals(8, l1.getHeight());
-
-        final RenderedImage l2 = mri.getLevelImage(2);
-        assertSame(l2, mri.getLevelImage(2));
-        assertEquals(DataBuffer.TYPE_DOUBLE, l2.getSampleModel().getDataType());
-        assertEquals(4, l2.getWidth());
-        assertEquals(4, l2.getHeight());
+        assertEquals(0, mli.getData().getSample(0, 0, 0));
+        assertEquals(1, mli.getData().getSample(1, 0, 0));
+        assertEquals(2, mli.getData().getSample(0, 1, 0));
+        assertEquals(3, mli.getData().getSample(1, 1, 0));
     }
 
-    private MultiLevelImage createGeophysicalSourceImage(PlanarImage src) {
-        if (src instanceof MultiLevelSource) {
-            MultiLevelSource multiLevelSource = (MultiLevelSource) src;
-            return new MultiLevelImage(new LIS(multiLevelSource));
-        }
-        final DefaultMultiLevelModel model = new DefaultMultiLevelModel(3, new AffineTransform(), new Rectangle(8, 8));
-        return new MultiLevelImage(new LIS(new DefaultMultiLevelSource(src, model)));
-    }
 
     static PlanarImage createSourceImage(int w, int h) {
         final BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
@@ -58,24 +35,5 @@ public class MultiLevelImageTest extends TestCase {
         bi.getRaster().setSample(0, 1, 0, 2);
         bi.getRaster().setSample(1, 1, 0, 3);
         return PlanarImage.wrapRenderedImage(bi);
-    }
-
-    private static class LIS extends AbstractMultiLevelSource {
-        MultiLevelSource multiLevelSource;
-
-        private LIS(MultiLevelSource multiLevelSource) {
-            super(multiLevelSource.getModel());
-            this.multiLevelSource = multiLevelSource;
-        }
-
-        protected RenderedImage createLevelImage(int level) {
-            final RenderedImage sourceLevelImage = multiLevelSource.getLevelImage(level);
-            return createTargetLevelImage(sourceLevelImage);
-        }
-
-        protected RenderedImage createTargetLevelImage(RenderedImage sourceLevelImage) {
-            final RenderedOp dst = FormatDescriptor.create(PlanarImage.wrapRenderedImage(sourceLevelImage), DataBuffer.TYPE_DOUBLE, null);
-            return MultiplyConstDescriptor.create(dst, new double[]{2.5}, null);
-        }
     }
 }
