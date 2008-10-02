22d62b19b53e4a5f9e0d8a818629bdf0e4c3e87e
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-10-02 12:26:23 +0000

    using special action to show warning if the product is not map projected.
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@3244 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-geotiff/src/main/java/org/esa/beam/dataio/geotiff/GeoTiffExportAction.java
@@ -0,0 +1,39 @@
+package org.esa.beam.dataio.geotiff;
+
+import org.esa.beam.framework.datamodel.MapGeoCoding;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.visat.VisatApp;
+import org.esa.beam.visat.actions.ProductExportAction;
+
+import javax.swing.JOptionPane;
+import java.io.File;
+
+/**
+ * todo - add API doc
+ *
+ * @author Marco Peters
+ * @version $Revision: $ $Date: $
+ * @since BEAM 4.5
+ */
+public class GeoTiffExportAction extends ProductExportAction {
+
+    @Override
+    protected File promptForFile(Product product) {
+        if(!(product.getGeoCoding() instanceof MapGeoCoding)) {
+            final String message = String.format("The product %s is not map projected.\n" +
+                                                 "Un-projected raster data is not well supported by other GIS software.\n" +
+                                                 "\n" +
+                                                 "Do you want to export the product without a map projection?",
+                                                 product.getName());
+            final int answer = JOptionPane.showConfirmDialog(VisatApp.getApp().getMainFrame(), message, getText(),
+                                                             JOptionPane.YES_NO_OPTION,
+                                                             JOptionPane.WARNING_MESSAGE);
+            if(answer != JOptionPane.YES_OPTION) {
+                return null;
+            }
+
+        }
+        return super.promptForFile(product);
+
+    }
+}
