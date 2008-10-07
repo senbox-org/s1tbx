77f814382d56f62b34263b93897bf4bba1133f66
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-10-07 07:15:26 +0000

    moved help to geotiff module
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@3260 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-geotiff/src/main/resources/doc/help/geotiff.hs
@@ -0,0 +1,24 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+        PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+        "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="2.0">
+    <title>GeoTIFF IO Help</title>
+    <maps>
+        <homeID>top</homeID>
+        <mapref location="map.jhm"/>
+    </maps>
+    <view mergetype="javax.help.UniteAppendMerge">
+        <name>TOC</name>
+        <label>Contents</label>
+        <type>javax.help.TOCView</type>
+        <data>toc.xml</data>
+    </view>
+    <view>
+        <name>Search</name>
+        <label>Search</label>
+        <type>javax.help.SearchView</type>
+        <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+    </view>
+</helpset>
