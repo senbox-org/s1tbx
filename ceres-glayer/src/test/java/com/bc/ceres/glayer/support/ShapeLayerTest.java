42617da95b4d1ccf6caa1671b198992fb20b8101
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-10-27 09:02:47 +0000

    fix
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@3396 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-glayer/src/test/java/com/bc/ceres/glayer/support/ShapeLayerTest.java
+++ ceres-glayer/src/test/java/com/bc/ceres/glayer/support/ShapeLayerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static com.bc.ceres.glayer.Assert2D.*;
 
 import java.awt.Shape;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.util.List;
@@ -37,14 +38,14 @@ public class ShapeLayerTest  {
         layer = new ShapeLayer(new Shape[0]);
         assertEquals(null, layer.getModelBounds());
 
-        Shape shape = new Rectangle2D.Double(10.0, -30.0, 320.0, 200.0);
+        Shape shape = new Rectangle(10, -30, 320, 200);
         layer = new ShapeLayer(new Shape[]{shape});
         assertNotNull(layer.getModelBounds());
-        assertEquals(new Rectangle2D.Double(10.0, -30.0, 320.0, 200.0), layer.getModelBounds());
+        assertEquals(new Rectangle(10, -30, 320, 200), layer.getModelBounds());
 
         final AffineTransform s2m = new AffineTransform(0.5, 0, 0, 0.5, -25.5, 50.3);
         layer = new ShapeLayer(new Shape[]{shape}, s2m);
         assertNotNull(layer.getModelBounds());
-        assertEquals(new Rectangle2D.Double(-25.5, 50.3, 160.0, 100.0), layer.getModelBounds());
+        assertEquals(new Rectangle2D.Double(10 * 0.5 -25.5, -30 * 0.5 + 50.3, 320 * 0.5, 200*0.5), layer.getModelBounds());
     }
 }
