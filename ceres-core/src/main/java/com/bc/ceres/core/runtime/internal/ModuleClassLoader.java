1a89ae9f65a2c2510342461e4aafc8c83cc3bd0b
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-10-28 13:12:16 +0000

    improved performance of loading resources by caching already resolved resource urls
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@3425 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-core/src/main/java/com/bc/ceres/core/runtime/internal/ModuleClassLoader.java
+++ ceres-core/src/main/java/com/bc/ceres/core/runtime/internal/ModuleClassLoader.java
@@ -1,12 +1,15 @@
 package com.bc.ceres.core.runtime.internal;
 
-import java.net.URLClassLoader;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Collections;
-import java.util.Iterator;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A class loader used for modules. It adds the following features to the {@link URLClassLoader}.
@@ -19,6 +22,7 @@ class ModuleClassLoader extends URLClassLoader {
 
     private ClassLoader[] delegates;
     private URL[] nativeUrls;
+    private Map<ClassLoader, Map<String, List<URL>>> resolvedResources;
 
     public ModuleClassLoader(ClassLoader[] delegates,
                              URL[] dependencyUrls,
@@ -27,6 +31,7 @@ class ModuleClassLoader extends URLClassLoader {
         super(dependencyUrls, parent);
         this.nativeUrls = nativeUrls;
         this.delegates = delegates;
+        resolvedResources = new HashMap<ClassLoader, Map<String, List<URL>>>();
     }
 
     @Override
@@ -68,18 +73,35 @@ class ModuleClassLoader extends URLClassLoader {
 
     @Override
     public Enumeration<URL> findResources(final String name) throws IOException {
-        // todo - check - first search in delegates???
 
         final Enumeration<URL> resources = super.findResources(name);
         if (delegates.length == 0) {
             return resources;
         }
 
-        final HashSet<URL> urls = new HashSet<URL>(Collections.list(resources));
+        final Set<URL> urls = new HashSet<URL>(Collections.list(resources));
         for (ClassLoader delegate : delegates) {
-            urls.addAll(Collections.list(delegate.getResources(name)));
+            final Map<String, List<URL>> resourceMap = getResourceMap(delegate);
+            if(resourceMap.containsKey(name)) {
+                urls.addAll(resourceMap.get(name));
+            }else {
+                final List<URL> urlArrayList = Collections.list(delegate.getResources(name));
+                urls.addAll(urlArrayList);
+                resourceMap.put(name, urlArrayList);
+            }
         }
         return Collections.enumeration(urls) ;
+    }
+
+    private Map<String, List<URL>> getResourceMap(ClassLoader delegate) {
+        Map<String, List<URL>> resourceMap;
+        if(resolvedResources.containsKey(delegate)) {
+            resourceMap = resolvedResources.get(delegate);
+        }else {
+            resourceMap = new HashMap<String, List<URL>>();
+            resolvedResources.put(delegate, resourceMap);
+        }
+        return resourceMap;
     }
 
     @Override
