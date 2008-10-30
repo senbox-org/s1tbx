cb3a653af629cfa59f75b2e9450ff5f108aeb38e
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-10-30 10:59:54 +0000

    check for null
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@3483 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-ui/src/main/java/com/bc/swing/desktop/TabbedDesktopPane.java
+++ beam-ui/src/main/java/com/bc/swing/desktop/TabbedDesktopPane.java
@@ -18,10 +18,10 @@ package com.bc.swing.desktop;
 
 import com.jidesoft.swing.JideTabbedPane;
 
+import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -32,14 +32,6 @@ import java.beans.PropertyVetoException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.InternalFrameAdapter;
-import javax.swing.event.InternalFrameEvent;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
 
 /**
  * @author Norman Fomferra (norman.fomferra@brockmann-consult.de)
@@ -222,7 +214,10 @@ public class TabbedDesktopPane extends JPanel {
 // <JIDE>
         tabbedPane.setCloseAction(new AbstractAction() {
             public void actionPerformed(ActionEvent e) {
-                closeFrame(getSelectedFrame());
+                final JInternalFrame selectedFrame = getSelectedFrame();
+                if(selectedFrame != null) {
+                    closeFrame(selectedFrame);
+                }
             }
         });
         tabbedPane.setShowCloseButton(true);
