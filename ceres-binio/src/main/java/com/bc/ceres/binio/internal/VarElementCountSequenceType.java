b2da956c17ebc564cd9b990fde4de0291d1b926d
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-11-27 15:33:43 +0000

    can now specify element count resolver in sequence type
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@3787 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ ceres-binio/src/main/java/com/bc/ceres/binio/internal/VarElementCountSequenceType.java
@@ -0,0 +1,69 @@
+package com.bc.ceres.binio.internal;
+
+import com.bc.ceres.binio.*;
+import com.bc.ceres.binio.util.TypeBuilder;
+import com.bc.ceres.core.Assert;
+
+import java.io.IOException;
+
+public abstract class VarElementCountSequenceType extends AbstractType implements VarSequenceType {
+    private final String name;
+    private final Type elementType;
+
+    protected VarElementCountSequenceType(Type elementType) {
+        this(elementType.getName() + "[]", elementType);
+    }
+
+    protected VarElementCountSequenceType(String name, Type elementType) {
+        Assert.notNull(name, "name");
+        Assert.notNull(elementType, "elementType");
+        this.name = name;
+        this.elementType = elementType;
+    }
+
+    public SequenceType resolve(CollectionData parent) throws IOException {
+        int elementCount = resolveElementCount(parent);
+        if (elementCount < 0) {
+            throw new IllegalStateException("elementCount < 0");
+        }
+        return TypeBuilder.SEQ(elementType, elementCount);
+    }
+
+    protected abstract int resolveElementCount(CollectionData parent) throws IOException;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public final int getSize() {
+        return -1;
+    }
+
+    @Override
+    public final Type getElementType() {
+        return elementType;
+    }
+
+    @Override
+    public final int getElementCount() {
+        return -1;
+    }
+
+    @Override
+    public final boolean isCollectionType() {
+        return true;
+    }
+
+    @Override
+    public final boolean isSequenceType() {
+        return true;
+    }
+
+    @Override
+    public final void visit(TypeVisitor visitor) {
+        getElementType().visit(visitor);
+        visitor.accept(this);
+    }
+}
