2825c0c8c9b4eb89702027ecfb3ece36a97748ff
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-01-13 09:52:27 +0000

    Reimplemented DefaulMultiLevelImage
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@4019 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-glayer/src/main/java/com/bc/ceres/glevel/support/DefaultMultiLevelImage.java
+++ ceres-glayer/src/main/java/com/bc/ceres/glevel/support/DefaultMultiLevelImage.java
@@ -1,40 +1,56 @@
 package com.bc.ceres.glevel.support;
 
-import com.bc.ceres.glevel.MultiLevelSource;
-import com.bc.ceres.glevel.MultiLevelModel;
 import com.bc.ceres.glevel.MultiLevelImage;
+import com.bc.ceres.glevel.MultiLevelModel;
+import com.bc.ceres.glevel.MultiLevelSource;
 
-import javax.media.jai.RenderedImageAdapter;
+import javax.media.jai.ImageLayout;
+import javax.media.jai.PlanarImage;
+import java.awt.Rectangle;
+import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
 
 /**
- * Adapts a JAI {@link javax.media.jai.PlanarImage PlanarImage} to the {@link com.bc.ceres.glevel.MultiLevelSource} interface.
+ * Adapts a JAI {@link javax.media.jai.PlanarImage PlanarImage} to the
+ * {@link com.bc.ceres.glevel.MultiLevelSource} interface.
  * The image data provided by this {@code PlanarImage} corresponds to the level zero image of the given
  * {@code MultiLevelSource}.
  *
  * @author Norman Fomferra
  * @version $revision$ $date$
  */
-public class DefaultMultiLevelImage extends RenderedImageAdapter implements MultiLevelImage {
+public class DefaultMultiLevelImage extends PlanarImage implements MultiLevelImage {
 
     private final MultiLevelSource source;
 
+    /**
+     * Constructs a new multi-level image from the given source.
+     *
+     * @param source The multi-level image source.
+     */
     public DefaultMultiLevelImage(MultiLevelSource source) {
-        super(source.getImage(0));
+        super(new ImageLayout(source.getImage(0)), null, null);
         this.source = source;
     }
 
-    public MultiLevelSource getSource() {
+    /**
+     * @return The multi-level image source.
+     */
+    public final MultiLevelSource getSource() {
         return source;
     }
 
+    /////////////////////////////////////////////////////////////////////////
+    // MultiLevelImage interface
+
     @Override
-    public MultiLevelModel getModel() {
+    public final MultiLevelModel getModel() {
         return source.getModel();
     }
 
     @Override
-    public RenderedImage getImage(int level) {
+    public final RenderedImage getImage(int level) {
         return source.getImage(level);
     }
 
@@ -44,8 +60,31 @@ public class DefaultMultiLevelImage extends RenderedImageAdapter implements Mult
     }
 
     @Override
-    public synchronized void dispose() {
-        reset();
+    public void dispose() {
+        source.reset();
         super.dispose();
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    // PlanarImage interface
+
+    @Override
+    public final Raster getTile(int x, int y) {
+        return getImage(0).getTile(x, y);
+    }
+
+    @Override
+    public final Raster getData() {
+        return getImage(0).getData();
+    }
+
+    @Override
+    public final Raster getData(Rectangle rect) {
+        return getImage(0).getData(rect);
+    }
+
+    @Override
+    public final WritableRaster copyData(WritableRaster raster) {
+        return getImage(0).copyData(raster);
     }
 }
--- ceres-glayer/src/test/java/com/bc/ceres/glevel/support/DefaultMultiLevelImageTest.java
+++ ceres-glayer/src/test/java/com/bc/ceres/glevel/support/DefaultMultiLevelImageTest.java
@@ -4,30 +4,112 @@ import junit.framework.TestCase;
 
 import javax.media.jai.PlanarImage;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.awt.geom.AffineTransform;
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+import com.bc.ceres.glevel.MultiLevelSource;
 
 public class DefaultMultiLevelImageTest extends TestCase {
-    public void testAllProperties() {
-        final PlanarImage sourceImage = createSourceImage(2, 2);
-        final DefaultMultiLevelSource mls = new DefaultMultiLevelSource(sourceImage, 1);
-        final DefaultMultiLevelImage mli = new DefaultMultiLevelImage(mls);
+
+    public void testSamplesAreProvidedFromSource() {
+        PlanarImage sourceImage = createSingleBandedByteImage(2, 2);
+        DefaultMultiLevelSource mls = new DefaultMultiLevelSource(sourceImage, 1);
+        DefaultMultiLevelImage mli = new DefaultMultiLevelImage(mls);
 
         assertSame(mls, mli.getSource());
-        assertSame(sourceImage, mli.getWrappedImage());
-        assertSame(mls.getImage(0), mli.getWrappedImage());
+        assertSame(sourceImage, mli.getImage(0));
+        assertSame(mls.getImage(0), mli.getImage(0));
 
-        assertEquals(0, mli.getData().getSample(0, 0, 0));
-        assertEquals(1, mli.getData().getSample(1, 0, 0));
-        assertEquals(2, mli.getData().getSample(0, 1, 0));
-        assertEquals(3, mli.getData().getSample(1, 1, 0));
+        assertEquals(11, mli.getData().getSample(0, 0, 0));
+        assertEquals(22, mli.getData().getSample(1, 0, 0));
+        assertEquals(33, mli.getData().getSample(0, 1, 0));
+        assertEquals(44, mli.getData().getSample(1, 1, 0));
     }
 
+    public void testLevelInstances() {
+        DefaultMultiLevelImage mli = createSomeDefaultMultiLevelImage();
 
-    static PlanarImage createSourceImage(int w, int h) {
+        RenderedImage a0 = mli.getImage(0);
+        assertNotNull(a0);
+        assertSame(a0, mli.getImage(0));
+        RenderedImage a1 = mli.getImage(1);
+        assertNotNull(a1);
+        assertSame(a1, mli.getImage(1));
+
+        mli.reset();
+
+        RenderedImage b0 = mli.getImage(0);
+        assertNotNull(b0);
+        assertSame(b0, mli.getImage(0));
+        assertNotSame(a0, b0);
+
+        RenderedImage b1 = mli.getImage(1);
+        assertNotNull(b1);
+        assertSame(b1, mli.getImage(1));
+        assertNotSame(a1, b1);
+    }
+
+    public void testProperties() {
+        DefaultMultiLevelImage mli = createSomeDefaultMultiLevelImage();
+        PCL pcl = new PCL();
+
+        assertEquals(Image.UndefinedProperty, mli.getProperty("_x"));
+
+        mli.addPropertyChangeListener(pcl);
+        mli.setProperty("_x", 4384);
+
+        assertEquals(4384, mli.getProperty("_x"));
+        assertEquals(Integer.class, mli.getPropertyClass("_x"));
+        assertEquals("_x;", pcl.trace);
+
+        String[] propertyNames = mli.getPropertyNames();
+        assertEquals(1, propertyNames.length);
+        assertEquals("_x", propertyNames[0]);
+
+        propertyNames = mli.getPropertyNames("_");
+        assertEquals(1, propertyNames.length);
+        assertEquals("_x", propertyNames[0]);
+
+        mli.removeProperty("_x");
+        assertEquals(Image.UndefinedProperty, mli.getProperty("_x"));
+    }
+
+    private DefaultMultiLevelImage createSomeDefaultMultiLevelImage() {
+        DefaultMultiLevelModel model = new DefaultMultiLevelModel(2, new AffineTransform(), 256, 256);
+        MultiLevelSource mls = new TestMultiLevelSource(model);
+        return new DefaultMultiLevelImage(mls);
+    }
+
+    static PlanarImage createSingleBandedByteImage(int w, int h) {
         final BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
-        bi.getRaster().setSample(0, 0, 0, 0);
-        bi.getRaster().setSample(1, 0, 0, 1);
-        bi.getRaster().setSample(0, 1, 0, 2);
-        bi.getRaster().setSample(1, 1, 0, 3);
+        bi.getRaster().setSample(0, 0, 0, 11);
+        bi.getRaster().setSample(1, 0, 0, 22);
+        bi.getRaster().setSample(0, 1, 0, 33);
+        bi.getRaster().setSample(1, 1, 0, 44);
         return PlanarImage.wrapRenderedImage(bi);
+    }
+
+    private static class TestMultiLevelSource extends AbstractMultiLevelSource {
+        public TestMultiLevelSource(DefaultMultiLevelModel model) {
+            super(model);
+        }
+
+        @Override
+            protected RenderedImage createImage(int level) {
+            // todo - The knowledge how width and height are computed should go either into AbstractMultiLevelSource or into the MultiLevelModel (nf 20090113)
+            int width = (int) Math.floor(256 / getModel().getScale(level));
+            int height = (int) Math.floor(256 / getModel().getScale(level));
+            return createSingleBandedByteImage(width, height);
+        }
+    }
+
+    private static class PCL implements PropertyChangeListener {
+        String trace = "";
+        public void propertyChange(PropertyChangeEvent evt) {
+           trace += evt.getPropertyName()+";";
+        }
     }
 }
