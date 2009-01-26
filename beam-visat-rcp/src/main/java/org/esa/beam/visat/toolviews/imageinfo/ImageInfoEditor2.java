0a004315a326e3a1d3a9bef6fe148b8329b10150
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-01-26 14:16:24 +0000

    fix
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4107 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/imageinfo/ImageInfoEditor2.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/imageinfo/ImageInfoEditor2.java
@@ -20,29 +20,27 @@ import com.bc.ceres.core.ProgressMonitor;
 import com.bc.ceres.core.SubProgressMonitor;
 import com.bc.ceres.swing.ActionLabel;
 import com.bc.ceres.swing.progress.ProgressMonitorSwingWorker;
-import org.esa.beam.framework.datamodel.Stx;
 import org.esa.beam.framework.datamodel.RasterDataNode;
+import org.esa.beam.framework.datamodel.Stx;
 import org.esa.beam.framework.ui.ImageInfoEditor;
 import org.esa.beam.framework.ui.ImageInfoEditorModel;
 import org.esa.beam.framework.ui.UIUtils;
 import org.esa.beam.framework.ui.product.ProductSceneView;
 import org.esa.beam.util.math.MathUtils;
 
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingWorker;
-import javax.swing.JComponent;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -82,43 +80,36 @@ class ImageInfoEditor2 extends ImageInfoEditor {
     private JPanel createStxOverlayComponent() {
         JPanel stxOverlayComponent = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         stxOverlayComponent.setOpaque(false);
-        stxOverlayComponent.setBorder(new EmptyBorder(4, 0, 0, 8));
 
         final ImageInfoEditorModel model = getModel();
         if (!showExtraInfo || model == null) {
             return stxOverlayComponent;
         }
 
+        stxOverlayComponent.setBorder(new EmptyBorder(4, 0, 0, 8));
+
         JComponent labels = new JComponent() {
             @Override
             protected void paintComponent(Graphics g) {
                 g.setColor(getBackground());
-                final Rectangle bounds = getBounds();
-                g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+                g.fillRect(0, 0, getWidth(), getHeight());
             }
         };
-        labels.setLayout(new GridBagLayout());
-        labels.setBackground(new Color(255, 255, 255, 140));
-
+        labels.setBorder(new EmptyBorder(0, 2, 0, 2));
+        labels.setLayout(new GridLayout(-1, 1));
+        labels.setBackground(new Color(255, 255, 255, 127));
         stxOverlayComponent.add(labels);
 
-        final GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        labels.add(new JLabel("Name: " + model.getParameterName()), gbc);
-        gbc.gridy++;
-        labels.add(new JLabel("Unit: " + model.getParameterUnit()), gbc);
+        labels.add(new JLabel("Name: " + model.getParameterName()));
+        labels.add(new JLabel("Unit: " + model.getParameterUnit()));
 
         final Stx stx = model.getSampleStx();
         if (stx == null) {
             return stxOverlayComponent;
         }
 
-        gbc.gridy++;
-        labels.add(new JLabel("Min: " + MathUtils.round(model.getMinSample(), 1000.0)), gbc);
-        gbc.gridy++;
-        labels.add(new JLabel("Max: " + MathUtils.round(model.getMaxSample(), 1000.0)), gbc);
+        labels.add(new JLabel("Min: " + MathUtils.round(model.getMinSample(), 1000.0)));
+        labels.add(new JLabel("Max: " + MathUtils.round(model.getMaxSample(), 1000.0)));
         if (stx.getResolutionLevel() > 0) {
             final ActionLabel label = new ActionLabel("Rough statistics!");
             label.setToolTipText("Click to compute accurate statistics.");
@@ -127,8 +118,7 @@ class ImageInfoEditor2 extends ImageInfoEditor {
                     askUser();
                 }
             });
-            gbc.gridy++;
-            labels.add(label, gbc);
+            labels.add(label);
         }
 
         return stxOverlayComponent;
