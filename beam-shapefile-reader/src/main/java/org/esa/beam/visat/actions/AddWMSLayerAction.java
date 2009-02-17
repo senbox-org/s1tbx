8f540d120933745945c511d97d01dc1df53d6214
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-02-17 16:49:04 +0000

    Experimenting with GeoTools!
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4276 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-shapefile-reader/src/main/java/org/esa/beam/visat/actions/AddWMSLayerAction.java
@@ -0,0 +1,154 @@
+package org.esa.beam.visat.actions;
+
+import com.bc.ceres.glayer.Layer;
+import org.esa.beam.framework.datamodel.GeoCoding;
+import org.esa.beam.framework.datamodel.RasterDataNode;
+import org.esa.beam.framework.help.HelpSys;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.ui.command.ExecCommand;
+import org.esa.beam.framework.ui.product.ProductSceneView;
+import org.esa.beam.util.PropertyMap;
+import org.esa.beam.util.SystemUtils;
+import org.esa.beam.util.io.BeamFileChooser;
+import org.esa.beam.util.io.BeamFileFilter;
+import org.esa.beam.visat.VisatApp;
+import org.geotools.data.wms.WebMapServer;
+import org.geotools.data.wms.request.GetMapRequest;
+import org.geotools.data.wms.response.GetMapResponse;
+import org.geotools.data.ows.CRSEnvelope;
+import org.geotools.data.ows.WMSCapabilities;
+
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.text.MessageFormat;
+import java.net.URL;
+
+public class AddWMSLayerAction extends ExecCommand {
+
+    private static final String DLG_TITLE = "Import Shape";
+
+    @Override
+    public void actionPerformed(final CommandEvent event) {
+        importWMS(VisatApp.getApp());
+        VisatApp.getApp().updateState();
+    }
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        final ProductSceneView productSceneView = VisatApp.getApp().getSelectedProductSceneView();
+        setEnabled(productSceneView != null);
+    }
+
+    private void importWMS(final VisatApp visatApp) {
+        final PropertyMap propertyMap = visatApp.getPreferences();
+        final BeamFileChooser fileChooser = new BeamFileChooser();
+        HelpSys.enableHelpKey(fileChooser, getHelpId());
+        fileChooser.setDialogTitle(DLG_TITLE);
+        fileChooser.setFileFilter(
+                new BeamFileFilter("SHAPE", new String[]{".shp"}, "ESRI Shapefiles"));/*I18N*/
+        fileChooser.setCurrentDirectory(getIODir(propertyMap));
+        final int result = fileChooser.showOpenDialog(visatApp.getMainFrame());
+        if (result == JFileChooser.APPROVE_OPTION) {
+            final File file = fileChooser.getSelectedFile();
+            if (file != null) {
+                setIODir(propertyMap, file.getAbsoluteFile().getParentFile());
+                loadShape(visatApp, file);
+            }
+        }
+    }
+
+    public static void localWMS(final VisatApp visatApp) throws Exception {
+        final ProductSceneView view = visatApp.getSelectedProductSceneView();
+        if (view == null) {
+            return;
+        }
+
+        URL url = new URL("http://www.geovista.psu.edu/geoserver/wms");
+//        URL url = new URL("http://localhost:8080/geoserver/wms?REQUEST=GetCapabilities");
+
+        WebMapServer wms = new WebMapServer(url);
+
+		WMSCapabilities caps = wms.getCapabilities();
+
+		org.geotools.data.ows.Layer layer = null;
+        for (org.geotools.data.ows.Layer test : caps.getLayerList()) {
+            if (test.getName() != null && test.getName().length() != 0) {
+                layer = test;
+                break;
+            }
+        }
+		GetMapRequest mapRequest = wms.createGetMapRequest();
+		mapRequest.addLayer(layer);
+
+		mapRequest.setDimensions("400", "400");
+		mapRequest.setFormat("image/png");
+
+		CRSEnvelope bbox = new CRSEnvelope("EPSG:4326",-100.0, -70, 25, 40 );
+		mapRequest.setBBox(bbox);
+
+		System.out.println(mapRequest.getFinalURL());
+
+		GetMapResponse response = wms.issueRequest( mapRequest );
+        WMSMapLayer wmslayer = new WMSMapLayer(wms, layer);
+        final List<Layer> children = view.getRootLayer().getChildren();
+       //children.add(children.size() - 1, wmslayer);
+        wmslayer.setVisible(false);
+        wmslayer.setVisible(true);
+    }
+
+    private static void loadShape(final VisatApp visatApp, final File file) {
+        final ProductSceneView view = visatApp.getSelectedProductSceneView();
+        if (view == null) {
+            return;
+        }
+
+
+        final StringBuilder builder = new StringBuilder("Failed to import shapefile.\n");
+        final RasterDataNode raster = view.getRaster();
+        final GeoCoding geoCoding = raster.getProduct().getGeoCoding();
+        if (geoCoding == null || !geoCoding.canGetPixelPos()) {
+            builder.append("Suitable geo-coding required for reading ESRI shapefiles.");
+            visatApp.showErrorDialog(DLG_TITLE, builder.toString()); /*I18N*/
+            return;
+        }
+
+        Layer shapeLayer;
+        try {
+            shapeLayer = readShape(file, geoCoding);
+            if (shapeLayer == null) {
+                builder.append("All coordinates out of the product''s scene bounds.");
+                visatApp.showErrorDialog(DLG_TITLE,builder.toString());
+                return;
+            }
+        } catch (IOException e) {
+            builder.append(MessageFormat.format("An I/O Error occured:\n{0}", e.getMessage()));
+            visatApp.showErrorDialog(DLG_TITLE, builder.toString());
+            return;
+        }
+
+        final List<Layer> children = view.getRootLayer().getChildren();
+        children.add(children.size() - 1, shapeLayer);
+        shapeLayer.setVisible(false);
+        shapeLayer.setVisible(true);
+    }
+
+    public static Layer readShape(File file, GeoCoding geoCoding) throws IOException {
+        return new ShapefileLayer(file);
+    }
+
+
+
+    private static void setIODir(final PropertyMap propertyMap, final File dir) {
+        if (dir != null) {
+            propertyMap.setPropertyString("shape.io.dir", dir.getPath());
+        }
+    }
+
+    private static File getIODir(final PropertyMap propertyMap) {
+        final File dir = SystemUtils.getUserHomeDir();
+        return new File(propertyMap.getPropertyString("shape.io.dir", dir.getPath()));
+    }
+
+}
---
+++ beam-shapefile-reader/src/main/java/org/esa/beam/visat/actions/ShapefileLayer.java
@@ -0,0 +1,232 @@
+package org.esa.beam.visat.actions;
+
+import com.bc.ceres.glayer.Layer;
+import com.bc.ceres.grender.Rendering;
+import com.bc.ceres.grender.ViewportListener;
+import com.bc.ceres.grender.Viewport;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.DataStore;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.map.DefaultMapContext;
+import org.geotools.map.MapContext;
+import org.geotools.map.MapLayer;
+import org.geotools.renderer.label.LabelCacheImpl;
+import org.geotools.renderer.lite.LabelCache;
+import org.geotools.renderer.lite.StreamingRenderer;
+import org.geotools.renderer.lite.gridcoverage2d.StyleVisitorAdapter;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Fill;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.Rule;
+import org.geotools.styling.SLD;
+import org.geotools.styling.SLDParser;
+import org.geotools.styling.Style;
+import org.geotools.styling.StyleBuilder;
+import org.geotools.styling.Symbolizer;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.filter.FilterFactory;
+import org.opengis.filter.expression.Expression;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import javax.swing.JOptionPane;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * todo - add API doc
+ *
+ * @author Marco Peters
+ * @version $Revision: $ $Date: $
+ * @since BEAM 4.6
+ */
+class ShapefileLayer extends Layer {
+
+    private static final org.geotools.styling.StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
+    private static final FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory(null);
+
+    private MapContext mapContext;
+    private CoordinateReferenceSystem crs;
+
+    private StreamingRenderer renderer;
+    private LabelCache labelCache;
+
+    ShapefileLayer(File file) throws IOException {
+
+        ShapefileDataStore shapefile = new ShapefileDataStore(file.toURI().toURL());
+        FeatureSource<SimpleFeatureType, SimpleFeature> featureSource = shapefile.getFeatureSource();
+        FeatureType schema = shapefile.getSchema();
+
+        crs = schema.getGeometryDescriptor().getCoordinateReferenceSystem();
+
+        Style style = createStyle(file, schema);        
+
+        setName(style.getName());
+        mapContext = new DefaultMapContext(crs);
+        mapContext.addLayer(featureSource, style);
+        renderer = new StreamingRenderer();
+        workaroundLabelCacheBug();
+        renderer.setContext(mapContext);
+    }
+
+    private void workaroundLabelCacheBug() {
+        Map hints = renderer.getRendererHints();
+        if (hints == null) {
+            hints = new HashMap();
+        }
+        if (hints.containsKey(StreamingRenderer.LABEL_CACHE_KEY)) {
+            labelCache = (LabelCache) hints.get(StreamingRenderer.LABEL_CACHE_KEY);
+        } else {
+            labelCache = new LabelCacheImpl();
+            hints.put(StreamingRenderer.LABEL_CACHE_KEY, labelCache);
+        }
+        renderer.setRendererHints(hints);
+    }
+
+    @Override
+    protected void renderLayer(final Rendering rendering) {
+        Rectangle bounds = rendering.getViewport().getViewBounds();
+        final AffineTransform m2vTransform = rendering.getViewport().getViewToModelTransform();
+        Rectangle2D d = m2vTransform.createTransformedShape(bounds).getBounds2D();
+        ReferencedEnvelope mapArea = new ReferencedEnvelope(d, crs);
+        mapContext.setAreaOfInterest(mapArea);
+
+        applyOpacity(getStyle().getOpacity());
+
+        labelCache.clear();
+
+        renderer.paint(rendering.getGraphics(), bounds, mapArea);
+    }
+  
+    private void applyOpacity(final double opacity) {
+        StyleBuilder sb = new StyleBuilder();
+        final Expression opa = sb.literalExpression(opacity);
+        final MapLayer layer = mapContext.getLayer(0);
+        if (layer != null) {
+
+            List<? extends org.opengis.style.FeatureTypeStyle> sty = layer.getStyle().featureTypeStyles();
+
+            List<? extends org.opengis.style.Rule> rules = sty.get(0).rules();
+            for (org.opengis.style.Rule r : rules) {
+                    final List<? extends org.opengis.style.Symbolizer> symbolizers = r.symbolizers();
+                    for (org.opengis.style.Symbolizer symbolizer : symbolizers) {
+                        if (symbolizer instanceof PolygonSymbolizer) {
+                            PolygonSymbolizer polySym = (PolygonSymbolizer) symbolizer;
+                            polySym.getStroke().setOpacity(opa);
+                            polySym.getFill().setOpacity(opa);
+                        }
+                    }
+            }
+        }
+    }
+
+    private static Style createStyle(File file, FeatureType schema) {
+        File sld = toSLDFile(file);
+        if (sld.exists()) {
+            return createFromSLD(sld);
+        }
+        Class type = schema.getGeometryDescriptor().getType().getBinding();
+        if (type.isAssignableFrom(Polygon.class)
+            || type.isAssignableFrom(MultiPolygon.class)) {
+            return createPolygonStyle();
+        } else if (type.isAssignableFrom(LineString.class)
+                   || type.isAssignableFrom(MultiLineString.class)) {
+            return createLineStyle();
+        } else {
+            return createPointStyle();
+        }
+    }
+
+    /**
+     * Figure out the URL for the "sld" file
+     */
+    public static File toSLDFile(File file) {
+        String filename = file.getAbsolutePath();
+        if (filename.endsWith(".shp") || filename.endsWith(".dbf")
+            || filename.endsWith(".shx")) {
+            filename = filename.substring(0, filename.length() - 4);
+            filename += ".sld";
+        } else if (filename.endsWith(".SLD") || filename.endsWith(".SLD")
+                   || filename.endsWith(".SLD")) {
+            filename = filename.substring(0, filename.length() - 4);
+            filename += ".SLD";
+        }
+        return new File(filename);
+    }
+
+    private static Style createFromSLD(File sld) {
+        try {
+            SLDParser stylereader = new SLDParser(styleFactory, sld.toURI().toURL());
+            Style[] style = stylereader.readXML();
+            return style[0];
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+
+    private static Style createPointStyle() {
+        PointSymbolizer symbolizer = styleFactory.createPointSymbolizer();
+        symbolizer.getGraphic().setSize(filterFactory.literal(1));
+
+        Rule rule = styleFactory.createRule();
+        rule.setSymbolizers(new Symbolizer[]{symbolizer});
+        FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle();
+        fts.setRules(new Rule[]{rule});
+
+        Style style = styleFactory.createStyle();
+        style.addFeatureTypeStyle(fts);
+        return style;
+    }
+
+    private static Style createLineStyle() {
+        LineSymbolizer symbolizer = styleFactory.createLineSymbolizer();
+        SLD.setLineColour(symbolizer, Color.BLUE);
+        symbolizer.getStroke().setWidth(filterFactory.literal(1));
+        symbolizer.getStroke().setColor(filterFactory.literal(Color.BLUE));
+
+        Rule rule = styleFactory.createRule();
+        rule.setSymbolizers(new Symbolizer[]{symbolizer});
+        FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle();
+        fts.setRules(new Rule[]{rule});
+
+        Style style = styleFactory.createStyle();
+        style.addFeatureTypeStyle(fts);
+        return style;
+    }
+
+    private static Style createPolygonStyle() {
+        PolygonSymbolizer symbolizer = styleFactory.createPolygonSymbolizer();
+        Fill fill = styleFactory.createFill(
+                filterFactory.literal("#FFAA00"),
+                filterFactory.literal(0.5)
+        );
+        symbolizer.setFill(fill);
+        Rule rule = styleFactory.createRule();
+        rule.setSymbolizers(new Symbolizer[]{symbolizer});
+        FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle();
+        fts.setRules(new Rule[]{rule});
+
+        Style style = styleFactory.createStyle();
+        style.addFeatureTypeStyle(fts);
+        return style;
+    }
+
+}
