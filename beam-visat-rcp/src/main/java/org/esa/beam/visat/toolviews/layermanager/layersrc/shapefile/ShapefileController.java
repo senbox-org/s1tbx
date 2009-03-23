12bd8c13aa830e74c854996161ae51a30a723361
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-03-23 16:21:32 +0000

    add Shapefile controller
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4519 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/layermanager/layersrc/shapefile/ShapefileController.java
@@ -0,0 +1,50 @@
+/*
+ * $Id: $
+ *
+ * Copyright (C) 2009 by Brockmann Consult (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation. This program is distributed in the hope it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.esa.beam.visat.toolviews.layermanager.layersrc.shapefile;
+
+import org.esa.beam.framework.ui.assistant.AbstractAppAssistantPage;
+import org.esa.beam.framework.ui.assistant.AppAssistantPageContext;
+import org.esa.beam.visat.toolviews.layermanager.LayerSourceController;
+
+/**
+ * todo - add API doc
+ *
+ * @author Marco Zuehlke
+ * @version $Revision$ $Date$
+ * @since BEAM 4.6
+ */
+public class ShapefileController implements LayerSourceController {
+
+    @Override
+    public boolean isApplicable(AppAssistantPageContext pageContext) {
+        return true;
+    }
+    
+    @Override
+    public AbstractAppAssistantPage getFirstPage(AppAssistantPageContext pageContext) {
+        return new ShapefileAssistantPage();
+    }
+
+    @Override
+    public void cancel() {
+    }
+
+    @Override
+    public boolean finish(AppAssistantPageContext pageContext) {
+        return pageContext.getCurrentPage().performFinish(pageContext);
+    }
+}
