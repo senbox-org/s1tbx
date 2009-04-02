fab083a669895160f3391f36a28f8fa3e9e41bce
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-04-02 09:07:00 +0000

    new: color converter
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@4643 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ ceres-binding/src/main/java/com/bc/ceres/binding/converters/ColorConverter.java
@@ -0,0 +1,68 @@
+package com.bc.ceres.binding.converters;
+
+import com.bc.ceres.binding.ConversionException;
+import com.bc.ceres.binding.Converter;
+
+import java.awt.Color;
+import java.text.MessageFormat;
+
+public class ColorConverter implements Converter<Color> {
+
+    @Override
+    public Class<Color> getValueType() {
+        return Color.class;
+    }
+
+    @Override
+    public Color parse(String text) throws ConversionException {
+        if (text.isEmpty()) {
+            return null;
+        }
+
+        final int[] array;
+        try {
+            final ArrayConverter converter = new ArrayConverter(int[].class, new IntegerConverter());
+            array = (int[]) converter.parse(text);
+        } catch (ConversionException e) {
+            throw new ConversionException(
+                    MessageFormat.format("Cannot parse ''{0}'' into a color: {1}", text, e.getMessage()), e);
+        }
+
+        if (array.length < 3) {
+            throw new ConversionException(
+                    MessageFormat.format("Cannot parse ''{0}'' into a color.", text));
+        }
+
+        final int r = array[0];
+        final int g = array[1];
+        final int b = array[2];
+        final int a;
+
+        if (array.length == 4) {
+            a = array[3];
+        } else {
+            a = 255;
+        }
+
+        try {
+            return new Color(r, g, b, a);
+        } catch (Exception e) {
+            throw new ConversionException(
+                    MessageFormat.format("Cannot parse ''{0}'' into a color: {1}", text, e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public String format(Color value) {
+        if (value == null) {
+            return "";
+        }
+        
+        final int r = value.getRed();
+        final int g = value.getGreen();
+        final int b = value.getBlue();
+        final int a = value.getAlpha();
+
+        return String.format("%d,%d,%d,%d", r, g, b, a);
+    }
+}
---
+++ ceres-binding/src/test/java/com/bc/ceres/binding/converters/ColorConverterTest.java
@@ -0,0 +1,48 @@
+package com.bc.ceres.binding.converters;
+
+import com.bc.ceres.binding.ConversionException;
+import com.bc.ceres.binding.Converter;
+
+import java.awt.Color;
+
+public class ColorConverterTest extends AbstractConverterTest {
+
+    private ColorConverter converter;
+
+    @Override
+    public Converter getConverter() {
+        if (converter == null) {
+            converter = new ColorConverter();
+        }
+        return converter;
+    }
+
+    @Override
+    public void testConverter() throws ConversionException {
+        testValueType(Color.class);
+
+        testParseSuccess(new Color(17, 11, 67), "17,11,67");
+        testParseSuccess(new Color(17, 11, 67, 127), "17, \t11, 67, 127");
+        testParseSuccess(null, "");
+
+        testFormatSuccess("17,11,67,255", new Color(17, 11, 67));
+        testFormatSuccess("17,11,67,127", new Color(17, 11, 67, 127));
+        testFormatSuccess("", null);
+
+        assertNullCorrectlyHandled();
+
+        try {
+            converter.parse("17,11");
+            fail();
+        } catch (ConversionException expected) {
+            // ignore
+        }
+
+        try {
+            converter.parse("17,11,67,1024");
+            fail();
+        } catch (ConversionException expected) {
+            // ignore
+        }
+    }
+}
