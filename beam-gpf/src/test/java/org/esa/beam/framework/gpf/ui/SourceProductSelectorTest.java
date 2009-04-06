1aaee1f882719b3d12ab805bedfc52788e992446
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-04-06 08:45:02 +0000

    Handling HeadlessException
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4706 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf/src/test/java/org/esa/beam/framework/gpf/ui/SourceProductSelectorTest.java
+++ beam-gpf/src/test/java/org/esa/beam/framework/gpf/ui/SourceProductSelectorTest.java
@@ -2,8 +2,8 @@ package org.esa.beam.framework.gpf.ui;
 
 import junit.framework.TestCase;
 import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.ui.AppContext;
 
+import java.awt.HeadlessException;
 import java.io.File;
 
 /**
@@ -28,85 +28,114 @@ public class SourceProductSelectorTest extends TestCase {
     }
 
     public void testCreatedUIComponentsNotNull() {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
-        selector.initProducts();
-        assertNotNull(selector.getProductNameLabel());
-        assertNotNull(selector.getProductNameComboBox());
-        assertNotNull(selector.getProductFileChooserButton());
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
+            selector.initProducts();
+            assertNotNull(selector.getProductNameLabel());
+            assertNotNull(selector.getProductNameComboBox());
+            assertNotNull(selector.getProductFileChooserButton());
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
 
     public void testCreatedUIComponentsAreSame() {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
-        selector.initProducts();
-        assertSame(selector.getProductNameLabel(), selector.getProductNameLabel());
-        assertSame(selector.getProductNameComboBox(), selector.getProductNameComboBox());
-        assertSame(selector.getProductFileChooserButton(), selector.getProductFileChooserButton());
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source:");
+            selector.initProducts();
+            assertSame(selector.getProductNameLabel(), selector.getProductNameLabel());
+            assertSame(selector.getProductNameComboBox(), selector.getProductNameComboBox());
+            assertSame(selector.getProductFileChooserButton(), selector.getProductFileChooserButton());
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
 
     public void testSetSelectedProduct() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
-        selector.initProducts();
-        Product selectedProduct = selector.getSelectedProduct();
-        assertSame(appContext.getSelectedProduct(), selectedProduct);
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+            selector.initProducts();
+            Product selectedProduct = selector.getSelectedProduct();
+            assertSame(appContext.getSelectedProduct(), selectedProduct);
 
-        selector.setSelectedProduct(defaultProducts[1]);
-        selectedProduct = selector.getSelectedProduct();
-        assertSame(defaultProducts[1], selectedProduct);
+            selector.setSelectedProduct(defaultProducts[1]);
+            selectedProduct = selector.getSelectedProduct();
+            assertSame(defaultProducts[1], selectedProduct);
 
-        Product oldProduct = new Product("new", "T1", 0, 0);
-        oldProduct.setFileLocation(new File(""));
-        selector.setSelectedProduct(oldProduct);
-        selectedProduct = selector.getSelectedProduct();
-        assertSame(oldProduct, selectedProduct);
+            Product oldProduct = new Product("new", "T1", 0, 0);
+            oldProduct.setFileLocation(new File(""));
+            selector.setSelectedProduct(oldProduct);
+            selectedProduct = selector.getSelectedProduct();
+            assertSame(oldProduct, selectedProduct);
 
-        Product newProduct = new Product("new", "T2", 0, 0);
-        selector.setSelectedProduct(newProduct);
-        selectedProduct = selector.getSelectedProduct();
-        assertSame(newProduct, selectedProduct);
-        assertNull(oldProduct.getFileLocation()); // assert that old product is disposed
+            Product newProduct = new Product("new", "T2", 0, 0);
+            selector.setSelectedProduct(newProduct);
+            selectedProduct = selector.getSelectedProduct();
+            assertSame(newProduct, selectedProduct);
+            assertNull(oldProduct.getFileLocation()); // assert that old product is disposed
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
 
     public void testNewProductIsDisposed() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
-        selector.initProducts();
-        Product newProduct = new Product("new", "T1", 0, 0);
-        newProduct.setFileLocation(new File(""));
-        selector.setSelectedProduct(newProduct);
-        assertSame(newProduct, selector.getSelectedProduct());
-        selector.setSelectedProduct(defaultProducts[0]);
-        assertSame(defaultProducts[0], selector.getSelectedProduct());
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+            selector.initProducts();
+            Product newProduct = new Product("new", "T1", 0, 0);
+            newProduct.setFileLocation(new File(""));
+            selector.setSelectedProduct(newProduct);
+            assertSame(newProduct, selector.getSelectedProduct());
+            selector.setSelectedProduct(defaultProducts[0]);
+            assertSame(defaultProducts[0], selector.getSelectedProduct());
 
-        assertNotNull(newProduct.getFileLocation());
-        selector.releaseProducts();
-        assertNull(newProduct.getFileLocation()); // assert that new product is disposed, because it is not selected
+            assertNotNull(newProduct.getFileLocation());
+            selector.releaseProducts();
+            assertNull(newProduct.getFileLocation()); // assert that new product is disposed, because it is not selected
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
 
     public void testNewProductIsNotDisposed() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
-        selector.initProducts();
-        selector.setSelectedProduct(defaultProducts[0]);
-        assertSame(defaultProducts[0], selector.getSelectedProduct());
-        Product newProduct = new Product("new", "T1", 0, 0);
-        newProduct.setFileLocation(new File(""));
-        selector.setSelectedProduct(newProduct);
-        assertSame(newProduct, selector.getSelectedProduct());
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+            selector.initProducts();
+            selector.setSelectedProduct(defaultProducts[0]);
+            assertSame(defaultProducts[0], selector.getSelectedProduct());
+            Product newProduct = new Product("new", "T1", 0, 0);
+            newProduct.setFileLocation(new File(""));
+            selector.setSelectedProduct(newProduct);
+            assertSame(newProduct, selector.getSelectedProduct());
 
-        assertNotNull(newProduct.getFileLocation());
-        selector.releaseProducts();
-        assertNotNull(newProduct.getFileLocation()); // assert that new product is not disposed while it is selected
+            assertNotNull(newProduct.getFileLocation());
+            selector.releaseProducts();
+            assertNotNull(newProduct.getFileLocation()); // assert that new product is not disposed while it is selected
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
 
     public void testSetSelectedIndex() throws Exception {
-        SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
+        try {
+            SourceProductSelector selector = new SourceProductSelector(appContext, "Source");
 
-        selector.initProducts();
-        assertSame(defaultProducts[0], selector.getSelectedProduct());
+            selector.initProducts();
+            assertSame(defaultProducts[0], selector.getSelectedProduct());
 
-        selector.setSelectedIndex(1);
-        assertSame(defaultProducts[1], selector.getSelectedProduct());
+            selector.setSelectedIndex(1);
+            assertSame(defaultProducts[1], selector.getSelectedProduct());
 
-        selector.setSelectedIndex(2);
-        assertSame(defaultProducts[2], selector.getSelectedProduct());
+            selector.setSelectedIndex(2);
+            assertSame(defaultProducts[2], selector.getSelectedProduct());
+        } catch (HeadlessException e) {
+            warnHeadless();
+        }
     }
+
+    private void warnHeadless() {
+        System.out.println("A " + SourceProductSelectorTest.class + " test has not been performed: HeadlessException");
+    }
+
 
 }
