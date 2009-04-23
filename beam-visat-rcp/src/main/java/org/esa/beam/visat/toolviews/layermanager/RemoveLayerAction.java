28a60fcaecb57a7d3a71466a82b3ee84744d12e6
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-04-23 14:21:41 +0000

    disposing removed layer
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4889 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/layermanager/RemoveLayerAction.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/layermanager/RemoveLayerAction.java
@@ -3,22 +3,16 @@ package org.esa.beam.visat.toolviews.layermanager;
 import com.bc.ceres.glayer.Layer;
 import org.esa.beam.framework.ui.AppContext;
 import org.esa.beam.framework.ui.UIUtils;
-import org.esa.beam.framework.ui.product.ProductSceneView;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import java.awt.event.ActionEvent;
-import java.util.HashSet;
-import java.util.Set;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
 
 /**
-*
-* @author Marco Peters
-* @version $Revision: $ $Date: $
-* @since BEAM 4.6
-*/
+ * @author Marco Peters
+ * @version $Revision: $ $Date: $
+ * @since BEAM 4.6
+ */
 class RemoveLayerAction extends AbstractAction {
 
     private final AppContext appContext;
@@ -35,6 +29,7 @@ class RemoveLayerAction extends AbstractAction {
         Layer selectedLayer = appContext.getSelectedProductSceneView().getSelectedLayer();
         if (selectedLayer != null) {
             selectedLayer.getParent().getChildren().remove(selectedLayer);
+            selectedLayer.dispose();
         }
     }
 
