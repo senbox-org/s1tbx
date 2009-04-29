3028dc0a752aa5de128d2f5fd3c339255aa93600
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-04-29 11:24:36 +0000

    added overrides
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@4932 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-core/src/main/java/org/esa/beam/framework/datamodel/GcpDescriptor.java
+++ beam-core/src/main/java/org/esa/beam/framework/datamodel/GcpDescriptor.java
@@ -16,41 +16,50 @@ public class GcpDescriptor implements PlacemarkDescriptor {
     private GcpDescriptor() {
     }
 
+    @Override
     public String getShowLayerCommandId() {
         return "showGcpOverlay";
     }
 
+    @Override
     public String getRoleName() {
         return "gcp";
     }
 
+    @Override
     public String getRoleLabel() {
         return "GCP";
     }
 
+    @Override
     public Image getCursorImage() {
         return null;
     }
 
+    @Override
     public Point getCursorHotSpot() {
         return new Point();
     }
 
+    @Override
     public ProductNodeGroup<Pin> getPlacemarkGroup(Product product) {
         return product.getGcpGroup();
     }
 
+    @Override
     public PlacemarkSymbol createDefaultSymbol() {
         return PlacemarkSymbol.createDefaultGcpSymbol();
     }
 
+    @Override
     public PixelPos updatePixelPos(GeoCoding geoCoding, GeoPos geoPos, PixelPos pixelPos) {
         if (geoCoding == null || !geoCoding.canGetPixelPos()) {
-        return pixelPos;
-    }
+            return pixelPos;
+        }
         return geoCoding.getPixelPos(geoPos, pixelPos);
     }
 
+    @Override
     public GeoPos updateGeoPos(GeoCoding geoCoding, PixelPos pixelPos, GeoPos geoPos) {
         return geoPos;
     }
