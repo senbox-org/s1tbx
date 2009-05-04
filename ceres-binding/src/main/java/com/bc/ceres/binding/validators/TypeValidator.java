d7c84bb4bbdc8ded63783d231db6cf9948457863
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-05-04 09:01:20 +0000

    improved exception message
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@4967 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-binding/src/main/java/com/bc/ceres/binding/validators/TypeValidator.java
+++ ceres-binding/src/main/java/com/bc/ceres/binding/validators/TypeValidator.java
@@ -8,12 +8,16 @@ import java.text.MessageFormat;
 
 public class TypeValidator implements Validator {
 
+    @Override
     public void validateValue(ValueModel valueModel, Object value) throws ValidationException {
         final Class<?> type = valueModel.getDescriptor().getType();
         if (!isAssignableFrom(type, value)) {
+            String name = valueModel.getDescriptor().getDisplayName();
+            if (name == null) {
+                name = valueModel.getDescriptor().getName();
+            }
             throw new ValidationException(MessageFormat.format("Value for ''{0}'' is not of type ''{1}''.",
-                                                               valueModel.getDescriptor().getDisplayName(),
-                                                               type.getSimpleName()));
+                                                               name, type.getSimpleName()));
         }
     }
 
@@ -21,16 +25,16 @@ public class TypeValidator implements Validator {
         if (value == null) {
             return !type.isPrimitive();
         }
-        final Class<? extends Object> valueType = value.getClass();
+        final Class<?> valueType = value.getClass();
         return type.isAssignableFrom(valueType)
-                || type.isPrimitive()
-                && (type.equals(Boolean.TYPE) && valueType.equals(Boolean.class)
-                || type.equals(Character.TYPE) && valueType.equals(Character.class)
-                || type.equals(Byte.TYPE) && valueType.equals(Byte.class)
-                || type.equals(Short.TYPE) && valueType.equals(Short.class)
-                || type.equals(Integer.TYPE) && valueType.equals(Integer.class)
-                || type.equals(Long.TYPE) && valueType.equals(Long.class)
-                || type.equals(Float.TYPE) && valueType.equals(Float.class)
-                || type.equals(Double.TYPE) && valueType.equals(Double.class));
+               || type.isPrimitive()
+                  && (type.equals(Boolean.TYPE) && valueType.equals(Boolean.class)
+                      || type.equals(Character.TYPE) && valueType.equals(Character.class)
+                      || type.equals(Byte.TYPE) && valueType.equals(Byte.class)
+                      || type.equals(Short.TYPE) && valueType.equals(Short.class)
+                      || type.equals(Integer.TYPE) && valueType.equals(Integer.class)
+                      || type.equals(Long.TYPE) && valueType.equals(Long.class)
+                      || type.equals(Float.TYPE) && valueType.equals(Float.class)
+                      || type.equals(Double.TYPE) && valueType.equals(Double.class));
     }
 }
