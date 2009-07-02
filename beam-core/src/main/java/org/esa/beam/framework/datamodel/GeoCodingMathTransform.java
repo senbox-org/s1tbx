2099f899e7e6c1ef2e8e606cd0ddc816340546f3
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-07-02 12:29:31 +0000

    [BEAM-1023] Can not overlay compatible products
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@5842 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-core/src/main/java/org/esa/beam/framework/datamodel/GeoCodingMathTransform.java
+++ beam-core/src/main/java/org/esa/beam/framework/datamodel/GeoCodingMathTransform.java
@@ -76,12 +76,29 @@ public class GeoCodingMathTransform extends AbstractMathTransform {
         }
 
         GeoCodingMathTransform that = (GeoCodingMathTransform) o;
-
-        if (!geoCoding.equals(that.geoCoding)) {
+        if (areGeoCodingsEqual(geoCoding, that.geoCoding)) {
             return false;
         }
         return t == that.t;
 
+    }
+
+    private boolean areGeoCodingsEqual(GeoCoding geoCoding, GeoCoding otherGeoCoding) {
+        final PixelPos pixelPos0 = new PixelPos(0, 0);
+        final GeoPos geoPos = geoCoding.getGeoPos(pixelPos0, null);
+        final GeoPos thatGeoPos = otherGeoCoding.getGeoPos(pixelPos0, null);
+        if (!equalsGeoPos(geoPos, thatGeoPos)) {
+            return true;
+        }
+        final PixelPos pixelPos1 = new PixelPos(1, 1);
+        final GeoPos geoPos1 = geoCoding.getGeoPos(pixelPos1, null);
+        final GeoPos thatGeoPos1 = otherGeoCoding.getGeoPos(pixelPos1, null);
+        return !equalsGeoPos(geoPos1, thatGeoPos1);
+    }
+
+    private boolean equalsGeoPos(GeoPos geoPos, GeoPos thatGeoPos) {
+        return Math.abs(geoPos.getLat() - thatGeoPos.getLat()) < 1.0e-6 &&
+               Math.abs(geoPos.getLon() - thatGeoPos.getLon()) < 1.0e-6;
     }
 
     @Override
