14ca35d82a9f0d2179099dc08ee90d35059b97d6
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-08-21 14:14:53 +0000

    [CERES-41] Need better API for observing binding problems
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@6162 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-binding/src/main/java/com/bc/ceres/binding/validators/NotEmptyValidator.java
+++ ceres-binding/src/main/java/com/bc/ceres/binding/validators/NotEmptyValidator.java
@@ -7,10 +7,11 @@ import com.bc.ceres.binding.ValueModel;
 import java.text.MessageFormat;
 
 public class NotEmptyValidator implements Validator {
+    @Override
     public void validateValue(ValueModel valueModel, Object value) throws ValidationException {
         if (value == null || value.toString().trim().isEmpty()) {
             throw new ValidationException(MessageFormat.format("No value for ''{0}'' specified.", 
-                                                               valueModel.getDescriptor().getDisplayName()));
+                                                               valueModel.getDescriptor().getName()));
         }
     }
 }
