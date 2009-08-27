a5fbcd0b1074afca016ea3fc1afcc2fa5ecbae9a
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-08-27 11:19:26 +0000

    Revised DataContext interface
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@6243 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-binio/src/main/java/com/bc/ceres/binio/DataContext.java
+++ ceres-binio/src/main/java/com/bc/ceres/binio/DataContext.java
@@ -1,22 +1,103 @@
 package com.bc.ceres.binio;
 
 /**
- * The context provides the means to read from or write to a random access stream.
+ * The context provides the means to read from or write to a random access stream or file.
  *
  * @author Norman Fomferra
  * @version $Revision$ $Date$
  * @since Ceres 0.8
  */
 public interface DataContext {
+    /**
+     * @return the file format.
+     */
     DataFormat getFormat();
 
+    /**
+     * @return the associated I/O-handler used to read from or write to a random access stream or file.
+     */
     IOHandler getHandler();
 
+    /**
+     * Gets an instance of a compound with I/O starting at the given stream or file position.
+     * The type of the compound is the same as the format's compound type.
+     * In contrast to {@link #createData()}, the method will always return the same compound instance.
+     *
+     * @return The instance of a data compound.
+     *
+     * @see #createData()
+     * @see #getFormat()
+     */
     CompoundData getData();
 
+    /**
+     * Creates an instance of a compound with I/O starting
+     * at the beginning of the stream or file.
+     *
+     * @return An instance of a data compound.
+     *
+     * @see #getFormat()
+     */
+    CompoundData createData();
+
+    /**
+     * Creates an instance of a compound with the given type and with I/O starting
+     * at the given stream or file position.
+     *
+     * @param position The file position in bytes.
+     *
+     * @return An instance of a data compound.
+     *
+     * @see #getFormat()
+     */
+    CompoundData createData(long position);
+
+    /**
+     * Creates an instance of a compound with the given type and with I/O starting
+     * at the given stream or file position.
+     *
+     * @param type     The compound type.
+     * @param position The file position in bytes.
+     *
+     * @return An instance of a data compound.
+     */
+    CompoundData createData(CompoundType type, long position);
+
+    /**
+     * Disposes this context and releases all associated resources.
+     */
+    void dispose();
+
+
+
+    /**
+     * Creates an instance of a compound with I/O starting
+     * at the given stream or file position.
+     * The type of the compound is the same as the format's compound type.
+     *
+     * @param position The file position in bytes.
+     *
+     * @return An instance of a data compound.
+     *
+     * @see #createData()
+     * @see #getFormat()
+     * @deprecated use {@link #createData(long)} instead
+     */
+    @Deprecated
     CompoundData getData(long position);
 
+    /**
+     * Creates an instance of a compound with the given type and with I/O starting
+     * at the given stream or file position.
+     *
+     * @param type     The compound type.
+     * @param position The file position in bytes.
+     *
+     * @return An instance of a data compound.
+     *
+     * @see #getFormat()
+     * @deprecated use {@link #createData(long)} instead
+     */
+    @Deprecated
     CompoundData getData(CompoundType type, long position);
-
-    void dispose();
 }
