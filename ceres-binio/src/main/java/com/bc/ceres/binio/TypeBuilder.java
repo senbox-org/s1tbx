3af4eced9f78dd0a247ebcbb22cbfd06b77e6f6a
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-08 07:24:14 +0000

    added javadoc
    git-svn-id: https://www.brockmann-consult.de/svn/os/ceres/trunk@6402 d90d2d48-2b33-0410-a973-e72f85796a02

--- ceres-binio/src/main/java/com/bc/ceres/binio/TypeBuilder.java
+++ ceres-binio/src/main/java/com/bc/ceres/binio/TypeBuilder.java
@@ -2,6 +2,7 @@ package com.bc.ceres.binio;
 
 import com.bc.ceres.binio.internal.CompoundMemberImpl;
 import com.bc.ceres.binio.internal.CompoundTypeImpl;
+import com.bc.ceres.binio.internal.GrowableSequenceTypeImpl;
 import com.bc.ceres.binio.internal.SequenceTypeImpl;
 import com.bc.ceres.binio.internal.VarElementCountSequenceTypeImpl;
 
@@ -29,33 +30,117 @@ import com.bc.ceres.binio.internal.VarElementCountSequenceTypeImpl;
  * </pre>
  */
 public class TypeBuilder {
+    /**
+     * The 8-bit signed integer type.
+     */
     public final static SimpleType BYTE = SimpleType.BYTE;
+    /**
+     * The 8-bit unsigned integer type.
+     */
     public final static SimpleType UBYTE = SimpleType.UBYTE;
+    /**
+     * The 16-bit signed integer type.
+     */
     public final static SimpleType SHORT = SimpleType.SHORT;
+    /**
+     * The 16-bit unsigned integer type.
+     */
     public final static SimpleType USHORT = SimpleType.USHORT;
+    /**
+     * The 32-bit unsigned integer type.
+     */
     public final static SimpleType INT = SimpleType.INT;
+    /**
+     * The 32-bit unsigned integer type.
+     */
     public final static SimpleType UINT = SimpleType.UINT;
+    /**
+     * The 64-bit signed integer type.
+     */
     public final static SimpleType LONG = SimpleType.LONG;
+    /**
+     * The 64-bit unsigned integer type.
+     */
     public final static SimpleType ULONG = SimpleType.ULONG;
+    /**
+     * The 32-bit IEEE floating point type.
+     */
     public final static SimpleType FLOAT = SimpleType.FLOAT;
+    /**
+     * The 64-bit IEEE floating point type.
+     */
     public final static SimpleType DOUBLE = SimpleType.DOUBLE;
 
+    /**
+     * Creates a sequence type for the given element type and count.
+     * If element count is greater or equal to zero, a fixed-length sequence type
+     * will be created.
+     * If element count is less than zero, a growable sequence type
+     * will be created.
+     *
+     * @param elementType  The sequence's element type.
+     * @param elementCount The sequence's element count.
+     *
+     * @return A sequence type.
+     */
     public static SequenceType SEQUENCE(Type elementType, int elementCount) {
-        return new SequenceTypeImpl(elementType, elementCount);
+        if (elementCount >= 0) {
+            return new SequenceTypeImpl(elementType, elementCount);
+        } else {
+            return new GrowableSequenceTypeImpl(elementType);
+        }
     }
 
+    /**
+     * Creates a dynamic sequence type whose element count is resolved
+     * by a compound member determined by the given member name.
+     * The member must have integer type.
+     *
+     * @param elementType The sequence's element type.
+     * @param memberName  The parent compount's member name.
+     *
+     * @return A dynamic sequence type.
+     */
     public static VarSequenceType VAR_SEQUENCE(Type elementType, String memberName) {
         return new VarElementCountSequenceTypeImpl(elementType, memberName);
     }
 
+    /**
+     * Creates a dynamic sequence type whose element count is resolved
+     * by a compound member determined by the given member index.
+     * The member must have integer type.
+     *
+     * @param elementType The sequence's element type.
+     * @param memberIndex The parent compount's member index.
+     *
+     * @return A dynamic sequence type.
+     */
     public static VarSequenceType VAR_SEQUENCE(Type elementType, int memberIndex) {
         return new VarElementCountSequenceTypeImpl(elementType, memberIndex);
     }
 
+    /**
+     * Creates a compound member.
+     *
+     * @param name The member's name.
+     * @param type The member's type.
+     *
+     * @return The compound member.
+     *
+     * @see #COMPOUND(String, CompoundMember[])
+     */
     public static CompoundMember MEMBER(String name, Type type) {
         return new CompoundMemberImpl(name, type);
     }
 
+    /**
+     * Creates a compound type.
+     *
+     * @param name    The compound's name.
+     * @param members The compound's members.
+     *
+     * @return The compound type.
+     */
     public static CompoundType COMPOUND(String name, CompoundMember... members) {
         return new CompoundTypeImpl(name, members);
     }
