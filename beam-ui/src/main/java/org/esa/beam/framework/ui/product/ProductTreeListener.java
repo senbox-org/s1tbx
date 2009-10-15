4fd758b92491e9c4a01d7dcd05abeea37e4f86c4
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-15 15:25:14 +0000

    api doc
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6484 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-ui/src/main/java/org/esa/beam/framework/ui/product/ProductTreeListener.java
+++ beam-ui/src/main/java/org/esa/beam/framework/ui/product/ProductTreeListener.java
@@ -16,12 +16,15 @@
  */
 package org.esa.beam.framework.ui.product;
 
-import java.util.EventListener;
-
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.MetadataElement;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.TiePointGrid;
+
+import java.util.EventListener;
+
+// todo - this is a stupid interface, use SelectionService/SelectionProvide instead (nf, 10.2009)
+
 
 /**
  * A listener which is listening for events occuring in a <code>ProductTree</code>.
@@ -35,31 +38,47 @@ public interface ProductTreeListener extends EventListener {
 
     /**
      * Called when a product has been added to the tree.
+     *
+     * @param product The product.
      */
     void productAdded(Product product);
 
     /**
      * Called when a product has been removed from the tree.
+     *
+     * @param product The product.
      */
     void productRemoved(Product product);
 
     /**
      * Called when a product has been selected in the tree.
+     *
+     * @param product    The selected product.
+     * @param clickCount The number of mouse clicks.
      */
     void productSelected(Product product, int clickCount);
 
     /**
      * Called when a product's metadata element has been selected in the tree.
+     *
+     * @param matadataElement The selected metadata element.
+     * @param clickCount      The number of mouse clicks.
      */
-    void metadataElementSelected(MetadataElement group, int clickCount);
+    void metadataElementSelected(MetadataElement matadataElement, int clickCount);
 
     /**
      * Called when a product's tie-point grid has been selected in the tree.
+     *
+     * @param tiePointGrid The selected tie-point grid.
+     * @param clickCount   The number of mouse clicks.
      */
     void tiePointGridSelected(TiePointGrid tiePointGrid, int clickCount);
 
     /**
      * Called when a product's band has been selected in the tree.
+     *
+     * @param band       The selected band.
+     * @param clickCount The number of mouse clicks.
      */
     void bandSelected(Band band, int clickCount);
 }
