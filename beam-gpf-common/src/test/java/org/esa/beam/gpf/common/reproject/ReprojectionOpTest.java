48dc27f94253521f42b7e6811f99a8d8bba3a727
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-16 07:23:29 +0000

    sorted tests methods
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6497 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/test/java/org/esa/beam/gpf/common/reproject/ReprojectionOpTest.java
+++ beam-gpf-common/src/test/java/org/esa/beam/gpf/common/reproject/ReprojectionOpTest.java
@@ -142,85 +142,6 @@ public class ReprojectionOpTest {
         assertTrue(targetProduct.isCompatibleProduct(collocationProduct, 1.0e-6f));
     }
     
-    @Test(expected = OperatorException.class)
-    public void testEmptyParameterMap() {
-        createReprojectedProduct();
-    }
-
-    @Test(expected = OperatorException.class)
-    public void testParameterAmbigouity_wkt_epsgCode() {
-        parameterMap.put("wkt", UTM33N_WKT);
-        parameterMap.put("crsCode", UTM33N_CODE);
-        createReprojectedProduct();
-    }
-    
-    @Test(expected = OperatorException.class)
-    public void testParameterAmbigouity_wkt_wktFile() {
-        parameterMap.put("wkt", UTM33N_WKT);
-        parameterMap.put("wktFile", wktFile);
-        createReprojectedProduct();
-    }
-    
-    @Test(expected = OperatorException.class)
-    public void testParameterAmbigouity_wkt_collocateProduct() {
-        final Map<String, Product> productMap = new HashMap<String, Product>(5);
-        productMap.put("source", sourceProduct);
-        productMap.put("collocate", sourceProduct);
-        parameterMap.put("wkt", UTM33N_WKT);
-        createReprojectedProduct(productMap);
-    }
-
-    @Test(expected = OperatorException.class)
-    public void testUnknownResamplingMethode() {
-        parameterMap.put("resampling", "Super_Duper_Resampling");
-        createReprojectedProduct();
-    }
-    
-    @Test(expected = OperatorException.class)
-    public void testMissingPixelSizeY() {
-        parameterMap.put("pixelSizeX", 0.024);
-        createReprojectedProduct();
-    }
-    
-    @Test(expected = OperatorException.class)
-    public void testMissingPixelSizeX() {
-        parameterMap.put("pixelSizeY", 0.024);
-        createReprojectedProduct();
-    }
-
-    @Test(expected = OperatorException.class)
-    public void testMissingReferencingPixelX() {
-        parameterMap.put("referencePixelY", 0.5);
-        parameterMap.put("easting", 1234.5);
-        parameterMap.put("northing", 1234.5);
-        createReprojectedProduct();
-    }
-
-    @Test(expected = OperatorException.class)
-    public void testMissingReferencingpixelY() {
-        parameterMap.put("referencePixelX", 0.5);
-        parameterMap.put("easting", 1234.5);
-        parameterMap.put("northing", 1234.5);
-        createReprojectedProduct();
-    }
-    
-
-    @Test(expected = OperatorException.class)
-    public void testMissingReferencingNorthing() {
-        parameterMap.put("referencePixelX", 0.5);
-        parameterMap.put("referencePixelY", 0.5);
-        parameterMap.put("easting", 1234.5);
-        createReprojectedProduct();
-    }
-
-    @Test(expected = OperatorException.class)
-    public void testMissingReferencingEasting() {
-        parameterMap.put("referencePixelX", 0.5);
-        parameterMap.put("referencePixelY", 0.5);
-        parameterMap.put("northing", 1234.5);
-        createReprojectedProduct();
-    }
-    
     @Test
     public void testUTM() throws IOException {
         parameterMap.put("crsCode", UTM33N_CODE);
@@ -311,7 +232,88 @@ public class ReprojectionOpTest {
         latGrid = targetPoduct.getBand("latGrid");
         assertNull(latGrid);
     }
-    
+
+
+    // Expected failure cases 
+    @Test(expected = OperatorException.class)
+    public void testEmptyParameterMap() {
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testParameterAmbigouity_wkt_epsgCode() {
+        parameterMap.put("wkt", UTM33N_WKT);
+        parameterMap.put("crsCode", UTM33N_CODE);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testParameterAmbigouity_wkt_wktFile() {
+        parameterMap.put("wkt", UTM33N_WKT);
+        parameterMap.put("wktFile", wktFile);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testParameterAmbigouity_wkt_collocateProduct() {
+        final Map<String, Product> productMap = new HashMap<String, Product>(5);
+        productMap.put("source", sourceProduct);
+        productMap.put("collocate", sourceProduct);
+        parameterMap.put("wkt", UTM33N_WKT);
+        createReprojectedProduct(productMap);
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testUnknownResamplingMethode() {
+        parameterMap.put("resampling", "Super_Duper_Resampling");
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testMissingPixelSizeY() {
+        parameterMap.put("pixelSizeX", 0.024);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testMissingPixelSizeX() {
+        parameterMap.put("pixelSizeY", 0.024);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testMissingReferencingPixelX() {
+        parameterMap.put("referencePixelY", 0.5);
+        parameterMap.put("easting", 1234.5);
+        parameterMap.put("northing", 1234.5);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testMissingReferencingpixelY() {
+        parameterMap.put("referencePixelX", 0.5);
+        parameterMap.put("easting", 1234.5);
+        parameterMap.put("northing", 1234.5);
+        createReprojectedProduct();
+    }
+
+
+    @Test(expected = OperatorException.class)
+    public void testMissingReferencingNorthing() {
+        parameterMap.put("referencePixelX", 0.5);
+        parameterMap.put("referencePixelY", 0.5);
+        parameterMap.put("easting", 1234.5);
+        createReprojectedProduct();
+    }
+
+    @Test(expected = OperatorException.class)
+    public void testMissingReferencingEasting() {
+        parameterMap.put("referencePixelX", 0.5);
+        parameterMap.put("referencePixelY", 0.5);
+        parameterMap.put("northing", 1234.5);
+        createReprojectedProduct();
+    }
+
     private Product createReprojectedProduct(Map<String, Product> sourceMap) {
         String operatorName = OperatorSpi.getOperatorAlias(ReprojectionOp.class);
         return GPF.createProduct(operatorName, parameterMap, sourceMap);
