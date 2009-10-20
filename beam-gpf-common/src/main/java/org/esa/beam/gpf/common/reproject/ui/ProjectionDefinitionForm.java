70e8e13ec5290ef17e76d48ab5923e52d46e994c
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-20 08:10:18 +0000

    ProjectionDefinitionForm
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6531 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/ProjectionDefinitionForm.java
@@ -0,0 +1,237 @@
+package org.esa.beam.gpf.common.reproject.ui;
+
+import com.bc.ceres.binding.ValueAccessor;
+import com.bc.ceres.binding.ValueContainer;
+import com.bc.ceres.binding.ValueDescriptor;
+import com.bc.ceres.binding.ValueModel;
+import com.bc.ceres.binding.swing.BindingContext;
+import com.bc.ceres.swing.TableLayout;
+import org.esa.beam.framework.ui.AbstractDialog;
+import org.esa.beam.framework.ui.ModalDialog;
+import org.esa.beam.framework.ui.ValueEditorsPane;
+import org.geotools.referencing.ReferencingFactoryFinder;
+import org.opengis.parameter.GeneralParameterDescriptor;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterDescriptor;
+import org.opengis.parameter.ParameterValue;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.AuthorityFactory;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.datum.DatumAuthorityFactory;
+import org.opengis.referencing.datum.GeodeticDatum;
+import org.opengis.referencing.operation.MathTransformFactory;
+import org.opengis.referencing.operation.OperationMethod;
+import org.opengis.referencing.operation.Projection;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Marco Peters
+ * @author Marco Zühlke
+ * @version $ Revision $ Date $
+ * @since BEAM 4.7
+ */
+public class ProjectionDefinitionForm extends JPanel {
+
+    private final List<GeodeticDatum> datumList;
+    private final List<OperationMethod> operationMethodList;
+    private ProjectionDefinitionForm.Model model;
+    private ValueContainer vc;
+    private final Window parent;
+
+    public ProjectionDefinitionForm(Window parent, List<OperationMethod> operationMethodList,
+                                List<GeodeticDatum> datumList) {
+        this.parent = parent;
+        this.operationMethodList = new ArrayList<OperationMethod>(operationMethodList);
+        this.datumList = new ArrayList<GeodeticDatum>(datumList);
+        Collections.sort(this.operationMethodList, new IdentifiedObjectComparator());
+        Collections.sort(this.datumList, new IdentifiedObjectComparator());
+        
+        model = new Model();
+        // todo - set Lat/Lon WGS84
+        model.transformation = this.operationMethodList.get(0);
+        model.datum = this.datumList.get(0);
+        updateModel(model);
+
+        vc = ValueContainer.createObjectBacked(model);
+        vc.addPropertyChangeListener(new UpdateListener());
+
+        creatUI();
+    }
+
+    private void creatUI() {
+        final TableLayout tableLayout = new TableLayout(2);
+        setLayout(tableLayout);
+        tableLayout.setTableFill(TableLayout.Fill.HORIZONTAL);
+        tableLayout.setTablePadding(4, 4);
+        tableLayout.setTableWeightY(0.5);
+        tableLayout.setTableAnchor(TableLayout.Anchor.NORTHWEST);
+
+        tableLayout.setColumnWeightX(0, 0.0);
+        tableLayout.setColumnWeightX(1, 1.0);
+        tableLayout.setCellAnchor(2, 1, TableLayout.Anchor.EAST);
+        tableLayout.setCellFill(2, 1, TableLayout.Fill.NONE);
+
+        final JLabel transformLabel = new JLabel("Transform:");
+        final JLabel datumLabel = new JLabel("Datum:");
+        final JComboBox operationComboBox = new JComboBox(operationMethodList.toArray());
+        operationComboBox.setRenderer(new IdentifiedObjectCellRenderer());
+        final JComboBox datumComboBox = new JComboBox(datumList.toArray());
+
+        datumComboBox.setRenderer(new IdentifiedObjectCellRenderer());
+        final JButton paramButton = new JButton("Parameters");
+        paramButton.addActionListener(new ParameterButtonListener());
+        add(transformLabel);
+        add(operationComboBox);
+        add(datumLabel);
+        add(datumComboBox);
+        add(tableLayout.createHorizontalSpacer());
+        add(paramButton);
+
+        final BindingContext context = new BindingContext(vc);
+        context.bind("transformation", operationComboBox);
+        context.bind("datum", datumComboBox);
+
+    }
+
+    private void updateModel(final Model model) {
+        model.parameters = model.transformation.getParameters().createValue();
+        if (model.datum != null) {
+            model.parameters.parameter("semi_major").setValue(model.datum.getEllipsoid().getSemiMajorAxis());
+            model.parameters.parameter("semi_minor").setValue(model.datum.getEllipsoid().getSemiMinorAxis());
+        }
+    }
+
+    private class UpdateListener implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if ("parameters".equals(evt.getPropertyName())) {
+                return;
+            }
+            updateModel(model);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<GeodeticDatum> datumList = createDatumList();
+        List<OperationMethod> methodList = createProjectionMethodList();
+        final JFrame frame = new JFrame("Projection Method Form Test");
+        final ProjectionDefinitionForm definitionForm = new ProjectionDefinitionForm(frame, methodList, datumList);
+        frame.setContentPane(definitionForm);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+
+
+    }
+
+    private static List<OperationMethod> createProjectionMethodList() {
+        MathTransformFactory factory = ReferencingFactoryFinder.getMathTransformFactory(null);
+        Set<OperationMethod> methods = factory.getAvailableMethods(Projection.class);
+        return new ArrayList<OperationMethod>(methods);
+    }
+
+    private static List<GeodeticDatum> createDatumList() {
+        DatumAuthorityFactory factory = ReferencingFactoryFinder.getDatumAuthorityFactory("EPSG", null);
+        List<String> datumCodes = retrieveCodes(GeodeticDatum.class, factory);
+        List<GeodeticDatum> datumList = new ArrayList<GeodeticDatum>(datumCodes.size());
+        for (String datumCode : datumCodes) {
+            try {
+                datumList.add(factory.createGeodeticDatum(datumCode));
+            } catch (FactoryException ignored) {
+            }
+        }
+        return datumList;
+    }
+
+    private static List<String> retrieveCodes(Class<? extends GeodeticDatum> crsType, AuthorityFactory factory) {
+        try {
+            Set<String> localCodes = factory.getAuthorityCodes(crsType);
+            return new ArrayList<String>(localCodes);
+        } catch (FactoryException ignore) {
+            return Collections.emptyList();
+        }
+    }
+
+    public static ValueContainer createValueContainer(ParameterValueGroup valueGroup) {
+        final ValueContainer vc = new ValueContainer();
+
+        final List<GeneralParameterValue> values = valueGroup.values();
+        for (GeneralParameterValue value : values) {
+            final GeneralParameterDescriptor descriptor = value.getDescriptor();
+            final Class valueType;
+            if (descriptor instanceof ParameterDescriptor) {
+                ParameterDescriptor parameterDescriptor = (ParameterDescriptor) descriptor;
+                valueType = parameterDescriptor.getValueClass();
+            } else {
+                valueType = Double.TYPE;
+            }
+
+            final String paramName = descriptor.getName().getCode();
+            final ValueDescriptor vd = new ValueDescriptor(paramName, valueType);
+            final ParameterValue<?> parameterValue = valueGroup.parameter(paramName);
+            if (parameterValue.getUnit() != null) {
+                vd.setUnit(String.valueOf(parameterValue.getUnit()));
+            }
+            vd.setDefaultConverter();
+            final ValueModel valueModel = new ValueModel(vd, new ValueAccessor() {
+                @Override
+                public Object getValue() {
+                    return parameterValue.getValue();
+                }
+
+                @Override
+                public void setValue(Object value) {
+                    parameterValue.setValue(value);
+                }
+            });
+            vc.addModel(valueModel);
+        }
+        return vc;
+    }
+
+    private static class Model {
+
+        private OperationMethod transformation;
+        private GeodeticDatum datum;
+        private ParameterValueGroup parameters;
+
+    }
+
+    private class ParameterButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            final String operationName = model.transformation.getName().getCode();
+            final ModalDialog modalDialog = new ModalDialog(parent, operationName + " - Parameters",
+                                                            ModalDialog.ID_OK_CANCEL, null);
+            final ParameterValueGroup workCopy = model.parameters.clone();
+            final ValueContainer valueContainer = createValueContainer(workCopy);
+            modalDialog.setContent(new ValueEditorsPane(valueContainer).createPanel());
+            if (modalDialog.show() == AbstractDialog.ID_OK) {
+                vc.setValue("parameters", workCopy);
+            }
+        }
+    }
+}
