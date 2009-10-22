669f4781651b70c3c776d5e028f73bb85937709b
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-22 16:11:25 +0000

    enable collocation only when possible
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6572 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/ReprojectionForm.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/ReprojectionForm.java
@@ -361,6 +361,8 @@ public class ReprojectionForm extends JTabbedPane {
     }
 
     private void updateUIState() {
+        boolean collocationEnabled = (collocateProductSelector.getProductCount()>0);
+        collocateButtonModel.setEnabled(collocationEnabled);
         final boolean collocate = collocateButtonModel.isSelected();
         collocateProductSelector.getProductNameComboBox().setEnabled(collocate);
         collocateProductSelector.getProductFileChooserButton().setEnabled(collocate);
