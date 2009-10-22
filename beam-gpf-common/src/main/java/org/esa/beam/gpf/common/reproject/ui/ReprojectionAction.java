518dcc293de51bc278dfa59df58b472e0324e0d6
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-22 12:56:47 +0000

    remove unused code
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6567 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/ReprojectionAction.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/ReprojectionAction.java
@@ -1,17 +1,8 @@
 package org.esa.beam.gpf.common.reproject.ui;
 
-import com.bc.ceres.core.ServiceRegistry;
-import com.bc.ceres.core.ServiceRegistryFactory;
-
 import org.esa.beam.framework.ui.ModelessDialog;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.visat.actions.AbstractVisatAction;
-import org.geotools.factory.FactoryIteratorProvider;
-import org.geotools.factory.GeoTools;
-import org.geotools.referencing.operation.MathTransformProvider;
-import org.opengis.referencing.operation.MathTransform;
-
-import java.util.Iterator;
 
 /**
  * Geographic collocation action.
@@ -21,22 +12,6 @@ import java.util.Iterator;
  */
 public class ReprojectionAction extends AbstractVisatAction {
     
-    //TODO move to an activator or so (mz 10.2009)
-//    static {
-//        final ServiceRegistry<MathTransformProvider> serviceRegistry = ServiceRegistryFactory.getInstance().getServiceRegistry(MathTransformProvider.class);
-//        GeoTools.addFactoryIteratorProvider(new FactoryIteratorProvider() {
-//            
-//            @Override
-//            public <T> Iterator<T> iterator(Class<T> category) {
-//                if (category.equals(serviceRegistry.getServiceType()) ) {
-//                    return (Iterator<T>) serviceRegistry.getServices().iterator();
-//                } else {
-//                    return null;
-//                }
-//            }
-//        });
-//    }
-
     private ModelessDialog dialog;
 
     @Override
