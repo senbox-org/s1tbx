0edddfb6a6936ac1317dc968f098631c1eb141ec
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-11-06 16:45:05 +0000

    Started MosaicOp
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6880 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/mosaic/MosaicOp.java
@@ -0,0 +1,346 @@
+package org.esa.beam.gpf.common.mosaic;
+
+import com.bc.ceres.glevel.MultiLevelImage;
+import com.bc.jexp.ParseException;
+import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.CrsGeoCoding;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.dataop.barithm.BandArithmetic;
+import org.esa.beam.framework.gpf.GPF;
+import org.esa.beam.framework.gpf.Operator;
+import org.esa.beam.framework.gpf.OperatorException;
+import org.esa.beam.framework.gpf.OperatorSpi;
+import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
+import org.esa.beam.framework.gpf.annotations.Parameter;
+import org.esa.beam.framework.gpf.annotations.SourceProducts;
+import org.esa.beam.framework.gpf.annotations.TargetProduct;
+import org.esa.beam.jai.ResolutionLevel;
+import org.esa.beam.jai.VirtualBandOpImage;
+import org.geotools.geometry.Envelope2D;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.geometry.Envelope;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import javax.media.jai.PlanarImage;
+import javax.media.jai.ROI;
+import javax.media.jai.RenderedOp;
+import javax.media.jai.operator.AddCollectionDescriptor;
+import javax.media.jai.operator.ConstantDescriptor;
+import javax.media.jai.operator.FormatDescriptor;
+import javax.media.jai.operator.MosaicDescriptor;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.DataBuffer;
+import java.awt.image.RenderedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Marco Peters
+ * @since BEAM 4.7
+ */
+@OperatorMetadata(alias = "Mosaic",
+                  version = "1.0",
+                  authors = "Marco Peters, Ralf Quast, Marco Zühlke",
+                  copyright = "(c) 2009 by Brockmann Consult",
+                  description = "Creates a mosaic product out of a set of source products.",
+                  internal = true)
+public class MosaicOp extends Operator {
+
+    @SourceProducts(count = -1, description = "The source products to be used for mosaic.")
+    private Product[] sourceProducts;
+
+    @TargetProduct
+    Product targetProduct;
+
+    @Parameter(itemAlias = "variable", description = "Specifies the bands in the target product.")
+    Variable[] outputVariables;
+
+    @Parameter(itemAlias = "condition", description = "Specifies valid pixels considered in the target product.")
+    Condition[] conditions;
+
+    @Parameter(description = "Specifies the way how conditions are combined.", defaultValue = "OR",
+               valueSet = {"OR", "AND"})
+    String combine;
+
+    @Parameter(description = "Specifies whether the target product shall be updated.")
+    boolean update;
+
+    @Parameter(alias = "epsgCode", description = "Specifies the coordinate reference system of the target product.",
+               defaultValue = "EPSG:4326")
+    String crsCode;
+
+    @Parameter(alias = "resampling", label = "Resampling Method", description = "The method used for resampling.",
+               valueSet = {"Nearest", "Bilinear", "Bicubic"}, defaultValue = "Nearest")
+    String resamplingName;
+
+    @Parameter(description = "Specifies the boundary of the target product in map units.")
+    GeoBounds boundary;
+
+    @Parameter(description = "Size of a pixel in X-direction in map units.")
+    double pixelSizeX;
+    @Parameter(description = "Size of a pixel in Y-direction in map units.")
+    double pixelSizeY;
+
+    private CoordinateReferenceSystem targetCRS;
+    private Product[] reprojectedProducts;
+
+
+    @Override
+    public void initialize() throws OperatorException {
+        targetProduct = createTargetProduct();
+        reprojectedProducts = createReprojectedProducts();
+
+        // for each product one 'mask' image is created, where all conditions are combined.
+        // 1.0 indicates combined condition is true and 0.0 indicates false.
+        List<PlanarImage> combinedConditionImageList = createCombinedConditionImageList();
+
+        // The countImage indicates the number of considered source samples for the target mosaic.
+        final RenderedImage countImage = AddCollectionDescriptor.create(combinedConditionImageList, null);
+
+        // for each product and for each variable one 'source' image is created.
+        List<List<RenderedImage>> sourceImageList = createSourceImages();
+
+        List<List<ROI>> maskImages;
+        try {
+            maskImages = createSourceMaskImages();
+        } catch (ParseException e) {
+            throw new OperatorException(e);
+        }
+
+        List<RenderedImage> mosaicImages = createMosaicImages(sourceImageList,
+                                                              maskImages,
+                                                              combinedConditionImageList,
+                                                              countImage);
+        addBands(targetProduct, mosaicImages, countImage);
+    }
+
+    private List<List<ROI>> createSourceMaskImages() throws ParseException {
+        List<List<ROI>> variableRoiList = new ArrayList<List<ROI>>(outputVariables.length);
+        for (Variable outputVariable : outputVariables) {
+            final ArrayList<ROI> roiList = new ArrayList<ROI>(reprojectedProducts.length);
+            variableRoiList.add(roiList);
+            for (Product product : reprojectedProducts) {
+                final String validMaskExpression = BandArithmetic.getValidMaskExpression(outputVariable.expression,
+                                                                                         new Product[]{product}, 0,
+                                                                                         null);
+                roiList.add(new ROI(createExpressionImage(validMaskExpression, product)));
+            }
+
+        }
+        return variableRoiList;
+    }
+
+    private List<RenderedImage> createMosaicImages(List<List<RenderedImage>> sourceImageList,
+                                                   List<List<ROI>> roiList,
+                                                   List<PlanarImage> combinedConditionImageList,
+                                                   RenderedImage countImage) {
+        List<RenderedImage> mosaicImages = new ArrayList<RenderedImage>(sourceImageList.size());
+        final PlanarImage[] sourceAlphas = combinedConditionImageList.toArray(
+                new PlanarImage[combinedConditionImageList.size()]);
+        for (int i = 0; i < sourceImageList.size(); i++) {
+            List<RenderedImage> sourceImages = sourceImageList.get(i);
+            final List<ROI> rois = roiList.get(i);
+            final RenderedImage[] renderedImages = sourceImages.toArray(new RenderedImage[sourceImages.size()]);
+            final ROI[] roiArray = rois.toArray(new ROI[sourceImages.size()]);
+            mosaicImages.add(MosaicDescriptor.create(renderedImages, MosaicDescriptor.MOSAIC_TYPE_BLEND,
+                                                     sourceAlphas, roiArray, null, null, null));
+        }
+
+        return mosaicImages;
+    }
+
+    private void addBands(Product product, List<RenderedImage> bandImages, RenderedImage countImage) {
+        for (int i = 0; i < outputVariables.length; i++) {
+            Variable outputVariable = outputVariables[i];
+            Band band = product.addBand(outputVariable.name, ProductData.TYPE_FLOAT32);
+            band.setDescription(outputVariable.expression);
+            band.setSourceImage(bandImages.get(i));
+        }
+
+        if (conditions != null) {
+            for (Condition condition : conditions) {
+                if (condition.output) {
+                    Band band = product.addBand(condition.name, ProductData.TYPE_INT32);
+                    band.setDescription(condition.expression);
+                    // The sum of all conditions of all sources is created.
+                    // 1.0 indicates condition is true and 0.0 indicates false.
+                    final RenderedOp sumImage = createConditionSumImage(condition);
+                    final RenderedOp reformatedImage = FormatDescriptor.create(sumImage, DataBuffer.TYPE_INT, null);
+                    band.setSourceImage(reformatedImage);
+                }
+            }
+        }
+
+        Band countBand = product.addBand("num_pixels", ProductData.TYPE_INT32);
+        countBand.setDescription("Number of considerd pixels");
+        final RenderedOp intCountImage = FormatDescriptor.create(countImage, DataBuffer.TYPE_INT, null);
+        countBand.setSourceImage(intCountImage);
+    }
+
+    private RenderedOp createConditionSumImage(Condition condition) {
+        final ArrayList<RenderedImage> renderedImageList = new ArrayList<RenderedImage>(reprojectedProducts.length);
+        for (Product reprojectedProduct : reprojectedProducts) {
+            // the condition images are used as sourceAlpha parameter for MosaicOpImage, they have to have the same
+            // data type as the source images. That's why we use normal expression images with data type FLOAT32.
+            renderedImageList.add(createExpressionImage(condition.expression, reprojectedProduct));
+        }
+        return AddCollectionDescriptor.create(renderedImageList, null);
+    }
+
+
+    private List<List<RenderedImage>> createSourceImages() {
+        List<List<RenderedImage>> conditionImageList = new ArrayList<List<RenderedImage>>(outputVariables.length);
+        for (Variable variable : outputVariables) {
+            final ArrayList<RenderedImage> renderedImageList = new ArrayList<RenderedImage>(reprojectedProducts.length);
+            conditionImageList.add(renderedImageList);
+            for (Product reprojectedProduct : reprojectedProducts) {
+                renderedImageList.add(createExpressionImage(variable.expression, reprojectedProduct));
+            }
+        }
+        return conditionImageList;
+    }
+
+    private List<PlanarImage> createCombinedConditionImageList() {
+        List<PlanarImage> combinedConditionImageList = new ArrayList<PlanarImage>(reprojectedProducts.length);
+        for (Product reprojectedProduct : reprojectedProducts) {
+            final PlanarImage image;
+            if (conditions != null && conditions.length > 0) {
+                final StringBuilder combinedExpression = new StringBuilder();
+                for (int i = 0; i < conditions.length; i++) {
+                    Condition condition = conditions[i];
+                    if (i != 0) {
+                        combinedExpression.append(" ").append(combine).append(" ");
+                    }
+                    combinedExpression.append(condition.expression);
+                }
+                // the condition images are used as sourceAlpha parameter for MosaicOpImage, they have to have the same
+                // data type as the source images. That's why we use normal expression images with data type FLOAT32.
+                image = createExpressionImage(combinedExpression.toString(), reprojectedProduct);
+            }else {
+                image = ConstantDescriptor.create((float) targetProduct.getSceneRasterWidth(),
+                                                  (float) targetProduct.getSceneRasterHeight(),
+                                                  new Float[]{1.0f}, null);
+            }
+            combinedConditionImageList.add(image);
+        }
+        return combinedConditionImageList;
+    }
+
+    private PlanarImage createExpressionImage(final String expression, Product product) {
+        final MultiLevelImage sourceImage = product.getBandAt(0).getSourceImage();
+        final ResolutionLevel resolutionLevel = ResolutionLevel.create(sourceImage.getModel(), 0);
+        final float fillValue = 0.0f;
+        return VirtualBandOpImage.create(expression, ProductData.TYPE_FLOAT32, fillValue, product, resolutionLevel);
+    }
+
+    private Product[] createReprojectedProducts() {
+        Product[] reprojProducts = new Product[sourceProducts.length];
+        final HashMap<String, Object> projParameters = createProjectionParameters();
+        for (int i = 0; i < sourceProducts.length; i++) {
+            HashMap<String, Product> projProducts = new HashMap<String, Product>();
+            projProducts.put("source", sourceProducts[i]);
+            projProducts.put("collocate", targetProduct);
+            reprojProducts[i] = GPF.createProduct("Reproject", projParameters, projProducts);
+        }
+        return reprojProducts;
+    }
+
+    private HashMap<String, Object> createProjectionParameters() {
+        HashMap<String, Object> projParameters = new HashMap<String, Object>();
+        projParameters.put("resamplingName", resamplingName);
+        projParameters.put("includeTiePointGrids",
+                           true);  // ensure tie-points are reprojected - don't relay on default value
+        return projParameters;
+    }
+
+
+    private Product createTargetProduct() {
+        Product product;
+        try {
+            targetCRS = CRS.decode(crsCode, true);
+            Rectangle2D.Double rect = new Rectangle2D.Double();
+            rect.setFrameFromDiagonal(boundary.west, boundary.north, boundary.east, boundary.south);
+            Envelope envelope = CRS.transform(new Envelope2D(DefaultGeographicCRS.WGS84, rect), targetCRS);
+            int width = (int) (envelope.getSpan(0) / pixelSizeX);
+            int height = (int) (envelope.getSpan(1) / pixelSizeY);
+            final AffineTransform mapTransform = new AffineTransform();
+            mapTransform.translate(boundary.west, boundary.north);
+            mapTransform.scale(pixelSizeX, -pixelSizeY);
+            mapTransform.translate(-0.5, -0.5);
+            CrsGeoCoding geoCoding = new CrsGeoCoding(targetCRS, new Rectangle(0, 0, width, height),
+                                                      mapTransform);
+            product = new Product("mosaic", "BEAM_MOSAIC", width, height);
+            product.setGeoCoding(geoCoding);
+        } catch (Exception e) {
+            throw new OperatorException(e);
+        }
+
+        return product;
+    }
+
+    public static class Variable {
+
+        @Parameter(description = "The name of the variable.")
+        String name;
+        @Parameter(description = "The expression of the variable.")
+        String expression;
+
+        public Variable() {
+        }
+
+        Variable(String name, String expression) {
+            this.name = name;
+            this.expression = expression;
+        }
+    }
+
+    public static class Condition {
+
+        @Parameter(description = "The name of the condition.")
+        String name;
+        @Parameter(description = "The expression of the condition.")
+        String expression;
+        @Parameter(description = "Whether the result of the condition shall be written.")
+        boolean output;
+
+    }
+
+    public static class GeoBounds {
+
+        @Parameter(description = "The western longitude.")
+        double west;
+        @Parameter(description = "The northern latitude.")
+        double north;
+        @Parameter(description = "The eastern longitude.")
+        double east;
+        @Parameter(description = "The southern latitude.")
+        double south;
+
+        public GeoBounds() {
+        }
+
+        GeoBounds(double west, double north, double east, double south) {
+            this.west = west;
+            this.north = north;
+            this.east = east;
+            this.south = south;
+        }
+    }
+
+    public static class Spi extends OperatorSpi {
+
+        public Spi() {
+            super(MosaicOp.class);
+        }
+    }
+
+
+    private class ConditionImageKey {
+
+    }
+}
---
+++ beam-gpf-common/src/test/java/org/esa/beam/gpf/common/mosaic/MosaicOpTest.java
@@ -0,0 +1,89 @@
+package org.esa.beam.gpf.common.mosaic;
+
+import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.CrsGeoCoding;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.util.Debug;
+import org.geotools.referencing.CRS;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
+
+import javax.media.jai.operator.ConstantDescriptor;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.io.IOException;
+
+/**
+ * @author Marco Peters
+ * @version $ Revision $ Date $
+ * @since BEAM 4.7
+ */
+public class MosaicOpTest {
+
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
+
+
+    private static Product product1;
+    private static Product product2;
+    private static Product product3;
+
+    @BeforeClass
+    public static void setup() throws FactoryException, TransformException, IOException {
+        product1 = createProduct("P1", 0, 0, 2.0f);
+        product2 = createProduct("P2", 5, -5, 3.0f);
+        product3 = createProduct("P3", -5, 5, 5.0f);
+    }
+
+    private static Product createProduct(final String name, final int easting, final int northing,
+                                         final float bandFillValue) throws FactoryException, TransformException {
+        final Product product = new Product(name, "T", WIDTH, HEIGHT);
+        product.addBand(createBand(bandFillValue));
+        final AffineTransform transform = new AffineTransform();
+        transform.translate(easting, northing);
+        transform.scale(1, -1);
+        transform.translate(-0.5, -0.5);
+        product.setGeoCoding(new CrsGeoCoding(CRS.decode("EPSG:4326"), new Rectangle(0, 0, WIDTH, HEIGHT), transform));
+        return product;
+    }
+
+    private static Band createBand(float fillValue) {
+        final Band band = new Band("b1", ProductData.TYPE_FLOAT32, WIDTH, HEIGHT);
+        band.setSourceImage(ConstantDescriptor.create((float) WIDTH, (float) HEIGHT, new Float[]{fillValue}, null));
+        return band;
+    }
+
+    @AfterClass
+    public static void teardown() {
+        product1.dispose();
+        product2.dispose();
+        product3.dispose();
+    }
+
+    @Test
+    public void testXXXXX() throws IOException {
+        final MosaicOp op = new MosaicOp();
+        op.setSourceProducts(new Product[]{product1, product2, product3});
+        op.outputVariables = new MosaicOp.Variable[]{
+                new MosaicOp.Variable("b1", "b1"),
+
+        };
+        op.boundary = new MosaicOp.GeoBounds(-10, 10, 10, -10);
+        op.pixelSizeX = 1;
+        op.pixelSizeY = 1;
+
+        final Product product = op.getTargetProduct();
+        Debug.setEnabled(true);
+//        final StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        final File outputFile = new File(
+//                "C:\\Dokumente und Einstellungen\\Marco Peters\\Eigene Dateien\\EOData\\temp\\MosaicOpTest2.dim");
+//        WriteOp.writeProduct(product, outputFile, ProductIO.DEFAULT_FORMAT_NAME,
+//                             new PrintWriterProgressMonitor(System.out));
+//        stopWatch.stopAndTrace("DONE");
+    }
+}
