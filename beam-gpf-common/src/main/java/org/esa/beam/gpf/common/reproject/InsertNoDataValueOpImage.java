9ebeb0323a7224130b0ccb72c22558dee4998701
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-11-11 17:36:41 +0000

    impl missing datatypes
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@6914 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/InsertNoDataValueOpImage.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/InsertNoDataValueOpImage.java
@@ -48,6 +48,18 @@ final class InsertNoDataValueOpImage extends PointOpImage {
         RasterAccessor m = new RasterAccessor(maskTile, destRect, maskRasterFormatTag, maskImage.getColorModel());
         RasterAccessor d = new RasterAccessor(dest, destRect, formatTags[1], getColorModel());
         switch (d.getDataType()) {
+            case 0: // '\0'
+                computeRectByte(s,  m, d, (byte) noDataValue);
+                break;
+
+            case 1: // '\001'
+            case 2: // '\002'
+                computeRectShort(s,  m, d, (short) noDataValue);
+                break;
+
+            case 3: // '\003'
+                computeRectInt(s,  m, d, (int) noDataValue);
+                break;
             case 4: // '\004'
                 computeRectFloat(s, m, d, (float) noDataValue);
                 break;
@@ -58,7 +70,139 @@ final class InsertNoDataValueOpImage extends PointOpImage {
         }
         d.copyDataToRaster();
     }
-
+    
+    private void computeRectByte(RasterAccessor src, RasterAccessor mask, RasterAccessor dst, byte rValue) {
+        int sLineStride = src.getScanlineStride();
+        int sPixelStride = src.getPixelStride();
+        int[] sBandOffsets = src.getBandOffsets();
+        byte[][] sData = src.getByteDataArrays();
+        
+        int mLineStride = mask.getScanlineStride();
+        int mPixelStride = mask.getPixelStride();
+        int[] mBandOffsets = mask.getBandOffsets();
+        byte[][] mData = mask.getByteDataArrays();
+        
+        int dwidth = dst.getWidth();
+        int dheight = dst.getHeight();
+        int dLineStride = dst.getScanlineStride();
+        int dPixelStride = dst.getPixelStride();
+        int[] dBandOffsets = dst.getBandOffsets();
+        byte[][] dData = dst.getByteDataArrays();
+        
+        byte[] s = sData[0];
+        byte[] m  = mData[0];
+        byte[] d = dData[0];
+        int sLineOffset = sBandOffsets[0];
+        int mLineOffset = mBandOffsets[0];
+        int dLineOffset = dBandOffsets[0];
+        for (int h = 0; h < dheight; h++) {
+            int sPixelOffset = sLineOffset;
+            int mPixelOffset = mLineOffset;
+            int dPixelOffset = dLineOffset;
+            sLineOffset += sLineStride;
+            mLineOffset += mLineStride;
+            dLineOffset += dLineStride;
+            for (int w = 0; w < dwidth; w++) {
+                if (m[mPixelOffset]!=0) {
+                    d[dPixelOffset] = s[sPixelOffset];
+                } else {
+                    d[dPixelOffset] = rValue;
+                }
+                sPixelOffset += sPixelStride;
+                mPixelOffset += mPixelStride;
+                dPixelOffset += dPixelStride;
+            }
+        }
+    }
+    
+    private void computeRectShort(RasterAccessor src, RasterAccessor mask, RasterAccessor dst, short rValue) {
+        int sLineStride = src.getScanlineStride();
+        int sPixelStride = src.getPixelStride();
+        int[] sBandOffsets = src.getBandOffsets();
+        short[][] sData = src.getShortDataArrays();
+        
+        int mLineStride = mask.getScanlineStride();
+        int mPixelStride = mask.getPixelStride();
+        int[] mBandOffsets = mask.getBandOffsets();
+        byte[][] mData = mask.getByteDataArrays();
+        
+        int dwidth = dst.getWidth();
+        int dheight = dst.getHeight();
+        int dLineStride = dst.getScanlineStride();
+        int dPixelStride = dst.getPixelStride();
+        int[] dBandOffsets = dst.getBandOffsets();
+        short[][] dData = dst.getShortDataArrays();
+        
+        short[] s = sData[0];
+        byte[] m  = mData[0];
+        short[] d = dData[0];
+        int sLineOffset = sBandOffsets[0];
+        int mLineOffset = mBandOffsets[0];
+        int dLineOffset = dBandOffsets[0];
+        for (int h = 0; h < dheight; h++) {
+            int sPixelOffset = sLineOffset;
+            int mPixelOffset = mLineOffset;
+            int dPixelOffset = dLineOffset;
+            sLineOffset += sLineStride;
+            mLineOffset += mLineStride;
+            dLineOffset += dLineStride;
+            for (int w = 0; w < dwidth; w++) {
+                if (m[mPixelOffset]!=0) {
+                    d[dPixelOffset] = s[sPixelOffset];
+                } else {
+                    d[dPixelOffset] = rValue;
+                }
+                sPixelOffset += sPixelStride;
+                mPixelOffset += mPixelStride;
+                dPixelOffset += dPixelStride;
+            }
+        }
+    }
+    
+    private void computeRectInt(RasterAccessor src, RasterAccessor mask, RasterAccessor dst, int rValue) {
+        int sLineStride = src.getScanlineStride();
+        int sPixelStride = src.getPixelStride();
+        int[] sBandOffsets = src.getBandOffsets();
+        int[][] sData = src.getIntDataArrays();
+        
+        int mLineStride = mask.getScanlineStride();
+        int mPixelStride = mask.getPixelStride();
+        int[] mBandOffsets = mask.getBandOffsets();
+        byte[][] mData = mask.getByteDataArrays();
+        
+        int dwidth = dst.getWidth();
+        int dheight = dst.getHeight();
+        int dLineStride = dst.getScanlineStride();
+        int dPixelStride = dst.getPixelStride();
+        int[] dBandOffsets = dst.getBandOffsets();
+        int[][] dData = dst.getIntDataArrays();
+        
+        int[] s = sData[0];
+        byte[] m  = mData[0];
+        int[] d = dData[0];
+        int sLineOffset = sBandOffsets[0];
+        int mLineOffset = mBandOffsets[0];
+        int dLineOffset = dBandOffsets[0];
+        for (int h = 0; h < dheight; h++) {
+            int sPixelOffset = sLineOffset;
+            int mPixelOffset = mLineOffset;
+            int dPixelOffset = dLineOffset;
+            sLineOffset += sLineStride;
+            mLineOffset += mLineStride;
+            dLineOffset += dLineStride;
+            for (int w = 0; w < dwidth; w++) {
+                if (m[mPixelOffset]!=0) {
+                    d[dPixelOffset] = s[sPixelOffset];
+                } else {
+                    d[dPixelOffset] = rValue;
+                }
+                sPixelOffset += sPixelStride;
+                mPixelOffset += mPixelStride;
+                dPixelOffset += dPixelStride;
+            }
+        }
+    }
+    
     private void computeRectFloat(RasterAccessor src, RasterAccessor mask, RasterAccessor dst, float rValue) {
         int sLineStride = src.getScanlineStride();
         int sPixelStride = src.getPixelStride();
