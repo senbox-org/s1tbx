88a83580321db31b5ccede82bed13b03004f2674
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-11-20 12:36:15 +0000

    removed main
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@7034 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/mosaic/ui/MosaicDialog.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/mosaic/ui/MosaicDialog.java
@@ -6,7 +6,6 @@ import com.bc.jexp.impl.ParserImpl;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.dataop.barithm.BandArithmetic;
 import org.esa.beam.framework.gpf.GPF;
-import org.esa.beam.framework.gpf.ui.DefaultAppContext;
 import org.esa.beam.framework.gpf.ui.SingleTargetProductDialog;
 import org.esa.beam.framework.gpf.ui.TargetProductSelector;
 import org.esa.beam.framework.ui.AppContext;
@@ -24,13 +23,6 @@ import java.util.Map;
 class MosaicDialog extends SingleTargetProductDialog {
 
     private final MosaicForm form;
-
-    public static void main(String[] args) {
-        final DefaultAppContext context = new DefaultAppContext("Mosaic");
-        final MosaicDialog dialog = new MosaicDialog("MosaicTestDialog", null, context);
-        dialog.show();
-
-    }
 
     MosaicDialog(final String title, final String helpID, AppContext appContext) {
         super(appContext, title, helpID);
