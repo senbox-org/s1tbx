a65e01633a87d286c9d41d7141534ec18f473ff7
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-11-23 13:40:13 +0000

    parsing sourceProducts
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@7065 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf/src/main/java/org/esa/beam/framework/gpf/graph/SourceList.java
+++ beam-gpf/src/main/java/org/esa/beam/framework/gpf/graph/SourceList.java
@@ -1,12 +1,13 @@
 package org.esa.beam.framework.gpf.graph;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import org.esa.beam.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A list of {@link NodeSource}s.
@@ -45,10 +46,12 @@ public class SourceList {
     
     public static class Converter implements com.thoughtworks.xstream.converters.Converter {
 
+        @Override
         public boolean canConvert(Class aClass) {
             return SourceList.class.equals(aClass);
         }
 
+        @Override
         public void marshal(Object object, HierarchicalStreamWriter hierarchicalStreamWriter,
                             MarshallingContext marshallingContext) {
             SourceList sourceList = (SourceList) object;
@@ -60,17 +63,27 @@ public class SourceList {
             }
         }
 
+        @Override
         public Object unmarshal(HierarchicalStreamReader hierarchicalStreamReader,
                                 UnmarshallingContext unmarshallingContext) {
             SourceList sourceList = new SourceList();
             while (hierarchicalStreamReader.hasMoreChildren()) {
                 hierarchicalStreamReader.moveDown();
                 String name = hierarchicalStreamReader.getNodeName();
-                String refid = hierarchicalStreamReader.getAttribute("refid");
-                if (refid == null) {
-                    refid = hierarchicalStreamReader.getValue().trim();
+                if ("sourceProducts".equals(name)) {
+                    final String sourceNodeIdsString = hierarchicalStreamReader.getValue().trim();
+                    final String[] sourceNodeIds = StringUtils.csvToArray(sourceNodeIdsString);
+                    for (int i = 0; i < sourceNodeIds.length; i++) {
+                        String sourceNodeId = sourceNodeIds[i];
+                        sourceList.addSource(new NodeSource(String.format("%s%d", name, i), sourceNodeId));
+                    }
+                } else {
+                    String refid = hierarchicalStreamReader.getAttribute("refid");
+                    if (refid == null) {
+                        refid = hierarchicalStreamReader.getValue().trim();
+                    }
+                    sourceList.addSource(new NodeSource(name, refid));
                 }
-                sourceList.addSource(new NodeSource(name, refid));
                 hierarchicalStreamReader.moveUp();
             }
             return sourceList;
