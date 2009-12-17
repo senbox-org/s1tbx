dc19e8b005023e9c6e5108745d9a149f6a79fb36
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-12-17 08:32:30 +0000

    ShowShapeOverlayAction uses selected or first VectorDataNode
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@7551 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowShapeOverlayAction.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowShapeOverlayAction.java
@@ -1,7 +1,11 @@
 package org.esa.beam.visat.actions;
 
 import com.bc.ceres.glayer.Layer;
+import com.bc.ceres.glayer.LayerFilter;
+import com.bc.ceres.glayer.support.LayerUtils;
 import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductNode;
+import org.esa.beam.framework.datamodel.VectorDataNode;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.framework.ui.product.ProductSceneView;
 import org.esa.beam.framework.ui.product.VectorDataLayer;
@@ -22,15 +26,19 @@ public class ShowShapeOverlayAction extends AbstractShowOverlayAction {
     }
 
     private static VectorDataLayer getSelectedVectorDataLayer(ProductSceneView sceneView) {
-        final Layer layer = sceneView.getSelectedLayer();
-        if (layer instanceof VectorDataLayer) {
-            final VectorDataLayer vectorLayer = (VectorDataLayer) layer;
-            final String typeName = vectorLayer.getVectorDataNode().getFeatureType().getTypeName();
-            if (Product.GEOMETRY_FEATURE_TYPE_NAME.equals(typeName)) {
-                return vectorLayer;
+        final ProductNode node = VisatApp.getApp().getSelectedProductNode();
+        if (node instanceof VectorDataNode) {
+            final LayerFilter geometryNodeLayerFilter = new GeometryVectorDataNodeLayerFilter((VectorDataNode) node);
+            final VectorDataLayer layer = (VectorDataLayer) LayerUtils.getChildLayer(sceneView.getRootLayer(),
+                                                                                     geometryNodeLayerFilter,
+                                                                                     LayerUtils.SearchMode.DEEP);
+            if (layer != null) {
+                return layer;
             }
         }
-        return null;
+        return (VectorDataLayer) LayerUtils.getChildLayer(sceneView.getRootLayer(),
+                                                          new GeometryLayerFilter(),
+                                                          LayerUtils.SearchMode.DEEP);
     }
 
     @Override
@@ -47,6 +55,38 @@ public class ShowShapeOverlayAction extends AbstractShowOverlayAction {
             }
         } else {
             setSelected(false);
+        }
+    }
+
+    private static class GeometryLayerFilter implements LayerFilter {
+
+        @Override
+        public boolean accept(Layer layer) {
+            if (layer instanceof VectorDataLayer) {
+                final VectorDataLayer vectorLayer = (VectorDataLayer) layer;
+                final String typeName = vectorLayer.getVectorDataNode().getFeatureType().getTypeName();
+                return Product.GEOMETRY_FEATURE_TYPE_NAME.equals(typeName);
+            }
+            return false;
+        }
+    }
+
+    private static class GeometryVectorDataNodeLayerFilter implements LayerFilter {
+
+        private final VectorDataNode vectorDataNode;
+
+        private GeometryVectorDataNodeLayerFilter(VectorDataNode vectorDataNode) {
+            this.vectorDataNode = vectorDataNode;
+        }
+
+        @Override
+        public boolean accept(Layer layer) {
+            if (layer instanceof VectorDataLayer && ((VectorDataLayer) layer).getVectorDataNode() == vectorDataNode) {
+                VectorDataLayer vecDataLayer = (VectorDataLayer) layer;
+                final String typeName = vecDataLayer.getVectorDataNode().getFeatureType().getTypeName();
+                return Product.GEOMETRY_FEATURE_TYPE_NAME.equals(typeName);
+            }
+            return false;
         }
     }
 }
