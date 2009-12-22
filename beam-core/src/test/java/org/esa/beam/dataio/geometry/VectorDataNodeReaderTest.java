d6d49fe06dc101d5e566db9cdd9b10a09c9b8d8a
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-12-22 16:41:37 +0000

    about to develop a VectorDataNodeReader
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@7666 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-core/src/test/java/org/esa/beam/dataio/geometry/VectorDataNodeReaderTest.java
+++ beam-core/src/test/java/org/esa/beam/dataio/geometry/VectorDataNodeReaderTest.java
@@ -4,10 +4,14 @@ import com.bc.ceres.binding.ConversionException;
 import com.bc.ceres.binding.Converter;
 import com.bc.ceres.binding.ConverterRegistry;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Coordinate;
 import junit.framework.TestCase;
 import org.esa.beam.util.io.CsvReader;
 import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.opengis.feature.simple.SimpleFeature;
@@ -23,21 +27,17 @@ public class VectorDataNodeReaderTest extends TestCase {
     public void testReader() throws IOException {
 
         StringReader reader = new StringReader(
-                "Test_FT\n"
-                        + "name:String\tgeom:Geometry\tpixel:Integer\tdescription\n"
-                        + "mark1\tPOINT(12.3 45.6)\t0\tThis is mark1.\n"
-                        + "mark2\tPOINT(78.9  0.1)\t1\tThis is mark2.\n"
-                        + "mark3\tPOINT(2.3 3.4)\t2\tThis is mark3.\n"
+                "Test_FT\tname:String\tgeom:Geometry\tpixel:Integer\tdescription\n"
+                        + "ID65\tmark1\tPOINT(12.3 45.6)\t0\tThis is mark1.\n"
+                        + "ID66\tmark2\tPOINT(78.9  0.1)\t1\tThis is mark2.\n"
+                        + "ID67\tmark3\tPOINT(2.3 3.4)\t2\tThis is mark3.\n"
         );
 
         JtsGeometryConverter.registerConverter();
 
         CsvReader csvReader = new CsvReader(reader, new char[]{'\t'});
 
-        String typeName = readTypeName(csvReader);
-        assertEquals("Test_FT", typeName);
-
-        SimpleFeatureType simpleFeatureType = readFeatureType(csvReader, typeName);
+        SimpleFeatureType simpleFeatureType = readFeatureType(csvReader);
         assertNotNull(simpleFeatureType);
         assertEquals("Test_FT", simpleFeatureType.getTypeName());
         assertEquals(4, simpleFeatureType.getAttributeCount());
@@ -65,7 +65,24 @@ public class VectorDataNodeReaderTest extends TestCase {
 
         FeatureCollection<SimpleFeatureType, SimpleFeature> fc = readerFeatures(csvReader, simpleFeatureType);
 
-        assertEquals(0, fc.size());
+        GeometryFactory gf = new GeometryFactory();
+
+        assertEquals(3, fc.size());
+        FeatureIterator<SimpleFeature> featureIterator = fc.features();
+
+        SimpleFeature f1 = featureIterator.next();
+        assertEquals("ID65", f1.getID());
+        assertEquals("mark1", f1.getAttribute(0));
+        //assertEquals(gf.createPoint(new Coordinate(12.3, 45.6)), f1.getAttribute(1));
+        assertEquals(0, f1.getAttribute(2));
+        assertEquals("This is mark1.", f1.getAttribute(3));
+
+        SimpleFeature f2 = featureIterator.next();
+        assertEquals("ID66", f2.getID());
+        assertEquals("mark2", f2.getAttribute(0));
+        //assertEquals(gf.createPoint(new Coordinate(78.9,  0.1)), f2.getAttribute(1));
+        assertEquals(1, f2.getAttribute(2));
+        assertEquals("This is mark2.", f2.getAttribute(3));
     }
 
     private FeatureCollection<SimpleFeatureType, SimpleFeature> readerFeatures(CsvReader csvReader, SimpleFeatureType simpleFeatureType) throws IOException {
@@ -81,66 +98,69 @@ public class VectorDataNodeReaderTest extends TestCase {
 
         DefaultFeatureCollection fc = new DefaultFeatureCollection("?", simpleFeatureType);
         SimpleFeatureBuilder sfb = new SimpleFeatureBuilder(simpleFeatureType);
-        long id = System.nanoTime();
         while (true) {
             String[] tokens = csvReader.readRecord();
             if (tokens == null) {
                 break;
             }
-            if (tokens.length != simpleFeatureType.getAttributeCount()) {
+            if (tokens.length != 1 + simpleFeatureType.getAttributeCount()) {
                 throw new IOException("Shit.");  // todo - msg
             }
+            sfb.reset();
+            String fid = null;
             for (int i = 0; i < tokens.length; i++) {
                 String token = tokens[i];
+                if (i == 0) {
+                    fid = token;
+                } else {
                 try {
-                    Object value = converters[i].parse(token);
-                    sfb.set(simpleFeatureType.getDescriptor(i).getLocalName(), value);
+                    Object value = converters[i-1].parse(token);
+                    sfb.set(simpleFeatureType.getDescriptor(i-1).getLocalName(), value);
                 } catch (ConversionException e) {
                     throw new IOException("Shit.", e);  // todo - msg
                 }
+                }
             }
-            fc.add(sfb.buildFeature("FID_" + Long.toHexString(id++)));
+            SimpleFeature simpleFeature = sfb.buildFeature(fid);
+            fc.add(simpleFeature);
         }
         return fc;
     }
 
-    private String readTypeName(CsvReader csvReader) throws IOException {
+    private SimpleFeatureType readFeatureType(CsvReader csvReader) throws IOException {
         String[] tokens = csvReader.readRecord();
-        if (tokens == null || tokens.length != 1) {
-            throw new IOException("Missing feature type name in first line.");
+        if (tokens == null || tokens.length <= 1) {
+            throw new IOException("Missing feature type definition in first line.");
         }
-        return tokens[0];
+        return createFeatureType(tokens);
     }
 
-    private SimpleFeatureType readFeatureType(CsvReader csvReader, String typeName) throws IOException {
-        String[] tokens = csvReader.readRecord();
-        if (tokens == null || tokens.length == 0) {
-            throw new IOException("Missing feature type definition in second line.");
-        }
-        return createFeatureType(typeName, tokens);
-    }
-
-    private SimpleFeatureType createFeatureType(String typeName, String[] tokens) throws IOException {
+    private SimpleFeatureType createFeatureType(String[] tokens) throws IOException {
         SimpleFeatureTypeBuilder sftb = new SimpleFeatureTypeBuilder();
-        sftb.setName(typeName);
         JavaTypeConverter jtc = new JavaTypeConverter();
         for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
-            final int colonPos = token.indexOf(':');
-            String attributeName = token;
-            Class<?> attributeType = String.class;
-            if (colonPos >= 0) {
-                attributeName = colonPos == 0 ? ("attribute_" + i) : token.substring(0, colonPos);
-                String attributeTypeName = token.substring(colonPos + 1).trim();
-                try {
-                    attributeType = jtc.parse(attributeTypeName);
-                } catch (ConversionException e) {
-                    throw new IOException(
-                            String.format("Type for attribute '%s' unknown: %s", attributeName, attributeTypeName), e);
+            if (i == 0) {
+                sftb.setName(tokens[0]);
+            } else {
+                String token = tokens[i];
+                final int colonPos = token.indexOf(':');
+                String attributeName = token;
+                Class<?> attributeType = String.class;
+                if (colonPos >= 0) {
+                    attributeName = colonPos == 0 ? ("attribute_" + i) : token.substring(0, colonPos);
+                    String attributeTypeName = token.substring(colonPos + 1).trim();
+                    try {
+                        attributeType = jtc.parse(attributeTypeName);
+                    } catch (ConversionException e) {
+                        throw new IOException(
+                                String.format("Type for attribute '%s' unknown: %s", attributeName, attributeTypeName), e);
+                    }
                 }
+                sftb.add(attributeName, attributeType);
             }
-            sftb.add(attributeName, attributeType);
         }
         return sftb.buildFeatureType();
     }
+
+
 }
