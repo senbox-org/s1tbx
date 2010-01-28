55df2c481223f4585a7a5f032bb6d2c059909a06
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-01-28 17:23:49 +0000

    ensured that all tranformation parameters are displayed in user interface
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@8121 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/projdef/OperationMethodCrsProvider.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/projdef/OperationMethodCrsProvider.java
@@ -1,11 +1,17 @@
 package org.esa.beam.gpf.common.reproject.ui.projdef;
 
 import org.esa.beam.framework.datamodel.GeoPos;
+import org.geotools.parameter.Parameter;
+import org.geotools.parameter.ParameterGroup;
 import org.geotools.referencing.AbstractIdentifiedObject;
 import org.geotools.referencing.ReferencingFactoryFinder;
 import org.geotools.referencing.cs.DefaultCartesianCS;
 import org.geotools.referencing.cs.DefaultEllipsoidalCS;
 import org.geotools.referencing.operation.DefiningConversion;
+import org.opengis.parameter.GeneralParameterDescriptor;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptorGroup;
 import org.opengis.parameter.ParameterValueGroup;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CRSFactory;
@@ -16,6 +22,7 @@ import org.opengis.referencing.operation.Conversion;
 import org.opengis.referencing.operation.OperationMethod;
 
 import java.util.HashMap;
+import java.util.List;
 
 class OperationMethodCrsProvider extends AbstractCrsProvider {
 
@@ -28,7 +35,23 @@ class OperationMethodCrsProvider extends AbstractCrsProvider {
 
     @Override
     public ParameterValueGroup getParameter() {
-        return delegate.getParameters().createValue();
+        // this call does not initialises optional values and so they are missed in our user interface
+//        return delegate.getParameters().createValue();
+        final ParameterDescriptorGroup descriptorGroup = delegate.getParameters();
+        final List<GeneralParameterDescriptor> parameterDescriptors = descriptorGroup.descriptors();
+        GeneralParameterValue[] parameterValues = new GeneralParameterValue[parameterDescriptors.size()];
+        for (int i = 0, parameterDescriptorsSize = parameterDescriptors.size(); i < parameterDescriptorsSize; i++) {
+            GeneralParameterDescriptor parameterDescriptor = parameterDescriptors.get(i);
+            parameterValues[i] = parameterDescriptor.createValue();
+            if (parameterValues[i] instanceof Parameter) {
+                Parameter p = (Parameter) parameterValues[i];
+                if (p.getValue() == null) {
+                    final ParameterDescriptor descriptor = p.getDescriptor();
+                    p.setValue(descriptor.getMinimumValue());
+                }
+            }
+        }
+        return new ParameterGroup(descriptorGroup, parameterValues);
     }
 
     @Override
