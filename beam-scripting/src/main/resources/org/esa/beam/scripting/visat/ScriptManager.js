0b80a65d57c86ad77336f67c7cb8773c01642631
norman <norman@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-02-01 16:37:18 +0000

    scripting update
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@8183 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-scripting/src/main/resources/org/esa/beam/scripting/visat/ScriptManager.js
+++ beam-scripting/src/main/resources/org/esa/beam/scripting/visat/ScriptManager.js
@@ -3,7 +3,7 @@ importPackage(org.esa.beam.framework.dataio);
 importPackage(org.esa.beam.framework.gpf);
 importPackage(org.esa.beam.framework.gpf.operators.common);
 importPackage(org.esa.beam.framework.gpf.operators.meris);
+importPackage(org.esa.beam.util);
 importPackage(org.esa.beam.visat);
 
 visat = VisatApp.getApp();
-retval = "OK."
