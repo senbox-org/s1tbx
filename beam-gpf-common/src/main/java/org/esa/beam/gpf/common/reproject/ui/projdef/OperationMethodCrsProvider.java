77c51fb719c3ea3a54bcac5d87fecf731b740663
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-02-02 09:03:54 +0000

    better creation of CRS
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/trunk@8191 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/projdef/OperationMethodCrsProvider.java
+++ beam-gpf-common/src/main/java/org/esa/beam/gpf/common/reproject/ui/projdef/OperationMethodCrsProvider.java
@@ -1,17 +1,12 @@
 package org.esa.beam.gpf.common.reproject.ui.projdef;
 
 import org.esa.beam.framework.datamodel.GeoPos;
-import org.geotools.parameter.Parameter;
-import org.geotools.parameter.ParameterGroup;
 import org.geotools.referencing.AbstractIdentifiedObject;
 import org.geotools.referencing.ReferencingFactoryFinder;
 import org.geotools.referencing.cs.DefaultCartesianCS;
 import org.geotools.referencing.cs.DefaultEllipsoidalCS;
+import org.geotools.referencing.operation.DefaultOperationMethod;
 import org.geotools.referencing.operation.DefiningConversion;
-import org.opengis.parameter.GeneralParameterDescriptor;
-import org.opengis.parameter.GeneralParameterValue;
-import org.opengis.parameter.ParameterDescriptor;
-import org.opengis.parameter.ParameterDescriptorGroup;
 import org.opengis.parameter.ParameterValueGroup;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CRSFactory;
@@ -19,10 +14,11 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.crs.GeographicCRS;
 import org.opengis.referencing.datum.GeodeticDatum;
 import org.opengis.referencing.operation.Conversion;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.MathTransformFactory;
 import org.opengis.referencing.operation.OperationMethod;
 
 import java.util.HashMap;
-import java.util.List;
 
 class OperationMethodCrsProvider extends AbstractCrsProvider {
 
@@ -35,32 +31,22 @@ class OperationMethodCrsProvider extends AbstractCrsProvider {
 
     @Override
     public ParameterValueGroup getParameter() {
-        // this call does not initialises optional values and so they are missed in our user interface
-//        return delegate.getParameters().createValue();
-        final ParameterDescriptorGroup descriptorGroup = delegate.getParameters();
-        final List<GeneralParameterDescriptor> parameterDescriptors = descriptorGroup.descriptors();
-        GeneralParameterValue[] parameterValues = new GeneralParameterValue[parameterDescriptors.size()];
-        for (int i = 0, parameterDescriptorsSize = parameterDescriptors.size(); i < parameterDescriptorsSize; i++) {
-            GeneralParameterDescriptor parameterDescriptor = parameterDescriptors.get(i);
-            parameterValues[i] = parameterDescriptor.createValue();
-            if (parameterValues[i] instanceof Parameter) {
-                Parameter p = (Parameter) parameterValues[i];
-                if (p.getValue() == null) {
-                    final ParameterDescriptor descriptor = p.getDescriptor();
-                    p.setValue(descriptor.getMinimumValue());
-                }
-            }
-        }
-        return new ParameterGroup(descriptorGroup, parameterValues);
+        return delegate.getParameters().createValue();
     }
 
     @Override
     public CoordinateReferenceSystem getCRS(final GeoPos referencePos, ParameterValueGroup parameters,
                                             GeodeticDatum datum) throws FactoryException {
         final CRSFactory crsFactory = ReferencingFactoryFinder.getCRSFactory(null);
+        // in some cases, depending on the parameters set, the effective transformation can be different
+        // from the transformation given by the OperationMethod.
+        // So we create a new one
+        final MathTransformFactory mtFactory = ReferencingFactoryFinder.getMathTransformFactory(null);
+        final MathTransform transform = mtFactory.createParameterizedTransform(parameters);
+        final DefaultOperationMethod operationMethod = new DefaultOperationMethod(transform);
 
-        final Conversion conversion = new DefiningConversion(AbstractIdentifiedObject.getProperties(delegate),
-                                                             delegate, parameters);
+        final Conversion conversion = new DefiningConversion(AbstractIdentifiedObject.getProperties(operationMethod),
+                                                             operationMethod, transform);
 
         final HashMap<String, Object> baseCrsProperties = new HashMap<String, Object>();
         baseCrsProperties.put("name", datum.getName().getCode());
@@ -69,7 +55,7 @@ class OperationMethodCrsProvider extends AbstractCrsProvider {
                                                                DefaultEllipsoidalCS.GEODETIC_2D);
 
         final HashMap<String, Object> projProperties = new HashMap<String, Object>();
-        projProperties.put("name", getName() + " / " + datum.getName().getCode());
+        projProperties.put("name", conversion.getName().getCode() + " / " + datum.getName().getCode());
         return crsFactory.createProjectedCRS(projProperties, baseCrs, conversion, DefaultCartesianCS.PROJECTED);
     }
 }
