b0ec4af77a3c61a5984b9abf243e8f3e64013aa4
marpet <marco.peters@brockmann-consult.de>
2010-02-18 10:02:33 +0100

    better doc

--- beam-gpf/src/main/java/org/esa/beam/framework/gpf/operators/common/BandMathOp.java
+++ beam-gpf/src/main/java/org/esa/beam/framework/gpf/operators/common/BandMathOp.java
@@ -48,20 +48,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This band maths operator can be used to create a product with multiple bands based on mathematical expression.
- * <br/>
  * <p>
+ * This band maths operator can be used to create a product with multiple bands based on mathematical expression.<br/>
  * All products specified as source must have the same width and height, otherwise the operator will fail.
- *
- * <p>
  * The geo-coding information and metadata for the target product is taken from the first source product.
+ * </p>
  * <p>
  * To reference a band of one of the source products within an expression use the following syntax:<br/>
+ * <br/>
  * <code>sourceProducts<b>#</b>.bandName</code><br/>
+ * <br/>
  * Where <b>#</b> means the index of the source product. The index is zero based.<br/>
  * The bands of the first source product (<code>sourceProducts<b>0</b></code>) can be referenced without this
  * product identifier.
- * 
+ * </p>
  */
 @OperatorMetadata(alias = "BandMath",
                   version = "1.0",
