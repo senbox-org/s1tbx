2a3b0f48acc8ca5ffcba9ca0c6cfc71cc3ad201a
Ralf Quast <ralf.quast@brockmann-consult.de>
2010-06-15 19:38:43 +0200

    update

--- ceres-jai/src/main/java/com/bc/ceres/jai/ModuleActivator.java
+++ ceres-jai/src/main/java/com/bc/ceres/jai/ModuleActivator.java
@@ -3,19 +3,32 @@ package com.bc.ceres.jai;
 import com.bc.ceres.core.CoreException;
 import com.bc.ceres.core.runtime.Activator;
 import com.bc.ceres.core.runtime.ModuleContext;
+import com.bc.ceres.jai.operator.ReinterpretDescriptor;
+import com.bc.ceres.jai.opimage.ReinterpretRIF;
 
 import javax.media.jai.JAI;
-import java.io.IOException;
+import javax.media.jai.OperationRegistry;
+import javax.media.jai.registry.RenderedRegistryMode;
+import java.util.logging.Level;
 
 public class ModuleActivator implements Activator {
 
+    private static final String META_INF_REGISTRY_FILE_JAI = "/META-INF/registryFile.jai";
+
     @Override
     public void start(ModuleContext moduleContext) throws CoreException {
-        try {
-            JAI.getDefaultInstance().getOperationRegistry().updateFromStream(getClass().getResourceAsStream(
-                    "/META-INF/registryFile.jai"));
-        } catch (IOException e) {
-            throw new CoreException(e);
+        final JAI instance = JAI.getDefaultInstance();
+        final OperationRegistry operationRegistry = instance.getOperationRegistry();
+        operationRegistry.registerDescriptor(new ReinterpretDescriptor());
+        operationRegistry.registerDescriptor(new ReinterpretDescriptor());
+        operationRegistry.registerFactory(RenderedRegistryMode.MODE_NAME, "reinterpret", "com.bc.ceres.jai",
+                                          new ReinterpretRIF());
+
+        final String[] descriptorNames = operationRegistry.getDescriptorNames("rendered");
+        if (moduleContext.getLogger().getLevel() == Level.FINE) {
+            for (String descriptorName : descriptorNames) {
+                moduleContext.getLogger().fine(descriptorName);
+            }
         }
     }
 
