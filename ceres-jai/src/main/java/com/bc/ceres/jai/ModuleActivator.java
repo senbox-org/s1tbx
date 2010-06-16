673cda26610ea81fbe0cfcdac3470b5a40c61f84
Ralf Quast <ralf.quast@brockmann-consult.de>
2010-06-16 10:09:33 +0200

    registered remaining JAI descriptors and factories

--- ceres-jai/src/main/java/com/bc/ceres/jai/ModuleActivator.java
+++ ceres-jai/src/main/java/com/bc/ceres/jai/ModuleActivator.java
@@ -3,33 +3,38 @@ package com.bc.ceres.jai;
 import com.bc.ceres.core.CoreException;
 import com.bc.ceres.core.runtime.Activator;
 import com.bc.ceres.core.runtime.ModuleContext;
+import com.bc.ceres.jai.operator.DFTConvolveDescriptor;
+import com.bc.ceres.jai.operator.ExpressionDescriptor;
+import com.bc.ceres.jai.operator.GeneralFilterDescriptor;
 import com.bc.ceres.jai.operator.ReinterpretDescriptor;
+import com.bc.ceres.jai.operator.XmlDescriptor;
+import com.bc.ceres.jai.opimage.DFTConvolveRIF;
+import com.bc.ceres.jai.opimage.ExpressionCRIF;
+import com.bc.ceres.jai.opimage.GeneralFilterRIF;
 import com.bc.ceres.jai.opimage.ReinterpretRIF;
+import com.bc.ceres.jai.opimage.XmlRIF;
 
 import javax.media.jai.JAI;
 import javax.media.jai.OperationRegistry;
+import javax.media.jai.RegistryElementDescriptor;
 import javax.media.jai.registry.RenderedRegistryMode;
-import java.util.logging.Level;
+import java.awt.image.renderable.RenderedImageFactory;
 
 public class ModuleActivator implements Activator {
 
-    private static final String META_INF_REGISTRY_FILE_JAI = "/META-INF/registryFile.jai";
-
     @Override
     public void start(ModuleContext moduleContext) throws CoreException {
-        final JAI instance = JAI.getDefaultInstance();
-        final OperationRegistry operationRegistry = instance.getOperationRegistry();
-        operationRegistry.registerDescriptor(new ReinterpretDescriptor());
-        operationRegistry.registerDescriptor(new ReinterpretDescriptor());
-        operationRegistry.registerFactory(RenderedRegistryMode.MODE_NAME, "reinterpret", "com.bc.ceres.jai",
-                                          new ReinterpretRIF());
+        register("xml", new XmlDescriptor(), new XmlRIF());
+        register("reinterpret", new ReinterpretDescriptor(), new ReinterpretRIF());
+        register("generalfilter", new GeneralFilterDescriptor(), new GeneralFilterRIF());
+        register("expression", new ExpressionDescriptor(), new ExpressionCRIF());
+        register("dftconvolve", new DFTConvolveDescriptor(), new DFTConvolveRIF());
+    }
 
-        final String[] descriptorNames = operationRegistry.getDescriptorNames("rendered");
-        if (moduleContext.getLogger().getLevel() == Level.FINE) {
-            for (String descriptorName : descriptorNames) {
-                moduleContext.getLogger().fine(descriptorName);
-            }
-        }
+    private void register(String descriptorName, RegistryElementDescriptor descriptor, RenderedImageFactory rif) {
+        final OperationRegistry registry = JAI.getDefaultInstance().getOperationRegistry();
+        registry.registerDescriptor(descriptor);
+        registry.registerFactory(RenderedRegistryMode.MODE_NAME, descriptorName, "com.bc.ceres.jai", rif);
     }
 
     @Override
