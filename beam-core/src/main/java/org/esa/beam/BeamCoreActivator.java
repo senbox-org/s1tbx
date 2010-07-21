33b62fb39f21109e87f7cc44086aa2162dc7be14
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-07-21 10:52:56 +0200

    import cleanup

--- beam-core/src/main/java/org/esa/beam/BeamCoreActivator.java
+++ beam-core/src/main/java/org/esa/beam/BeamCoreActivator.java
@@ -39,7 +39,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ServiceConfigurationError;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 
 /**
  * <p><i><b>IMPORTANT NOTE:</b>
