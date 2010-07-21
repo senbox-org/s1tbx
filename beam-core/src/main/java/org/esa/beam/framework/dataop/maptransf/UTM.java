952a98e5281c47a68bb50a3831897d890dfd8633
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-07-21 12:31:19 +0200

    update copyright

--- beam-core/src/main/java/org/esa/beam/framework/dataop/maptransf/UTM.java
+++ beam-core/src/main/java/org/esa/beam/framework/dataop/maptransf/UTM.java
@@ -1,8 +1,17 @@
 /*
- * $id$
+ * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
- * Copyright (c) 2003 Brockmann Consult GmbH. All right reserved.
- * http://www.brockmann-consult.de
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
  */
 package org.esa.beam.framework.dataop.maptransf;
 
--- beam-core/src/main/java/org/esa/beam/util/math/LookupTable.java
+++ beam-core/src/main/java/org/esa/beam/util/math/LookupTable.java
@@ -1,23 +1,18 @@
 /*
-    $Id: $
-
-    Copyright (c) 2006 Brockmann Consult. All rights reserved. Use is
-    subject to license terms.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the Lesser GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with the BEAM software; if not, download BEAM from
-    http://www.brockmann-consult.de/beam/ and install it.
-*/
+ * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
 package org.esa.beam.util.math;
 
 import java.text.MessageFormat;
