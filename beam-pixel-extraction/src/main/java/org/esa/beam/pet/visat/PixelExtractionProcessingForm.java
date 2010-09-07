c411bf6286947e476489154f3c242ab8ab5c3496
Marco Peters <marco.peters@brockmann-consult.de>
2010-09-07 15:23:15 +0200

    added dummies for each parameter

--- beam-pixel-extraction/src/main/java/org/esa/beam/pet/visat/PixelExtractionProcessingForm.java
+++ beam-pixel-extraction/src/main/java/org/esa/beam/pet/visat/PixelExtractionProcessingForm.java
@@ -19,15 +19,26 @@ package org.esa.beam.pet.visat;
 import com.bc.ceres.binding.Property;
 import com.bc.ceres.binding.PropertyContainer;
 import com.bc.ceres.swing.TableLayout;
+import com.bc.ceres.swing.binding.Binding;
 import com.bc.ceres.swing.binding.BindingContext;
+import com.bc.ceres.swing.binding.ComponentAdapter;
+import com.bc.ceres.swing.binding.internal.TextComponentAdapter;
 import org.esa.beam.framework.ui.AppContext;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerNumberModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PixelExtractionProcessingForm {
 
@@ -45,49 +56,100 @@ public class PixelExtractionProcessingForm {
     };
 
     private JPanel panel;
-    private AppContext appContext;
 
     public PixelExtractionProcessingForm(AppContext appContext, PropertyContainer container) {
-        this.appContext = appContext;
 
-        panel = new JPanel(createLayout());
-        final BindingContext bindingContext = new BindingContext(container);
-
-        panel.add(new JLabel("Product Type"));
-        panel.add(createProductTypeEditor(bindingContext));
-        panel.add(new JLabel(""));
-
-        panel.add(new JLabel("Square size"));
-        panel.add(createSquareSizeEditor(container, bindingContext));
-        panel.add(new JLabel(""));
-    }
-
-    private static TableLayout createLayout() {
         final TableLayout tableLayout = new TableLayout(3);
         tableLayout.setTableAnchor(TableLayout.Anchor.NORTHWEST);
         tableLayout.setTableFill(TableLayout.Fill.HORIZONTAL);
         tableLayout.setTablePadding(4, 4);
         tableLayout.setTableWeightX(0.0);
         tableLayout.setTableWeightY(0.0);
-        tableLayout.setColumnWeightX(1, 1.0);
         tableLayout.setCellWeightY(1, 1, 1.0);
         tableLayout.setCellFill(1, 1, TableLayout.Fill.BOTH);
-        return tableLayout;
+        tableLayout.setCellWeightY(2, 1, 1.0);
+        tableLayout.setCellFill(2, 1, TableLayout.Fill.BOTH);
+        tableLayout.setColumnWeightX(1, 1.0);
+
+        panel = new JPanel(tableLayout);
+        final BindingContext bindingContext = new BindingContext(container);
+
+        panel.add(new JLabel("Product type:"));
+        panel.add(createProductTypeEditor(bindingContext));
+        panel.add(new JLabel());
+
+        panel.add(new JLabel("Rasters:"));
+        final JScrollPane rasterScrollPane = new JScrollPane(createRasterEditor());
+        setScrollbarPolicy(rasterScrollPane);
+        panel.add(rasterScrollPane);
+        panel.add(new JLabel());
+
+        panel.add(new JLabel("Coordinates:"));
+        final JScrollPane coordScrollPane = new JScrollPane(createCoordinatesEditor());
+        setScrollbarPolicy(coordScrollPane);
+        panel.add(coordScrollPane);
+        panel.add(new JLabel());
+
+        panel.add(new JLabel("Pin file:"));
+        final JComponent[] pinFileComponents = createPinFileComponents(bindingContext);
+        panel.add(pinFileComponents[0]);
+        panel.add(pinFileComponents[1]);
+
+        panel.add(new JLabel("Square size:"));
+        panel.add(createSquareSizeEditor(container, bindingContext));
+        panel.add(new JLabel());
+
+        panel.add(tableLayout.createVerticalSpacer());
     }
 
-    private JComponent createProductTypeEditor(BindingContext binding) {
+    private void setScrollbarPolicy(JScrollPane scrollPane) {
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    }
+
+    private JComponent[] createPinFileComponents(BindingContext bindingContext) {
+        final JTextField textField = new JTextField();
+        final ComponentAdapter adapter = new TextComponentAdapter(textField);
+        final Binding binding = bindingContext.bind("coordinatesFile", adapter);
+        final JButton ellipsesButton = new JButton("...");
+        ellipsesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final JFileChooser fileChooser = new JFileChooser();
+                int i = fileChooser.showDialog(panel, "Select");
+                if (i == JFileChooser.APPROVE_OPTION && fileChooser.getSelectedFile() != null) {
+                    binding.setPropertyValue(fileChooser.getSelectedFile());
+                }
+            }
+        });
+        return new JComponent[]{textField, ellipsesButton};
+    }
+
+    private JComponent createRasterEditor() {
+        JList rasterList = new JList(new String[]{"radiance_1", "radiance_2"});
+        rasterList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        return rasterList;
+    }
+
+    private JComponent createCoordinatesEditor() {
+        JList rasterList = new JList(new String[]{"40.5,12.67", "35.8,25.7"});
+        rasterList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        return rasterList;
+    }
+
+    private JComponent createProductTypeEditor(BindingContext bindingContext) {
         final JComboBox productTypesBox = new JComboBox(PRODUCT_TYPES);
         productTypesBox.setEditable(true);
-        binding.bind("productType", productTypesBox);
+        bindingContext.bind("productType", productTypesBox);
         productTypesBox.setSelectedIndex(0);
         return productTypesBox;
     }
 
-    private JComponent createSquareSizeEditor(PropertyContainer container, BindingContext binding) {
+    private JComponent createSquareSizeEditor(PropertyContainer container, BindingContext bindingContext) {
         final Property squareSizeProperty = container.getProperty("squareSize");
         final Number defaultValue = (Number) squareSizeProperty.getDescriptor().getDefaultValue();
         final JSpinner spinner = new JSpinner(new SpinnerNumberModel(defaultValue, 1, null, 2));
-        binding.bind("squareSize", spinner);
+        bindingContext.bind("squareSize", spinner);
         return spinner;
     }
 
