46b91cbd7ea09af3f850ecec1e882cb97858a773
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-01-31 17:44:11 +0100

    Added test for XML parameterisation of BandMathsOp

--- beam-gpf/src/test/java/org/esa/beam/gpf/operators/standard/BandMathsOpTest.java
+++ beam-gpf/src/test/java/org/esa/beam/gpf/operators/standard/BandMathsOpTest.java
@@ -15,6 +15,10 @@
  */
 package org.esa.beam.gpf.operators.standard;
 
+import com.bc.ceres.binding.PropertyContainer;
+import com.bc.ceres.binding.PropertySet;
+import com.bc.ceres.binding.dom.DefaultDomConverter;
+import com.bc.ceres.binding.dom.DomElement;
 import com.bc.ceres.core.ProgressMonitor;
 import junit.framework.TestCase;
 import org.esa.beam.framework.datamodel.Band;
@@ -24,30 +28,39 @@ import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.ProductData;
 import org.esa.beam.framework.gpf.GPF;
 import org.esa.beam.framework.gpf.OperatorSpi;
+import org.esa.beam.framework.gpf.annotations.ParameterDescriptorFactory;
+import org.esa.beam.framework.gpf.graph.Graph;
+import org.esa.beam.framework.gpf.graph.GraphIO;
+import org.esa.beam.framework.gpf.graph.Node;
 import org.geotools.referencing.CRS;
 
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BandMathsOpTest extends TestCase {
 
-    private OperatorSpi spi;
+    private OperatorSpi bandMathsSpi;
+    private ReadOp.Spi readSpi;
 
     @Override
     protected void setUp() throws Exception {
-        spi = new BandMathsOp.Spi();
-        GPF.getDefaultInstance().getOperatorSpiRegistry().addOperatorSpi(spi);
+        readSpi = new ReadOp.Spi();
+        bandMathsSpi = new BandMathsOp.Spi();
+        GPF.getDefaultInstance().getOperatorSpiRegistry().addOperatorSpi(readSpi);
+        GPF.getDefaultInstance().getOperatorSpiRegistry().addOperatorSpi(bandMathsSpi);
     }
 
     @Override
     protected void tearDown() throws Exception {
-        GPF.getDefaultInstance().getOperatorSpiRegistry().removeOperatorSpi(spi);
+        GPF.getDefaultInstance().getOperatorSpiRegistry().removeOperatorSpi(bandMathsSpi);
+        GPF.getDefaultInstance().getOperatorSpiRegistry().removeOperatorSpi(readSpi);
     }
 
-    public void testSimpelstCase() throws Exception {
+    public void testSimplestCase() throws Exception {
         Map<String, Object> parameters = new HashMap<String, Object>();
         BandMathsOp.BandDescriptor[] bandDescriptors = new BandMathsOp.BandDescriptor[1];
         bandDescriptors[0] = createBandDescription("aBandName", "1.0", ProductData.TYPESTRING_FLOAT32);
@@ -83,7 +96,7 @@ public class BandMathsOpTest extends TestCase {
         assertNotNull(targetProduct.getGeoCoding());
     }
 
-    public void testSimpelstCaseWithFactoryMethod() throws Exception {
+    public void testSimplestCaseWithFactoryMethod() throws Exception {
         Product sourceProduct = createTestProduct(4, 4);
 
         BandMathsOp bandMathsOp = BandMathsOp.createBooleanExpressionBand("band1 > 0", sourceProduct);
@@ -179,7 +192,7 @@ public class BandMathsOpTest extends TestCase {
         parameters.put("targetBands", bandDescriptors);
 
         Product targetProduct = GPF.createProduct("BandMaths", parameters,
-                                                  new Product[]{sourceProduct1, sourceProduct2});
+                                                  sourceProduct1, sourceProduct2);
         Band band = targetProduct.getBand("aBandName");
 
         float[] actualValues = new float[16];
@@ -189,7 +202,43 @@ public class BandMathsOpTest extends TestCase {
         assertTrue(Arrays.equals(expectedValues, actualValues));
     }
 
-    private BandMathsOp.BandDescriptor createBandDescription(String bandName, String expression, String type) {
+    public void testGraph() throws Exception {
+        Graph graph = GraphIO.read(new InputStreamReader(getClass().getResourceAsStream("BandMathsOpTest.xml")));
+        Node bandMathsNode = graph.getNode("bandMathsNode");
+        DomElement configurationDomElement = bandMathsNode.getConfiguration();
+        ParameterDescriptorFactory parameterDescriptorFactory = new ParameterDescriptorFactory();
+
+        HashMap<String, Object> parameterMap = new HashMap<String, Object>();
+        PropertySet parameterSet = PropertyContainer.createMapBacked(parameterMap, BandMathsOp.class, parameterDescriptorFactory);
+        assertNotNull(parameterSet.getProperty("targetBands"));
+        assertNull(parameterSet.getProperty("targetBands").getValue());
+        assertNotNull(parameterSet.getProperty("variables"));
+        assertNull(parameterSet.getProperty("variables").getValue());
+
+        DefaultDomConverter domConverter = new DefaultDomConverter(BandMathsOp.class, parameterDescriptorFactory);
+        domConverter.convertDomToValue(configurationDomElement, parameterSet);
+        assertNotNull(parameterSet.getProperty("targetBands"));
+        assertNotNull(parameterSet.getProperty("targetBands").getValue());
+        assertNotNull(parameterSet.getProperty("variables"));
+        assertNotNull(parameterSet.getProperty("variables").getValue());
+
+        Object targetBandsObj = parameterSet.getProperty("targetBands").getValue();
+        assertTrue(targetBandsObj instanceof BandMathsOp.BandDescriptor[]);
+        BandMathsOp.BandDescriptor[] targetBands = (BandMathsOp.BandDescriptor[]) targetBandsObj;
+        assertEquals(2, targetBands.length);
+        assertEquals("reflec_13", targetBands[0].name);
+        assertEquals("reflec_14", targetBands[1].name);
+
+        Object variablesObj = parameterSet.getProperty("variables").getValue();
+        assertTrue(variablesObj instanceof BandMathsOp.Variable[]);
+        BandMathsOp.Variable[] variables = (BandMathsOp.Variable[]) variablesObj;
+        assertEquals(3, variables.length);
+        assertEquals("SOLAR_FLUX_13", variables[0].name);
+        assertEquals("SOLAR_FLUX_14", variables[1].name);
+        assertEquals("PI", variables[2].name);
+    }
+
+    private static BandMathsOp.BandDescriptor createBandDescription(String bandName, String expression, String type) {
         BandMathsOp.BandDescriptor bandDescriptor = new BandMathsOp.BandDescriptor();
         bandDescriptor.name = bandName;
         bandDescriptor.description = "aDescription";
@@ -199,7 +248,7 @@ public class BandMathsOpTest extends TestCase {
     }
 
 
-    private Product createTestProduct(int w, int h) {
+    private static Product createTestProduct(int w, int h) {
         Product testProduct = new Product("p", "t", w, h);
         Band band1 = testProduct.addBand("band1", ProductData.TYPE_INT32);
         int[] intValues = new int[w * h];
