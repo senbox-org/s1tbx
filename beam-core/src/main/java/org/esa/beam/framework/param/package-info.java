a57d47b56df305dfa37a07f32c74d06df2b1c82a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-02-02 18:01:36 +0100

    tried to deprecate entire package via package-info file

---
+++ beam-core/src/main/java/org/esa/beam/framework/param/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides utility classes for general parameter handling. There are several sub-packages for:
+ * <ul>
+ *  <li>Parameter editing {@link org.esa.beam.framework.param.editors editors}</li>
+ *  <li>GUI support {@link org.esa.beam.framework.param.editors user interface}</li>
+ *  <li>Parameter validation {@link org.esa.beam.framework.param.validators validation}</li>
+ * </ul>
+ */
+package org.esa.beam.framework.param;
