e8ec1e6d041803b16e8d422e370e8356714417f4
Norman <norman.fomferra@brockmann-consult.de>
2011-05-02 20:46:48 +0200

    [BEAM-1279] Add 64-bit HDF libaries for all platforms

--- src/main/bin/win/gpt.bat
+++ src/main/bin/win/gpt.bat
@@ -7,8 +7,8 @@ set BEAM4_HOME=${installer:sys.installationDir}
     -Dceres.context=beam ^
     "-Dbeam.mainClass=org.esa.beam.framework.gpf.main.Main" ^
     "-Dbeam.home=%BEAM4_HOME%" ^
-    "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%BEAM4_HOME%\modules\lib-hdf-${hdf.version}\lib\win\jhdf.dll" ^
-    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%BEAM4_HOME%\modules\lib-hdf-${hdf.version}\lib\win\jhdf5.dll" ^
+    "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%BEAM4_HOME%\modules\lib-hdf-${hdf.version}\lib\jhdf.dll" ^
+    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%BEAM4_HOME%\modules\lib-hdf-${hdf.version}\lib\jhdf5.dll" ^
     -jar "%BEAM4_HOME%\bin\ceres-launcher.jar" %*
 
 exit /B %ERRORLEVEL%
