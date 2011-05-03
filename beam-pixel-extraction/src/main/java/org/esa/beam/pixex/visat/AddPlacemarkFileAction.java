8d0aa005c433aa58b921cff59cb718c31da5b54b
marpet <marco.peters@brockmann-consult.de>
2011-05-03 15:15:05 +0200

    changed beam.petOp.lastOpenPlacemarkDir to beam.pixex.lastOpenPlacemarkDir

--- beam-pixel-extraction/src/main/java/org/esa/beam/pixex/visat/AddPlacemarkFileAction.java
+++ beam-pixel-extraction/src/main/java/org/esa/beam/pixex/visat/AddPlacemarkFileAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -35,7 +35,7 @@ import java.util.List;
 
 class AddPlacemarkFileAction extends AbstractAction {
 
-    private static final String LAST_OPEN_PLACEMARK_DIR = "beam.petOp.lastOpenPlacemarkDir";
+    private static final String LAST_OPEN_PLACEMARK_DIR = "beam.pixex.lastOpenPlacemarkDir";
 
     private final CoordinateTableModel tableModel;
     private final AppContext appContext;
