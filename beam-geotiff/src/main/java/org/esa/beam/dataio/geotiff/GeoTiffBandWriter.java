fbae57835a3ea593e4ba5b1bad01adac98456ad2
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-05-09 18:33:49 +0200

    [BEAM-1290] GeoTIFF writer can't write very large files

--- beam-geotiff/src/main/java/org/esa/beam/dataio/geotiff/GeoTiffBandWriter.java
+++ beam-geotiff/src/main/java/org/esa/beam/dataio/geotiff/GeoTiffBandWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -103,67 +103,68 @@ class GeoTiffBandWriter {
         if (!tempProduct.containsBand(sourceBand.getName())) {
             throw new IllegalArgumentException("'" + sourceBand.getName() + "' is not a band of the product");
         }
-        int bandDataType = ifd.getBandDataType();
+        final int bandDataType = ifd.getBandDataType();
         final int stripIndex = getStripIndex(sourceBand);
         final TiffValue[] offsetValues = ifd.getEntry(TiffTag.STRIP_OFFSETS).getValues();
         final long stripOffset = ((TiffLong) offsetValues[stripIndex]).getValue();
         final TiffValue[] bitsPerSampleValues = ifd.getEntry(TiffTag.BITS_PER_SAMPLE).getValues();
-        final int elemSize = ((TiffShort) bitsPerSampleValues[stripIndex]).getValue() / 8;
-        final int sourceWidthBytes = sourceBand.getSceneRasterWidth() * elemSize;
-        final int regionOffsetXInBytes = regionX * elemSize;
-        final int pixelOffset = sourceWidthBytes * regionY + regionOffsetXInBytes;
+        final long elemSize = ((TiffShort) bitsPerSampleValues[stripIndex]).getValue() / 8;
+        final long sourceWidthBytes = sourceBand.getSceneRasterWidth() * elemSize;
+        final long regionOffsetXInBytes = regionX * elemSize;
+        final long pixelOffset = sourceWidthBytes * regionY + regionOffsetXInBytes;
         final long startOffset = stripOffset + pixelOffset;
 
         pm.beginTask("Writing band '" + sourceBand.getName() + "'...", regionHeight);
         try {
             for (int y = 0; y < regionHeight; y++) {
                 ios.seek(startOffset + y * sourceWidthBytes);
+                final int stride = y * regionWidth;
                 if (bandDataType == ProductData.TYPE_UINT8) {
                     final byte[] data = new byte[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (byte) regionData.getElemUIntAt(y * regionWidth + x);
+                        data[x] = (byte) regionData.getElemUIntAt(stride + x);
                     }
                     ios.write(data);
                 } else if (bandDataType == ProductData.TYPE_INT8) {
                     final byte[] data = new byte[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (byte) regionData.getElemIntAt(y * regionWidth + x);
+                        data[x] = (byte) regionData.getElemIntAt(stride + x);
                     }
                     ios.write(data);
                 } else if (bandDataType == ProductData.TYPE_UINT16) {
                     final short[] data = new short[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (short) regionData.getElemUIntAt(y * regionWidth + x);
+                        data[x] = (short) regionData.getElemUIntAt(stride + x);
                     }
                     ios.writeShorts(data, 0, regionWidth);
                 } else if (bandDataType == ProductData.TYPE_INT16) {
                     final short[] data = new short[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (short) regionData.getElemIntAt(y * regionWidth + x);
+                        data[x] = (short) regionData.getElemIntAt(stride + x);
                     }
                     ios.writeShorts(data, 0, regionWidth);
                 } else if (bandDataType == ProductData.TYPE_UINT32) {
                     final int[] data = new int[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = (int) regionData.getElemUIntAt(y * regionWidth + x);
+                        data[x] = (int) regionData.getElemUIntAt(stride + x);
                     }
                     ios.writeInts(data, 0, regionWidth);
                 } else if (bandDataType == ProductData.TYPE_INT32) {
                     final int[] data = new int[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = regionData.getElemIntAt(y * regionWidth + x);
+                        data[x] = regionData.getElemIntAt(stride + x);
                     }
                     ios.writeInts(data, 0, regionWidth);
                 } else if (bandDataType == ProductData.TYPE_FLOAT32) {
                     final float[] data = new float[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = regionData.getElemFloatAt(y * regionWidth + x);
+                        data[x] = regionData.getElemFloatAt(stride + x);
                     }
                     ios.writeFloats(data, 0, regionWidth);
                 } else if (bandDataType == ProductData.TYPE_FLOAT64) {
                     final double[] data = new double[regionWidth];
                     for (int x = 0; x < regionWidth; x++) {
-                        data[x] = regionData.getElemDoubleAt(y * regionWidth + x);
+                        data[x] = regionData.getElemDoubleAt(stride + x);
                     }
                     ios.writeDoubles(data, 0, regionWidth);
                 }
