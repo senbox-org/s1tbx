7a0effdbbd92f7c20fe479dc943d650041da4382
Norman <norman.fomferra@brockmann-consult.de>
2011-05-31 07:00:47 +0200

    Editor factory now creates a (dummy) editor for feature type 'TrackPoint'

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/layermanager/editors/VectorDataLayerEditorFactory.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/layermanager/editors/VectorDataLayerEditorFactory.java
@@ -4,19 +4,19 @@ import com.bc.ceres.core.ExtensionFactory;
 import org.esa.beam.framework.ui.layer.AbstractLayerEditor;
 import org.esa.beam.framework.ui.layer.LayerEditor;
 import org.esa.beam.framework.ui.product.VectorDataLayer;
-import sun.plugin.javascript.navig4.Layer;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
 /**
- * todo - add api doc
+ * Experimental code: A factory that creates a specific {@link LayerEditor} for a given {@link VectorDataLayer}.
  *
  * @author Norman Fomferra
+ * @since BEAM 4.10
  */
 public class VectorDataLayerEditorFactory implements ExtensionFactory {
     @Override
-    public Object getExtension(Object object, Class<?> extensionType) {
+    public LayerEditor getExtension(Object object, Class<?> extensionType) {
         if (object instanceof VectorDataLayer) {
             VectorDataLayer vectorDataLayer = (VectorDataLayer) object;
             String featureTypeName = vectorDataLayer.getVectorDataNode().getFeatureType().getTypeName();
@@ -25,6 +25,13 @@ public class VectorDataLayerEditorFactory implements ExtensionFactory {
                     @Override
                     protected JComponent createControl() {
                         return new JLabel("I am an editor for features of type Placemark");
+                    }
+                };
+            } else if (featureTypeName.equals("TrackPoint")) {
+                return new AbstractLayerEditor() {
+                    @Override
+                    protected JComponent createControl() {
+                        return new JLabel("I am an editor for features of type TrackPoint");
                     }
                 };
             } else {
@@ -36,6 +43,6 @@ public class VectorDataLayerEditorFactory implements ExtensionFactory {
 
     @Override
     public Class<?>[] getExtensionTypes() {
-        return new Class<?>[ ] {LayerEditor.class};
+        return new Class<?>[]{LayerEditor.class};
     }
 }
