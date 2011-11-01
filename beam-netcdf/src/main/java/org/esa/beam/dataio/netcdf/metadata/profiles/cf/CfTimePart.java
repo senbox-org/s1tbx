0474462ad818c9469281168dfaa58c58f2c0ac22
marpet <marco.peters@brockmann-consult.de>
2011-11-01 08:56:13 +0100

    using addGlobalAttribute(attr) instead of addAttribute(null, attr)

--- beam-netcdf/src/main/java/org/esa/beam/dataio/netcdf/metadata/profiles/cf/CfTimePart.java
+++ beam-netcdf/src/main/java/org/esa/beam/dataio/netcdf/metadata/profiles/cf/CfTimePart.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -15,9 +15,9 @@
  */
 package org.esa.beam.dataio.netcdf.metadata.profiles.cf;
 
+import org.esa.beam.dataio.netcdf.ProfileReadContext;
 import org.esa.beam.dataio.netcdf.ProfileWriteContext;
 import org.esa.beam.dataio.netcdf.metadata.ProfilePartIO;
-import org.esa.beam.dataio.netcdf.ProfileReadContext;
 import org.esa.beam.dataio.netcdf.util.Constants;
 import org.esa.beam.dataio.netcdf.util.TimeUtils;
 import org.esa.beam.framework.datamodel.Product;
@@ -33,7 +33,8 @@ public class CfTimePart extends ProfilePartIO {
     @Override
     public void decode(ProfileReadContext ctx, Product p) throws IOException {
         NetcdfFile ncFile = ctx.getNetcdfFile();
-        p.setStartTime(TimeUtils.getSceneRasterTime(ncFile, Constants.START_DATE_ATT_NAME, Constants.START_TIME_ATT_NAME));
+        p.setStartTime(
+                TimeUtils.getSceneRasterTime(ncFile, Constants.START_DATE_ATT_NAME, Constants.START_TIME_ATT_NAME));
         p.setEndTime(TimeUtils.getSceneRasterTime(ncFile, Constants.STOP_DATE_ATT_NAME, Constants.STOP_TIME_ATT_NAME));
     }
 
@@ -42,11 +43,11 @@ public class CfTimePart extends ProfilePartIO {
         ProductData.UTC utc = p.getStartTime();
         NetcdfFileWriteable writeable = ctx.getNetcdfFileWriteable();
         if (utc != null) {
-            writeable.addAttribute(null, new Attribute(Constants.START_DATE_ATT_NAME, utc.format()));
+            writeable.addGlobalAttribute(new Attribute(Constants.START_DATE_ATT_NAME, utc.format()));
         }
         utc = p.getEndTime();
         if (utc != null) {
-            writeable.addAttribute(null, new Attribute(Constants.STOP_DATE_ATT_NAME, utc.format()));
+            writeable.addGlobalAttribute(new Attribute(Constants.STOP_DATE_ATT_NAME, utc.format()));
         }
     }
 }
