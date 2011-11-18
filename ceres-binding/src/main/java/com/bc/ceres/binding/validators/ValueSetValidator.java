6e2c4682c31ec61bba436714d90d1ef23696f804
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-11-18 14:46:11 +0100

    Made ValueSetValidator null-tolerant

--- ceres-binding/src/main/java/com/bc/ceres/binding/validators/ValueSetValidator.java
+++ ceres-binding/src/main/java/com/bc/ceres/binding/validators/ValueSetValidator.java
@@ -32,9 +32,11 @@ public class ValueSetValidator implements Validator {
 
     @Override
     public void validateValue(Property property, Object value) throws ValidationException {
-        if (!propertyDescriptor.getValueSet().contains(value)) {
-            throw new ValidationException(MessageFormat.format("Value for ''{0}'' is invalid: ''{1}''",
-                                                               property.getDescriptor().getDisplayName(), value));
+        if (value != null || propertyDescriptor.isNotNull()) {
+            if (!propertyDescriptor.getValueSet().contains(value)) {
+                throw new ValidationException(MessageFormat.format("Value for ''{0}'' is invalid: ''{1}''",
+                                                                   property.getDescriptor().getDisplayName(), value));
+            }
         }
     }
 }
