a7a31efdd6463cbc0c2994b0cb1eaa26ee43a002
TomBlock <tom.block@brockmann-consult.de>
2011-12-09 17:16:35 +0100

    intermediate tgz impl

--- beam-landsat-reader/src/main/java/org/esa/beam/dataio/landsat/tgz/VirtualDirTgz.java
+++ beam-landsat-reader/src/main/java/org/esa/beam/dataio/landsat/tgz/VirtualDirTgz.java
@@ -1,35 +1,38 @@
 package org.esa.beam.dataio.landsat.tgz;
 
 import com.bc.ceres.core.VirtualDir;
-import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.esa.beam.util.io.FileUtils;
+import org.xeustechnologies.jtar.TarEntry;
+import org.xeustechnologies.jtar.TarInputStream;
 
 import java.io.*;
 
 public class VirtualDirTgz extends VirtualDir {
 
-    private ArchiveInputStream archiveInputStream;
+    private final File archiveFile;
+    private File extractDir;
 
     public VirtualDirTgz(File tgz) throws IOException {
-        // @todo 1 tb/tb ungzip when compressed
-//        final InputStream fileInputStream = new FileInputStream(tgz);
-//        final FilterInputStream tgzInputStream = new BufferedInputStream(fileInputStream);
-//        try {
-//            archiveInputStream = new ArchiveStreamFactory().createArchiveInputStream(tgzInputStream);
-//        } catch (ArchiveException e) {
-//            throw new IOException(e.getMessage());
-//        }
+        if (tgz == null) {
+            throw new IllegalArgumentException("Input file shall not be null");
+        }
+        archiveFile = tgz;
+        extractDir = null;
     }
 
     @Override
     public String getBasePath() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return archiveFile.getPath();
     }
 
     @Override
     public InputStream getInputStream(String path) throws IOException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        ensureUnpacked();
+        final File file = new File(extractDir, path);
+        if (!file.isFile()) {
+            throw new IOException();
+        }
+        return new FileInputStream(file);
     }
 
     @Override
@@ -44,6 +47,88 @@ public class VirtualDirTgz extends VirtualDir {
 
     @Override
     public void close() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (extractDir != null) {
+            FileUtils.deleteTree(extractDir);
+            extractDir = null;
+        }
+    }
+
+    // @todo 3 tb/** this code is almost completey dublicated from com.bc.ceres.core.VirtualDir
+    // it maybe makes sense to move it to a file utility class
+    static File createTargetDirInTemp(String name) throws IOException {
+        File tempDir = null;
+        String tempDirName = System.getProperty("java.io.tmpdir");
+        if (tempDirName != null) {
+            tempDir = new File(tempDirName);
+        }
+        if (tempDir == null) {
+            tempDir = new File(new File(System.getProperty("user.home", ".")), ".beam/temp");
+            if (!tempDir.exists()) {
+                if (!tempDir.mkdirs()) {
+                    throw new IOException("unable to create directory: " + tempDir.getAbsolutePath());
+                }
+            }
+        }
+
+        File targetDir = new File(tempDir, name);
+        if (!targetDir.exists()) {
+            if (!targetDir.mkdirs()) {
+                throw new IOException("unable to create directory: " + targetDir.getAbsolutePath());
+            }
+        }
+
+        return targetDir;
+    }
+
+    static String getFilenameFromPath(String path) {
+        int lastSepIndex = path.lastIndexOf("/");
+        if (lastSepIndex == -1) {
+            lastSepIndex = path.lastIndexOf("\\");
+            if (lastSepIndex == -1) {
+                return path;
+            }
+        }
+
+        return path.substring(lastSepIndex + 1, path.length());
+    }
+
+    private void ensureUnpacked() throws IOException {
+        if (extractDir == null) {
+            extractDir = createTargetDirInTemp(archiveFile.getName());
+
+            final TarInputStream tis = new TarInputStream(new BufferedInputStream(new FileInputStream(archiveFile)));
+            TarEntry entry;
+
+            while ((entry = tis.getNextEntry()) != null) {
+                final String entryName = entry.getName();
+                if (entry.isDirectory()) {
+                    final File directory = new File(extractDir, entryName);
+                    if (!directory.mkdirs()) {
+                        throw new IOException("unable to create directory: " + directory.getAbsolutePath());
+                    }
+                    continue;
+                }
+
+                final String fileNameFromPath = getFilenameFromPath(entryName);
+                final int pathIndex = entryName.indexOf(fileNameFromPath);
+                final String tarPath = entryName.substring(0, pathIndex - 1);
+                final File targetDir = new File(extractDir, tarPath);
+                final File targetFile = new File(targetDir, fileNameFromPath);
+                if (!targetFile.createNewFile()) {
+                    throw new IOException("Unable to create file: " + targetFile.getAbsolutePath());
+                }
+
+                final OutputStream outStream = new BufferedOutputStream(new FileOutputStream(targetFile));
+                final byte data[] = new byte[1024 * 1024];
+                int count;
+                while ((count = tis.read(data)) != -1) {
+                    outStream.write(data, 0, count);
+                }
+
+                // @todo 3 tb/tb try finally - make sure everything is closed
+                outStream.flush();
+                outStream.close();
+            }
+        }
     }
 }
--- beam-landsat-reader/src/test/java/org/esa/beam/dataio/landsat/tgz/VirtualDirTgzTest.java
+++ beam-landsat-reader/src/test/java/org/esa/beam/dataio/landsat/tgz/VirtualDirTgzTest.java
@@ -1,22 +1,153 @@
 package org.esa.beam.dataio.landsat.tgz;
 
+import org.esa.beam.util.io.FileUtils;
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class VirtualDirTgzTest {
 
+    private VirtualDirTgz virtualDir;
+
     @Test
     public void testOpenTgz() throws IOException {
-        File testTgz = new File("./beam-landsat-reader/src/test/resources/org/esa/beam/dataio/landsat/tgz/test-archive.tgz");
+        File testTgz = getTestFile("test-archive.tgz");
+
+        virtualDir = new VirtualDirTgz(testTgz);
+        assertEquals(testTgz.getPath(), virtualDir.getBasePath());
+    }
+
+    @Test
+    public void testOpenTar() throws IOException {
+        File testTar = getTestFile("test-archive.tar");
+
+        virtualDir = new VirtualDirTgz(testTar);
+        assertEquals(testTar.getPath(), virtualDir.getBasePath());
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    @Test
+    public void testOpenNull() throws IOException {
+        try {
+            final VirtualDirTgz vdTgz = new VirtualDirTgz(null);
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException expected) {
+        }
+    }
+
+    @Test
+    public void testTar_getInputStream() throws IOException {
+        final File testTgz = getTestFile("test-archive.tar");
+
+        virtualDir = new VirtualDirTgz(testTgz);
+        final InputStream inputStream = virtualDir.getInputStream("test-archive/file2.txt");
+        try {
+            assertNotNull(inputStream);
+
+            final byte[] buffer = new byte[512];
+            int bytesRead = inputStream.read(buffer);
+            assertEquals(9, bytesRead);
+            assertEquals("content2", new String(buffer).trim());
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+    @Test
+    public void testTar_getInputStream_invalidPath() throws IOException {
+        final File testTgz = getTestFile("test-archive.tar");
+
+        virtualDir = new VirtualDirTgz(testTgz);
+        try {
+            virtualDir.getInputStream("test-archive/invalid_dir/no.file");
+            fail("IOException expected");
+        } catch (IOException expected) {
+        }
+    }
+
+    @Test
+    public void testCreateTargetDirInTemp_fromSystemPropertyTmpDir() throws IOException {
+        final String tempDirName = System.getProperty("java.io.tmpdir");
+        assertNotNull(tempDirName);
+
+        File dirInTemp = null;
+        try {
+            dirInTemp = VirtualDirTgz.createTargetDirInTemp("wurst");
+            assertNotNull(dirInTemp);
+            assertTrue(dirInTemp.isDirectory());
+            assertEquals(new File(tempDirName, "wurst").getAbsolutePath(), dirInTemp.getAbsolutePath());
+        } finally {
+            if (dirInTemp != null) {
+                FileUtils.deleteTree(dirInTemp);
+            }
+        }
+    }
+
+    @Test
+    public void testCreateTargetDirInTemp_fromSystemPropertyUserHome() throws IOException {
+        final String oldTempDir = System.getProperty("java.io.tmpdir");
+        System.clearProperty("java.io.tmpdir");
+        final String userHome = System.getProperty("user.home");
+        assertNotNull(userHome);
+
+        File dirInTemp = null;
+        try {
+            dirInTemp = VirtualDirTgz.createTargetDirInTemp("Schneck");
+            assertNotNull(dirInTemp);
+            assertTrue(dirInTemp.isDirectory());
+            assertEquals(new File(userHome, ".beam/temp/Schneck").getAbsolutePath(), dirInTemp.getAbsolutePath());
+        } finally {
+            System.setProperty("java.io.tmpdir", oldTempDir);
+            if (dirInTemp != null) {
+                FileUtils.deleteTree(dirInTemp);
+            }
+        }
+    }
+
+    @Test
+    public void testGetFilenameFromPath_Windows() {
+        final String fullPath = "C:\\bla\\blubber\\theFile.txt";
+        assertEquals("theFile.txt", VirtualDirTgz.getFilenameFromPath(fullPath));
+
+        final String relativePath = "bla\\schnuffi\\schnatter.txt";
+        assertEquals("schnatter.txt", VirtualDirTgz.getFilenameFromPath(relativePath));
+    }
+
+    @Test
+    public void testGetFilenameFromPath_Linux() {
+        final String fullPath = "/bla/blubber/theFile.txt";
+        assertEquals("theFile.txt", VirtualDirTgz.getFilenameFromPath(fullPath));
+
+        final String relativePath = "bla/schnuffi/schnatter.txt";
+        assertEquals("schnatter.txt", VirtualDirTgz.getFilenameFromPath(relativePath));
+    }
+
+    @Test
+    public void testGetFilenameFromPath_notAPath() {
+        final String file = "theFile.txt";
+        assertEquals(file, VirtualDirTgz.getFilenameFromPath(file));
+    }
+
+    @After
+    public void tearDown() {
+        if (virtualDir != null) {
+            virtualDir.close();
+        }
+    }
+
+    private File getTestFile(String file) {
+        File testTgz = new File("./beam-landsat-reader/src/test/resources/org/esa/beam/dataio/landsat/tgz/" + file);
         if (!testTgz.isFile()) {
-            testTgz = new File("./src/test/resources/org/esa/beam/dataio/landsat/tgz/test-archive.tgz");
+            testTgz = new File("./src/test/resources/org/esa/beam/dataio/landsat/tgz/" + file);
         }
         assertTrue(testTgz.isFile());
-
-        final VirtualDirTgz vdTgz = new VirtualDirTgz(testTgz);
+        return testTgz;
     }
 }
