972450857ae34f0883313cfd5550ebfe374dfa08
thomasstorm <thomas.storm@brockmann-consult.de>
2012-03-05 18:29:57 +0100

    extracted general classes for csv-output

---
+++ beam-csv-dataio/src/test/java/org/esa/beam/csv/dataio/writer/PixelCsvWriterTest.java
@@ -0,0 +1,55 @@
+package org.esa.beam.csv.dataio.writer;
+
+import org.esa.beam.framework.datamodel.PixelPos;
+import org.esa.beam.framework.datamodel.Product;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.*;
+
+/**
+ * @author Thomas Storm
+ */
+public class PixelCsvWriterTest {
+    
+    @Test
+    @Ignore
+    public void testWriteCsv() throws Exception {
+        final StringBuilder actual = new StringBuilder();
+        final PixelCsvWriter csvWriter = new PixelCsvWriter(new WriteStrategy() {
+            @Override
+            public void writeCsv(String fullOutput) throws IOException {
+                actual.append(fullOutput);
+            }
+        }, new CsvWriterBuilder.BeamOutputFormat());
+
+        final Product testProduct = new Product("testProduct", "someType", 10, 10);
+        final PixelPos[] pixelPositions = new PixelPos[2];
+        pixelPositions[0] = new PixelPos();
+        pixelPositions[1] = new PixelPos();
+        csvWriter.writeCsv(testProduct, pixelPositions);
+        
+        final StringBuilder expected = new StringBuilder();
+        expected.append("featureId\tattr_1:string\tattr_2:float");
+        expected.append("\n");
+        expected.append("0\tval_1\t10.0");
+        expected.append("\n");
+        expected.append("1\tval_2\t20.0");
+        expected.append("\n");
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void testIsValidInput() throws Exception {
+        final PixelCsvWriter csvWriter = new PixelCsvWriter(null, null);
+        final Product testProduct = new Product("testProduct", "someType", 10, 10);
+        final PixelPos[] pixelPositions = new PixelPos[10];
+
+        assertFalse(csvWriter.isValidInput());
+        assertFalse(csvWriter.isValidInput("", "", ""));
+        assertFalse(csvWriter.isValidInput(testProduct, new PixelPos(), new PixelPos()));
+        assertTrue(csvWriter.isValidInput(testProduct, pixelPositions));
+    }
+}
