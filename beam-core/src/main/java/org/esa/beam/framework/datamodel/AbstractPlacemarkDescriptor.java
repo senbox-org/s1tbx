8a5fe66dc4a011fc76a2dee7c9f261343e1fa679
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2012-04-17 15:23:14 +0200

    Added new method "setUserData"

--- beam-core/src/main/java/org/esa/beam/framework/datamodel/AbstractPlacemarkDescriptor.java
+++ beam-core/src/main/java/org/esa/beam/framework/datamodel/AbstractPlacemarkDescriptor.java
@@ -1,6 +1,7 @@
 package org.esa.beam.framework.datamodel;
 
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
 /**
  * The base class for {@link PlacemarkDescriptor} implementations.
@@ -10,6 +11,28 @@ import org.opengis.feature.simple.SimpleFeature;
  */
 public abstract class AbstractPlacemarkDescriptor implements PlacemarkDescriptor {
 
+    /**
+     * {@inheritDoc}
+     * <p/>
+     * The default implementation does nothing.
+     *
+     * @param featureType The feature type whose user data may or may not be altered.
+     * @see org.opengis.feature.simple.SimpleFeatureType#getUserData()
+     */
+    @Override
+    public void setUserData(SimpleFeatureType featureType) {
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p/>
+     * The default implementation returns
+     * <pre>
+     *     new Placemark(this, feature)
+     * </pre>.
+     *
+     * @param feature The feature to be wrapped.
+     */
     @Override
     public Placemark createPlacemark(SimpleFeature feature) {
         return new Placemark(this, feature);
