c3b936a552884fceeff4a345e3bbf5c79ed8d4d1
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2012-05-09 14:39:35 +0200

    Fixed VISAT debug scripts

--- src/main/bin/linux/visat-d.sh
+++ src/main/bin/linux/visat-d.sh
@@ -6,7 +6,7 @@ if [ -z "$BEAM4_HOME" ]; then
     echo
     echo Error: BEAM4_HOME not found in your environment.
     echo Please set the BEAM4_HOME variable in your environment to match the
-    echo location of the BEAM 4.x installation
+    echo location of the BEAM 4.x installation.
     echo
     exit 2
 fi
