fbf2012f1226ca29e72c58546935361f41f28f86
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2012-06-21 10:11:13 +0200

    close clicked tab and not the selected one

--- beam-ui/src/main/java/com/bc/swing/desktop/TabbedDesktopPane.java
+++ beam-ui/src/main/java/com/bc/swing/desktop/TabbedDesktopPane.java
@@ -228,9 +228,14 @@ public class TabbedDesktopPane extends JPanel {
         tabbedPane.setCloseAction(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                final JInternalFrame selectedFrame = getSelectedFrame();
-                if(selectedFrame != null) {
-                    closeFrame(selectedFrame);
+                Object component = e.getSource();
+                if (component instanceof InternalFrameProxy) {
+                    InternalFrameProxy placeHolder = (InternalFrameProxy) component;
+                    JInternalFrame selectedFrame = placeHolder.getInternalFrame();
+
+                    if(selectedFrame != null) {
+                        closeFrame(selectedFrame);
+                    }
                 }
             }
         });
