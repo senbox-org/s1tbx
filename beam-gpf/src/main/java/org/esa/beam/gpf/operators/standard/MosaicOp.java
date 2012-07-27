9c9856fedc942881f5407968dab98d97e029f011
marpet <marco.peters@brockmann-consult.de>
2012-07-27 11:04:34 +0200

    typo

--- beam-gpf/src/main/java/org/esa/beam/gpf/operators/standard/MosaicOp.java
+++ beam-gpf/src/main/java/org/esa/beam/gpf/operators/standard/MosaicOp.java
@@ -103,7 +103,7 @@ public class MosaicOp extends Operator {
                description = "The CRS of the target product, represented as WKT or authority code.")
     String crs;
 
-    @Parameter(description = "Wether the source product should be orthorectified.", defaultValue = "false")
+    @Parameter(description = "Whether the source product should be orthorectified.", defaultValue = "false")
     boolean orthorectify;
     @Parameter(description = "The name of the elevation model for the orthorectification.")
     String elevationModelName;
