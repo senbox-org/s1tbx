fbf80f5be832f237310e1c0a72d970b8067f14ad
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2012-10-18 12:01:20 +0200

    added exception handling

--- beam-statistics-op/src/main/java/org/esa/beam/statistics/ProductLoader.java
+++ beam-statistics-op/src/main/java/org/esa/beam/statistics/ProductLoader.java
@@ -9,6 +9,10 @@ import java.io.IOException;
 public class ProductLoader {
 
     public Product loadProduct(File path) throws IOException {
-        return ProductIO.readProduct(path);
+        try {
+            return ProductIO.readProduct(path);
+        } catch (IllegalArgumentException e) {
+            throw new IOException(e.getMessage());
+        }
     }
 }
