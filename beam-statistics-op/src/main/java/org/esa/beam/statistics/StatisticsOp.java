bfa4b17676d9dd6f924ec4cffc325c019017e1f5
thomasstorm <thomas_storm@brockmann-consult.de>
2012-10-25 15:12:07 +0200

    minor validation restructuring

--- beam-statistics-op/src/main/java/org/esa/beam/statistics/StatisticsOp.java
+++ beam-statistics-op/src/main/java/org/esa/beam/statistics/StatisticsOp.java
@@ -18,25 +18,6 @@ package org.esa.beam.statistics;
 
 import com.bc.ceres.binding.ConversionException;
 import com.bc.ceres.binding.Converter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.MalformedURLException;
-import java.text.MessageFormat;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.logging.Level;
-import javax.media.jai.Histogram;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.HistogramStxOp;
 import org.esa.beam.framework.datamodel.Product;
@@ -58,6 +39,26 @@ import org.esa.beam.statistics.output.StatisticsOutputter;
 import org.esa.beam.statistics.output.Util;
 import org.esa.beam.util.io.FileUtils;
 import org.esa.beam.util.io.WildcardMatcher;
+
+import javax.media.jai.Histogram;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.logging.Level;
 
 /**
  * An operator that is used to compute statistics for any number of source products, restricted to regions given by an
@@ -377,16 +378,16 @@ public class StatisticsOp extends Operator implements Output {
         }
         if (outputShapefile != null) {
             if (outputShapefile.isDirectory()) {
-                throw new OperatorException("Parameter 'outputShapefile' must not point to a directory.");
+                throw new OperatorException("Parameter 'outputShapefile' must point to a file.");
             }
             if (shapefile == null) {
-                throw new OperatorException("Parameter 'shapefile' must be given if outputShapefile shoud be created.");
-            }
-            if (shapefile.isDirectory()) {
-                throw new OperatorException("Parameter 'shapefile' must not point to a directory.");
+                throw new OperatorException("Parameter 'shapefile' must be provided if an output shapefile shall be created.");
             }
         }
 
+        if (shapefile != null && shapefile.isDirectory()) {
+            throw new OperatorException("Parameter 'shapefile' must point to a file.");
+        }
 
         for (int percentile : percentiles) {
             if (percentile < 0 || percentile > 100) {
