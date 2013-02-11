24d1bbc77cbb1d0097a4ef2c173bcc194eaf5f4b
marpet <marco.peters@brockmann-consult.de>
2013-02-11 14:10:18 +0100

    fixed tests which are writing to temporary files now closing writers correctly (cherry picked from commit 15451e2f2eabd5781ad04b8911428b0396979ff9)

--- beam-pixel-extraction/src/test/java/org/esa/beam/pixex/output/PixExMeasurementFactoryTest.java
+++ beam-pixel-extraction/src/test/java/org/esa/beam/pixex/output/PixExMeasurementFactoryTest.java
@@ -10,8 +10,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class PixExMeasurementFactoryTest {
 
