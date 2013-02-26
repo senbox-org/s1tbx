50c981603c2019cb76df9bb0b1c4092bc02804b5
thomasstorm <thomas_storm@brockmann-consult.de>
2013-02-26 11:41:12 +0100

    fixed usage of valid pixel expression

--- beam-statistics-op/src/main/java/org/esa/beam/statistics/StatisticsOp.java
+++ beam-statistics-op/src/main/java/org/esa/beam/statistics/StatisticsOp.java
@@ -160,7 +160,7 @@ public class StatisticsOp extends Operator implements Output {
         setDummyTargetProduct();
         validateInput();
 
-        final StatisticComputer statisticComputer = new StatisticComputer(shapefile, bandConfigurations, Util.computeBinCount(accuracy));
+        final StatisticComputer statisticComputer = new StatisticComputer(shapefile, bandConfigurations, Util.computeBinCount(accuracy), getLogger());
 
         final ProductValidator productValidator = new ProductValidator(Arrays.asList(bandConfigurations), startDate, endDate, getLogger());
         final ProductLoop productLoop = new ProductLoop(new ProductLoader(), productValidator, statisticComputer, getLogger());
@@ -323,7 +323,7 @@ public class StatisticsOp extends Operator implements Output {
         if (outputAsciiFile != null) {
             try {
                 final StringBuilder metadataFileName = new StringBuilder(
-                            FileUtils.getFilenameWithoutExtension(outputAsciiFile));
+                        FileUtils.getFilenameWithoutExtension(outputAsciiFile));
                 metadataFileName.append("_metadata.txt");
                 final File metadataFile = new File(outputAsciiFile.getParent(), metadataFileName.toString());
                 metadataOutputStream = new PrintStream(new FileOutputStream(metadataFile));
@@ -344,7 +344,7 @@ public class StatisticsOp extends Operator implements Output {
                 statisticsOutputters.add(FeatureStatisticsWriter.createFeatureStatisticsWriter(shapefile.toURI().toURL(), outputShapefile.getAbsolutePath(), bandNameCreator));
             } catch (MalformedURLException e) {
                 throw new OperatorException(
-                            "Unable to create shapefile outputter: shapefile '" + shapefile.getName() + "' is invalid.", e);
+                        "Unable to create shapefile outputter: shapefile '" + shapefile.getName() + "' is invalid.", e);
             } catch (FileNotFoundException e) {
                 throw new OperatorException("Unable to create shapefile outputter: maybe shapefile output directory does not exist?", e);
             }
@@ -384,7 +384,7 @@ public class StatisticsOp extends Operator implements Output {
         if ((sourceProducts == null || sourceProducts.length == 0) &&
             (sourceProductPaths == null || sourceProductPaths.length == 0)) {
             throw new OperatorException(
-                        "Either source products must be given or parameter 'sourceProductPaths' must be specified");
+                    "Either source products must be given or parameter 'sourceProductPaths' must be specified");
         }
         if (bandConfigurations == null) {
             throw new OperatorException("Parameter 'bandConfigurations' must be specified.");
