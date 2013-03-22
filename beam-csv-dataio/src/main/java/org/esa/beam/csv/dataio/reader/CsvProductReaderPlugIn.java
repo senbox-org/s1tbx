d6286350a31a2ba622cd47c16c9b9d6b597ddd2f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2013-03-22 11:13:00 +0100

    [BEAM-1480] (cherry-picked from 950ef55)

--- beam-csv-dataio/src/main/java/org/esa/beam/csv/dataio/reader/CsvProductReaderPlugIn.java
+++ beam-csv-dataio/src/main/java/org/esa/beam/csv/dataio/reader/CsvProductReaderPlugIn.java
@@ -16,14 +16,14 @@
 
 package org.esa.beam.csv.dataio.reader;
 
+import org.esa.beam.csv.dataio.CsvFile;
+import org.esa.beam.csv.dataio.CsvSourceParser;
 import org.esa.beam.framework.dataio.DecodeQualification;
 import org.esa.beam.framework.dataio.ProductReader;
 import org.esa.beam.framework.dataio.ProductReaderPlugIn;
 import org.esa.beam.util.StringUtils;
 import org.esa.beam.util.io.BeamFileFilter;
 import org.esa.beam.util.io.Constants;
-import org.esa.beam.csv.dataio.CsvFile;
-import org.esa.beam.csv.dataio.CsvSourceParser;
 import org.esa.beam.util.io.FileUtils;
 
 import java.io.File;
@@ -41,7 +41,7 @@ public class CsvProductReaderPlugIn implements ProductReaderPlugIn {
     @Override
     public DecodeQualification getDecodeQualification(Object input) {
         final File file = new File(input.toString());
-        if(!isFileExtensionValid(file)) {
+        if (!isFileExtensionValid(file)) {
             return DecodeQualification.UNABLE;
         }
 
@@ -85,7 +85,7 @@ public class CsvProductReaderPlugIn implements ProductReaderPlugIn {
 
     @Override
     public String[] getDefaultFileExtensions() {
-        return new String[]{".csv",".txt",".dat"};
+        return new String[]{".csv", ".txt"};
     }
 
     @Override
