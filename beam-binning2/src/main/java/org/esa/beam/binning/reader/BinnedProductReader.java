054f9d12b369bdd06faf627a0cee1573d4111266
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-03-26 17:49:20 +0100

    fixed reading of binned product written by binning2 (cherry-picked from f662562)

--- beam-binning2/src/main/java/org/esa/beam/binning/reader/BinnedProductReader.java
+++ beam-binning2/src/main/java/org/esa/beam/binning/reader/BinnedProductReader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
 package org.esa.beam.binning.reader;
 
 import com.bc.ceres.core.ProgressMonitor;
@@ -321,9 +337,13 @@ public class BinnedProductReader extends AbstractProductReader {
     }
 
     private int[] getXValuesForBin(int binIndexInGrid, int row) {
-        final int numberOfBinsInRow = planetaryGrid.getNumCols(row);
-        final int firstBinIndex = (int) planetaryGrid.getFirstBinIndex(row);
-        final double binIndexInRow = binIndexInGrid - firstBinIndex;
+        int numberOfBinsInRow = planetaryGrid.getNumCols(row);
+        int firstBinIndex = (int) planetaryGrid.getFirstBinIndex(row);
+        if (firstBinIndex >= binIndexInGrid) {
+            numberOfBinsInRow = planetaryGrid.getNumCols(row - 1);
+            firstBinIndex = (int) planetaryGrid.getFirstBinIndex(row -1) + 1;
+        }
+        double binIndexInRow = binIndexInGrid - firstBinIndex;
         final double longitudeExtent = 360.0 / numberOfBinsInRow;
         final double smallestLongitude = binIndexInRow * longitudeExtent;
         final double largestLongitude = smallestLongitude + longitudeExtent;
