6a4c9aecdf21cdbcef78b60dc6349d7d6a16c7aa
marpet <marco.peters@brockmann-consult.de>
2013-04-02 11:49:57 +0200

    changed title in helpset (cherry-picked from b048a32)

--- beam-opendap/src/main/resources/doc/help/opendap-client.hs
+++ beam-opendap/src/main/resources/doc/help/opendap-client.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>BEAM OPeNDAP-client Help</title>
+    <title>OPeNDAP Client Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.jhm"/>
