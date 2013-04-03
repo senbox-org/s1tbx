ae96ea46b65d2b1c21ed65873f5489e78f263aa4
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-04-03 11:27:05 +0200

    [BEAM-1483] Percentil Aggregator shares state

--- beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorPercentile.java
+++ beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorPercentile.java
@@ -1,21 +1,5 @@
 /*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see http://www.gnu.org/licenses/
- */
-
-/*
- * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * Copyright (C) 2013 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -58,6 +42,7 @@ public class AggregatorPercentile extends AbstractAggregator {
 
     private final int varIndex;
     private final int percentage;
+    private final String mlName;
 
     public AggregatorPercentile(VariableContext varCtx, String varName, Integer percentage, Number fillValue) {
         this(varCtx, varName, percentage != null ? percentage : 90, fillValue);
@@ -81,6 +66,7 @@ public class AggregatorPercentile extends AbstractAggregator {
         }
         this.varIndex = varCtx.getVariableIndex(varName);
         this.percentage = percentage;
+        this.mlName = "ml." + varName;
     }
 
     @Override
@@ -102,19 +88,18 @@ public class AggregatorPercentile extends AbstractAggregator {
     @Override
     public void initTemporal(BinContext ctx, WritableVector vector) {
         vector.set(0, 0.0f);
-        ctx.put("ml", new GrowableVector(256));
+        ctx.put(mlName, new GrowableVector(256));
     }
 
     @Override
-    public void aggregateTemporal(BinContext ctx, Vector spatialVector, int numSpatialObs,
-                                  WritableVector temporalVector) {
-        GrowableVector measurementsVec = ctx.get("ml");
+    public void aggregateTemporal(BinContext ctx, Vector spatialVector, int numSpatialObs, WritableVector temporalVector) {
+        GrowableVector measurementsVec = ctx.get(mlName);
         measurementsVec.add(spatialVector.get(0));
     }
 
     @Override
     public void completeTemporal(BinContext ctx, int numTemporalObs, WritableVector temporalVector) {
-        GrowableVector measurementsVec = ctx.get("ml");
+        GrowableVector measurementsVec = ctx.get(mlName);
         float[] measurements = measurementsVec.getElements();
         Arrays.sort(measurements);
         temporalVector.set(0, computePercentile(percentage, measurements));
