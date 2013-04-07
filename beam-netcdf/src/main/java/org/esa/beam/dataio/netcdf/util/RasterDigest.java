77f13ef12ba5cde569e2994492bb4ff1aa7e7178
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-04-07 21:15:13 +0200

    Scale bands in HDF-EOS to biggest resolution (cherry picked from commit f9a5e06d20fc4bb5eb187d255ea19cdbc2d1b16d)

--- beam-netcdf/src/main/java/org/esa/beam/dataio/netcdf/util/RasterDigest.java
+++ beam-netcdf/src/main/java/org/esa/beam/dataio/netcdf/util/RasterDigest.java
@@ -34,11 +34,17 @@ public class RasterDigest {
 
     private final DimKey rasterDim;
     private final Variable[] variables;
+    private ScaledVariable[] scaledVariables;
 
 
     public RasterDigest(DimKey rasterDim, Variable[] variables) {
+        this(rasterDim, variables, new ScaledVariable[0]);
+    }
+
+    public RasterDigest(DimKey rasterDim, Variable[] variables, ScaledVariable[] scaledVariables) {
         this.rasterDim = rasterDim;
         this.variables = variables;
+        this.scaledVariables = scaledVariables;
     }
 
     public DimKey getRasterDim() {
@@ -49,14 +55,45 @@ public class RasterDigest {
         return variables;
     }
 
-    public static RasterDigest createRasterDigest(final Group group) {
-        Map<DimKey, List<Variable>> variableListMap = getVariableListMap(group);
+    public ScaledVariable[] getScaledVariables() {
+        return scaledVariables;
+    }
+
+    public static RasterDigest createRasterDigest(final Group ...groups) {
+        Map<DimKey, List<Variable>> variableListMap = new HashMap<DimKey, List<Variable>>();
+        for (Group group : groups) {
+            collectVariableLists(group, variableListMap);
+        }
         if (variableListMap.isEmpty()) {
             return null;
         }
         final DimKey rasterDim = getBestRasterDim(variableListMap);
         final Variable[] rasterVariables = getRasterVariables(variableListMap, rasterDim);
-        return new RasterDigest(rasterDim, rasterVariables);
+        final ScaledVariable[] scaledVariables = getScaledVariables(variableListMap, rasterDim);
+        return new RasterDigest(rasterDim, rasterVariables, scaledVariables);
+    }
+
+    private static ScaledVariable[] getScaledVariables(Map<DimKey, List<Variable>> variableListMap, DimKey rasterDim) {
+        List<ScaledVariable> scaledVariableList = new ArrayList<ScaledVariable>();
+        for (DimKey dimKey : variableListMap.keySet()) {
+            if (!dimKey.equals(rasterDim)){
+                double scaleX = getScale(dimKey.getDimensionX(), rasterDim.getDimensionX());
+                double scaleY = getScale(dimKey.getDimensionY(), rasterDim.getDimensionY());
+                if (scaleX == Math.round(scaleX) && scaleX == scaleY) {
+                    List<Variable> variableList = variableListMap.get(dimKey);
+                    for (Variable variable : variableList) {
+                        scaledVariableList.add(new ScaledVariable((float)scaleX, variable));
+                    }
+                }
+            }
+        }
+        return scaledVariableList.toArray(new ScaledVariable[scaledVariableList.size()]);
+    }
+
+    private static double getScale(Dimension scaledDim, Dimension rasterDim) {
+        double length = scaledDim.getLength();
+        double rasterLength = rasterDim.getLength();
+        return rasterLength / length;
     }
 
     static Variable[] getRasterVariables(Map<DimKey, List<Variable>> variableLists,
@@ -86,12 +123,6 @@ public class RasterDigest {
         }
 
         return bestRasterDim;
-    }
-
-    static Map<DimKey, List<Variable>> getVariableListMap(final Group group) {
-        Map<DimKey, List<Variable>> variableLists = new HashMap<DimKey, List<Variable>>();
-        collectVariableLists(group, variableLists);
-        return variableLists;
     }
 
     static void collectVariableLists(Group group, Map<DimKey, List<Variable>> variableLists) {
