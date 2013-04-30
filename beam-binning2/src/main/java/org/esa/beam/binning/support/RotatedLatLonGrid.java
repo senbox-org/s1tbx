173f6f70dffa3a297dfe2278cc32c54fa0712c51
sabine <sabine.embacher@brockmann-consult.de>
2013-04-30 09:55:12 +0200

    implement RotatedLatLonGrid and tests (cherry picked from commit 1c47cb3)

--- beam-binning2/src/main/java/org/esa/beam/binning/support/RotatedLatLonGrid.java
+++ beam-binning2/src/main/java/org/esa/beam/binning/support/RotatedLatLonGrid.java
@@ -1,50 +1,63 @@
 package org.esa.beam.binning.support;
 
 import org.esa.beam.binning.PlanetaryGrid;
+import org.esa.beam.framework.datamodel.Rotator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * A {@link PlanetaryGrid} implementation representing a rotated Lat/Lon grid.
  */
-public class RotatedLatLonGrid implements PlanetaryGrid {
+public class RotatedLatLonGrid extends PlateCarreeGrid {
+
+    private final Rotator rotator;
+
+    public RotatedLatLonGrid(int numRows, double rotatedLat, double rotatedLon) {
+        super(numRows);
+        rotator = new Rotator(rotatedLon, rotatedLat);
+    }
 
     @Override
     public long getBinIndex(double lat, double lon) {
-        return 0;
+        final double[] lons = {lon};
+        final double[] lats = {lat};
+        rotator.transform(lons, lats);
+        return super.getBinIndex(lats[0], lons[0]);
     }
 
-    @Override
-    public int getRowIndex(long bin) {
-        return 0;
-    }
-
-    @Override
-    public long getNumBins() {
-        return 0;
-    }
-
-    @Override
-    public int getNumRows() {
-        return 0;
-    }
-
-    @Override
-    public int getNumCols(int row) {
-        return 0;
-    }
-
-    @Override
-    public long getFirstBinIndex(int row) {
-        return 0;
-    }
-
-    @Override
-    public double getCenterLat(int row) {
-        return 0;
-    }
+//    @Override
+//    public double getCenterLat(int row) {
+//        throw new NotImplementedException();
+//    }
 
     @Override
     public double[] getCenterLatLon(long bin) {
-        return new double[0];
+        final double[] centerLatLon = super.getCenterLatLon(bin);
+        final double[] lons = {centerLatLon[1]};
+        final double[] lats = {centerLatLon[0]};
+        rotator.transformInversely(lons, lats);
+        centerLatLon[0] = lats[0];
+        centerLatLon[1] = lons[0];
+        return centerLatLon;
+    }
+
+    public static double normalizeLat(double lat) {
+        while (lat > 90) {
+            lat -= 180;
+        }
+        while (lat < -90) {
+            lat += 180;
+        }
+        return lat;
+    }
+
+    public static double normalizeLon(double lon) {
+        while (lon > 180) {
+            lon -= 360;
+        }
+        while (lon < -180) {
+            lon += 360;
+        }
+        return lon;
     }
 
     // For later use when the PlanetaryGrid can be added as SPI
