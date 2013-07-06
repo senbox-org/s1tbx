d16818a7aefb35d57c47dd8cb20e345791e55c0f
lveci <lveci@4dfd5dd7-222b-49e7-bb6e-f5bcf0e0cfd1>
2013-07-06 15:20:25 +0000

    update versions

---
+++ nest-src/dist/bitrock/package.bat
@@ -0,0 +1,22 @@
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_installer64.xml windows
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_update64.xml windows
+
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_installer.xml windows
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_installer.xml linux
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_installer.xml linux-x64
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_installer.xml osx
+
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_update.xml windows
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_update.xml linux
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_update.xml linux-x64
+
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build C:\Build\5.0\NEST\nest\nest-src\dist\bitrock\bitrock_update.xml osx
