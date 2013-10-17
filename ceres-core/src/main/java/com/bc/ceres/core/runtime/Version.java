b44ae7a58ebff881ea7e268a978019a73342d187
marpet <marco.peters@brockmann-consult.de>
2013-10-17 10:03:21 +0200

    corrected API-Doc of toString in Version removed simple copies of parent method documentation

--- ceres-core/src/main/java/com/bc/ceres/core/runtime/Version.java
+++ ceres-core/src/main/java/com/bc/ceres/core/runtime/Version.java
@@ -24,6 +24,7 @@ import com.bc.ceres.core.Assert;
  * @author Norman Fomferra
  */
 public class Version implements Comparable {
+
     private String text;
     private int[] numbers;
     private String qualifier;
@@ -44,6 +45,7 @@ public class Version implements Comparable {
      * If no number was found <code>1.[&lt;qualifier&gt;]</code> is assumed, e.g. "M1", is the same as "1.0.0.M1".
      *
      * @param text the text to parse
+     *
      * @return the version identifier
      */
     public static Version parseVersion(String text) {
@@ -192,25 +194,10 @@ public class Version implements Comparable {
     }
 
     /**
-     * Returns a string representation of the object. In general, the
-     * <code>toString</code> method returns a string that
-     * "textually represents" this object. The result should
-     * be a concise but informative representation that is easy for a
-     * person to read.
-     * It is recommended that all subclasses override this method.
-     * <p/>
-     * The <code>toString</code> method for class <code>Object</code>
-     * returns a string consisting of the name of the class of which the
-     * object is an instance, the at-sign character `<code>@</code>', and
-     * the unsigned hexadecimal representation of the hash code of the
-     * object. In other words, this method returns a string equal to the
-     * value of:
-     * <blockquote>
-     * <pre>
-     * getClass().getName() + '@' + Integer.toHexString(hashCode())
-     * </pre></blockquote>
+     * Returns the string representation of the version in the
+     * form <code>{major}.{minor}.{micro}-{qualifier}</code>.
      *
-     * @return a string representation of the object.
+     * @return a string representation of the version.
      */
     @Override
     public String toString() {
@@ -222,7 +209,7 @@ public class Version implements Comparable {
                 }
                 sb.append(versionNumber);
             }
-            if (qualifier.length() > 0) {
+            if (!qualifier.isEmpty()) {
                 sb.append('-');
                 sb.append(qualifier);
             }
@@ -231,94 +218,12 @@ public class Version implements Comparable {
         return text;
     }
 
-    /**
-     * Compares this object with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.<p>
-     * <p/>
-     * In the foregoing description, the notation
-     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-     * <tt>0</tt>, or <tt>1</tt> according to whether the value of <i>expression</i>
-     * is negative, zero or positive.
-     * <p/>
-     * The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-     * <tt>y.compareTo(x)</tt> throws an exception.)<p>
-     * <p/>
-     * The implementor must also ensure that the relation is transitive:
-     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-     * <tt>x.compareTo(z)&gt;0</tt>.<p>
-     * <p/>
-     * Finally, the implementer must ensure that <tt>x.compareTo(y)==0</tt>
-     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-     * all <tt>z</tt>.<p>
-     * <p/>
-     * It is strongly recommended, but <i>not</i> strictly required that
-     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
-     * class that implements the <tt>Comparable</tt> interface and violates
-     * this condition should clearly indicate this fact.  The recommended
-     * language is "Note: this class has a natural ordering that is
-     * inconsistent with equals."
-     *
-     * @param o the Object to be compared.
-     * @return a negative integer, zero, or a positive integer as this object
-     *         is less than, equal to, or greater than the specified object.
-     * @throws ClassCastException if the specified object's type prevents it
-     *                            from being compared to this Object.
-     */
+    @Override
     public int compareTo(Object o) {
         Version other = (Version) o;
         return compare(this, other);
     }
 
-    /**
-     * Indicates whether some other object is "equal to" this one.
-     * <p/>
-     * The <code>equals</code> method implements an equivalence relation
-     * on non-null object references:
-     * <ul>
-     * <li>It is <i>reflexive</i>: for any non-null reference value
-     * <code>x</code>, <code>x.equals(x)</code> should return
-     * <code>true</code>.
-     * <li>It is <i>symmetric</i>: for any non-null reference values
-     * <code>x</code> and <code>y</code>, <code>x.equals(y)</code>
-     * should return <code>true</code> if and only if
-     * <code>y.equals(x)</code> returns <code>true</code>.
-     * <li>It is <i>transitive</i>: for any non-null reference values
-     * <code>x</code>, <code>y</code>, and <code>z</code>, if
-     * <code>x.equals(y)</code> returns <code>true</code> and
-     * <code>y.equals(z)</code> returns <code>true</code>, then
-     * <code>x.equals(z)</code> should return <code>true</code>.
-     * <li>It is <i>consistent</i>: for any non-null reference values
-     * <code>x</code> and <code>y</code>, multiple invocations of
-     * <tt>x.equals(y)</tt> consistently return <code>true</code>
-     * or consistently return <code>false</code>, provided no
-     * information used in <code>equals</code> comparisons on the
-     * objects is modified.
-     * <li>For any non-null reference value <code>x</code>,
-     * <code>x.equals(null)</code> should return <code>false</code>.
-     * </ul>
-     * <p/>
-     * The <tt>equals</tt> method for class <code>Object</code> implements
-     * the most discriminating possible equivalence relation on objects;
-     * that is, for any non-null reference values <code>x</code> and
-     * <code>y</code>, this method returns <code>true</code> if and only
-     * if <code>x</code> and <code>y</code> refer to the same object
-     * (<code>x == y</code> has the value <code>true</code>).
-     * <p/>
-     * Note that it is generally necessary to override the <tt>hashCode</tt>
-     * method whenever this method is overridden, so as to maintain the
-     * general contract for the <tt>hashCode</tt> method, which states
-     * that equal objects must have equal hash codes.
-     *
-     * @param obj the reference object with which to compare.
-     * @return <code>true</code> if this object is the same as the obj
-     *         argument; <code>false</code> otherwise.
-     * @see #hashCode()
-     * @see java.util.Hashtable
-     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -334,41 +239,6 @@ public class Version implements Comparable {
         return false;
     }
 
-    /**
-     * Returns a hash code value for the object. This method is
-     * supported for the benefit of hashtables such as those provided by
-     * <code>java.util.Hashtable</code>.
-     * <p/>
-     * The general contract of <code>hashCode</code> is:
-     * <ul>
-     * <li>Whenever it is invoked on the same object more than once during
-     * an execution of a Java application, the <tt>hashCode</tt> method
-     * must consistently return the same integer, provided no information
-     * used in <tt>equals</tt> comparisons on the object is modified.
-     * This integer need not remain consistent from one execution of an
-     * application to another execution of the same application.
-     * <li>If two objects are equal according to the <tt>equals(Object)</tt>
-     * method, then calling the <code>hashCode</code> method on each of
-     * the two objects must produce the same integer result.
-     * <li>It is <em>not</em> required that if two objects are unequal
-     * according to the {@link Object#equals(Object)}
-     * method, then calling the <tt>hashCode</tt> method on each of the
-     * two objects must produce distinct integer results.  However, the
-     * programmer should be aware that producing distinct integer results
-     * for unequal objects may improve the performance of hashtables.
-     * </ul>
-     * <p/>
-     * As much as is reasonably practical, the hashCode method defined by
-     * class <tt>Object</tt> does return distinct integers for distinct
-     * objects. (This is typically implemented by converting the internal
-     * address of the object into an integer, but this implementation
-     * technique is not required by the
-     * Java<font size="-2"><sup>TM</sup></font> programming language.)
-     *
-     * @return a hash code value for this object.
-     * @see Object#equals(Object)
-     * @see java.util.Hashtable
-     */
     @Override
     public int hashCode() {
         int n = 0;
