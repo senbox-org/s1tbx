3a96d0d987b3967a32ce50bd0a9cebd226ded29f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2013-12-18 18:05:00 +0100

    updated testing class

--- beam-ui/src/test/java/org/esa/beam/framework/ui/product/spectrum/SpectrumChooserMainForManualTesting.java
+++ beam-ui/src/test/java/org/esa/beam/framework/ui/product/spectrum/SpectrumChooserMainForManualTesting.java
@@ -4,12 +4,6 @@ import com.jidesoft.utils.Lm;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.ProductData;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class SpectrumChooserMainForManualTesting {
     /*
      * Used for testing UI
@@ -24,34 +18,23 @@ public class SpectrumChooserMainForManualTesting {
         spectra[2] = new DisplayableSpectrum(DisplayableSpectrum.ALTERNATIVE_DEFAULT_SPECTRUM_NAME);
         spectra[2].addBand(createBand(11), spectra[2].isSelected());
 
-        final JFrame frame = new JFrame();
-        frame.setSize(new Dimension(100, 100));
-        JButton button = new JButton("Choose Spectrum");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SpectrumChooser chooser = new SpectrumChooser(frame, spectra, "");
-                chooser.show();
-            }
-        });
-        frame.add(button);
-        frame.setVisible(true);
+        SpectrumChooser chooser = new SpectrumChooser(null, spectra, "");
+        chooser.show();
     }
 
     private static DisplayableSpectrum createSpectrum(int offset) {
         int numBands = 5;
         String name = "Radiances";
-        Band[] bands = new Band[numBands];
+        final DisplayableSpectrum spectrum = new DisplayableSpectrum(name + " " + (offset + 1));
+        final boolean selected = offset % 2 == 1;
+        spectrum.setSelected(selected);
         final int bandOffset = numBands * offset;
         for (int i = 0; i < numBands; i++) {
-            bands[i] = createBand(i + bandOffset);
+            spectrum.addBand(createBand(i + bandOffset), selected);
         }
-        return new DisplayableSpectrum(name + " " + (offset + 1), bands);
+        return spectrum;
     }
 
-    /*
-     * Used for testing UI
-     */
     static private Band createBand(int index) {
         final Band band = new Band("Radiance_" + (index + 1), ProductData.TYPE_INT16, 100, 100);
         band.setDescription("Radiance for band " + (index + 1));
