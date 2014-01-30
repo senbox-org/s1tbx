a3a3dfd223ba066e2bf0099524f7f4037eddb355
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-01-30 17:50:38 +0100

    improve performance of CSV reader for huge files

--- beam-csv-dataio/src/main/java/org/esa/beam/csv/dataio/CsvFile.java
+++ beam-csv-dataio/src/main/java/org/esa/beam/csv/dataio/CsvFile.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
- * 
+ * Copyright (C) 2014 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option)
@@ -9,7 +9,7 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
@@ -19,6 +19,7 @@ package org.esa.beam.csv.dataio;
 import com.bc.ceres.binding.ConversionException;
 import com.bc.ceres.binding.Converter;
 import com.bc.ceres.binding.ConverterRegistry;
+import com.sun.media.imageio.stream.FileChannelImageInputStream;
 import org.esa.beam.dataio.geometry.VectorDataNodeIO;
 import org.esa.beam.framework.datamodel.ProductData;
 import org.esa.beam.util.converters.JavaTypeConverter;
@@ -32,17 +33,19 @@ import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import javax.imageio.stream.FileImageInputStream;
 import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.text.SimpleDateFormat;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -71,9 +74,10 @@ public class CsvFile implements CsvSourceParser, CsvSource {
     private boolean recordsParsed = false;
 
     private ImageInputStream stream;
-    
+
     private int headerByteSize;
     private int propertiesByteSize;
+    private Converter<?>[] converters;
 
     private CsvFile(String csv) throws IOException {
         this(new File(csv), null);
@@ -83,7 +87,8 @@ public class CsvFile implements CsvSourceParser, CsvSource {
         this.csv = csv;
         ConverterRegistry.getInstance().setConverter(ProductData.UTC.class, new UTCConverter());
         this.crs = crs;
-        stream = new FileImageInputStream(csv);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(csv, "r");
+        stream = new FileChannelImageInputStream(randomAccessFile.getChannel());
     }
 
     public static CsvSourceParser createCsvSourceParser(String csv) throws IOException {
@@ -92,25 +97,61 @@ public class CsvFile implements CsvSourceParser, CsvSource {
 
     @Override
     public void checkReadingFirstRecord() throws IOException {
-       Converter<?>[] converters = VectorDataNodeIO.getConverters(simpleFeatureType);
-       skipToLine(0);
-       final String line = stream.readLine() ;
-       final String[] tokens = getTokens(line);
+        skipToLine(0);
+        final String line = stream.readLine();
+        final String[] tokens = getTokens(line);
 
-       for (int i = 0; i < converters.length; i++) {
-           Converter<?> converter = converters[i];
-           try {
-               converter.parse(tokens[i + (hasFeatureId ? 1 : 0)]);
-           } catch (ConversionException e) {
-               throw new IOException(e);
-           }
-       }
-   }
+        for (int i = 0; i < converters.length; i++) {
+            Converter<?> converter = converters[i];
+            try {
+                converter.parse(tokens[i + (hasFeatureId ? 1 : 0)]);
+            } catch (ConversionException e) {
+                throw new IOException(e);
+            }
+        }
+    }
+
+    @Override
+    public Object[] parseRecords(final int offset, final int numRecords, final String rowName) throws IOException {
+        AttributeDescriptor attributeDescriptor = simpleFeatureType.getDescriptor(rowName);
+        int expectedTokenCount = simpleFeatureType.getAttributeCount();
+        expectedTokenCount += hasFeatureId ? 1 : 0;
+        int rowIndex = simpleFeatureType.getAttributeDescriptors().indexOf(attributeDescriptor);
+        int tokenIndex = rowIndex + (hasFeatureId ? 1 : 0);
+
+        List<Object> values = new ArrayList<>(numRecords);
+        skipToLine(offset);
+        String line;
+        long featureCount = offset;
+        while ((numRecords == -1 || featureCount < offset + numRecords) && (line = stream.readLine()) != null) {
+            String[] tokens = getTokens(line);
+            if (tokens == null) {
+                break;
+            }
+
+            if (tokens.length != expectedTokenCount) {
+                continue;
+            }
+            featureCount++;
+
+            String token = tokens[tokenIndex];
+            try {
+                Object value = null;
+                if (!VectorDataNodeIO.NULL_TEXT.equals(token)) {
+                    value = converters[rowIndex].parse(token);
+                }
+                values.add(value);
+            } catch (ConversionException e) {
+                BeamLogManager.getSystemLogger().warning(String.format("Problem in '%s': %s",
+                                                                       csv.getPath(), e.getMessage()));
+            }
+            bytePositionForOffset.put(featureCount, stream.getStreamPosition());
+        }
+        return values.toArray();
+    }
 
     @Override
     public void parseRecords(int offset, int numRecords) throws IOException {
-        Converter<?>[] converters = VectorDataNodeIO.getConverters(simpleFeatureType);
-
         featureCollection = new ListFeatureCollection(simpleFeatureType);
         SimpleFeatureBuilder builder = new SimpleFeatureBuilder(simpleFeatureType);
 
@@ -159,6 +200,7 @@ public class CsvFile implements CsvSourceParser, CsvSource {
     public CsvSource parseMetadata() throws IOException {
         parseProperties();
         parseHeader();
+        converters = VectorDataNodeIO.getConverters(simpleFeatureType);
         return this;
     }
 
@@ -227,7 +269,7 @@ public class CsvFile implements CsvSourceParser, CsvSource {
             }
             propertiesByteSize += (stream.getStreamPosition() - posInStream);
             posInStream = stream.getStreamPosition();
-            
+
             line = line.substring(1);
             int pos = line.indexOf('=');
             if (pos == -1) {
@@ -355,56 +397,56 @@ public class CsvFile implements CsvSourceParser, CsvSource {
         return false;
     }
 
-    private static class UTCConverter implements Converter<ProductData.UTC> {
+private static class UTCConverter implements Converter<ProductData.UTC> {
 
-        @Override
-        public Class<? extends ProductData.UTC> getValueType() {
-            return ProductData.UTC.class;
-        }
+    @Override
+    public Class<? extends ProductData.UTC> getValueType() {
+        return ProductData.UTC.class;
+    }
 
-        @Override
-        public ProductData.UTC parse(String text) throws ConversionException {
-            try {
-                return ProductData.UTC.parse(text, Constants.TIME_PATTERN);
-            } catch (java.text.ParseException e) {
-                throw new ConversionException(e);
-            }
-        }
-
-        @Override
-        public String format(ProductData.UTC value) {
-            final SimpleDateFormat sdf = new SimpleDateFormat(Constants.TIME_PATTERN);
-            return sdf.format(value.getAsDate());
+    @Override
+    public ProductData.UTC parse(String text) throws ConversionException {
+        try {
+            return ProductData.UTC.parse(text, Constants.TIME_PATTERN);
+        } catch (java.text.ParseException e) {
+            throw new ConversionException(e);
         }
     }
 
-    private class CsvJavaTypeConverter extends JavaTypeConverter {
+    @Override
+    public String format(ProductData.UTC value) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(Constants.TIME_PATTERN);
+        return sdf.format(value.getAsDate());
+    }
+}
 
-        @Override
-        public Class parse(String text) throws ConversionException {
-            Class result;
+private class CsvJavaTypeConverter extends JavaTypeConverter {
+
+    @Override
+    public Class parse(String text) throws ConversionException {
+        Class result;
+        try {
+            result = super.parse(text);
+        } catch (ConversionException e) {
             try {
-                result = super.parse(text);
-            } catch (ConversionException e) {
-                try {
-                    if (contains(Constants.TIME_NAMES, text.toLowerCase())) {
-                        result = getClass().getClassLoader().loadClass(ProductData.UTC.class.getName());
-                    } else if ("ubyte".toLowerCase().equals(text.toLowerCase())) {
-                        result = getClass().getClassLoader().loadClass(Byte.class.getName());
-                    } else if ("ushort".toLowerCase().equals(text.toLowerCase())) {
-                        result = getClass().getClassLoader().loadClass(Short.class.getName());
-                    } else if ("uint".toLowerCase().equals(text.toLowerCase())) {
-                        result = getClass().getClassLoader().loadClass(Integer.class.getName());
-                    } else {
-                        throw new ConversionException(e);
-                    }
-                } catch (ClassNotFoundException e1) {
-                    throw new ConversionException(e1);
+                if (contains(Constants.TIME_NAMES, text.toLowerCase())) {
+                    result = getClass().getClassLoader().loadClass(ProductData.UTC.class.getName());
+                } else if ("ubyte".toLowerCase().equals(text.toLowerCase())) {
+                    result = getClass().getClassLoader().loadClass(Byte.class.getName());
+                } else if ("ushort".toLowerCase().equals(text.toLowerCase())) {
+                    result = getClass().getClassLoader().loadClass(Short.class.getName());
+                } else if ("uint".toLowerCase().equals(text.toLowerCase())) {
+                    result = getClass().getClassLoader().loadClass(Integer.class.getName());
+                } else {
+                    throw new ConversionException(e);
                 }
+            } catch (ClassNotFoundException e1) {
+                throw new ConversionException(e1);
             }
-            return result;
         }
+        return result;
     }
+}
 
 
 }
