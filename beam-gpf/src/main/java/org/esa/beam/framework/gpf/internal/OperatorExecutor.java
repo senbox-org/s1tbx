080750ce2353974f321ec1f853095dd855907344
marpet <marco.peters@brockmann-consult.de>
2014-03-06 08:40:19 +0100

    not assuming that there is always at least one image of a band
    could be that the product has only vector data

--- beam-gpf/src/main/java/org/esa/beam/framework/gpf/internal/OperatorExecutor.java
+++ beam-gpf/src/main/java/org/esa/beam/framework/gpf/internal/OperatorExecutor.java
@@ -190,16 +190,18 @@ public class OperatorExecutor {
 
     private void scheduleRowColumnBand(Semaphore semaphore, ProgressMonitor pm) {
         //better handle stack operators, should equal well work for normal operators
-        final TileComputationListener tcl = new OperatorTileComputationListenerStack(semaphore, images, pm);
-        final TileComputationListener[] listeners = new TileComputationListener[]{tcl};
-        for (int tileY = 0; tileY < tileCountY; tileY++) {
-            for (int tileX = 0; tileX < tileCountX; tileX++) {
-                scheduleTile(images[0], tileX, tileY, semaphore, listeners, pm);
-            }
-            if (scheduleRowsSeparate) {
-                // wait until all threads / tiles are finished
-                acquirePermits(semaphore, parallelism);
-                semaphore.release(parallelism);
+        if (images.length >= 1) {
+            final TileComputationListener tcl = new OperatorTileComputationListenerStack(semaphore, images, pm);
+            final TileComputationListener[] listeners = new TileComputationListener[]{tcl};
+            for (int tileY = 0; tileY < tileCountY; tileY++) {
+                for (int tileX = 0; tileX < tileCountX; tileX++) {
+                    scheduleTile(images[0], tileX, tileY, semaphore, listeners, pm);
+                }
+                if (scheduleRowsSeparate) {
+                    // wait until all threads / tiles are finished
+                    acquirePermits(semaphore, parallelism);
+                    semaphore.release(parallelism);
+                }
             }
         }
     }
@@ -208,7 +210,7 @@ public class OperatorExecutor {
                               TileComputationListener[] listeners, ProgressMonitor pm) {
 
         BeamLogManager.getSystemLogger().finest(String.format("Scheduling tile x=%d/%d y=%d/%d for %s",
-                tileX + 1, tileCountX, tileY + 1, tileCountY, image));
+                                                              tileX + 1, tileCountX, tileY + 1, tileCountY, image));
 
         checkForCancelation(pm);
         acquirePermits(semaphore, 1);
