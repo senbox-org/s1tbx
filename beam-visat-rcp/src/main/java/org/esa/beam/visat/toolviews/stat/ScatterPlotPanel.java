187442a74a50556bb6bb3776856b1f2465506471
marpet <marco.peters@brockmann-consult.de>
2014-03-07 08:50:53 +0100

    don't use umlaute in code

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/stat/ScatterPlotPanel.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/stat/ScatterPlotPanel.java
@@ -562,7 +562,7 @@ class ScatterPlotPanel extends ChartPagePanel {
         yAxisOptionPanel.add(yLogCheck, BorderLayout.SOUTH);
 
         final JCheckBox acceptableCheck = new JCheckBox("Show tolerance range");
-        JLabel fieldPräfix = new JLabel("+/-");
+        JLabel fieldPrefix = new JLabel("+/-");
         final JTextField acceptableField = new JTextField();
         acceptableField.setPreferredSize(new Dimension(40, acceptableField.getPreferredSize().height));
         acceptableField.setHorizontalAlignment(JTextField.RIGHT);
@@ -570,7 +570,7 @@ class ScatterPlotPanel extends ChartPagePanel {
         bindingContext.bind(PROPERTY_NAME_SHOW_ACCEPTABLE_DEVIATION, acceptableCheck);
         bindingContext.bind(PROPERTY_NAME_ACCEPTABLE_DEVIATION, acceptableField);
         bindingContext.getBinding(PROPERTY_NAME_ACCEPTABLE_DEVIATION).addComponent(percentLabel);
-        bindingContext.getBinding(PROPERTY_NAME_ACCEPTABLE_DEVIATION).addComponent(fieldPräfix);
+        bindingContext.getBinding(PROPERTY_NAME_ACCEPTABLE_DEVIATION).addComponent(fieldPrefix);
         bindingContext.bindEnabledState(PROPERTY_NAME_ACCEPTABLE_DEVIATION, true,
                                         PROPERTY_NAME_SHOW_ACCEPTABLE_DEVIATION, true);
 
@@ -578,7 +578,7 @@ class ScatterPlotPanel extends ChartPagePanel {
         GridBagConstraints confidencePanelConstraints = GridBagUtils.createConstraints(
                 "anchor=NORTHWEST,fill=HORIZONTAL,insets.top=5,weighty=0,weightx=1");
         GridBagUtils.addToPanel(confidencePanel, acceptableCheck, confidencePanelConstraints, "gridy=0,gridwidth=3");
-        GridBagUtils.addToPanel(confidencePanel, fieldPräfix, confidencePanelConstraints,
+        GridBagUtils.addToPanel(confidencePanel, fieldPrefix, confidencePanelConstraints,
                                 "weightx=0,insets.left=22,gridy=1,gridx=0,insets.top=4,gridwidth=1");
         GridBagUtils.addToPanel(confidencePanel, acceptableField, confidencePanelConstraints,
                                 "weightx=1,gridx=1,insets.left=2,insets.top=2");
