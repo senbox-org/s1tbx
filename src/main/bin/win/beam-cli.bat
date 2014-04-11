bcff6c8baf0e2ec8e82314d3f1a99d37107ce813
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-04-11 15:44:38 +0200

    Removed references to unused shell commands

--- src/main/bin/win/beam-cli.bat
+++ src/main/bin/win/beam-cli.bat
@@ -7,11 +7,6 @@ echo.
 @echo The following command-line tools are available:
 @echo   gpt.bat            - General Graph Processing Tool
 @echo   pconvert.bat       - General product conversion and quicklook generation
-@echo   binning.bat        - General level 3 binning processor
-@echo   flhmci.bat         - General FLH / MCI processor
-@echo   meris-cloud.bat    - Envisat/MERIS cloud probability processor
-@echo   meris-smac.bat     - Envisat/MERIS atmospheric correction (SMAC)
-@echo   aatsr-sst.bat      - Envisat/AATSR sea surface temperature processor
 @echo   visat-d.bat        - VISAT application launcher for debugging
 @echo Typing the name of the tool will output its usage information.
 echo.
