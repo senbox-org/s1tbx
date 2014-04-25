64926e29e8280e705b466e5e1c7688b12b48a75c
junlu <junlu@4dfd5dd7-222b-49e7-bb6e-f5bcf0e0cfd1>
2014-04-25 16:25:52 +0000

    separated srcIdx from tgtIdx

--- nest-op-calibration/src/main/java/org/esa/nest/gpf/Radarsat2Calibrator.java
+++ nest-op-calibration/src/main/java/org/esa/nest/gpf/Radarsat2Calibrator.java
@@ -210,31 +210,34 @@ public class Radarsat2Calibrator extends BaseCalibrator implements Calibrator {
 
         final ProductData trgData = targetTile.getDataBuffer();
         final TileIndex srcIndex = new TileIndex(sourceRaster1);
+        final TileIndex tgtIndex = new TileIndex(targetTile);
 
         final int maxY = y0 + h;
         final int maxX = x0 + w;
 
         double sigma, dn, i, q;
-        int index;
+        int srcIdx, tgtIdx;
 
         for (int y = y0; y < maxY; ++y) {
             srcIndex.calculateStride(y);
-            for (int x = x0; x < maxX; ++x) {
+            tgtIndex.calculateStride(y);
 
-                index = srcIndex.getIndex(x);
+            for (int x = x0; x < maxX; ++x) {
+                srcIdx = srcIndex.getIndex(x);
+                tgtIdx = tgtIndex.getIndex(x);
 
                 if (bandUnit == Unit.UnitType.AMPLITUDE) {
-                    dn = srcData1.getElemDoubleAt(index);
+                    dn = srcData1.getElemDoubleAt(srcIdx);
                     sigma = dn*dn;
                 } else if (bandUnit == Unit.UnitType.INTENSITY) {
-                    sigma = srcData1.getElemDoubleAt(index);
+                    sigma = srcData1.getElemDoubleAt(srcIdx);
                 } else if (bandUnit == Unit.UnitType.REAL || bandUnit == Unit.UnitType.IMAGINARY) {
                     if (outputImageInComplex) {
                         // the sigma below is actually i or q, we still call it sigma just for convenient
-                        sigma = srcData1.getElemDoubleAt(index);
+                        sigma = srcData1.getElemDoubleAt(srcIdx);
                     } else {
-                        i = srcData1.getElemDoubleAt(index);
-                        q = srcData2.getElemDoubleAt(index);
+                        i = srcData1.getElemDoubleAt(srcIdx);
+                        q = srcData2.getElemDoubleAt(srcIdx);
                         sigma = i * i + q * q;
                     }
                 } else {
@@ -264,7 +267,7 @@ public class Radarsat2Calibrator extends BaseCalibrator implements Calibrator {
                     }
                 }
 
-                trgData.setElemDoubleAt(index, sigma);
+                trgData.setElemDoubleAt(tgtIdx, sigma);
             }
         }
     }
