87a44ca5a202efdbb59e81e3934e6b1c3903731f
marpet <marco.peters@brockmann-consult.de>
2014-05-02 17:34:21 +0200

    added comments to ndvi_op-info.xml

--- beam-python/src/main/resources/beampy-operator-example/ndvi_op.py
+++ beam-python/src/main/resources/beampy-operator-example/ndvi_op.py
@@ -16,18 +16,16 @@ class MerisNdviTileComputer:
         width = merisProduct.getSceneRasterWidth()
         height = merisProduct.getSceneRasterHeight()
 
-        self.parameterA = operator.getParameter('a', 1.0)
-        print('Parameter A is', self.parameterA)
+        self.lowFactor = operator.getParameter('lowerFactor')
+        self.lowerBandName = operator.getParameter('lowerName')
 
-        self.parameterB = operator.getParameter('b', 1.0)
-        print('Parameter B is', self.parameterB)
+        self.upperFactor = operator.getParameter('upperFactor')
+        self.upperBandName = operator.getParameter('upperName')
 
-        self.b7 = self.getBand(merisProduct, 'radiance_7')
-        self.b10 = self.getBand(merisProduct, 'radiance_10')
+        self.lowerBand = self.getBand(merisProduct, self.lowerBandName)
+        self.upperBand = self.getBand(merisProduct, self.upperBandName)
 
         ndviProduct = beampy.Product('pyNDVI', 'pyNDVI', width, height)
-        # ndviProduct.setPreferredTileSize(200, 200)
-        # ndviProduct.setPreferredTileSize(width, height)
         self.ndviBand = ndviProduct.addBand('ndvi', beampy.ProductData.TYPE_FLOAT32)
         self.ndviFlagsBand = ndviProduct.addBand('ndvi_flags', beampy.ProductData.TYPE_UINT8)
 
@@ -35,14 +33,14 @@ class MerisNdviTileComputer:
 
     def compute(self, operator, targetTiles, targetRectangle):
 
-        b7Tile = operator.getSourceTile(self.b7, targetRectangle)
-        b10Tile = operator.getSourceTile(self.b10, targetRectangle)
+        lowerTile = operator.getSourceTile(self.lowerBand, targetRectangle)
+        upperTile = operator.getSourceTile(self.upperBand, targetRectangle)
 
         ndviTile = targetTiles.get(self.ndviBand)
         ndviFlagsTile = targetTiles.get(self.ndviFlagsBand)
 
-        b7Data = b7Tile.getSamplesFloat()
-        b10Data = b10Tile.getSamplesFloat()
+        b7Data = lowerTile.getSamplesFloat()
+        b10Data = upperTile.getSamplesFloat()
 
         r7 = numpy.array(b7Data, dtype=numpy.float32)
         r10 = numpy.array(b10Data, dtype=numpy.float32)
