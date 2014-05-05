64d249cadedcebcc99dfffd178aede2fbd951a0e
Luis Veci <lveci@array.ca>
2014-05-05 15:13:36 -0400

    remove visatapp dependency

--- nest-core/src/main/java/org/esa/nest/util/ftpUtils.java
+++ nest-core/src/main/java/org/esa/nest/util/ftpUtils.java
@@ -19,7 +19,6 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
-import org.esa.beam.visat.VisatApp;
 import org.esa.nest.gpf.StatusProgressMonitor;
 import org.jdom2.Attribute;
 import org.jdom2.Document;
@@ -40,7 +39,7 @@ public final class ftpUtils {
     private final String password;
     private boolean ftpClientConnected = false;
 
-    public enum FTPError { FILE_NOT_FOUND, OK, READ_ERROR }
+    public enum FTPError {FILE_NOT_FOUND, OK, READ_ERROR}
 
     public ftpUtils(final String server) throws IOException {
         this(server, "anonymous", "anonymous");
@@ -61,7 +60,7 @@ public final class ftpUtils {
             ftpClientConnected = ftpClient.login(user, password);
         if (!ftpClientConnected) {
             disconnect();
-            throw new IOException("Unable to connect to "+server);
+            throw new IOException("Unable to connect to " + server);
         } else {
             ftpClient.enterLocalPassiveMode();
             ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
@@ -70,7 +69,7 @@ public final class ftpUtils {
     }
 
     public void disconnect() throws IOException {
-        if(ftpClientConnected) {
+        if (ftpClientConnected) {
             ftpClient.logout();
             ftpClient.disconnect();
         }
@@ -80,35 +79,35 @@ public final class ftpUtils {
         BufferedOutputStream fos = null;
         InputStream fis = null;
         try {
-            System.out.println("ftp retrieving "+remotePath);
-            
+            System.out.println("ftp retrieving " + remotePath);
+
             fis = ftpClient.retrieveFileStream(remotePath);
-            if(fis == null) {
+            if (fis == null) {
                 final int code = ftpClient.getReplyCode();
-                System.out.println("error code:"+code + " on " + remotePath);
-                if(code == 550)
+                System.out.println("error code:" + code + " on " + remotePath);
+                if (code == 550)
                     return FTPError.FILE_NOT_FOUND;
                 else
                     return FTPError.READ_ERROR;
             }
 
             final File parentFolder = localFile.getParentFile();
-            if(!parentFolder.exists()) {
+            if (!parentFolder.exists()) {
                 parentFolder.mkdirs();
             }
             fos = new BufferedOutputStream(new FileOutputStream(localFile.getAbsolutePath()));
 
             final StatusProgressMonitor status = new StatusProgressMonitor(fileSize,
-                    "Downloading "+localFile.getName()+"... ");
+                    "Downloading " + localFile.getName() + "... ");
             status.setAllowStdOut(false);
 
             final int size = 4096;//32768;
             final byte[] buf = new byte[size];
             int n;
             int total = 0;
-            while ((n = fis.read(buf, 0, size)) > -1)  {
+            while ((n = fis.read(buf, 0, size)) > -1) {
                 fos.write(buf, 0, n);
-                if(fileSize != null) {
+                if (fileSize != null) {
                     total += n;
                     status.worked(total);
                 } else {
@@ -123,7 +122,7 @@ public final class ftpUtils {
         } catch (SocketException e) {
             System.out.println(e.getMessage());
             connect();
-            throw new SocketException(e.getMessage()+"\nPlease verify that FTP is not blocked by your firewall.");
+            throw new SocketException(e.getMessage() + "\nPlease verify that FTP is not blocked by your firewall.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             connect();
@@ -143,8 +142,8 @@ public final class ftpUtils {
     }
 
     public static long getFileSize(final FTPFile[] fileList, final String remoteFileName) {
-        for(FTPFile file : fileList) {
-            if(file.getName().equalsIgnoreCase(remoteFileName)) {
+        for (FTPFile file : fileList) {
+            if (file.getName().equalsIgnoreCase(remoteFileName)) {
                 return file.getSize();
             }
         }
@@ -158,7 +157,7 @@ public final class ftpUtils {
     public static String getPathFromSettings(final String tag) {
         String path = Settings.instance().get(tag);
         path = path.replace("\\", "/");
-        if(!path.endsWith("/"))
+        if (!path.endsWith("/"))
             path += "/";
         return path;
     }
@@ -167,21 +166,21 @@ public final class ftpUtils {
 
         boolean useCachedListing = true;
         final String tmpDirUrl = ResourceUtils.getApplicationUserTempDataDir().getAbsolutePath();
-        final File listingFile = new File(tmpDirUrl+"//"+server+".listing.xml");
-        if(!listingFile.exists())
+        final File listingFile = new File(tmpDirUrl + "//" + server + ".listing.xml");
+        if (!listingFile.exists())
             useCachedListing = false;
 
         final Map<String, Long> fileSizeMap = new HashMap<String, Long>(900);
 
-        if(useCachedListing) {
+        if (useCachedListing) {
             Document doc = null;
             try {
                 doc = XMLSupport.LoadXML(listingFile.getAbsolutePath());
-            } catch(IOException e) {
+            } catch (IOException e) {
                 useCachedListing = false;
             }
 
-            if(useCachedListing) {
+            if (useCachedListing) {
                 final Element root = doc.getRootElement();
                 boolean listingFound = false;
 
@@ -190,38 +189,38 @@ public final class ftpUtils {
                     if (!(c1 instanceof Element)) continue;
                     final Element remotePathElem = (Element) c1;
                     final Attribute pathAttrib = remotePathElem.getAttribute("path");
-                    if(pathAttrib != null && pathAttrib.getValue().equalsIgnoreCase(remotePath)) {
+                    if (pathAttrib != null && pathAttrib.getValue().equalsIgnoreCase(remotePath)) {
                         listingFound = true;
                         final List children2 = remotePathElem.getContent();
                         for (Object c2 : children2) {
                             if (!(c2 instanceof Element)) continue;
                             final Element fileElem = (Element) c2;
                             final Attribute attrib = fileElem.getAttribute("size");
-                            if(attrib != null) {
+                            if (attrib != null) {
                                 try {
                                     fileSizeMap.put(fileElem.getName(), attrib.getLongValue());
-                                } catch(Exception e) {
+                                } catch (Exception e) {
                                     //
                                 }
                             }
                         }
                     }
                 }
-                if(!listingFound)
+                if (!listingFound)
                     useCachedListing = false;
             }
         }
-        if(!useCachedListing) {
+        if (!useCachedListing) {
             try {
                 final FTPFile[] remoteFileList = ftp.getRemoteFileList(remotePath);
 
                 writeRemoteFileList(remoteFileList, server, remotePath, listingFile);
 
-                for (FTPFile ftpFile : remoteFileList)  {
+                for (FTPFile ftpFile : remoteFileList) {
                     fileSizeMap.put(ftpFile.getName(), ftpFile.getSize());
-                }                  
-            } catch(Exception e) {
-                System.out.println("Unable to get remote file list "+e.getMessage());
+                }
+            } catch (Exception e) {
+                System.out.println("Unable to get remote file list " + e.getMessage());
             }
         }
 
@@ -229,7 +228,7 @@ public final class ftpUtils {
     }
 
     private static void writeRemoteFileList(final FTPFile[] remoteFileList, final String server,
-                                           final String remotePath, final File file) {
+                                            final String remotePath, final File file) {
 
         final Element root = new Element("remoteFileListing");
         root.setAttribute("server", server);
@@ -239,7 +238,7 @@ public final class ftpUtils {
         remotePathElem.setAttribute("path", remotePath);
         root.addContent(remotePathElem);
 
-        for (FTPFile ftpFile : remoteFileList)  {
+        for (FTPFile ftpFile : remoteFileList) {
             final Element fileElem = new Element(ftpFile.getName());
             fileElem.setAttribute("size", String.valueOf(ftpFile.getSize()));
             remotePathElem.addContent(fileElem);
@@ -247,22 +246,12 @@ public final class ftpUtils {
         XMLSupport.SaveXML(doc, file.getAbsolutePath());
     }
 
-    public static boolean testFTP(final String remoteFTP, final String remotePath) {
-        try {
-            final ftpUtils ftp = new ftpUtils(remoteFTP);
+    public static boolean testFTP(final String remoteFTP, final String remotePath) throws IOException {
+        final ftpUtils ftp = new ftpUtils(remoteFTP);
 
-            final FTPFile[] remoteFileList = ftp.getRemoteFileList(remotePath);
-            ftp.disconnect();
+        final FTPFile[] remoteFileList = ftp.getRemoteFileList(remotePath);
+        ftp.disconnect();
 
-            return (remoteFileList != null);
-
-        } catch(Exception e) {
-            final String errMsg = "Error connecting via FTP: "+e.getMessage();
-            System.out.println(errMsg);
-            if(VisatApp.getApp() != null) {
-                VisatApp.getApp().showErrorDialog(errMsg);
-            }
-        }
-        return false;
+        return (remoteFileList != null);
     }
 }
