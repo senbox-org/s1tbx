0c0e40c6f89db2059e81d054df306b89bbbf1da9
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-05-07 17:52:47 +0200

    Fix

--- beam-python/src/main/resources/beampy-examples/beampy_subset.py
+++ beam-python/src/main/resources/beampy-examples/beampy_subset.py
@@ -7,12 +7,12 @@ SubsetOp = beampy.jpy.get_type('org.esa.beam.gpf.operators.standard.SubsetOp')
 JtsGeometryConverter = beampy.jpy.get_type('org.esa.beam.util.converters.JtsGeometryConverter')
 
 def wkt2geom(wkt):
-    return JtsGeometryConverter.newJtsGeometryConverter().parse(wkt)
+    return JtsGeometryConverter().parse(wkt)
 
 if len(sys.argv) != 3:
     print("usage: %s <file> <geometry-wkt>" % sys.argv[0])
-    print("       %s ./TEST.N1 'POLYGON((15.786082 45.30223, 11.798364 46.118263, 10.878688 43.61961, 14.722727"
-          "42.85818, 15.786082 45.30223))'" % sys.argv[0])
+    print("       %s ./TEST.N1 \"POLYGON((15.786082 45.30223, 11.798364 46.118263, 10.878688 43.61961, 14.722727"
+          "42.85818, 15.786082 45.30223))\"" % sys.argv[0])
     sys.exit(1)
 
 file = sys.argv[1]
