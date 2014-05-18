5a00026360fc9e41fe5d9209e2046d65a3a320d3
Luis Veci <lveci@array.ca>
2014-05-18 19:13:22 -0400

    remove beam

--- beam/beam-reader-tests/README.txt
+++
@@ -1,6 +0,0 @@
-The usage of this module is described in the BEAM wiki:
-Using Reader Acceptance Test - http://www.brockmann-consult.de/beam-wiki/x/gYC8Aw
-
-
-
-
--- beam/beam-scripting/src/main/resources/org/esa/beam/scripting/visat/ScriptManager.js
+++
@@ -1,9 +0,0 @@
-importPackage(org.esa.beam.framework.datamodel);
-importPackage(org.esa.beam.framework.dataio);
-importPackage(org.esa.beam.framework.gpf);
-importPackage(org.esa.beam.framework.gpf.operators.common);
-importPackage(org.esa.beam.framework.gpf.operators.meris);
-importPackage(org.esa.beam.util);
-importPackage(org.esa.beam.visat);
-
-visat = VisatApp.getApp();
