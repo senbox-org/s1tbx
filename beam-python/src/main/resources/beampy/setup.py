87c652257875b9b2803bf80965fdaf786b5c2e81
thomasstorm <thomas_storm@brockmann-consult.de>
2014-06-25 17:28:57 +0200

    [BEAM-1686] Installation fails when setuptools is not installed

--- beam-python/src/main/resources/beampy/setup.py
+++ beam-python/src/main/resources/beampy/setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-import sys
-
 BEAMPY_NAME = 'beampy'
-BEAMPY_VERSION = '5.0.1'
+BEAMPY_VERSION = '5.0.2'
 BEAMPY_SUMMARY = 'BEAM Python API'
 BEAMPY_AUTHOR = 'Norman Fomferra, Brockmann Consult GmbH'
 BEAMPY_JPY = 'jpy>=0.7.2'
 
-if sys.version_info < (3,):
+try:
+    import setuptools
+    use_distutils = False
+except ImportError:
+    use_distutils = True
+
+if use_distutils:
     from distutils.core import setup
     setup(name=BEAMPY_NAME,
           version=BEAMPY_VERSION,
