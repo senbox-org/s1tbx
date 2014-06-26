9c1eebaf06c8c35ab78dd78a393214699e0293cc
TomBlock <tom.block@brockmann-consult.de>
2014-06-26 10:51:29 +0200

    implemented name aggregator

--- beam-binning/src/main/java/org/esa/beam/binning/operator/ProductNameMetaAggregator.java
+++ beam-binning/src/main/java/org/esa/beam/binning/operator/ProductNameMetaAggregator.java
@@ -1,15 +1,30 @@
 package org.esa.beam.binning.operator;
 
+import org.esa.beam.framework.datamodel.MetadataAttribute;
 import org.esa.beam.framework.datamodel.MetadataElement;
 import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.ProductData;
 
 class ProductNameMetaAggregator {
 
+    private final MetadataElement source_products;
+    private int aggregatedCount;
+
+    ProductNameMetaAggregator() {
+        source_products = new MetadataElement("source_products");
+        aggregatedCount = 0;
+    }
+
     MetadataElement getMetadata() {
-        return new MetadataElement("source_products");
+        return source_products;
     }
 
     void aggregateMetadata(Product product) {
+        final MetadataElement productElement = new MetadataElement("source_product." + Integer.toString(aggregatedCount));
+        final MetadataAttribute nameAttribute = new MetadataAttribute("name", new ProductData.ASCII(product.getName()), true);
+        productElement.addAttribute(nameAttribute);
 
+        source_products.addElementAt(productElement, aggregatedCount);
+        ++aggregatedCount;
     }
 }
--- beam-binning/src/test/java/org/esa/beam/binning/operator/ProductNameMetaAggregatorTest.java
+++ beam-binning/src/test/java/org/esa/beam/binning/operator/ProductNameMetaAggregatorTest.java
@@ -1,5 +1,6 @@
 package org.esa.beam.binning.operator;
 
+import org.esa.beam.framework.datamodel.MetadataAttribute;
 import org.esa.beam.framework.datamodel.MetadataElement;
 import org.esa.beam.framework.datamodel.Product;
 import org.junit.Before;
@@ -20,17 +21,58 @@ public class ProductNameMetaAggregatorTest {
     @Test
     public void testAggregateNoProducts() {
         final MetadataElement metadataElement = aggregator.getMetadata();
-        assertNotNull(metadataElement);
-        assertEquals("source_products", metadataElement.getName());
-        assertEquals(0, metadataElement.getNumAttributes());
+        assertCorrectNameAndNoAttributes(metadataElement);
         assertEquals(0, metadataElement.getNumElements());
     }
 
     @Test
     public void testAggregateOneProduct() {
-        final Product product = new Product("product_1", "test_type", 2, 2);
+        final Product product = createProduct(1);
 
         aggregator.aggregateMetadata(product);
-        // @todo 1 tb/tb continue here 2014-06-25
+
+        final MetadataElement metadataElement = aggregator.getMetadata();
+        assertCorrectNameAndNoAttributes(metadataElement);
+
+        assertEquals(1, metadataElement.getNumElements());
+        assertProductElementAt(metadataElement, 0);
+    }
+
+    @Test
+    public void testAggregateThreeProducts() {
+        Product product = createProduct(1);
+        aggregator.aggregateMetadata(product);
+
+        product = createProduct(2);
+        aggregator.aggregateMetadata(product);
+
+        product = createProduct(3);
+        aggregator.aggregateMetadata(product);
+
+        final MetadataElement metadataElement = aggregator.getMetadata();
+        assertCorrectNameAndNoAttributes(metadataElement);
+
+        assertEquals(3, metadataElement.getNumElements());
+        assertProductElementAt(metadataElement, 0);
+        assertProductElementAt(metadataElement, 1);
+        assertProductElementAt(metadataElement, 2);
+    }
+
+    private void assertProductElementAt(MetadataElement metadataElement, int index) {
+        final MetadataElement productElement = metadataElement.getElementAt(index);
+        assertEquals("source_product." + Integer.toString(index), productElement.getName());
+        final MetadataAttribute nameAttribute = productElement.getAttribute("name");
+        assertNotNull(nameAttribute);
+        assertEquals("product_" + Integer.toString(index + 1), nameAttribute.getData().getElemString());
+    }
+
+    private Product createProduct(int number) {
+        return new Product("product_" + Integer.toString(number), "test_type", 2, 2);
+    }
+
+    private void assertCorrectNameAndNoAttributes(MetadataElement metadataElement) {
+        assertNotNull(metadataElement);
+        assertEquals("source_products", metadataElement.getName());
+        assertEquals(0, metadataElement.getNumAttributes());
     }
 }
