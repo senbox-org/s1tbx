bcd45bbd32b121e1dc34c290b34acf96efde9b7d
TomBlock <tom.block@brockmann-consult.de>
2014-06-26 16:47:36 +0200

    implemented and attached firstHistory aggregator

--- beam-binning/src/main/java/org/esa/beam/binning/operator/metadata/MetadataAggregatorFactory.java
+++ beam-binning/src/main/java/org/esa/beam/binning/operator/metadata/MetadataAggregatorFactory.java
@@ -4,10 +4,13 @@ package org.esa.beam.binning.operator.metadata;
 public class MetadataAggregatorFactory {
 
     private static final String NAME = "NAME";
+    private static final String FIRST_HISTORY = "FIRST_HISTORY";
 
     public static MetadataAggregator create(String name) {
         if (NAME.equalsIgnoreCase(name)) {
             return new ProductNameMetaAggregator();
+        } else if (FIRST_HISTORY.equalsIgnoreCase(name)) {
+            return new FirstHistoryMetaAggregator();
         }
 
         throw new IllegalArgumentException("Unknown metadata aggregator name: " + name);
