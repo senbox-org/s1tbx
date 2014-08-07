c5b68ef4985726259bdcfa1988b4d9d637e04641
Luis Veci <lveci@array.ca>
2014-08-07 17:23:03 -0400

    rename nest-src

--- s1tbx-config/dist/bitrock/package.bat
+++ s1tbx-config/dist/bitrock/package.bat
@@ -1,22 +1,22 @@
 
-"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer64.xml windows
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\s1tbx-config\dist\bitrock\bitrock_installer64.xml windows
 
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update64.xml windows
-
-
-"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml windows
-
-"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml linux
-
-"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml linux-x64
-
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml osx
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1s1tbx-config-src\dist\bitrock\bitrock_update64.xml windows
 
 
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml windows
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBs1tbx-confignest-src\dist\bitrock\bitrock_installer.xml windows
 
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml linux
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\s1tbx-configtbx\nest-src\dist\bitrock\bitrock_installer.xml linux
 
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml linux-x64
+"C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\s1tbx-configX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml linux-x64
 
-rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml osx
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build E:\Bus1tbx-configS1TBX\s1tbx\nest-src\dist\bitrock\bitrock_installer.xml osx
+
+
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" build Es1tbx-config6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml windows
+
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe" buis1tbx-configild\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml linux
+
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.exe"s1tbx-config:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml linux-x64
+
+rem "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.5.0\bin\builder-cli.s1tbx-configld E:\Build\6.0\S1TBX\s1tbx\nest-src\dist\bitrock\bitrock_update.xml osx
