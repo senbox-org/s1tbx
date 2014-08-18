a8de11fa648631931da67ef602d64ddb487aa5a2
Luis Veci <lveci@array.ca>
2014-08-18 11:22:49 -0400

    add test for ArcBinGridReader and fix DbfReadingTest

---
+++ beam-arcbingrid-reader/src/test/java/org/esa/beam/dataio/arcbin/ArcBinGridTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2014 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.beam.dataio.arcbin;
+
+import org.esa.beam.framework.dataio.DecodeQualification;
+import org.esa.beam.framework.dataio.ProductIO;
+import org.esa.beam.framework.dataio.ProductReader;
+import org.esa.beam.framework.datamodel.Product;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Test ArcBinGrid reader
+ */
+public class ArcBinGridTest {
+
+
+    @Test
+    public void testProductIO() throws URISyntaxException, IOException {
+        File file = new File(getClass().getResource("elevation/hdr.adf").toURI());
+        Product product = ProductIO.readProduct(file);
+        assertNotNull(product);
+
+        assert(product.getProductReader() instanceof ArcBinGridReader);
+    }
+
+    @Test
+    public void testReader() throws URISyntaxException, IOException {
+        File file = new File(getClass().getResource("elevation/hdr.adf").toURI());
+        ArcBinGridReaderPlugIn plugin = new ArcBinGridReaderPlugIn();
+        assert(plugin.getDecodeQualification(file) == DecodeQualification.INTENDED);
+
+        ProductReader reader = plugin.createReaderInstance();
+        assertNotNull(reader);
+
+        reader.readProductNodes(file, null);
+    }
+}
