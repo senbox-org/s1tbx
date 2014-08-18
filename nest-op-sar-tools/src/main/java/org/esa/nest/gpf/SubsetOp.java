b7bf1771a58aa2eaf489556056b04a8d14f452a3
Luis Veci <lveci@array.ca>
2014-08-18 16:35:06 -0400

    use own pixelregionfinder

--- nest-op-sar-tools/src/main/java/org/esa/nest/gpf/SubsetOp.java
+++ nest-op-sar-tools/src/main/java/org/esa/nest/gpf/SubsetOp.java
@@ -16,14 +16,12 @@
 package org.esa.nest.gpf;
 
 import com.bc.ceres.core.ProgressMonitor;
-import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.simplify.DouglasPeuckerSimplifier;
 import org.esa.beam.framework.dataio.ProductReader;
 import org.esa.beam.framework.dataio.ProductSubsetBuilder;
 import org.esa.beam.framework.dataio.ProductSubsetDef;
-import org.esa.beam.framework.datamodel.Band;
-import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.datamodel.ProductData;
-import org.esa.beam.framework.datamodel.VirtualBand;
+import org.esa.beam.framework.datamodel.*;
 import org.esa.beam.framework.gpf.Operator;
 import org.esa.beam.framework.gpf.OperatorException;
 import org.esa.beam.framework.gpf.OperatorSpi;
@@ -32,12 +30,21 @@ import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.Parameter;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
 import org.esa.beam.framework.gpf.annotations.TargetProduct;
+import org.esa.beam.util.ProductUtils;
 import org.esa.beam.util.converters.JtsGeometryConverter;
 import org.esa.snap.gpf.OperatorUtils;
 
 import java.awt.*;
+import java.awt.Polygon;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.lang.Math.*;
+import static java.lang.Math.ceil;
 
 @OperatorMetadata(alias = "SpatialSubset",
         category = "Utilities",
@@ -132,13 +139,12 @@ public class SubsetOp extends Operator {
     }
 
     public static Rectangle computePixelRegion(Product product, Geometry geoRegion, int numBorderPixels) {
-        final Geometry productGeometry = org.esa.beam.gpf.operators.standard.SubsetOp.computeProductGeometry(product);
+        final Geometry productGeometry = computeProductGeometry(product);
         final Geometry regionIntersection = geoRegion.intersection(productGeometry);
         if (regionIntersection.isEmpty()) {
             return new Rectangle();
         }
-        final org.esa.beam.gpf.operators.standard.SubsetOp.PixelRegionFinder pixelRegionFinder =
-                new org.esa.beam.gpf.operators.standard.SubsetOp.PixelRegionFinder(product.getGeoCoding());
+        final PixelRegionFinder pixelRegionFinder = new PixelRegionFinder(product.getGeoCoding());
         regionIntersection.apply(pixelRegionFinder);
         final Rectangle pixelRegion = pixelRegionFinder.getPixelRegion();
         pixelRegion.grow(numBorderPixels, numBorderPixels);
@@ -166,6 +172,76 @@ public class SubsetOp extends Operator {
             OperatorUtils.catchOperatorException(getId(), e);
         } finally {
             pm.done();
+        }
+    }
+
+    static Geometry computeProductGeometry(Product product) {
+        final GeneralPath[] paths = ProductUtils.createGeoBoundaryPaths(product);
+        final com.vividsolutions.jts.geom.Polygon[] polygons = new com.vividsolutions.jts.geom.Polygon[paths.length];
+        final GeometryFactory factory = new GeometryFactory();
+        for (int i = 0; i < paths.length; i++) {
+            polygons[i] = convertAwtPathToJtsPolygon(paths[i], factory);
+        }
+        final DouglasPeuckerSimplifier peuckerSimplifier = new DouglasPeuckerSimplifier(
+                polygons.length == 1 ? polygons[0] : factory.createMultiPolygon(polygons));
+        return peuckerSimplifier.getResultGeometry();
+    }
+
+    private static com.vividsolutions.jts.geom.Polygon convertAwtPathToJtsPolygon(Path2D path, GeometryFactory factory) {
+        final PathIterator pathIterator = path.getPathIterator(null);
+        ArrayList<double[]> coordList = new ArrayList<double[]>();
+        int lastOpenIndex = 0;
+        while (!pathIterator.isDone()) {
+            final double[] coords = new double[6];
+            final int segType = pathIterator.currentSegment(coords);
+            if (segType == PathIterator.SEG_CLOSE) {
+                // we should only detect a single SEG_CLOSE
+                coordList.add(coordList.get(lastOpenIndex));
+                lastOpenIndex = coordList.size();
+            } else {
+                coordList.add(coords);
+            }
+            pathIterator.next();
+        }
+        final Coordinate[] coordinates = new Coordinate[coordList.size()];
+        for (int i1 = 0; i1 < coordinates.length; i1++) {
+            final double[] coord = coordList.get(i1);
+            coordinates[i1] = new Coordinate(coord[0], coord[1]);
+        }
+
+        return factory.createPolygon(factory.createLinearRing(coordinates), null);
+    }
+
+    private static class PixelRegionFinder implements CoordinateFilter {
+
+        private final GeoCoding geoCoding;
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+
+        private PixelRegionFinder(GeoCoding geoCoding) {
+            this.geoCoding = geoCoding;
+            x1 = Integer.MAX_VALUE;
+            x2 = Integer.MIN_VALUE;
+            y1 = Integer.MAX_VALUE;
+            y2 = Integer.MIN_VALUE;
+        }
+
+        @Override
+        public void filter(Coordinate coordinate) {
+            final GeoPos geoPos = new GeoPos((float) coordinate.y, (float) coordinate.x);
+            final PixelPos pixelPos = geoCoding.getPixelPos(geoPos, null);
+            if (pixelPos.isValid()) {
+                x1 = min(x1, (int) floor(pixelPos.x));
+                x2 = max(x2, (int) ceil(pixelPos.x));
+                y1 = min(y1, (int) floor(pixelPos.y));
+                y2 = max(y2, (int) ceil(pixelPos.y));
+            }
+        }
+
+        public Rectangle getPixelRegion() {
+            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
         }
     }
 
