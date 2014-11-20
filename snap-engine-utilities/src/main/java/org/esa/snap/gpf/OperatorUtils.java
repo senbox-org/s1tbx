b7f3c502586fa184faa6a0e48d17004588a8544e
Luis Veci <lveci@array.ca>
2014-11-20 14:29:55 -0500

    move check for polarization to input validator

--- snap-engine-utilities/src/main/java/org/esa/snap/gpf/OperatorUtils.java
+++ snap-engine-utilities/src/main/java/org/esa/snap/gpf/OperatorUtils.java
@@ -93,10 +93,11 @@ public final class OperatorUtils {
         final String pol = getPolarizationFromBandName(bandName);
         if (pol != null) {
             return pol;
-        } else {
+        } else if(absRoot != null) {
             final String[] mdsPolar = getProductPolarization(absRoot);
             return mdsPolar[0];
         }
+        return "";
     }
 
     public static String getPolarizationFromBandName(final String bandName) {
@@ -212,16 +213,10 @@ public final class OperatorUtils {
         return false;
     }
 
-    public static boolean isQuadPol(final Product product) {
+    public static boolean isCalibrated(final Product product) {
         final MetadataElement absRoot = AbstractMetadata.getAbstractedMetadata(product);
-        if (absRoot != null) {
-            if (!AbstractMetadata.isNoData(absRoot, AbstractMetadata.mds1_tx_rx_polar) &&
-                    !AbstractMetadata.isNoData(absRoot, AbstractMetadata.mds2_tx_rx_polar) &&
-                    !AbstractMetadata.isNoData(absRoot, AbstractMetadata.mds2_tx_rx_polar) &&
-                    !AbstractMetadata.isNoData(absRoot, AbstractMetadata.mds4_tx_rx_polar))
-                return true;
-        }
-        return false;
+        return (absRoot != null &&
+                absRoot.getAttribute(AbstractMetadata.abs_calibration_flag).getData().getElemBoolean());
     }
 
     /**
