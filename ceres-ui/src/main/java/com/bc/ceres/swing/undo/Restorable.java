249029dde1805fa21b35d53ec5ffa1c957b95e3e
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-11-27 17:49:22 +0100

    Removed outdated TODOs

--- ceres-ui/src/main/java/com/bc/ceres/swing/undo/Restorable.java
+++ ceres-ui/src/main/java/com/bc/ceres/swing/undo/Restorable.java
@@ -16,9 +16,6 @@
 
 package com.bc.ceres.swing.undo;
 
-// todo - why is AbstractUndoableEdit a Serializable?
-// todo - make Restorable a Serializable for the same reason?
-// todo - make the memento a Serializable for the same reason?
 
 /**
  * An object implementing this interface can create a memento by which it's
