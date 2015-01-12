b037d2afef51765bd320bcf1bc2a55edf9831045
marpet <marco.peters@brockmann-consult.de>
2015-01-12 17:12:06 +0100

    new icons for product explorer

--- beam-opendap/src/main/java/org/esa/beam/opendap/ShowOpendapClientAction.java
+++ beam-opendap/src/main/java/org/esa/beam/opendap/ShowOpendapClientAction.java
@@ -1,11 +1,14 @@
 package org.esa.beam.opendap;
 
+import org.esa.beam.framework.gpf.ui.DefaultAppContext;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.opendap.ui.OpendapAccessPanel;
 import org.esa.beam.visat.actions.AbstractVisatAction;
 
+import javax.swing.Action;
 import javax.swing.JDialog;
 import java.awt.Dimension;
+import java.util.Map;
 
 public class ShowOpendapClientAction extends AbstractVisatAction {
 
@@ -19,5 +22,16 @@ public class ShowOpendapClientAction extends AbstractVisatAction {
         final Dimension size = jDialog.getSize();
         jDialog.setPreferredSize(size);
         jDialog.setVisible(true);
+    }
+
+    void static create(Map<String, ?> params) {
+        final OpendapAccessPanel opendapAccessPanel = new OpendapAccessPanel(????, params.get("helpId"));
+        final JDialog jDialog = new JDialog(getAppContext().getApplicationWindow(), "OPeNDAP Access");
+        jDialog.setContentPane(opendapAccessPanel);
+        jDialog.pack();
+        final Dimension size = jDialog.getSize();
+        jDialog.setPreferredSize(size);
+        jDialog.setVisible(true);
+
     }
 }
