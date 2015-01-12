49b85005802435e85a855c768e4e01ac1daa4b21
Luis Veci <lveci@array.ca>
2015-01-12 16:05:10 -0500

    generify command line scripts

--- s1tbx-config/main/bin/win64/gpt.bat
+++ s1tbx-config/main/bin/win64/gpt.bat
@@ -3,15 +3,15 @@
 set S1TBX_HOME=${installer:sys.installationDir}
 
 "%S1TBX_HOME%\jre\bin\java.exe" ^
-    -Xmx${installer:maxHeapSize} ^
+    -Xms512M -Xmx${installer:maxHeapSize} ^
     -Xverify:none -XX:+AggressiveOpts -XX:+UseFastAccessorMethods ^
     -XX:+UseParallelGC -XX:+UseNUMA -XX:+UseLoopPredicate ^
-    -Dceres.context=s1tbx ^
-    "-Ds1tbx.mainClass=org.esa.beam.framework.gpf.main.GPT" ^
-    "-Ds1tbx.home=%S1TBX_HOME%" ^
-    "-Ds1tbx.debug=false" ^
-	"-Ds1tbx.consoleLog=false" ^
-	"-Ds1tbx.logLevel=WARNING" ^
+    -Dceres.context=${installer:context} ^
+    "-D${installer:context}.mainClass=org.esa.beam.framework.gpf.main.GPT" ^
+    "-D${installer:context}.home=%S1TBX_HOME%" ^
+    "-D${installer:context}.debug=false" ^
+	"-D${installer:context}.consoleLog=false" ^
+	"-D${installer:context}.logLevel=WARNING" ^
     "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%S1TBX_HOME%\jhdf.dll" ^
     "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%S1TBX_HOME%\jhdf5.dll" ^
     -jar "%S1TBX_HOME%\bin\snap-launcher.jar" %*
