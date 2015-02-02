e41b9798e2669a382c0fe2c5d9829bd2b75ba0ee
lveci <lveci@array.ca>
2015-02-02 10:13:38 -0500

    fix scripts

--- s1tbx-config/main/bin/linux/dat.sh
+++ s1tbx-config/main/bin/linux/dat.sh
@@ -1,18 +1,16 @@
 #! /bin/sh
 
-set S1TBX_HOME=${installer:sys.installationDir}
-
-$S1TBX_HOME/jre/bin/java \
+${installer:sys.installationDir}/jre/bin/java \
     -Xms512M -Xmx${installer:maxHeapSize} \
     -Xverify:none -XX:+AggressiveOpts -XX:+UseFastAccessorMethods \
     -XX:+UseParallelGC -XX:+UseNUMA -XX:+UseLoopPredicate \
     -Dceres.context=${installer:context} \
-    "-D${installer:context}.home=$S1TBX_HOME" \
+    "-D${installer:context}.home=${installer:sys.installationDir}" \
     "-D${installer:context}.debug=false" \
-    "-Djava.library.path=$PATH:$S1TBX_HOME" \
-	"-Dncsa.hdf.hdflib.HDFLibrary.hdflib=$S1TBX_HOME/libjhdf.so" \
-    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=$S1TBX_HOME/libjhdf5.so" \
-    -jar "$S1TBX_HOME/bin/snap-launcher.jar" -d "$@"
+    "-Djava.library.path=$PATH:${installer:sys.installationDir}" \
+	"-Dncsa.hdf.hdflib.HDFLibrary.hdflib=${installer:sys.installationDir}/libjhdf.so" \
+    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=${installer:sys.installationDir}/libjhdf5.so" \
+    -jar "${installer:sys.installationDir}/bin/snap-launcher.jar" -d "$@"
 
 exit $?
 
--- s1tbx-config/main/bin/macosx/gpt.command
+++ s1tbx-config/main/bin/macosx/gpt.command
@@ -1,15 +1,13 @@
 #! /bin/sh
 
-set S1TBX_HOME=${installer:sys.installationDir}
-
-$S1TBX_HOME/jre/bin/java \
+${installer:sys.installationDir}/jre/bin/java \
 	-Xmx${installer:maxHeapSize} \
 	-Dceres.context=${installer:context} \
 	"-D${installer:context}.mainClass-org.esa.beam.framework.gpf.main.Main" \
-	-D${installer:context}.home="$S1TBX_HOME" \
-	-Djava.library.path="$PATH:$S1TBX_HOME" \
-	"-Dncsa.hdf.hdflib.HDFLibrary.hdflib=$S1TBX_HOME/modules/lib-hdf-2.3/lib/macosx/libjhdf.so" \
-	"-Dncsa.hdf.hdf5lib.H5.hdf5lib=$S1TBX_HOME/modules/lib-hdf-2.3/lib/macosx/libjhdf5.so" \
-	-jar "$S1TBX_HOME/bin/snap-launcher.jar" -d "$@" 
+	-D${installer:context}.home="${installer:sys.installationDir}" \
+	-Djava.library.path="$PATH:${installer:sys.installationDir}" \
+	"-Dncsa.hdf.hdflib.HDFLibrary.hdflib=${installer:sys.installationDir}/modules/lib-hdf-2.3/lib/macosx/libjhdf.so" \
+	"-Dncsa.hdf.hdf5lib.H5.hdf5lib=${installer:sys.installationDir}/modules/lib-hdf-2.3/lib/macosx/libjhdf5.so" \
+	-jar "${installer:sys.installationDir}/bin/snap-launcher.jar" -d "$@" 
 
 exit $?
