0804d42d8103df552c9e106f664f41c478a3af39
lveci <lveci@array.ca>
2015-02-07 21:46:46 -0500

    pow

--- s1tbx-io/src/main/java/org/esa/nest/dataio/ceos/alos/AlosPalsarProductDirectory.java
+++ s1tbx-io/src/main/java/org/esa/nest/dataio/ceos/alos/AlosPalsarProductDirectory.java
@@ -360,10 +360,10 @@ class AlosPalsarProductDirectory extends CEOSProductDirectory {
             for (int j = 0; j < gridHeight; j++) {
                 for (int i = 0; i < gridWidth; i++) {
                     angles[k] = (float)((a0 + a1 * rangeDist[k] / 1000.0 +
-                            a2 * FastMath.pow(rangeDist[k] / 1000.0, 2.0) +
-                            a3 * FastMath.pow(rangeDist[k] / 1000.0, 3.0) +
-                            a4 * FastMath.pow(rangeDist[k] / 1000.0, 4.0) +
-                            a5 * FastMath.pow(rangeDist[k] / 1000.0, 5.0)) * Constants.RTOD);
+                            a2 * Math.pow(rangeDist[k] / 1000.0, 2.0) +
+                            a3 * Math.pow(rangeDist[k] / 1000.0, 3.0) +
+                            a4 * Math.pow(rangeDist[k] / 1000.0, 4.0) +
+                            a5 * Math.pow(rangeDist[k] / 1000.0, 5.0)) * Constants.RTOD);
                     k++;
                 }
             }
