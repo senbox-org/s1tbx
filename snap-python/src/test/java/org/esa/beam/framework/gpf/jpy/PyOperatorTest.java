64af920aa12ec9f539df2be8727cc59c180295d8
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-02-20 17:32:47 +0100

    SNAP-79  added test

--- snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorTest.java
+++ snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorTest.java
@@ -2,18 +2,37 @@ package org.esa.beam.framework.gpf.jpy;
 
 
 import org.esa.beam.framework.gpf.main.GPT;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * @author Norman Fomferra
  */
 public class PyOperatorTest {
+    private final static String MODULE_PATH = "C:\\Users\\Norman\\JavaProjects\\senbox\\snap-engine\\snap-python\\target\\classes\\beampy-examples";
+
+    static {
+        System.setProperty("snap.snappy.ext", MODULE_PATH);
+    }
+
+
+
+    @Ignore
     public static void main(String[] args) throws Exception {
-        GPT.main("PyOp",
+        GPT.main("py_ndvi_op",
                  "-q", "4",
                  "-e",
-                 "-PpythonModulePath=C:\\Users\\Norman\\JavaProjects\\beam\\beam\\beam-python\\src\\main\\resources\\examples",
-                 "-PpythonModuleName=ndvi_op",
-                 "-PpythonClassName=MerisNdviTileComputer",
-                 "C:\\Users\\Norman\\JavaProjects\\jpy\\beampy\\MER_RR__1P.N1");
+                 "-PlowerName=radiance_13",
+                 "-PupperName=radiance_7",
+                 "-Ssource=C:\\Users\\Norman\\EOData\\MER_FRS_1PNMAP20070709_111419_000001722059_00395_28004_0001.N1");
+    }
+
+    @Test
+    public void testOp() throws Exception {
+        PyOperator operator = new PyOperator();
+        operator.setPythonModulePath(MODULE_PATH);
+        operator.setPythonModuleName("ndvi_op");
+        operator.setPythonClassName("NdviOp");
+
     }
 }
