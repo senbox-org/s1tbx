241cc32f3742da3831428977e14b3e2943e5a5dc
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-02-27 14:52:23 +0100

    SNAP-79  Fixed tests

--- snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorSpiTest.java
+++ snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorSpiTest.java
@@ -4,7 +4,12 @@ package org.esa.beam.framework.gpf.jpy;
 import org.esa.beam.framework.gpf.GPF;
 import org.esa.beam.framework.gpf.OperatorSpi;
 import org.esa.beam.framework.gpf.OperatorSpiRegistry;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -13,8 +18,14 @@ import static org.junit.Assert.assertNotNull;
  */
 public class PyOperatorSpiTest {
 
-    static {
-        System.setProperty("snap.snappy.ext", "C:\\Users\\Norman\\JavaProjects\\senbox\\snap-engine\\snap-python\\target\\classes\\beampy-examples");
+    @BeforeClass
+    public static void init() {
+        URL resource = PyOperatorSpi.class.getResource("/beampy-examples");
+        assertNotNull(resource);
+        File file = new File(URI.create(resource.toString()));
+        assertNotNull(file.isDirectory());
+        System.setProperty("snap.snappy.ext", file.getPath());
+        System.out.println("snap.snappy.ext = " + System.getProperty("snap.snappy.ext"));
     }
 
     @Test
--- snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorTest.java
+++ snap-python/src/test/java/org/esa/beam/framework/gpf/jpy/PyOperatorTest.java
@@ -1,21 +1,26 @@
 package org.esa.beam.framework.gpf.jpy;
 
 
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.gpf.descriptor.DefaultOperatorDescriptor;
 import org.esa.beam.framework.gpf.main.GPT;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Norman Fomferra
  */
 public class PyOperatorTest {
-    private final static String MODULE_PATH = "C:\\Users\\Norman\\JavaProjects\\senbox\\snap-engine\\snap-python\\target\\classes\\beampy-examples";
 
-    static {
-        System.setProperty("snap.snappy.ext", MODULE_PATH);
+    @BeforeClass
+    public static void init() {
+        PyOperatorSpiTest.init();
     }
-
-
 
     @Ignore
     public static void main(String[] args) throws Exception {
@@ -29,10 +34,25 @@ public class PyOperatorTest {
 
     @Test
     public void testOp() throws Exception {
+        Product source = new Product("X", "Y", 100, 100);
+        source.addBand("radiance_7", "80.0");
+        source.addBand("radiance_13", "120.0");
+
+        DefaultOperatorDescriptor descriptor = DefaultOperatorDescriptor.fromXml(PyOperatorSpi.class.getResource("/beampy-examples/beampy-ndvi-operator/ndvi_op-info.xml"), getClass().getClassLoader());
+        PyOperatorSpi spi = new PyOperatorSpi(descriptor);
+
         PyOperator operator = new PyOperator();
-        operator.setPythonModulePath(MODULE_PATH);
+        operator.setSpi(spi);
+        operator.setPythonModulePath(new File(System.getProperty("snap.snappy.ext"), "beampy-ndvi-operator").getPath());
         operator.setPythonModuleName("ndvi_op");
         operator.setPythonClassName("NdviOp");
+        operator.setParameter("lowerName", "radiance_13");
+        operator.setParameter("upperName", "radiance_7");
+        operator.setSourceProduct("source", source);
+        Product target = operator.getTargetProduct();
 
+        assertNotNull(target);
+        assertNotNull(target.getBand("ndvi"));
+        assertNotNull(target.getBand("ndvi_flags"));
     }
 }
