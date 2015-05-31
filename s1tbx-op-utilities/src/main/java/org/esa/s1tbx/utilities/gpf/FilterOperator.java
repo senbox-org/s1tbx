cb12ceb631e0362c57fb7cf169b07602b9213c55
lveci <lveci@array.ca>
2015-05-31 04:19:19 -0400

    rename utilities category to tools

--- s1tbx-op-utilities/src/main/java/org/esa/s1tbx/utilities/gpf/FilterOperator.java
+++ s1tbx-op-utilities/src/main/java/org/esa/s1tbx/utilities/gpf/FilterOperator.java
@@ -39,7 +39,7 @@ import java.util.Map;
  * that can compute bands independently of each other.
  */
 @OperatorMetadata(alias = "Image-Filter",
-        category = "Utilities",
+        category = "Tools",
         authors = "Jun Lu, Luis Veci",
         copyright = "Copyright (C) 2015 by Array Systems Computing Inc.",
         description = "Common Image Processing Filters")
