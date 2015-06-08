2ccdbc8594f8f786d9155c750196b73f4fa8e3f4
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-06-08 13:19:45 +0200

    Fixed resource loading

--- snap-core/src/test/java/org/esa/snap/util/ResourceInstallerTest.java
+++ snap-core/src/test/java/org/esa/snap/util/ResourceInstallerTest.java
@@ -6,12 +6,23 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Marco Peters
@@ -35,7 +46,7 @@ public class ResourceInstallerTest {
         targetDir.getFileSystem().close();
     }
 
-   @Test
+    @Test
     public void testInstall() throws Exception {
         ResourceInstaller resourceInstaller = new ResourceInstaller(sourceDir.resolve("org/esa/snap/dataio/dimap"), targetDir);
         resourceInstaller.install(".*xml", ProgressMonitor.NULL);
@@ -44,12 +55,61 @@ public class ResourceInstallerTest {
         assertEquals(4, targetFileList.toArray().length);
     }
 
-   @Test
+    @Test
     public void testInstall_withGlob() throws Exception {
         ResourceInstaller resourceInstaller = new ResourceInstaller(sourceDir, targetDir);
         resourceInstaller.install("glob:**/*xml", ProgressMonitor.NULL);
         assertEquals(1, Files.list(targetDir).toArray().length);
         Stream<Path> targetFileList = Files.list(targetDir.resolve("org/esa/snap/dataio/dimap/spi"));
         assertEquals(4, targetFileList.toArray().length);
-   }
+    }
+
+    /*
+    @Test
+    public void testResourcesFromDir() throws Exception {
+        URL resource = ResourceInstallerTest.class.getResource("/resource-testdata");
+        assertNotNull(resource);
+        testResourcesFromClassLoader(new URLClassLoader(new URL[]{resource}));
+    }
+
+    @Test
+    public void testResourcesFromJar() throws Exception {
+        URL resource = ResourceInstallerTest.class.getResource("/resource-testdata.jar");
+        assertNotNull(resource);
+        testResourcesFromClassLoader(new URLClassLoader(new URL[]{resource}));
+    }
+
+    private void testResourcesFromClassLoader(ClassLoader cl) throws URISyntaxException, IOException {
+        Enumeration<URL> auxdata = cl.getResources("/auxdata");
+        while (auxdata.hasMoreElements()) {
+            URL url = auxdata.nextElement();
+            System.out.println("url = " + url);
+        }
+
+        URL auxdataDirUrl = cl.getResource("auxdata");
+        assertNotNull(auxdataDirUrl);
+        Path auxdataDir = Paths.get(auxdataDirUrl.toURI());
+        assertTrue(Files.isDirectory(auxdataDir));
+
+        List<Path> auxdataDirList = Files.list(auxdataDir).collect(Collectors.toList());
+        Collections.sort(auxdataDirList);
+        assertEquals(2, auxdataDirList.size());
+        assertEquals("file-1.txt", auxdataDirList.get(0).getFileName().toString());
+        assertEquals("file-2.txt", auxdataDirList.get(1).getFileName().toString());
+        assertEquals("file-3.txt", auxdataDirList.get(2).getFileName().toString());
+        assertEquals("subdir", auxdataDirList.get(3).getFileName().toString());
+
+        URL auxdataSubdirUrl = cl.getResource("/auxdata/subdir");
+        assertNotNull(auxdataSubdirUrl);
+        Path auxdataSubdir = Paths.get(auxdataSubdirUrl.toURI());
+        assertTrue(Files.isDirectory(auxdataSubdir));
+
+        List<Path> auxdataSubdirList = Files.list(auxdataSubdir).collect(Collectors.toList());
+        Collections.sort(auxdataSubdirList);
+        assertEquals(3, auxdataSubdirList.size());
+        assertEquals("file-A.txt", auxdataSubdirList.get(0).getFileName().toString());
+        assertEquals("file-B.txt", auxdataSubdirList.get(1).getFileName().toString());
+        assertEquals("file-C.txt", auxdataSubdirList.get(2).getFileName().toString());
+    }
+*/
 }
