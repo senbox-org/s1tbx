f365b4ab21e647c2875737a1586bedea59dea5bc
lveci <lveci@array.ca>
2015-06-16 22:34:06 -0400

    clean up s1tbx-config

--- s1tbx-config/main/bin/win/cli.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-IF ["%S1TBX_HOME%"]==[] echo "S1TBX_HOME is not defined. Please set S1TBX_HOME=installation_folder"
-
-IF [%S1TBX_HOME:~-1%]==[/] set S1TBX_HOME=%S1TBX_HOME:~0,-1%
-IF [%S1TBX_HOME:~-1%]==[\] set S1TBX_HOME=%S1TBX_HOME:~0,-1%
-
-%comspec% /k gpt.bat -h
-
