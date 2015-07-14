5e30ba06c88af05b0c33e3e51a27cf672784a442
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-14 15:09:28 +0200

    Fixed SNAP-121, SNAP-122

--- s1tbx-kit/src/main/java/org/esa/s1tbx/about/S1tbxAboutBox.java
+++ s1tbx-kit/src/main/java/org/esa/s1tbx/about/S1tbxAboutBox.java
@@ -5,16 +5,31 @@
  */
 package org.esa.s1tbx.about;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
 import org.esa.snap.rcp.about.AboutBox;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.Modules;
+
+import java.awt.BorderLayout;
 
 /**
  * @author Norman
  */
 @AboutBox(displayName = "S1TBX", position = 10)
-public class S1tbxAboutBox extends JLabel {
+public class S1tbxAboutBox extends JPanel {
     
     public S1tbxAboutBox() {
-        super("<html>This is the incredible <b>Sentinel-1 Toolbox</b>");
+        super(new BorderLayout(4, 4));
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+        ModuleInfo moduleInfo = Modules.getDefault().ownerOf(S1tbxAboutBox.class);
+        ImageIcon aboutImage = new ImageIcon(S1tbxAboutBox.class.getResource("about_s1tbx.jpg"));
+        JLabel iconLabel = new JLabel(aboutImage);
+        add(iconLabel, BorderLayout.CENTER);
+        add(new JLabel("<html><b>Sentinel-1 Toolbox (S1TBX) version " + moduleInfo.getImplementationVersion() + "</b>", SwingConstants.RIGHT), BorderLayout.SOUTH);
     }
 }
