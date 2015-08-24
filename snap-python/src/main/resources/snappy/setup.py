79df06dc11f68cc1a42b9d01ffd13678cbf9432b
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-08-24 13:30:50 +0200

    Fixed, so that it can be used to permanently install snappy

--- snap-python/src/main/resources/snappy/setup.py
+++ snap-python/src/main/resources/snappy/setup.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python3
-
 SNAPPY_NAME = 'snappy'
-SNAPPY_VERSION = '2.0-SNAPSHOT'
+SNAPPY_VERSION = '2.0.0'
 SNAPPY_SUMMARY = 'snappy - The SNAP Python API'
 SNAPPY_AUTHOR = 'SNAP Development Team, Brockmann Consult GmbH'
 
-# todo - fix this setup script so that the parent directory ('..' = 'snappy') including its
-# content is installed into Python
-
 from distutils.core import setup
-setup(name=SNAPPY_NAME,
-      version=SNAPPY_VERSION,
-      description=SNAPPY_SUMMARY,
-      author=SNAPPY_AUTHOR,
-      py_modules=['../' + SNAPPY_NAME])
+import os
+
+old_cwd = os.getcwd()
+new_cwd = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+os.chdir(new_cwd)
+
+try:
+    setup(
+        name=SNAPPY_NAME,
+        version=SNAPPY_VERSION,
+        description=SNAPPY_SUMMARY,
+        author=SNAPPY_AUTHOR,
+        packages=[SNAPPY_NAME],
+        package_data={SNAPPY_NAME: ['*.so', '*.pyd', '*.dll', '*.dll', '*.properties', '*.ini', '*.info']}
+    )
+finally:
+    os.chdir(old_cwd)
