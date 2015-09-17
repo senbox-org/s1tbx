5d8778f20810416674cadb00c61776f24865104b
lveci <lveci@array.ca>
2015-09-16 22:45:06 -0400

    RGBProfiles for dual and quad pol products

---
+++ s1tbx-commons/src/main/java/org/esa/s1tbx/commons/RGBProfiles.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.s1tbx.commons;
+
+import org.esa.snap.framework.datamodel.RGBImageProfile;
+import org.esa.snap.framework.datamodel.RGBImageProfileManager;
+
+/**
+ * RGB Profiles for dual pol and quad pol products
+ */
+public class RGBProfiles {
+
+    private enum TYPE {RATIO, MULTIPLE, DIFFERENCE}
+
+    static void registerRGBProfiles() {
+        final RGBImageProfileManager manager = RGBImageProfileManager.getInstance();
+        manager.addProfile(new RGBImageProfile("Pauli",
+                                               new String[]{
+                                                       "((i_HH-i_VV)*(i_HH-i_VV)+(q_HH-q_VV)*(q_HH-q_VV))/2",
+                                                       "((i_HV+i_VH)*(i_HV+i_VH)+(q_HV+q_VH)*(q_HV+q_VH))/2",
+                                                       "((i_HH+i_VV)*(i_HH+i_VV)+(q_HH+q_VV)*(q_HH+q_VV))/2"
+                                               }
+        ));
+        manager.addProfile(new RGBImageProfile("Sinclair",
+                                               new String[]{
+                                                       "i_VV*i_VV+q_VV*q_VV",
+                                                       "((i_HV+i_VH)*(i_HV+i_VH)+(q_HV+q_VH)*(q_HV+q_VH))/4",
+                                                       "i_HH*i_HH+q_HH*q_HH"
+                                               }
+        ));
+
+        addProfile(manager, TYPE.RATIO);
+        addProfile(manager, TYPE.MULTIPLE);
+        addProfile(manager, TYPE.DIFFERENCE);
+    }
+
+    private static void addProfile(final RGBImageProfileManager manager, final TYPE profileType) {
+        // Intensity
+        manager.addProfile(createDPProfile(profileType, "Intensity", "HH", "HV", ""));
+        manager.addProfile(createDPProfile(profileType, "Intensity", "VV", "VH", ""));
+        manager.addProfile(createDPProfile(profileType, "Intensity", "HH", "VV", ""));
+        // Intensity dB
+        manager.addProfile(createDPProfile(profileType, "Intensity", "HH", "HV", "_db"));
+        manager.addProfile(createDPProfile(profileType, "Intensity", "VV", "VH", "_db"));
+        manager.addProfile(createDPProfile(profileType, "Intensity", "HH", "VV", "_db"));
+
+        // Sigma0
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "HH", "HV", ""));
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "VV", "VH", ""));
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "HH", "VV", ""));
+        // Sigma0 dB
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "HH", "HV", "_db"));
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "VV", "VH", "_db"));
+        manager.addProfile(createDPProfile(profileType, "Sigma0", "HH", "VV", "_db"));
+    }
+
+    private static RGBImageProfile createDPProfile(final TYPE profileType,
+                                                   final String name, final String pol1, final String pol2,
+                                                   final String suffix) {
+        switch (profileType) {
+            case RATIO:
+                return new RGBImageProfile("Dual Pol Ratio " + name + suffix + " " + pol1 + "+" + pol2,
+                                           new String[]{
+                                                   name + "_" + pol1 + suffix,
+                                                   name + "_" + pol2 + suffix,
+                                                   name + "_" + pol1 + suffix + "/" + name + "_" + pol2 + suffix
+                                           }
+                );
+            case MULTIPLE:
+                return new RGBImageProfile("Dual Pol Multiple " + name + suffix + " " + pol1 + "+" + pol2,
+                                           new String[]{
+                                                   name + "_" + pol1 + suffix,
+                                                   name + "_" + pol2 + suffix,
+                                                   "abs(" + name + "_" + pol1 + suffix + "*" + name + "_" + pol2 + suffix + ")"
+                                           }
+                );
+            case DIFFERENCE:
+                return new RGBImageProfile("Dual Pol Difference " + name + suffix + " " + pol1 + "+" + pol2,
+                                           new String[]{
+                                                   name + "_" + pol2 + suffix,
+                                                   name + "_" + pol1 + suffix,
+                                                   "abs(" + name + "_" + pol1 + suffix + "-" + name + "_" + pol2 + suffix + ")"
+                                           }
+                );
+            default:
+                return null;
+        }
+    }
+}
---
+++ s1tbx-commons/src/main/java/org/esa/s1tbx/commons/S1TBXSetup.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.s1tbx.commons;
+
+import com.bc.ceres.core.ProgressMonitor;
+import org.esa.snap.util.ResourceInstaller;
+import org.esa.snap.util.SystemUtils;
+import org.esa.snap.util.io.TreeCopier;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Helper class to install resources
+ * Usually called within OnStartUp of a module
+ */
+public class S1TBXSetup {
+
+    static{
+        RGBProfiles.registerRGBProfiles();
+    }
+
+    public static void installColorPalettes(final Class callingClass, final String path) {
+        final Path moduleBasePath = ResourceInstaller.findModuleCodeBasePath(callingClass);
+        final Path auxdataDir = getColorPalettesDir();
+        Path sourcePath = moduleBasePath.resolve(path);
+        final ResourceInstaller resourceInstaller = new ResourceInstaller(sourcePath, auxdataDir);
+
+        try {
+            resourceInstaller.install(".*.cpd", ProgressMonitor.NULL);
+        } catch (IOException e) {
+            SystemUtils.LOG.severe("Unable to install colour palettes "+moduleBasePath+" to "+auxdataDir+" "+e.getMessage());
+        }
+    }
+
+    private static Path getColorPalettesDir() {
+        return SystemUtils.getAuxDataPath().resolve("color_palettes");
+    }
+
+    private static Path getGraphsDir() {
+        return SystemUtils.getApplicationDataDir().toPath().resolve("graphs");
+    }
+
+    public static void installGraphs(final Class callingClass, final String path) {
+        final Path moduleBasePath = ResourceInstaller.findModuleCodeBasePath(callingClass);
+        final Path srcGraphPath = moduleBasePath.resolve(path);
+        final Path dstGraphPath = getGraphsDir();
+        //final ResourceInstaller resourceInstaller = new ResourceInstaller(moduleBasePath, "org/esa/s1tbx/graphs/",
+        //                                                                  dstGraphPath);
+
+        try {
+            if (!Files.exists(dstGraphPath)) {
+                Files.createDirectories(dstGraphPath);
+            }
+            TreeCopier.copy(srcGraphPath, dstGraphPath);
+        } catch (IOException e) {
+            SystemUtils.LOG.severe("Unable to install graphs "+srcGraphPath+" to "+dstGraphPath+" "+e.getMessage());
+        }
+    }
+}
