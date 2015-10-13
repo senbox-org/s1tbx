170a25d81463ad8632cef363133dbb6ac2c0607c
marpet <marco.peters@brockmann-consult.de>
2015-10-13 07:58:10 +0200

    fixed and added test

--- snap-core/src/test/java/org/esa/snap/core/datamodel/ProductTimeCodingTest.java
+++ snap-core/src/test/java/org/esa/snap/core/datamodel/ProductTimeCodingTest.java
@@ -22,7 +22,7 @@ public class ProductTimeCodingTest {
     }
 
     @Test
-    public void testTimeCodingIsOptional() throws Exception {
+    public void testSceneTimeCodingIsOptional() throws Exception {
         Product product = new Product("A", "B", 1, 1);
         assertNull(product.getSceneTimeCoding());
 
@@ -32,8 +32,9 @@ public class ProductTimeCodingTest {
         product.setSceneTimeCoding(null);
         assertNull(product.getSceneTimeCoding());
     }
+
     @Test
-    public void testPropertyChangeEventOnTimeCodingChange() {
+    public void testPropertyChangeEventOnSceneTimeCodingChange() {
         final Product product = new Product("pn", "pt", 20, 30);
         final MyProductNodeListener nodeListener = new MyProductNodeListener();
         product.addProductNodeListener(nodeListener);
@@ -47,6 +48,30 @@ public class ProductTimeCodingTest {
         final ProductNodeEvent secondEvent = events.get(1);
         assertSame(product, firstEvent.getSourceNode());
         assertSame(product, secondEvent.getSourceNode());
+        assertEquals(Product.PROPERTY_NAME_SCENE_TIME_CODING, firstEvent.getPropertyName());
+        assertEquals(Product.PROPERTY_NAME_SCENE_TIME_CODING, secondEvent.getPropertyName());
+        assertNull(firstEvent.getOldValue());
+        assertSame(timeCoding, firstEvent.getNewValue());
+        assertSame(timeCoding, secondEvent.getOldValue());
+        assertNull(secondEvent.getNewValue());
+    }
+
+    @Test
+    public void testPropertyChangeEventOnTimeCodingChange() {
+        final Product product = new Product("pn", "pt", 20, 30);
+        final Band band = product.addBand("dummy", ProductData.TYPE_INT16);
+        final MyProductNodeListener nodeListener = new MyProductNodeListener();
+        product.addProductNodeListener(nodeListener);
+
+        band.setTimeCoding(timeCoding);
+        band.setTimeCoding(null);
+
+        final ArrayList<ProductNodeEvent> events = nodeListener.events;
+        assertEquals(2, events.size());
+        final ProductNodeEvent firstEvent = events.get(0);
+        final ProductNodeEvent secondEvent = events.get(1);
+        assertSame(band, firstEvent.getSourceNode());
+        assertSame(band, secondEvent.getSourceNode());
         assertEquals(RasterDataNode.PROPERTY_NAME_TIME_CODING, firstEvent.getPropertyName());
         assertEquals(RasterDataNode.PROPERTY_NAME_TIME_CODING, secondEvent.getPropertyName());
         assertNull(firstEvent.getOldValue());
