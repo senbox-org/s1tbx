8598dd01429cd7b4c08482543db08e869afc665e
Luis Veci <lveci@array.ca>
2015-11-12 15:45:52 -0500

    exclude RAW

--- s1tbx-op-calibration/src/test/java/org/esa/s1tbx/calibration/gpf/TestCalibrationOp.java
+++ s1tbx-op-calibration/src/test/java/org/esa/s1tbx/calibration/gpf/TestCalibrationOp.java
@@ -40,7 +40,7 @@ public class TestCalibrationOp {
     private final static OperatorSpi spi = new CalibrationOp.Spi();
     private TestProcessor testProcessor;
 
-    private String[] productTypeExemptions = {"_BP", "XCA", "WVW", "WVI", "WVS", "WSS", "OCN", "DOR", "GeoTIFF", "SCS_U"};
+    private String[] productTypeExemptions = {"_BP", "XCA", "RAW", "WVW", "WVI", "WVS", "WSS", "OCN", "DOR", "GeoTIFF", "SCS_U"};
     private String[] exceptionExemptions = {"not supported",
             "calibration has already been applied",
             "The product has already been calibrated",
