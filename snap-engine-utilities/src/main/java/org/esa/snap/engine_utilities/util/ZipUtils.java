acaf9ede4efd7e13697af81a2babb1dba675f85c
lveci <lveci@array.ca>
2015-12-31 23:12:16 -0500

    fix resource leaks

--- snap-engine-utilities/src/main/java/org/esa/snap/engine_utilities/util/ZipUtils.java
+++ snap-engine-utilities/src/main/java/org/esa/snap/engine_utilities/util/ZipUtils.java
@@ -40,7 +40,6 @@ import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.util.Optional;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
@@ -98,45 +97,28 @@ public class ZipUtils {
     }
 
     public static boolean isValid(final File file) {
-        ZipFile zipfile = null;
-        ZipInputStream zis = null;
-        try {
-            zipfile = new ZipFile(file);
-            zis = new ZipInputStream(new FileInputStream(file));
-            ZipEntry ze = zis.getNextEntry();
-            if(ze == null) {
+        try (ZipFile zipfile = new ZipFile(file)) {
+            try (ZipInputStream zis = new ZipInputStream(new FileInputStream(file))) {
+                ZipEntry ze = zis.getNextEntry();
+                if (ze == null) {
+                    return false;
+                }
+                while (ze != null) {
+                    // if it throws an exception fetching any of the following then we know the file is corrupted.
+                    try (InputStream in = zipfile.getInputStream(ze)) {
+                        ze.getCrc();
+                        ze.getCompressedSize();
+                        ze = zis.getNextEntry();
+                    } catch (IOException e) {
+                        return false;
+                    }
+                }
+                return true;
+            } catch (IOException e) {
                 return false;
             }
-            while(ze != null) {
-                // if it throws an exception fetching any of the following then we know the file is corrupted.
-                zipfile.getInputStream(ze);
-                ze.getCrc();
-                ze.getCompressedSize();
-                ze.getName();
-                ze = zis.getNextEntry();
-            }
-            return true;
-        } catch (ZipException e) {
-            return false;
         } catch (IOException e) {
             return false;
-        } finally {
-            try {
-                if (zipfile != null) {
-                    zipfile.close();
-                    zipfile = null;
-                }
-            } catch (IOException e) {
-                return false;
-            } try {
-                if (zis != null) {
-                    zis.close();
-                    zis = null;
-                }
-            } catch (IOException e) {
-                return false;
-            }
-
         }
     }
 
