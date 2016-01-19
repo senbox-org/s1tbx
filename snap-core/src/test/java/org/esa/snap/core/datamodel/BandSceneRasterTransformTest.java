7c119325ba28c0bd1e97f37dfe09089556e488c7
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2016-01-19 15:43:44 +0100

    added affinetransform2d as class

--- snap-core/src/test/java/org/esa/snap/core/datamodel/BandSceneRasterTransformTest.java
+++ snap-core/src/test/java/org/esa/snap/core/datamodel/BandSceneRasterTransformTest.java
@@ -1,16 +1,10 @@
 package org.esa.snap.core.datamodel;
 
-import org.esa.snap.core.transform.MathTransform2D;
-import org.geotools.referencing.operation.transform.AffineTransform2D;
+import org.esa.snap.core.transform.AffineTransform2D;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.opengis.referencing.operation.NoninvertibleTransformException;
 
-import java.awt.geom.AffineTransform;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 public class BandSceneRasterTransformTest {
 
@@ -26,7 +20,7 @@ public class BandSceneRasterTransformTest {
 
         assertSame(SceneRasterTransform.IDENTITY, band1.getSceneRasterTransform());
 
-        DefaultSceneRasterTransform transform = new DefaultSceneRasterTransform(new AffineMathTransform2D(1, 2, 3, 4, 5, 6), new AffineMathTransform2D(1, 2, 3, 4, 5, 6));
+        DefaultSceneRasterTransform transform = new DefaultSceneRasterTransform(new AffineTransform2D(1, 2, 3, 4, 5, 6), new AffineTransform2D(1, 2, 3, 4, 5, 6));
         band1.setSceneRasterTransform(transform);
 
         assertSame(transform, band1.getSceneRasterTransform());
@@ -84,49 +78,10 @@ public class BandSceneRasterTransformTest {
         assertSame(SceneRasterTransform.IDENTITY, band1.getSceneRasterTransform());
         assertSame(SceneRasterTransform.IDENTITY, band1.getSceneRasterTransform());
 
-        DefaultSceneRasterTransform transform = new DefaultSceneRasterTransform(new AffineMathTransform2D(1, 2, 3, 4, 5, 6), new AffineMathTransform2D(1, 2, 3, 4, 5, 6));
+        DefaultSceneRasterTransform transform = new DefaultSceneRasterTransform(new AffineTransform2D(1, 2, 3, 4, 5, 6), new AffineTransform2D(1, 2, 3, 4, 5, 6));
         band1.setSceneRasterTransform(transform);
 
         assertSame(transform, band1.getSceneRasterTransform());
     }
 
-    private class AffineMathTransform2D extends AffineTransform2D implements MathTransform2D {
-
-        private final AffineTransform transform;
-
-        /**
-         * Constructs a new affine transform with the same coefficient than the specified transform.
-         */
-        public AffineMathTransform2D(AffineTransform transform) {
-            super(transform);
-            this.transform = transform;
-        }
-
-        /**
-         * Constructs a new {@code AffineTransform2D} from 6 values representing the 6 specifiable
-         * entries of the 3&times;3 transformation matrix. Those values are given unchanged to the
-         * {@link AffineTransform#AffineTransform(double, double, double, double, double, double) super
-         * class constructor}.
-         *
-         * @since 2.5
-         */
-        public AffineMathTransform2D(double m00, double m10, double m01, double m11, double m02, double m12) {
-            super(m00, m10, m01, m11, m02, m12);
-            transform = new AffineTransform(m00, m10, m01, m11, m02, m12);
-        }
-
-        /**
-         * Creates the inverse transform of this object.
-         *
-         * @throws NoninvertibleTransformException if this transform can't be inverted.
-         */
-        @Override
-        public MathTransform2D inverse() throws NoninvertibleTransformException {
-            try {
-                return new AffineMathTransform2D(transform.createInverse());
-            } catch (java.awt.geom.NoninvertibleTransformException e) {
-                throw new NoninvertibleTransformException(e.getMessage());
-            }
-        }
-    }
 }
