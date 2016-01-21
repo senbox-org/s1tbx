2b9417cf96276727586297dda543215b661316bf
jun--lu <junlu@array.ca>
2016-01-21 15:14:32 -0500

    modified to handle burst selection

--- s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/gpf/ui/TOPSARSplitOpUI.java
+++ s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/gpf/ui/TOPSARSplitOpUI.java
@@ -36,6 +36,12 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
     private final JComboBox<String> subswathCombo = new JComboBox<>();
     private final JList<String> polList = new JList<>();
 
+    private final JLabel numBurstsLabelPart1 = new JLabel("Number of Bursts:");
+    private final JLabel numBurstsLabelPart2 = new JLabel("0");
+    private final JTextField firstBurstIndex = new JTextField("");
+    private final JTextField lastBurstIndex = new JTextField("");
+    private int numBursts = 0;
+
     @Override
     public JComponent CreateOpTab(String operatorName, Map<String, Object> parameterMap, AppContext appContext) {
 
@@ -47,6 +53,9 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
 
     @Override
     public void initParameters() {
+
+        firstBurstIndex.setText(String.valueOf(paramMap.get("firstBurstIndex")));
+        lastBurstIndex.setText(String.valueOf(paramMap.get("lastBurstIndex")));
 
         if (sourceProducts != null && sourceProducts.length > 0) {
             final MetadataElement absRoot = AbstractMetadata.getAbstractedMetadata(sourceProducts[0]);
@@ -71,12 +80,41 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
             subswathCombo.setSelectedItem(subswath);
 
             OperatorUIUtils.initParamList(polList, Sentinel1Utils.getProductPolarizations(absRoot),
-                    (String[])paramMap.get("selectedPolarisations"));
+                    (String[]) paramMap.get("selectedPolarisations"));
+
+            try {
+                Sentinel1Utils su = new Sentinel1Utils(sourceProducts[0]);
+                Sentinel1Utils.SubSwathInfo[] subSwathInfo = su.getSubSwath();
+                for (int i = 0; i < subSwathInfo.length; i++) {
+                    if (subSwathInfo[i].subSwathName.contains(subswath)) {
+                        numBursts = subSwathInfo[i].numOfBursts;
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                numBursts = 0;
+            }
+
+            final String text = Integer.toString(numBursts);
+            numBurstsLabelPart2.setText(text);
         }
     }
 
     @Override
     public UIValidation validateParameters() {
+        if (sourceProducts != null) {
+            final int i0 = Integer.parseInt(firstBurstIndex.getText());
+            final int i1 = Integer.parseInt(lastBurstIndex.getText());
+            if (i1 < i0) {
+                return new UIValidation(UIValidation.State.ERROR, "The last burst index must not be smaller than" +
+                        " the first burst index");
+            }
+
+            if (i1 > numBursts) {
+                return new UIValidation(UIValidation.State.ERROR, "The last burst index must not be greater than" +
+                        " the total number of bursts");
+            }
+        }
         return new UIValidation(UIValidation.State.OK, "");
     }
 
@@ -88,6 +126,19 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
             paramMap.put("subswath", subswathValue);
         }
         OperatorUIUtils.updateParamList(polList, paramMap, "selectedPolarisations");
+
+        if (firstBurstIndex.getText().isEmpty()) {
+            paramMap.put("firstBurstIndex", 0);
+        } else {
+            paramMap.put("firstBurstIndex", Integer.parseInt(firstBurstIndex.getText()));
+        }
+
+        if (lastBurstIndex.getText().isEmpty()) {
+            paramMap.put("lastBurstIndex", 0);
+        } else {
+            paramMap.put("lastBurstIndex", Integer.parseInt(lastBurstIndex.getText()));
+        }
+
     }
 
     private JComponent createPanel() {
@@ -96,8 +147,15 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
         final GridBagConstraints gbc = DialogUtils.createGridBagConstraints();
 
         DialogUtils.addComponent(contentPane, gbc, "Subswath:", subswathCombo);
-        ++gbc.gridy;
+        gbc.gridy++;
         DialogUtils.addComponent(contentPane, gbc, "Polarisations:", polList);
+
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, numBurstsLabelPart1, numBurstsLabelPart2);
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, "First Burst Index:", firstBurstIndex);
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, "Last Burst Index:", lastBurstIndex);
 
         DialogUtils.fillPanel(contentPane, gbc);
 
