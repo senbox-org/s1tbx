78a36548642131d2c031b30e7c5c747b12f39dc9
Luis Veci <lveci@array.ca>
2016-01-22 14:06:07 -0500

    add map and slider selection to TOPS split

--- s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/gpf/ui/TOPSARSplitOpUI.java
+++ s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/gpf/ui/TOPSARSplitOpUI.java
@@ -15,17 +15,26 @@
  */
 package org.esa.s1tbx.sentinel1.gpf.ui;
 
+import com.jidesoft.swing.RangeSlider;
 import org.esa.s1tbx.insar.gpf.Sentinel1Utils;
+import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.MetadataElement;
 import org.esa.snap.engine_utilities.datamodel.AbstractMetadata;
 import org.esa.snap.graphbuilder.gpf.ui.BaseOperatorUI;
 import org.esa.snap.graphbuilder.gpf.ui.OperatorUIUtils;
 import org.esa.snap.graphbuilder.gpf.ui.UIValidation;
+import org.esa.snap.graphbuilder.gpf.ui.worldmap.NestWorldMapPane;
+import org.esa.snap.graphbuilder.gpf.ui.worldmap.WorldMapUI;
 import org.esa.snap.graphbuilder.rcp.utils.DialogUtils;
 import org.esa.snap.ui.AppContext;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -36,16 +45,47 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
     private final JComboBox<String> subswathCombo = new JComboBox<>();
     private final JList<String> polList = new JList<>();
 
-    private final JLabel numBurstsLabelPart1 = new JLabel("Number of Bursts:");
-    private final JLabel numBurstsLabelPart2 = new JLabel("0");
-    private final JTextField firstBurstIndex = new JTextField("");
-    private final JTextField lastBurstIndex = new JTextField("");
-    private int numBursts = 0;
+    private final JLabel burstLabel = new JLabel("");
+    private final RangeSlider burstRange = new RangeSlider(RangeSlider.HORIZONTAL);
+    private final Map<String, Integer> swathBurstsMap = new HashMap<>();
+
+    private final WorldMapUI worldMapUI = new WorldMapUI();
+    private Sentinel1Utils su = null;
 
     @Override
     public JComponent CreateOpTab(String operatorName, Map<String, Object> parameterMap, AppContext appContext) {
 
         initializeOperatorUI(operatorName, parameterMap);
+
+        subswathCombo.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                Integer max = swathBurstsMap.get(subswathCombo.getSelectedItem());
+                if (max != null) {
+                    burstRange.setMaximum(max);
+                }
+
+                if (su != null) {
+                    worldMapUI.getModel().setAdditionalGeoBoundaries(getSelectedBoundaries());
+                }
+            }
+        });
+
+        burstRange.setMinimum(1);
+        burstRange.setMaximum(8);
+        burstRange.setMajorTickSpacing(1);
+
+        burstRange.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                burstLabel.setText(burstRange.getLowValue() + " to " + burstRange.getHighValue());
+
+                if (su != null) {
+                    worldMapUI.getModel().setAdditionalGeoBoundaries(getSelectedBoundaries());
+                }
+            }
+        });
+
         final JComponent panel = createPanel();
         initParameters();
         return new JScrollPane(panel);
@@ -54,28 +94,25 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
     @Override
     public void initParameters() {
 
-        firstBurstIndex.setText(String.valueOf(paramMap.get("firstBurstIndex")));
-        lastBurstIndex.setText(String.valueOf(paramMap.get("lastBurstIndex")));
-
         if (sourceProducts != null && sourceProducts.length > 0) {
             final MetadataElement absRoot = AbstractMetadata.getAbstractedMetadata(sourceProducts[0]);
 
             final String acquisitionMode = absRoot.getAttributeString(AbstractMetadata.ACQUISITION_MODE);
             subswathCombo.removeAllItems();
-            if(acquisitionMode.equals("IW")) {
+            if (acquisitionMode.equals("IW")) {
                 subswathCombo.addItem("IW1");
                 subswathCombo.addItem("IW2");
                 subswathCombo.addItem("IW3");
-            } else if(acquisitionMode.equals("EW")) {
+            } else if (acquisitionMode.equals("EW")) {
                 subswathCombo.addItem("EW1");
                 subswathCombo.addItem("EW2");
                 subswathCombo.addItem("EW3");
                 subswathCombo.addItem("EW4");
                 subswathCombo.addItem("EW5");
             }
-            String subswath = (String)paramMap.get("subswath");
-            if(subswath == null) {
-                subswath = acquisitionMode+'1';
+            String subswath = (String) paramMap.get("subswath");
+            if (subswath == null) {
+                subswath = acquisitionMode + '1';
             }
             subswathCombo.setSelectedItem(subswath);
 
@@ -83,62 +120,48 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
                     (String[]) paramMap.get("selectedPolarisations"));
 
             try {
-                Sentinel1Utils su = new Sentinel1Utils(sourceProducts[0]);
-                Sentinel1Utils.SubSwathInfo[] subSwathInfo = su.getSubSwath();
-                for (int i = 0; i < subSwathInfo.length; i++) {
-                    if (subSwathInfo[i].subSwathName.contains(subswath)) {
-                        numBursts = subSwathInfo[i].numOfBursts;
-                        break;
-                    }
+                su = new Sentinel1Utils(sourceProducts[0]);
+                for (int ss = 0; ss < subswathCombo.getItemCount(); ++ss) {
+                    String swath = subswathCombo.getItemAt(ss);
+                    swathBurstsMap.put(swath, su.getNumOfBursts(swath));
                 }
             } catch (Exception e) {
-                numBursts = 0;
+
             }
 
-            final String text = Integer.toString(numBursts);
-            numBurstsLabelPart2.setText(text);
+            burstRange.setMaximum(swathBurstsMap.get(subswath));
+
+            worldMapUI.getModel().setAutoZoomEnabled(true);
+            worldMapUI.getModel().setProducts(sourceProducts);
+            worldMapUI.getModel().setSelectedProduct(sourceProducts[0]);
+            worldMapUI.getWorlMapPane().zoomToProduct(sourceProducts[0]);
+            worldMapUI.getWorlMapPane().revalidate();
+        }
+
+        burstRange.setLowValue((int) paramMap.get("firstBurstIndex"));
+        burstRange.setHighValue((int) paramMap.get("lastBurstIndex"));
+
+        if (burstRange.getHighValue() > burstRange.getMaximum()) {
+            burstRange.setHighValue(burstRange.getMaximum());
         }
     }
 
     @Override
     public UIValidation validateParameters() {
-        if (sourceProducts != null) {
-            final int i0 = Integer.parseInt(firstBurstIndex.getText());
-            final int i1 = Integer.parseInt(lastBurstIndex.getText());
-            if (i1 < i0) {
-                return new UIValidation(UIValidation.State.ERROR, "The last burst index must not be smaller than" +
-                        " the first burst index");
-            }
-
-            if (i1 > numBursts) {
-                return new UIValidation(UIValidation.State.ERROR, "The last burst index must not be greater than" +
-                        " the total number of bursts");
-            }
-        }
         return new UIValidation(UIValidation.State.OK, "");
     }
 
     @Override
     public void updateParameters() {
 
-        String subswathValue = (String)subswathCombo.getSelectedItem();
-        if(subswathValue != null) {
+        String subswathValue = (String) subswathCombo.getSelectedItem();
+        if (subswathValue != null) {
             paramMap.put("subswath", subswathValue);
         }
         OperatorUIUtils.updateParamList(polList, paramMap, "selectedPolarisations");
 
-        if (firstBurstIndex.getText().isEmpty()) {
-            paramMap.put("firstBurstIndex", 0);
-        } else {
-            paramMap.put("firstBurstIndex", Integer.parseInt(firstBurstIndex.getText()));
-        }
-
-        if (lastBurstIndex.getText().isEmpty()) {
-            paramMap.put("lastBurstIndex", 0);
-        } else {
-            paramMap.put("lastBurstIndex", Integer.parseInt(lastBurstIndex.getText()));
-        }
-
+        paramMap.put("firstBurstIndex", burstRange.getLowValue());
+        paramMap.put("lastBurstIndex", burstRange.getHighValue());
     }
 
     private JComponent createPanel() {
@@ -151,14 +174,47 @@ public class TOPSARSplitOpUI extends BaseOperatorUI {
         DialogUtils.addComponent(contentPane, gbc, "Polarisations:", polList);
 
         gbc.gridy++;
-        DialogUtils.addComponent(contentPane, gbc, numBurstsLabelPart1, numBurstsLabelPart2);
+        DialogUtils.addComponent(contentPane, gbc, "Bursts:", burstLabel);
         gbc.gridy++;
-        DialogUtils.addComponent(contentPane, gbc, "First Burst Index:", firstBurstIndex);
+        DialogUtils.addComponent(contentPane, gbc, "", burstRange);
+
+        final NestWorldMapPane worldPane = worldMapUI.getWorlMapPane();
+        worldPane.setPreferredSize(new Dimension(500, 130));
+
         gbc.gridy++;
-        DialogUtils.addComponent(contentPane, gbc, "Last Burst Index:", lastBurstIndex);
+        DialogUtils.addComponent(contentPane, gbc, "", worldPane);
 
         DialogUtils.fillPanel(contentPane, gbc);
 
         return contentPane;
+    }
+
+    private GeoPos[][] getSelectedBoundaries() {
+        final Sentinel1Utils.SubSwathInfo[] subswaths = su.getSubSwath();
+        final String subswath = (String) subswathCombo.getSelectedItem();
+
+        for (Sentinel1Utils.SubSwathInfo swath : subswaths) {
+            if (swath.subSwathName.contains(subswath)) {
+                final int numBursts = burstRange.getHighValue() - burstRange.getLowValue() + 1;
+                final GeoPos[][] geoBound = new GeoPos[numBursts][4];
+
+                for (int i = 0; i < numBursts; ++i) {
+                    geoBound[i] = getBox(burstRange.getLowValue() - 1 + i, swath);
+                }
+                return geoBound;
+            }
+        }
+        return null;
+    }
+
+    private GeoPos[] getBox(final int i, final Sentinel1Utils.SubSwathInfo swath) {
+        final int numPoints = swath.latitude[0].length - 1;
+
+        final GeoPos[] geoBound = new GeoPos[4];
+        geoBound[0] = new GeoPos(swath.latitude[i][0], swath.longitude[i][0]);
+        geoBound[1] = new GeoPos(swath.latitude[i][numPoints], swath.longitude[i][numPoints]);
+        geoBound[2] = new GeoPos(swath.latitude[i + 1][numPoints], swath.longitude[i + 1][numPoints]);
+        geoBound[3] = new GeoPos(swath.latitude[i + 1][0], swath.longitude[i + 1][0]);
+        return geoBound;
     }
 }
