74f93aad7e4e0f2a18c122e8e8d1572448deef7d
lveci <lveci@array.ca>
2016-01-22 09:55:13 -0500

    init data on getPixelDouble

--- s1tbx-io/src/main/java/org/esa/s1tbx/io/ceos/alos/TiePointGridLazy.java
+++ s1tbx-io/src/main/java/org/esa/s1tbx/io/ceos/alos/TiePointGridLazy.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright (C) 2016 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
 package org.esa.s1tbx.io.ceos.alos;
 
 import com.bc.ceres.core.ProgressMonitor;
+import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.TiePointGrid;
 import org.esa.snap.core.util.SystemUtils;
 
@@ -12,6 +28,7 @@ import java.io.IOException;
 public class TiePointGridLazy extends TiePointGrid {
 
     private boolean hasData;
+    private Product product;
 
     /**
      * Constructs a new <code>TiePointGrid</code> with the given tie point grid properties.
@@ -28,55 +45,57 @@ public class TiePointGridLazy extends TiePointGrid {
      * @param tiePoints    the tie-point data values, must be an array of the size <code>gridWidth * gridHeight</code>
      */
     public TiePointGridLazy(String name,
-                        int gridWidth,
-                        int gridHeight,
-                        double offsetX,
-                        double offsetY,
-                        double subSamplingX,
-                        double subSamplingY,
-                        float[] tiePoints) {
+                            int gridWidth,
+                            int gridHeight,
+                            double offsetX,
+                            double offsetY,
+                            double subSamplingX,
+                            double subSamplingY,
+                            float[] tiePoints) {
         super(name, gridWidth, gridHeight, offsetX, offsetY, subSamplingX, subSamplingY, tiePoints, DISCONT_NONE);
-        hasData = false;
+        this.hasData = false;
+        this.product = product;
+    }
+
+    private void initData() {
+        try {
+            // read all or nothing
+            getProductReader().readTiePointGridRasterData(this, 0, 0, getGridWidth(), getGridHeight(), getGridData(),
+                                                          ProgressMonitor.NULL);
+            hasData = true;
+        } catch (IOException e) {
+            SystemUtils.LOG.severe("Unable to load TPG: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public double getPixelDouble(double x, double y) {
+        if (!hasData) {
+            initData();
+        }
+        return super.getPixelDouble(x, y);
     }
 
     @Override
     public int[] getPixels(int x, int y, int w, int h, int[] pixels, ProgressMonitor pm) {
-        if(!hasData) {
-            try {
-                // read all or nothing
-                getProductReader().readTiePointGridRasterData(this, 0, 0, getGridWidth(), getGridHeight(), getGridData(), pm);
-                hasData = true;
-            } catch (IOException e) {
-                SystemUtils.LOG.severe("Unable to load TPG: "+e.getMessage());
-            }
+        if (!hasData) {
+            initData();
         }
         return super.getPixels(x, y, w, h, pixels, pm);
     }
 
     @Override
     public double[] getPixels(int x, int y, int w, int h, double[] pixels, ProgressMonitor pm) {
-        if(!hasData) {
-            try {
-                // read all or nothing
-                getProductReader().readTiePointGridRasterData(this, 0, 0, getGridWidth(), getGridHeight(), getGridData(), pm);
-                hasData = true;
-            } catch (IOException e) {
-                SystemUtils.LOG.severe("Unable to load TPG: "+e.getMessage());
-            }
+        if (!hasData) {
+            initData();
         }
         return super.getPixels(x, y, w, h, pixels, pm);
     }
 
     @Override
     public float[] getPixels(int x, int y, int w, int h, float[] pixels, ProgressMonitor pm) {
-        if(!hasData) {
-            try {
-                // read all or nothing
-                getProductReader().readTiePointGridRasterData(this, 0, 0, getGridWidth(), getGridHeight(), getGridData(), pm);
-                hasData = true;
-            } catch (IOException e) {
-                SystemUtils.LOG.severe("Unable to load TPG: "+e.getMessage());
-            }
+        if (!hasData) {
+            initData();
         }
         return super.getPixels(x, y, w, h, pixels, pm);
     }
