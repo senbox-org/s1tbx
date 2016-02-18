48f8761adf564fbaab869dc47be96aab6901e4ca
marpet <marco.peters@brockmann-consult.de>
2016-02-18 18:44:33 +0100

    [SNAP-388] Provide a isBitSet function in Band Maths

--- snap-core/src/test/java/org/esa/snap/core/jexp/impl/FunctionsTest.java
+++ snap-core/src/test/java/org/esa/snap/core/jexp/impl/FunctionsTest.java
@@ -1,16 +1,50 @@
 package org.esa.snap.core.jexp.impl;
 
+import org.esa.snap.core.jexp.EvalEnv;
+import org.esa.snap.core.jexp.Parser;
+import org.esa.snap.core.jexp.Term;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * @author Norman
- */
 public class FunctionsTest {
+
+    private EvalEnv env;
+    private Parser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new EvalEnv() {};
+        parser = new ParserImpl();
+    }
+
     @Test
     public void testGetAll() throws Exception {
         int size = Functions.getAll().size();
         assertTrue(size >= 40);
+    }
+
+    @Test
+    public void testBitSet() throws Exception {
+        assertTrue(Functions.BIT_SET.evalB(env, new Term[]{
+                parser.parse("1"),
+                parser.parse("0")
+        }));
+
+        assertTrue(Functions.BIT_SET.evalB(env, new Term[]{
+                parser.parse("0x20"),
+                parser.parse("5")
+        }));
+        assertFalse(Functions.BIT_SET.evalB(env, new Term[]{
+                parser.parse("0x20"),
+                parser.parse("4")
+        }));
+
+        assertTrue(Functions.BIT_SET.evalB(env, new Term[]{
+                parser.parse("-2147483647 - 1"), // Actually this -2147483648 (0x80000000) but due to bug SNAP-389 the workaround must be used
+                parser.parse("31")
+        }));
+
     }
 }
