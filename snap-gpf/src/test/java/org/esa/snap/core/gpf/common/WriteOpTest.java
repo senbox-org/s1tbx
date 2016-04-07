826806bad2ebf8550d3f907b5ffddcf2bd03311c
marpet <marco.peters@brockmann-consult.de>
2016-04-07 09:24:28 +0200

    [459] SubsetOp throws exception if no intersection with region

--- snap-gpf/src/test/java/org/esa/snap/core/gpf/common/WriteOpTest.java
+++ snap-gpf/src/test/java/org/esa/snap/core/gpf/common/WriteOpTest.java
@@ -47,6 +47,7 @@ import javax.media.jai.TileScheduler;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.StringReader;
+import java.nio.file.Files;
 
 import static junit.framework.TestCase.*;
 
@@ -134,6 +135,24 @@ public class WriteOpTest {
         // assertEquals(40, placemarkProductNodeGroup.getNodeCount());
 
         productOnDisk.dispose();
+    }
+
+    @Test
+    public void testWritingEmptyProduct() throws Exception {
+        Product product = new Product("empty", "EMPTY", 0, 0);
+
+        File testDir = Files.createTempDirectory("WriteOpTestDir").toFile();
+        try {
+            File testOuptutFile = new File(testDir, "file.dim");
+            WriteOp writeOp = new WriteOp(product, testOuptutFile, "BEAM-DIMAP");
+            writeOp.writeProduct(ProgressMonitor.NULL);
+
+            // assert that it is not written
+            assertFalse(testOuptutFile.isFile());
+        } finally {
+            testDir.deleteOnExit();
+        }
+
     }
 
     @Test
