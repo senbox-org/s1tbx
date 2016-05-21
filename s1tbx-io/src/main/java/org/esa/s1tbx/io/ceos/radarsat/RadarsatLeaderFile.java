7ba236a9884280c99601b1b444d29c72cb13256a
lveci <lveci@array.ca>
2016-05-21 18:52:56 -0400

    fix rs1 definition files

--- s1tbx-io/src/main/java/org/esa/s1tbx/io/ceos/radarsat/RadarsatLeaderFile.java
+++ s1tbx-io/src/main/java/org/esa/s1tbx/io/ceos/radarsat/RadarsatLeaderFile.java
@@ -15,7 +15,11 @@
  */
 package org.esa.s1tbx.io.ceos.radarsat;
 
+import org.esa.s1tbx.io.binary.BinaryDBReader;
+import org.esa.s1tbx.io.binary.BinaryFileReader;
+import org.esa.s1tbx.io.binary.BinaryRecord;
 import org.esa.s1tbx.io.ceos.CEOSLeaderFile;
+import org.jdom2.Document;
 
 import javax.imageio.stream.ImageInputStream;
 import java.io.IOException;
@@ -26,9 +30,76 @@ class RadarsatLeaderFile extends CEOSLeaderFile {
     private final static String mission = "radarsat";
     private final static String leader_recordDefinitionFile = "leader_file.xml";
 
+    private final static Document leaderXML = BinaryDBReader.loadDefinitionFile(mission, leader_recordDefinitionFile);
+    private final static Document sceneXML = BinaryDBReader.loadDefinitionFile(mission, scene_recordDefinitionFile);
+    private final static Document mapProjXML = BinaryDBReader.loadDefinitionFile(mission, mapproj_recordDefinitionFile);
+    private final static Document platformXML = BinaryDBReader.loadDefinitionFile(mission, platformPosition_recordDefinitionFile);
+    private final static Document attitudeXML = BinaryDBReader.loadDefinitionFile(mission, attitude_recordDefinitionFile);
+    private final static Document radiometricXML = BinaryDBReader.loadDefinitionFile(mission, radiometric_recordDefinitionFile);
+    private final static Document dataQualityXML = BinaryDBReader.loadDefinitionFile(mission, dataQuality_recordDefinitionFile);
+    private final static Document facilityXML = BinaryDBReader.loadDefinitionFile(mission, facility_recordDefinitionFile);
+    private final static Document histogramXML = BinaryDBReader.loadDefinitionFile(mission, histogram_recordDefinitionFile);
+    private final static Document detailProcXML = BinaryDBReader.loadDefinitionFile(mission, detailedProcessing_recordDefinitionFile);
+    private final static Document radiometricCompXML = BinaryDBReader.loadDefinitionFile(mission, radiometric_comp_recordDefinitionFile);
+
     public RadarsatLeaderFile(final ImageInputStream stream) throws IOException {
-        super(stream, mission, leader_recordDefinitionFile);
+        final BinaryFileReader reader = new BinaryFileReader(stream);
 
+        final Document leaderXML = BinaryDBReader.loadDefinitionFile(mission, leader_recordDefinitionFile);
+        leaderFDR = new BinaryRecord(reader, -1, leaderXML, leader_recordDefinitionFile);
+        reader.seek(leaderFDR.getRecordEndPosition());
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of data set summary records"); ++i) {
+            sceneHeaderRecord = new BinaryRecord(reader, -1, sceneXML, scene_recordDefinitionFile);
+            reader.seek(sceneHeaderRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of map projection data records"); ++i) {
+            mapProjRecord = new BinaryRecord(reader, -1, mapProjXML, mapproj_recordDefinitionFile);
+            reader.seek(mapProjRecord.getRecordEndPosition());
+        }
+
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of platform pos. data records"); ++i) {
+            platformPositionRecord = new BinaryRecord(reader, -1, platformXML, platformPosition_recordDefinitionFile);
+            reader.seek(platformPositionRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of attitude data records"); ++i) {
+            attitudeRecord = new BinaryRecord(reader, -1, attitudeXML, attitude_recordDefinitionFile);
+            reader.seek(attitudeRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of radiometric data records"); ++i) {
+            radiometricRecord = new BinaryRecord(reader, -1, radiometricXML, radiometric_recordDefinitionFile);
+            reader.seek(radiometricRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of rad. compensation records"); ++i) {
+            radiometricCompRecord = new BinaryRecord(reader, -1, radiometricCompXML, radiometric_comp_recordDefinitionFile);
+            reader.seek(radiometricCompRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of data quality summary records"); ++i) {
+            dataQualityRecord = new BinaryRecord(reader, -1, dataQualityXML, dataQuality_recordDefinitionFile);
+            reader.seek(dataQualityRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of data histograms records"); ++i) {
+            histogramRecord = new BinaryRecord(reader, -1, histogramXML, histogram_recordDefinitionFile);
+            reader.seek(histogramRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of det. processing records"); ++i) {
+            detailedProcessingRecord = new BinaryRecord(reader, -1, detailProcXML, detailedProcessing_recordDefinitionFile);
+            reader.seek(detailedProcessingRecord.getRecordEndPosition());
+        }
+
+        for (int i = 0; i < leaderFDR.getAttributeInt("Number of facility data records"); ++i) {
+            facilityRecord = new BinaryRecord(reader, -1, facilityXML, facility_recordDefinitionFile);
+            reader.seek(facilityRecord.getRecordEndPosition());
+        }
+
+        reader.close();
     }
-
 }
