47babc5be6debb01141c36b95c52f47ff9b65550
jun--lu <junlu@array.ca>
2016-05-24 14:26:39 -0400

    modified to skip denoise for pixel whose intensity is less than noise power

--- s1tbx-op-calibration/src/main/java/org/esa/s1tbx/calibration/gpf/Sentinel1RemoveThermalNoiseOp.java
+++ s1tbx-op-calibration/src/main/java/org/esa/s1tbx/calibration/gpf/Sentinel1RemoveThermalNoiseOp.java
@@ -497,7 +497,7 @@ public final class Sentinel1RemoveThermalNoiseOp extends Operator {
 
                     double value = dn2 - lut[xx];
                     if(value < 0) {
-                        value = 1000;       // small intensity value; if too small, calibration will make it nodatavalue
+                        value = dn2;       // small intensity value; if too small, calibration will make it nodatavalue
                     }
                     trgData.setElemDoubleAt(tgtIdx, value);
                 }
