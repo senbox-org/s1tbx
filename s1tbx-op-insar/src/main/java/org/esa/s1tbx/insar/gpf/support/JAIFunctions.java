0d5a904e71cbe695781a181a745d88fcd5819258
Luis Veci <lveci@array.ca>
2016-08-09 08:53:58 -0400

    move JAIFunctions to support

--- s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/support/JAIFunctions.java
+++ s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/support/JAIFunctions.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
-package org.esa.s1tbx.insar.gpf.coregistration;
+package org.esa.s1tbx.insar.gpf.support;
 
 import javax.media.jai.BorderExtender;
 import javax.media.jai.Interpolation;
