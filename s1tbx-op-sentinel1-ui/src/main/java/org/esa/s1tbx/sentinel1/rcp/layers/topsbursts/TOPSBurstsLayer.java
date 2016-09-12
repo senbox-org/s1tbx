0c726d2abd3a5c68afc260dbc9ab51970265c0f8
Luis Veci <lveci@array.ca>
2016-09-12 11:02:41 -0400

    move layer to S1 module

--- s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/rcp/layers/topsbursts/TOPSBurstsLayer.java
+++ s1tbx-op-sentinel1-ui/src/main/java/org/esa/s1tbx/sentinel1/rcp/layers/topsbursts/TOPSBurstsLayer.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
-package org.esa.s1tbx.dat.layers.topsbursts;
+package org.esa.s1tbx.sentinel1.rcp.layers.topsbursts;
 
 import com.bc.ceres.binding.PropertySet;
 import com.bc.ceres.glayer.Layer;
