58e05963459078ba6b14ab616efd2e7edbc817d2
marpet <marco.peters@brockmann-consult.de>
2016-09-28 13:28:09 +0200

    make BandConfiguration usable from outside the package

--- snap-statistics/src/main/java/org/esa/snap/statistics/BandConfiguration.java
+++ snap-statistics/src/main/java/org/esa/snap/statistics/BandConfiguration.java
@@ -4,18 +4,14 @@ import org.esa.snap.core.gpf.annotations.Parameter;
 
 public class BandConfiguration {
 
-    @Parameter(description = "The name of the band in the source products. If empty, parameter 'expression' must " +
-            "be provided.")
-    String sourceBandName;
+    @Parameter(description = "The name of the band in the source products. If empty, parameter 'expression' must be provided.")
+    public String sourceBandName;
 
-    @Parameter(description =
-                       "The band maths expression serving as input band. If empty, parameter 'sourceBandName'" +
-                               "must be provided.")
-    String expression;
+    @Parameter(description = "The band maths expression serving as input band. If empty, parameter 'sourceBandName' must be provided.")
+    public String expression;
 
-    @Parameter(description = "The band maths expression serving as criterion for whether to consider pixels for " +
-            "computation.")
-    String validPixelExpression;
+    @Parameter(description = "The band maths expression serving as criterion for whether to consider pixels for computation.")
+    public String validPixelExpression;
 
     public BandConfiguration() {
         // used by DOM converter
