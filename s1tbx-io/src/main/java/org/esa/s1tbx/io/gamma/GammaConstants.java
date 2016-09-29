f2be24a82dcfde55d04aa590c7eb40e8e259319d
Luis Veci <lveci@array.ca>
2016-09-29 11:38:37 -0400

    add metadata to the par file

--- s1tbx-io/src/main/java/org/esa/s1tbx/io/gamma/GammaConstants.java
+++ s1tbx-io/src/main/java/org/esa/s1tbx/io/gamma/GammaConstants.java
@@ -1,36 +1,48 @@
 package org.esa.s1tbx.io.gamma;
 
-public class GammaConstants {
+class GammaConstants {
 
-    public static final String HEADER_KEY_NAME = "title";
-    public static final String HEADER_KEY_SAMPLES = "range_samples";
-    public static final String HEADER_KEY_SAMPLES1 = "range_samp_1";
-    public static final String HEADER_KEY_WIDTH = "width";
-    public static final String HEADER_KEY_NCOLUMNS = "ncolumns";
-    public static final String HEADER_KEY_LINES = "azimuth_lines";
-    public static final String HEADER_KEY_LINES1 = "az_samp_1";
-    public static final String HEADER_KEY_HEIGHT = "height";
-    public static final String HEADER_KEY_NLINES = "nlines";
-    public static final String HEADER_KEY_BANDS = "bands";
-    public static final String HEADER_KEY_HEADER_OFFSET = "line_header_size";
-    public static final String HEADER_KEY_DATA_TYPE = "image_format";
-    public static final String HEADER_KEY_SENSOR_TYPE = "sensor";
-    public static final String HEADER_KEY_BYTE_ORDER = "byte order";
-    public static final String HEADER_KEY_BAND_NAMES = "band names";
-    public static final String HEADER_KEY_DESCRIPTION = "sensor";
+    static final String HEADER_KEY_NAME = "title";
+    static final String HEADER_KEY_SAMPLES = "range_samples";
+    static final String HEADER_KEY_SAMPLES1 = "range_samp_1";
+    static final String HEADER_KEY_WIDTH = "width";
+    static final String HEADER_KEY_NCOLUMNS = "ncolumns";
+    static final String HEADER_KEY_LINES = "azimuth_lines";
+    static final String HEADER_KEY_LINES1 = "az_samp_1";
+    static final String HEADER_KEY_HEIGHT = "height";
+    static final String HEADER_KEY_NLINES = "nlines";
+    static final String HEADER_KEY_BANDS = "bands";
+    static final String HEADER_KEY_HEADER_OFFSET = "line_header_size";
+    static final String HEADER_KEY_DATA_TYPE = "image_format";
+    static final String HEADER_KEY_SENSOR_TYPE = "sensor";
+    static final String HEADER_KEY_BYTE_ORDER = "byte order";
+    static final String HEADER_KEY_BAND_NAMES = "band names";
+    static final String HEADER_KEY_DESCRIPTION = "sensor";
 
-    public static final String HEADER_KEY_DATE = "date";
-    public static final String HEADER_KEY_START_TIME = "start_time";
-    public static final String HEADER_KEY_END_TIME = "end_time";
-    public static final String HEADER_KEY_LINE_TIME_INTERVAL = "azimuth_line_time";
+    static final String HEADER_KEY_DATE = "date";
+    static final String HEADER_KEY_START_TIME = "start_time";
+    static final String HEADER_KEY_CENTER_TIME = "center_time";
+    static final String HEADER_KEY_END_TIME = "end_time";
+    static final String HEADER_KEY_LINE_TIME_INTERVAL = "azimuth_line_time";
 
-    public static final String HEADER_KEY_RADAR_FREQUENCY = "radar_frequency";
-    public static final String HEADER_KEY_PRF = "prf";
-    public static final String HEADER_KEY_RANGE_LOOKS = "range_looks";
-    public static final String HEADER_KEY_AZIMUTH_LOOKS = "azimuth_looks";
+    static final String HEADER_KEY_RADAR_FREQUENCY = "radar_frequency";
+    static final String HEADER_KEY_PRF = "prf";
+    static final String HEADER_KEY_RANGE_LOOKS = "range_looks";
+    static final String HEADER_KEY_AZIMUTH_LOOKS = "azimuth_looks";
+    static final String HEADER_KEY_IMAGE_GEOMETRY = "image_geometry";
+    static final String HEADER_KEY_CENTER_LATITUDE = "center_latitude";
+    static final String HEADER_KEY_CENTER_LONGITUDE = "center_longitude";
+    static final String HEADER_KEY_RANGE_PIXEL_SPACING = "range_pixel_spacing";
+    static final String HEADER_KEY_AZIMUTH_PIXEL_SPACING = "azimuth_pixel_spacing";
 
-    public final static String SLC_EXTENSION = ".rslc";
-    public final static String PAR_EXTENSION = ".par";
+    static final String HEADER_KEY_NUM_STATE_VECTORS = "number_of_state_vectors";
+    static final String HEADER_KEY_TIME_FIRST_STATE_VECTORS = "time_of_first_state_vector";
+    static final String HEADER_KEY_STATE_VECTOR_INTERVAL = "state_vector_interval";
+    static final String HEADER_KEY_STATE_VECTOR_POSITION = "state_vector_position";
+    static final String HEADER_KEY_STATE_VECTOR_VELOCITY = "state_vector_velocity";
+
+    final static String SLC_EXTENSION = ".rslc";
+    final static String PAR_EXTENSION = ".par";
 
     private GammaConstants() {
     }
--- s1tbx-io/src/main/java/org/esa/s1tbx/io/gamma/HeaderWriter.java
+++ s1tbx-io/src/main/java/org/esa/s1tbx/io/gamma/HeaderWriter.java
@@ -16,17 +16,21 @@
 package org.esa.s1tbx.io.gamma;
 
 import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.MetadataElement;
+import org.esa.snap.core.datamodel.PixelPos;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.util.SystemUtils;
 import org.esa.snap.core.util.io.FileUtils;
 import org.esa.snap.engine_utilities.datamodel.AbstractMetadata;
+import org.esa.snap.engine_utilities.datamodel.OrbitStateVector;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Calendar;
 
 /**
  * Writer par header
@@ -40,8 +44,10 @@ class HeaderWriter {
     private boolean isComplex;
     private boolean isCoregistered;
     private final GammaProductWriter writer;
+    private final ProductData.UTC dateDay;  // start date to the day
 
     private final static String sep = ":\t";
+    private final static double daysToSeconds = 12 * 60 * 60;
 
     HeaderWriter(final GammaProductWriter writer, final Product srcProduct, final File userOutputFile) {
         this.writer = writer;
@@ -60,6 +66,9 @@ class HeaderWriter {
         }
         this.outputFile = createParFile(userOutputFile);
         this.baseFileName = FileUtils.getFilenameWithoutExtension(this.outputFile);
+
+        Calendar cal = srcProduct.getStartTime().getAsCalendar();
+        dateDay = new ProductData.UTC(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
     }
 
     String getBaseFileName() {
@@ -70,24 +79,95 @@ class HeaderWriter {
         final String oldEOL = System.getProperty("line.separator");
         System.setProperty("line.separator", "\n");
         final FileOutputStream out = new FileOutputStream(outputFile);
-        try(PrintStream p = new PrintStream(out)) {
+        try(final PrintStream p = new PrintStream(out)) {
 
             p.println(GammaConstants.HEADER_KEY_NAME + sep + srcProduct.getName());
             p.println(GammaConstants.HEADER_KEY_SENSOR_TYPE + sep + absRoot.getAttributeString(AbstractMetadata.MISSION));
+            p.println(GammaConstants.HEADER_KEY_DATE + sep + writeDate());
+            p.println(GammaConstants.HEADER_KEY_START_TIME + sep + writeStartTime());
+            p.println(GammaConstants.HEADER_KEY_CENTER_TIME + sep + writeCenterTime());
+            p.println(GammaConstants.HEADER_KEY_END_TIME + sep + writeEndTime());
+            p.println(GammaConstants.HEADER_KEY_LINE_TIME_INTERVAL + sep + absRoot.getAttributeString(AbstractMetadata.line_time_interval));
             p.println(GammaConstants.HEADER_KEY_SAMPLES + sep + srcProduct.getSceneRasterWidth());
             p.println(GammaConstants.HEADER_KEY_LINES + sep + srcProduct.getSceneRasterHeight());
-            p.println(GammaConstants.HEADER_KEY_DATA_TYPE + sep + getDataType());
-            p.println(GammaConstants.HEADER_KEY_LINE_TIME_INTERVAL + sep + absRoot.getAttributeString(AbstractMetadata.line_time_interval));
             p.println(GammaConstants.HEADER_KEY_RANGE_LOOKS + sep + absRoot.getAttributeInt(AbstractMetadata.range_looks));
             p.println(GammaConstants.HEADER_KEY_AZIMUTH_LOOKS + sep + absRoot.getAttributeInt(AbstractMetadata.azimuth_looks));
+            p.println(GammaConstants.HEADER_KEY_DATA_TYPE + sep + getDataType());
+            p.println(GammaConstants.HEADER_KEY_IMAGE_GEOMETRY + sep + writeImageGeometry());
+            writeCenterLatLon(p);
+            p.println(GammaConstants.HEADER_KEY_RANGE_PIXEL_SPACING + sep + absRoot.getAttributeInt(AbstractMetadata.range_spacing));
+            p.println(GammaConstants.HEADER_KEY_AZIMUTH_PIXEL_SPACING + sep + absRoot.getAttributeInt(AbstractMetadata.azimuth_spacing));
             p.println(GammaConstants.HEADER_KEY_RADAR_FREQUENCY + sep + absRoot.getAttributeString(AbstractMetadata.radar_frequency));
             p.println(GammaConstants.HEADER_KEY_PRF + sep + absRoot.getAttributeString(AbstractMetadata.pulse_repetition_frequency));
+            writeOrbitStateVectors(p);
 
             p.flush();
         } catch (Exception e) {
             throw new IOException("GammaWriter unable to write par file " + e.getMessage());
         } finally {
             System.setProperty("line.separator", oldEOL);
+        }
+    }
+
+    private String writeDate() {
+        Calendar cal = srcProduct.getStartTime().getAsCalendar();
+        return cal.get(Calendar.YEAR) + "  " + cal.get(Calendar.MONTH) + "  " + cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+    private String writeStartTime() {
+        double diff = srcProduct.getStartTime().getMJD() - dateDay.getMJD();
+        double seconds = diff * daysToSeconds;
+        return seconds + sep + "s";
+    }
+
+    private String writeCenterTime() {
+        double center = (srcProduct.getStartTime().getMJD() +
+                (srcProduct.getEndTime().getMJD() - srcProduct.getStartTime().getMJD())/2.0);
+        double seconds = (center - dateDay.getMJD()) * daysToSeconds;
+        return seconds + sep + "s";
+    }
+
+    private String writeEndTime() {
+        double diff = srcProduct.getEndTime().getMJD() - dateDay.getMJD();
+        double seconds = diff * daysToSeconds;
+        return seconds + sep + "s";
+    }
+
+    private String writeImageGeometry() {
+        if(absRoot.getAttributeString(AbstractMetadata.sample_type).equals("COMPLEX") ||
+                absRoot.getAttributeInt(AbstractMetadata.srgr_flag, 0) == 0) {
+            return "SLANT_RANGE";
+        }
+        return "GROUND_RANGE";
+    }
+
+    private void writeCenterLatLon(final PrintStream p) {
+        GeoPos geoPos = srcProduct.getSceneGeoCoding().getGeoPos(
+                new PixelPos(srcProduct.getSceneRasterWidth()/2, srcProduct.getSceneRasterHeight()/2), null);
+
+        p.println(GammaConstants.HEADER_KEY_CENTER_LATITUDE + sep + geoPos.getLat() + sep + "degrees");
+        p.println(GammaConstants.HEADER_KEY_CENTER_LONGITUDE + sep + geoPos.getLon() + sep + "degrees");
+    }
+
+    private void writeOrbitStateVectors(final PrintStream p) {
+        final OrbitStateVector[] osvList = AbstractMetadata.getOrbitStateVectors(absRoot);
+        if(osvList != null && osvList.length > 0) {
+            double seconds = (osvList[0].time_mjd - dateDay.getMJD()) * daysToSeconds;
+            double seconds2 = (osvList[1].time_mjd - dateDay.getMJD()) * daysToSeconds;
+            double interval = seconds2 - seconds;
+
+            p.println(GammaConstants.HEADER_KEY_NUM_STATE_VECTORS + sep + osvList.length);
+            p.println(GammaConstants.HEADER_KEY_TIME_FIRST_STATE_VECTORS + sep + seconds + sep + "s");
+            p.println(GammaConstants.HEADER_KEY_STATE_VECTOR_INTERVAL + sep + interval + sep + "s");
+
+            int num = 1;
+            for(OrbitStateVector osv : osvList) {
+                p.println(GammaConstants.HEADER_KEY_STATE_VECTOR_POSITION +'_' + num + sep +
+                    osv.x_pos +sep+ osv.y_pos +sep+ osv.z_pos +sep+ "m   m   m");
+                p.println(GammaConstants.HEADER_KEY_STATE_VECTOR_VELOCITY +'_' + num + sep +
+                        osv.x_vel +sep+ osv.y_vel +sep+ osv.z_vel +sep+ "m/s m/s m/s");
+                ++num;
+            }
         }
     }
 
