fa93064d1f0f74092d8b5b3d5a3d98d4b48bf9b8
lveci <lveci@array.ca>
2016-12-16 13:14:19 -0500

    move CRSGeoCodingHandler

--- s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/support/CRSGeoCodingHandler.java
+++ s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/support/CRSGeoCodingHandler.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
-package org.esa.s1tbx.sar.gpf.geometric;
+package org.esa.s1tbx.insar.gpf.support;
 
 import org.esa.snap.core.datamodel.CrsGeoCoding;
 import org.esa.snap.core.datamodel.GeoPos;
