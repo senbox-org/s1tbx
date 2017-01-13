84434a84526cc2dfa2e47f66340cc11083b4ef86
lveci <lveci@array.ca>
2017-01-13 15:08:38 -0500

    update graphnode list

--- s1tbx-op-insar-ui/src/main/java/org/esa/s1tbx/insar/rcp/dialogs/VirtualStackCoregistrationDialog.java
+++ s1tbx-op-insar-ui/src/main/java/org/esa/s1tbx/insar/rcp/dialogs/VirtualStackCoregistrationDialog.java
@@ -65,7 +65,7 @@ public class VirtualStackCoregistrationDialog extends BatchGraphDialog {
             graphExecutorList.add(cloneGraphEx);
 
             // copy UI parameter to clone
-            final List<GraphNode> cloneGraphNodes = cloneGraphEx.GetGraphNodes();
+            final GraphNode[] cloneGraphNodes = cloneGraphEx.GetGraphNodes();
             for (GraphNode cloneNode : cloneGraphNodes) {
                 final GraphNode node = graphEx.getGraphNodeList().findGraphNode(cloneNode.getID());
                 //               if (node != null)
