e3611fce20105ab13e4f0566d12a116b6a41df27
marpet <marco.peters@brockmann-consult.de>
2017-02-16 11:39:10 +0100

    fixed shapefile writing

--- snap-statistics/src/main/java/org/esa/snap/statistics/output/EsriShapeFileWriter.java
+++ snap-statistics/src/main/java/org/esa/snap/statistics/output/EsriShapeFileWriter.java
@@ -1,12 +1,12 @@
 package org.esa.snap.statistics.output;
 
-import org.geotools.data.DataStore;
 import org.geotools.data.DataUtilities;
 import org.geotools.data.DefaultTransaction;
+import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.data.simple.SimpleFeatureStore;
 import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +20,9 @@ class EsriShapeFileWriter {
     private static final String FILE_EXTENSION_SHAPEFILE = ".shp";
 
     static void write(List<SimpleFeature> features, File file) throws IOException {
+        if(features.isEmpty()) {
+            return;
+        }
         String basename = file.getName();
         if (basename.endsWith(FILE_EXTENSION_SHAPEFILE)) {
             basename = basename.substring(0, basename.length() - 4);
@@ -28,23 +31,26 @@ class EsriShapeFileWriter {
 
         ShapefileDataStoreFactory factory = new ShapefileDataStoreFactory();
         Map<String, Serializable> map = Collections.singletonMap("url", shapefile.toURI().toURL());
-        DataStore dataStore = factory.createNewDataStore(map);
+        ShapefileDataStore dataStore = (ShapefileDataStore) factory.createNewDataStore(map);
         SimpleFeature simpleFeature = features.get(0);
-        SimpleFeatureType simpleFeatureType = simpleFeature.getType();
-        final String typeName = simpleFeatureType.getName().getLocalPart();
-        dataStore.createSchema(simpleFeatureType);
-        SimpleFeatureStore featureStore = (SimpleFeatureStore) dataStore.getFeatureSource(typeName);
-        DefaultTransaction transaction = new DefaultTransaction("X");
-        featureStore.setTransaction(transaction);
-        featureStore.addFeatures(DataUtilities.collection(features));
-        try {
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            throw new IOException("Cannot write shapefile. Error:\n" + e.getMessage(), e);
-        } finally {
-            dataStore.dispose();
-            transaction.close();
+        dataStore.createSchema(simpleFeature.getType());
+        SimpleFeatureSource featureSource = dataStore.getFeatureSource();
+        if (featureSource instanceof SimpleFeatureStore) {
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+            DefaultTransaction transaction = new DefaultTransaction("X");
+            featureStore.setTransaction(transaction);
+            featureStore.addFeatures(DataUtilities.collection(features));
+            try {
+                transaction.commit();
+            } catch (Exception e) {
+                transaction.rollback();
+                throw new IOException("Cannot write shapefile. Error:\n" + e.getMessage(), e);
+            } finally {
+                dataStore.dispose();
+                transaction.close();
+            }
+        } else {
+            throw new IOException("Cannot write shapefile. Error:\n" + "Write access not supported for " + file);
         }
     }
 }
