36ba1678a1499250196cb59c23ed301f71c00c32
marpet <marco.peters@brockmann-consult.de>
2017-02-20 09:59:27 +0100

    [SNAP-688] Method isCompatibleProduct() reporting severe errors

--- snap-core/src/main/java/org/esa/snap/core/datamodel/Product.java
+++ snap-core/src/main/java/org/esa/snap/core/datamodel/Product.java
@@ -1499,20 +1499,20 @@ public class Product extends ProductNode {
             return true;
         }
         if (getSceneRasterWidth() != product.getSceneRasterWidth()) {
-            SystemUtils.LOG.severe("raster width " + product.getSceneRasterWidth() + " not equal to " + getSceneRasterWidth());
+            SystemUtils.LOG.info("raster width " + product.getSceneRasterWidth() + " not equal to " + getSceneRasterWidth());
             return false;
         }
         if (getSceneRasterHeight() != product.getSceneRasterHeight()) {
-            SystemUtils.LOG.severe("raster width " + product.getSceneRasterHeight() + " not equal to " + getSceneRasterHeight());
+            SystemUtils.LOG.info("raster width " + product.getSceneRasterHeight() + " not equal to " + getSceneRasterHeight());
             return false;
         }
         if (getSceneGeoCoding() == null && product.getSceneGeoCoding() != null) {
-            SystemUtils.LOG.severe("no geocoding in master but in source");
+            SystemUtils.LOG.info("no geocoding in master but in source");
             return false;
         }
         if (getSceneGeoCoding() != null) {
             if (product.getSceneGeoCoding() == null) {
-                SystemUtils.LOG.severe("no geocoding in source but in master");
+                SystemUtils.LOG.info("no geocoding in source but in master");
                 return false;
             }
 
@@ -1525,7 +1525,7 @@ public class Product extends ProductNode {
             getSceneGeoCoding().getGeoPos(pixelPos, geoPos1);
             product.getSceneGeoCoding().getGeoPos(pixelPos, geoPos2);
             if (!equalsLatLon(geoPos1, geoPos2, eps)) {
-                SystemUtils.LOG.severe("first scan line left corner " + geoPos2 + " not equal to " + geoPos1);
+                SystemUtils.LOG.info("first scan line left corner " + geoPos2 + " not equal to " + geoPos1);
                 return false;
             }
 
@@ -1534,7 +1534,7 @@ public class Product extends ProductNode {
             getSceneGeoCoding().getGeoPos(pixelPos, geoPos1);
             product.getSceneGeoCoding().getGeoPos(pixelPos, geoPos2);
             if (!equalsLatLon(geoPos1, geoPos2, eps)) {
-                SystemUtils.LOG.severe("first scan line right corner " + geoPos2 + " not equal to " + geoPos1);
+                SystemUtils.LOG.info("first scan line right corner " + geoPos2 + " not equal to " + geoPos1);
                 return false;
             }
 
@@ -1543,7 +1543,7 @@ public class Product extends ProductNode {
             getSceneGeoCoding().getGeoPos(pixelPos, geoPos1);
             product.getSceneGeoCoding().getGeoPos(pixelPos, geoPos2);
             if (!equalsLatLon(geoPos1, geoPos2, eps)) {
-                SystemUtils.LOG.severe("last scan line left corner " + geoPos2 + " not equal to " + geoPos1);
+                SystemUtils.LOG.info("last scan line left corner " + geoPos2 + " not equal to " + geoPos1);
                 return false;
             }
 
@@ -1552,7 +1552,7 @@ public class Product extends ProductNode {
             getSceneGeoCoding().getGeoPos(pixelPos, geoPos1);
             product.getSceneGeoCoding().getGeoPos(pixelPos, geoPos2);
             if (!equalsLatLon(geoPos1, geoPos2, eps)) {
-                SystemUtils.LOG.severe("last scan line right corner " + geoPos2 + " not equal to " + geoPos1);
+                SystemUtils.LOG.info("last scan line right corner " + geoPos2 + " not equal to " + geoPos1);
                 return false;
             }
         }
