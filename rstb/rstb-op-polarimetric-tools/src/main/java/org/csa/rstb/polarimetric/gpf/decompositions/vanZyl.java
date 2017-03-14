673bb4dc7571228e93973c8482437b6213bb6f2b
jun--lu <junlu@array.ca>
2017-03-14 12:15:00 -0400

    updated algorithm

--- rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/decompositions/vanZyl.java
+++ rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/decompositions/vanZyl.java
@@ -24,6 +24,7 @@ import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.Tile;
 import org.esa.snap.engine_utilities.datamodel.Unit;
+import org.esa.snap.engine_utilities.eo.Constants;
 import org.esa.snap.engine_utilities.gpf.TileIndex;
 
 import java.awt.*;
@@ -111,8 +112,9 @@ public class vanZyl extends DecompositionBase implements Decomposition {
             PolOpUtils.getDataBuffer(op, bandList.srcBands, sourceRectangle, sourceProductType, sourceTiles, dataBuffers);
             final TileIndex srcIndex = new TileIndex(sourceTiles[0]);
 
-            double alpha, mu, rhoRe, rhoIm, rho2, eta, delta, lambda1, lambda2, lambda3, fs, fd, fv, tmp1, tmp2;
-            double Alpha2, Beta2, Ps, Pd, Pv;
+            double C11, C22, C33, ratio, HHHHv, VVVVv, HVHVv, HHVVvre, C13_re, C13_im, sq_rt, alp1, alp2, alp3, alpmin, FV;
+            double alpha, mu, rhoRe, rhoIm, rho2, eta, delta, lambda1, lambda2, tmp1, tmp2;
+            double Lambda1, Lambda2, AlphaRe, AlphaIm, BetaRe, BetaIm, Ps, Pd, Pv;
             for (int y = y0; y < maxY; ++y) {
                 trgIndex.calculateStride(y);
                 for (int x = x0; x < maxX; ++x) {
@@ -131,32 +133,96 @@ public class vanZyl extends DecompositionBase implements Decomposition {
                         PolOpUtils.t3ToC3(Tr, Ti, Cr, Ci);
                     }
 
-                    alpha = Cr[0][0];
-                    mu = Cr[2][2] / Cr[0][0];
-                    eta = Cr[1][1] / Cr[0][0];
-                    rhoRe = Cr[0][2] / Cr[0][0];
-                    rhoIm = Ci[0][2] / Cr[0][0];
+                    C11 = Cr[0][0];
+                    C22 = Cr[1][1];
+                    C33 = Cr[2][2];
+                    C13_re = Cr[0][2];
+                    C13_im = Ci[0][2];
+
+                    ratio = 10.0*Math.log10(C33/C11);
+                    if (ratio <= -2.0) {
+                        HHHHv = 8.0;
+                        VVVVv = 3.0;
+                        HVHVv = 4.0;
+                        HHVVvre = 2.0;
+                    } else if (ratio > 2.0) {
+                        HHHHv = 3.0;
+                        VVVVv = 8.0;
+                        HVHVv = 4.0;
+                        HHVVvre = 2.0;
+                    } else {
+                        HHHHv = 3.0;
+                        VVVVv = 3.0;
+                        HVHVv = 2.0;
+                        HHVVvre = 1.0;
+                    }
+
+                    sq_rt = C11*VVVVv + C33*HHHHv - 2.*C13_re*HHVVvre;
+                    sq_rt = sq_rt*sq_rt - 4.0*(HHVVvre*HHVVvre - HHHHv*VVVVv)*(C13_re*C13_re + C13_im*C13_im - C11*C33);
+                    sq_rt = Math.sqrt(sq_rt + Constants.EPS);
+
+                    alp1 = 2.0*C13_re*HHVVvre - (C11*VVVVv + C33*HHHHv) + sq_rt;
+                    alp1 = alp1 / 2.0 / (HHVVvre - HHHHv*VVVVv + Constants.EPS);
+
+                    alp2 = 2.0*C13_re*HHVVvre - (C11*VVVVv + C33*HHHHv) - sq_rt;
+                    alp2 = alp2 / 2.0 / (HHVVvre - HHHHv*VVVVv + Constants.EPS);
+
+                    alp3 = C22 / HVHVv;
+
+                    alpmin = Math.min(Math.min(alp1, alp2), alp3);
+                    if (ratio <= -2.0) {
+                        FV = 15.0 * alpmin;
+                        C11 = C11 - 8.0*alpmin;
+                        C33 = C33 - 3.0*alpmin;
+                        C13_re = C13_re - 2.0*alpmin;
+                    } else if (ratio > 2.0) {
+                        FV = 15.0 * alpmin;
+                        C11 = C11 - 3.0*alpmin;
+                        C33 = C33 - 8.0*alpmin;
+                        C13_re = C13_re - 2.0*alpmin;
+                    } else {
+                        FV = 8.0*alpmin;
+                        C11 = C11 - 3.0*alpmin;
+                        C33 = C33 - 3.0*alpmin;
+                        C13_re = C13_re - 1.0*alpmin;
+                    }
+
+                    alpha = C11;
+                    mu = C33 / C11;
+                    eta = C22 / C11;
+                    rhoRe = C13_re / C11;
+                    rhoIm = C13_im / C11;
                     rho2 = rhoRe * rhoRe + rhoIm * rhoIm;
 
                     delta = Math.sqrt((1.0 - mu) * (1.0 - mu) + 4.0 * rho2);
                     lambda1 = 0.5 * alpha * (1.0 + mu + delta);
                     lambda2 = 0.5 * alpha * (1.0 + mu - delta);
-                    lambda3 = alpha * eta;
 
                     tmp1 = (mu - 1.0 + delta) * (mu - 1.0 + delta);
                     tmp2 = tmp1 + 4.0 * rho2;
-                    fs = lambda1 * tmp1 / tmp2;
-                    Alpha2 = 4.0 * rho2 / tmp1;
-                    Ps = fs * (1.0 + Alpha2);
+                    Lambda1 = lambda1 * tmp1 / tmp2;
 
                     tmp1 = (mu - 1.0 - delta) * (mu - 1.0 - delta);
                     tmp2 = tmp1 + 4.0 * rho2;
-                    fd = lambda2 * tmp1 / tmp2;
-                    Beta2 = 4.0 * rho2 / tmp1;
-                    Pd = fd * (1.0 + Beta2);
+                    Lambda2 = lambda2 * tmp1 / tmp2;
 
-                    fv = lambda3;
-                    Pv = fv;
+                    AlphaRe = 2.0*rhoRe/(mu - 1.0 + delta);
+                    AlphaIm = 2.0*rhoIm/(mu - 1.0 + delta);
+                    BetaRe  = 2.0*rhoRe/(mu - 1.0 - delta);
+                    BetaIm  = 2.0*rhoIm/(mu - 1.0 - delta);
+
+                    tmp1 = Lambda1*((1.0 + AlphaRe)*(1.0 + AlphaRe) + AlphaIm*AlphaIm);
+                    tmp2 = Lambda2*((1.0 - AlphaRe)*(1.0 - AlphaRe) + AlphaIm*AlphaIm);
+
+                    if (tmp1 > tmp2) {
+                        Ps = Lambda1 * (1 + AlphaRe*AlphaRe + AlphaIm*AlphaIm);
+                        Pd = Lambda2 * (1 + BetaRe*BetaRe + BetaIm*BetaIm);
+                    } else {
+                        Pd = Lambda1 * (1 + AlphaRe*AlphaRe + AlphaIm*AlphaIm);
+                        Ps = Lambda2 * (1 + BetaRe*BetaRe + BetaIm*BetaIm);
+                    }
+
+                    Pv = FV;
 
                     Ps = scaleDb(Ps, bandList.spanMin, bandList.spanMax);
                     Pd = scaleDb(Pd, bandList.spanMin, bandList.spanMax);
