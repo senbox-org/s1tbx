8a7392ca5a7d224308d360ebc81fde1d6b5d6791
jun--lu <junlu@array.ca>
2017-03-20 16:09:36 -0400

    fixed a bug with external dem handling

--- s1tbx-op-insar-ui/src/main/java/org/esa/s1tbx/insar/gpf/ui/CoherenceOpUI.java
+++ s1tbx-op-insar-ui/src/main/java/org/esa/s1tbx/insar/gpf/ui/CoherenceOpUI.java
@@ -194,7 +194,11 @@ public class CoherenceOpUI extends BaseOperatorUI {
         final String demNameParam = (String) paramMap.get("demName");
         if (demNameParam != null) {
             ElevationModelDescriptor descriptor = ElevationModelRegistry.getInstance().getDescriptor(demNameParam);
-            demName.setSelectedItem(DEMFactory.getDEMDisplayName(descriptor));
+            if(descriptor != null) {
+                demName.setSelectedItem(DEMFactory.getDEMDisplayName(descriptor));
+            } else {
+                demName.setSelectedItem(demNameParam);
+            }
         }
 
         final File extFile = (File)paramMap.get("externalDEMFile");
