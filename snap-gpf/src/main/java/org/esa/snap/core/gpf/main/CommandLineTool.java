f760125f7f0d40e1f2a757f710073f56f4826318
marpet <marco.peters@brockmann-consult.de>
2017-04-21 14:24:36 +0200

    [SNAP-742] Properly scale memory values shown in diagnose information

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/main/CommandLineTool.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/main/CommandLineTool.java
@@ -169,15 +169,15 @@ class CommandLineTool implements GraphProcessingObserver {
         commandLineContext.print("Edit .snap/etc/snap.properties or gpt -c ${cachesize-in-GB}G -q ${parallelism} \n");
     }
 
-    static String fromBytes(long bytes) {
+    private String fromBytes(long bytes) {
         if(bytes > CommandLineArgs.G) {
-            return String.format("%.1f GB",(double)bytes / CommandLineArgs.G);
+            return Math.round(bytes / CommandLineArgs.G) + " GB";
         } else if(bytes > CommandLineArgs.M) {
-            return String.format("%.1f MB",(double)bytes / CommandLineArgs.M);
+            return Math.round(bytes / CommandLineArgs.M) + " MB";
         } else if(bytes > CommandLineArgs.K) {
-            return String.format("%.1f KB",(double)bytes / CommandLineArgs.K);
+            return Math.round(bytes / CommandLineArgs.K) + " KB";
         }
-        return String.format("%d B", bytes);
+        return bytes + " B";
     }
 
     private void run() throws Exception {
