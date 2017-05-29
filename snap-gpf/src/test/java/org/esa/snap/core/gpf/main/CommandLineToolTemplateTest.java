03ed9168eb1a351a3f82c0ee39b58c93a5987bf8
marpet <marco.peters@brockmann-consult.de>
2017-05-29 10:52:11 +0200

    [SNAP-765] Envi header files are wrong for multi-resolution products
    (cherry picked from commit f10ba90)

--- snap-gpf/src/test/java/org/esa/snap/core/gpf/main/CommandLineToolTemplateTest.java
+++ snap-gpf/src/test/java/org/esa/snap/core/gpf/main/CommandLineToolTemplateTest.java
@@ -93,8 +93,8 @@ public class CommandLineToolTemplateTest {
                         "\n" +
                         "    <target>\n" +
                         "        <name>projected_MERIS</name>\n" +
-                        "        <width>250</width>\n" +
-                        "        <height>500</height>\n" +
+                        "        <width>251</width>\n" +
+                        "        <height>501</height>\n" +
                         "    </target>\n" +
                         "\n" +
                         "    <parameterMetadata>\n" +
@@ -131,7 +131,7 @@ public class CommandLineToolTemplateTest {
         assertEquals("<html>\n" +
                         "<body>\n" +
                         "Size of MERIS: 10 x 10 pixels<br/>\n" +
-                        "Size of projected_MERIS: 250 x 500 pixels<br/>\n" +
+                        "Size of projected_MERIS: 251 x 501 pixels<br/>\n" +
                         "Extra data:<br/>\n" +
                         "processingCenter = BC<br/>\n" +
                         "softwareName = BEAM<br/>\n" +
@@ -217,8 +217,8 @@ public class CommandLineToolTemplateTest {
                         "\n" +
                         "    <target>\n" +
                         "        <name>projected_MERIS</name>\n" +
-                        "        <width>250</width>\n" +
-                        "        <height>500</height>\n" +
+                        "        <width>251</width>\n" +
+                        "        <height>501</height>\n" +
                         "    </target>\n" +
                         "\n" +
                         "    <parameterMetadata>\n" +
@@ -266,7 +266,7 @@ public class CommandLineToolTemplateTest {
         assertEquals("<html>\n" +
                         "<body>\n" +
                         "Size of MERIS: 10 x 10 pixels<br/>\n" +
-                        "Size of projected_MERIS: 250 x 500 pixels<br/>\n" +
+                        "Size of projected_MERIS: 251 x 501 pixels<br/>\n" +
                         "Extra data:<br/>\n" +
                         "processingCenter = BC<br/>\n" +
                         "softwareName = BEAM<br/>\n" +
