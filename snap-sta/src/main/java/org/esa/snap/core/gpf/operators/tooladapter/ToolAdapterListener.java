1b5308d83adffc7b80b3ed39acb2b81dc5496a30
Cosmin Cara <kraftek@c-s.ro>
2017-06-27 15:15:24 +0200

    Correction for variable update

--- snap-sta/src/main/java/org/esa/snap/core/gpf/operators/tooladapter/ToolAdapterListener.java
+++ snap-sta/src/main/java/org/esa/snap/core/gpf/operators/tooladapter/ToolAdapterListener.java
@@ -8,4 +8,5 @@ import org.esa.snap.core.gpf.descriptor.ToolAdapterOperatorDescriptor;
 public interface ToolAdapterListener {
     void adapterAdded(ToolAdapterOperatorDescriptor operatorDescriptor);
     void adapterRemoved(ToolAdapterOperatorDescriptor operatorDescriptor);
+    default void adapterUpdated(ToolAdapterOperatorDescriptor operatorDescriptor) { };
 }
