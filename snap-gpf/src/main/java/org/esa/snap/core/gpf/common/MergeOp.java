108c16113046f5c36c4f1de7a373067f81799ba2
marpet <marco.peters@brockmann-consult.de>
2017-07-11 15:04:18 +0200

    [SNAP-695] Implement exclude for Merge operator
    (cherry picked from commit 053307c)

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/common/MergeOp.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/common/MergeOp.java
@@ -31,8 +31,12 @@ import org.esa.snap.core.util.ProductUtils;
 import org.esa.snap.core.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -53,16 +57,14 @@ import java.util.regex.Pattern;
  * @author Thomas Storm
  */
 @OperatorMetadata(alias = "Merge",
-                  category = "Raster",
-                  description = "Allows copying raster data from any number of source products to a specified 'master'" +
-                          " product.",
-                  authors = "BEAM team",
-                  version = "1.1",
-                  copyright = "(c) 2012 by Brockmann Consult",
-                  internal = false)
+        category = "Raster",
+        description = "Allows merging of several source products by using specified 'master' as reference product.",
+        authors = "SNAP Team",
+        version = "1.2",
+        copyright = "(c) 2012 by Brockmann Consult")
 public class MergeOp extends Operator {
 
-    @SourceProduct(description = "The master product, which receives nodes from subsequently provided products.")
+    @SourceProduct(description = "The master, which serves as the reference, e.g. providing the geo-information.")
     private Product masterProduct;
 
     @SourceProducts(description = "The products to be merged into the master product.")
@@ -72,72 +74,117 @@ public class MergeOp extends Operator {
     private Product targetProduct;
 
     @Parameter(itemAlias = "include",
-               description = "Defines nodes to be included in the master product. If no includes are provided, all" +
-                       " nodes are copied.")
+            description = "Defines nodes to be included in the master product. If no includes are provided, all" +
+                    " nodes are copied.")
     private NodeDescriptor[] includes;
 
     @Parameter(itemAlias = "exclude",
-               description = "Defines nodes to be excluded from the target product (not supported yet).")
+            description = "Defines nodes to be excluded from the target product. Excludes have precedence above includes.")
     private NodeDescriptor[] excludes;
 
     @Parameter(defaultValue = "1.0E-5f",
-               description = "Defines the maximum lat/lon error in degree between the products. If set to NaN no check " +
-                             "for compatible geographic boundary is performed")
+            description = "Defines the maximum lat/lon error in degree between the products. If set to NaN no check " +
+                    "for compatible geographic boundary is performed")
     private float geographicError;
 
     @Override
     public void initialize() throws OperatorException {
-        targetProduct = masterProduct;
+        targetProduct = new Product(masterProduct.getName(),
+                                    masterProduct.getProductType(),
+                                    masterProduct.getSceneRasterWidth(),
+                                    masterProduct.getSceneRasterHeight());
+
+        ProductUtils.copyProductNodes(masterProduct, targetProduct);
+
         validateSourceProducts();
-        if (includes == null || includes.length == 0) {
-            List<NodeDescriptor> nodeDescriptorList = new ArrayList<NodeDescriptor>();
-            for (final Product sourceProduct : sourceProducts) {
-                if (sourceProduct != targetProduct) {
-                    for (String bandName : sourceProduct.getBandNames()) {
-                        final NodeDescriptor nodeDescriptor = new NodeDescriptor();
-                        nodeDescriptor.name = bandName;
-                        nodeDescriptor.productId = getSourceProductId(sourceProduct);
-                        nodeDescriptorList.add(nodeDescriptor);
-                    }
-                }
-            }
-            includes = nodeDescriptorList.toArray(new NodeDescriptor[nodeDescriptorList.size()]);
-        }
 
-        if (!(excludes == null || excludes.length == 0)) {
-            throw new OperatorException("Defining excludes is not supported yet.");
-        }
-        Set<Product> allSrcProducts = new HashSet<Product>();
-        for (NodeDescriptor nodeDescriptor : includes) {
-            Product srcProduct = getSourceProduct(nodeDescriptor.productId);
-            if (srcProduct == targetProduct) {
-                continue;
-            }
-            if (StringUtils.isNotNullAndNotEmpty(nodeDescriptor.name)) {
-                if (StringUtils.isNotNullAndNotEmpty(nodeDescriptor.newName)) {
-                    copyBandWithFeatures(srcProduct, nodeDescriptor.name, nodeDescriptor.newName);
-                } else {
-                    copyBandWithFeatures(srcProduct, nodeDescriptor.name, nodeDescriptor.name);
-                }
-                allSrcProducts.add(srcProduct);
-            } else if (StringUtils.isNotNullAndNotEmpty(nodeDescriptor.namePattern)) {
-                Pattern pattern = Pattern.compile(nodeDescriptor.namePattern);
-                for (String bandName : srcProduct.getBandNames()) {
-                    Matcher matcher = pattern.matcher(bandName);
-                    if (matcher.matches()) {
-                        copyBandWithFeatures(srcProduct, bandName, bandName);
-                        allSrcProducts.add(srcProduct);
+        ArrayList<Product> sourceProductList = new ArrayList<>();
+        sourceProductList.addAll(Arrays.asList(sourceProducts));
+        sourceProductList.add(0, masterProduct);
+        Map<String, List<NodeDescriptor>> inclusionMap = createDesciptorMap(includes == null ? new NodeDescriptor[0] : includes);
+        addDefaultInclusions(sourceProductList, inclusionMap);
+
+        Map<String, List<NodeDescriptor>> exclusionMap = createDesciptorMap(excludes == null ? new NodeDescriptor[0] : excludes);
+
+        Set<Product> usedProducts = new HashSet<>();
+        for (Map.Entry<String, List<NodeDescriptor>> entry : inclusionMap.entrySet()) {
+            String productId = entry.getKey();
+            Product product = getSourceProduct(productId);
+            List<NodeDescriptor> includesList = entry.getValue();
+            for (NodeDescriptor includeDescriptor : includesList) {
+                Pattern inclPattern = includeDescriptor.getCompiledNamePattern();
+                for (String bandName : product.getBandNames()) {
+                    Matcher inclMatcher = inclPattern.matcher(bandName);
+                    if (inclMatcher.matches()) {
+                        if(!shallBandBeExcluded(bandName, productId, exclusionMap)) {
+                            String newName = StringUtils.isNotNullAndNotEmpty(includeDescriptor.newName) ? includeDescriptor.newName : bandName;
+                            copyBandWithFeatures(product, bandName, newName);
+                            usedProducts.add(product);
+                        }
                     }
                 }
             }
         }
 
-        for (Product srcProduct : allSrcProducts) {
-            if (srcProduct != targetProduct) {
-                mergeAutoGrouping(srcProduct);
-                ProductUtils.copyMasks(srcProduct, targetProduct);
-                ProductUtils.copyOverlayMasks(srcProduct, targetProduct);
+        for (Product product : usedProducts) {
+            mergeAutoGrouping(product);
+            ProductUtils.copyMasks(product, targetProduct);
+            ProductUtils.copyOverlayMasks(product, targetProduct);
+        }
+    }
+
+    private boolean shallBandBeExcluded(String bandName, String productId, Map<String, List<NodeDescriptor>> exclusionMap) {
+        List<NodeDescriptor> excludesList = exclusionMap.get(productId);
+        if (excludesList != null) {
+            for (NodeDescriptor excludeDescriptor : excludesList) {
+                Pattern exclPattern = excludeDescriptor.getCompiledNamePattern();
+                Matcher exclMatcher = exclPattern.matcher(bandName);
+                if (exclMatcher.matches()) {
+                    return true;
+                }
             }
+        }
+        return false;
+    }
+
+    // if no inclusions is defined, all bands of the product shall be included
+    private void addDefaultInclusions(ArrayList<Product> sourceProductList, Map<String, List<NodeDescriptor>> inclusionMap) {
+        for (final Product sourceProduct : sourceProductList) {
+            String productId = getSourceProductId(sourceProduct);
+            if (!inclusionMap.containsKey(productId)) {
+                final NodeDescriptor nodeDescriptor = new NodeDescriptor();
+                nodeDescriptor.namePattern = ".*";
+                nodeDescriptor.productId = productId;
+                inclusionMap.put(nodeDescriptor.productId, Collections.singletonList(nodeDescriptor));
+            }
+        }
+    }
+
+    private Map<String, List<NodeDescriptor>> createDesciptorMap(NodeDescriptor[] descriptors) {
+        Map<String, List<NodeDescriptor>> map = new LinkedHashMap<>();
+        for (NodeDescriptor nd : descriptors) {
+            validateNodeDescriptor(nd);
+            List<NodeDescriptor> descriptorList = map.get(nd.productId);
+            if (descriptorList == null) {
+                ArrayList<NodeDescriptor> list = new ArrayList<>();
+                list.add(nd);
+                map.put(nd.productId, list);
+            } else {
+                descriptorList.add(nd);
+            }
+        }
+        return map;
+    }
+
+    private void validateNodeDescriptor(NodeDescriptor nd) {
+        if(StringUtils.isNullOrEmpty(nd.productId)) {
+            throw new OperatorException("Missing product id for an include or exclude description");
+        }
+        if (StringUtils.isNullOrEmpty(nd.name) && StringUtils.isNullOrEmpty(nd.namePattern)) {
+            throw new OperatorException(String.format("Neither 'name' nor 'namePattern' given node descriptor with product id '%s'", nd.productId));
+        }
+        if (StringUtils.isNotNullAndNotEmpty(nd.newName) && StringUtils.isNotNullAndNotEmpty(nd.namePattern)) {
+            throw new OperatorException(String.format("Property 'newName' cannot be used with 'namePattern' in node descriptor with product id '%s'", nd.productId));
         }
     }
 
@@ -185,12 +232,15 @@ public class MergeOp extends Operator {
         getLogger().warning("Wrongly configured operator. Tiles should not be requested.");
     }
 
+    @SuppressWarnings({"SameParameterValue", "WeakerAccess"})
     public static class NodeDescriptor {
 
         private String productId;
         private String name;
         private String newName;
         private String namePattern;
+
+        private String exclRegex;
 
         public void setProductId(String productId) {
             this.productId = productId;
@@ -208,6 +258,12 @@ public class MergeOp extends Operator {
             this.namePattern = namePattern;
         }
 
+        private Pattern getCompiledNamePattern() {
+            if (exclRegex == null) {
+                exclRegex = StringUtils.isNotNullAndNotEmpty(name) ? name : namePattern;
+            }
+            return Pattern.compile(exclRegex);
+        }
     }
 
 
--- snap-gpf/src/test/java/org/esa/snap/core/gpf/common/MergeOpTest.java
+++ snap-gpf/src/test/java/org/esa/snap/core/gpf/common/MergeOpTest.java
@@ -19,7 +19,6 @@ package org.esa.snap.core.gpf.common;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.gpf.OperatorException;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -29,15 +28,11 @@ import static org.junit.Assert.*;
  */
 public class MergeOpTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
     public void testMergeOp_includeAll() throws Exception {
         final Product productA = new Product("dummy1", "mergeOpTest", 10, 10);
         final Product productB = new Product("dummy2", "mergeOpTest", 10, 10);
-        
+
         productA.addBand("A", ProductData.TYPE_FLOAT32);
         productB.addBand("B", ProductData.TYPE_FLOAT32);
 
@@ -49,7 +44,48 @@ public class MergeOpTest {
         assertTrue(mergedProduct.containsBand("A"));
         assertTrue(mergedProduct.containsBand("B"));
         assertEquals("dummy1", mergedProduct.getName());
-        assertSame(productA, mergedProduct);
+    }
+
+    @Test
+    public void testMergeOp_usedForRenaming() throws Exception {
+        final Product master = new Product("dummy1", "mergeOpTest", 10, 10);
+        final Product second = new Product("dummy2", "mergeOpTest", 10, 10);
+
+        master.addBand("A", ProductData.TYPE_FLOAT32);
+        master.addBand("B", ProductData.TYPE_FLOAT32);
+        master.addBand("C", ProductData.TYPE_FLOAT32);
+        second.addBand("John", ProductData.TYPE_FLOAT32);
+
+        final MergeOp.NodeDescriptor aDescriptor = new MergeOp.NodeDescriptor();
+        aDescriptor.setName("A");
+        aDescriptor.setNewName("Alvin");
+        aDescriptor.setProductId("masterProduct");
+        final MergeOp.NodeDescriptor bDescriptor = new MergeOp.NodeDescriptor();
+        bDescriptor.setName("B");
+        bDescriptor.setNewName("BigBird");
+        bDescriptor.setProductId("masterProduct");
+        final MergeOp.NodeDescriptor cDescriptor = new MergeOp.NodeDescriptor();
+        cDescriptor.setName("C");
+        cDescriptor.setNewName("Charlie");
+        cDescriptor.setProductId("masterProduct");
+        final MergeOp.NodeDescriptor johnDescriptor = new MergeOp.NodeDescriptor();
+        johnDescriptor.setName("John");
+        johnDescriptor.setNewName("Jane");
+        johnDescriptor.setProductId("second");
+        final MergeOp.NodeDescriptor[] nodeDescriptors = new MergeOp.NodeDescriptor[]{
+                aDescriptor, bDescriptor, cDescriptor, johnDescriptor};
+
+        final MergeOp mergeOp = new MergeOp();
+        mergeOp.setSourceProduct("masterProduct", master);
+        mergeOp.setSourceProduct("second", second);
+        mergeOp.setParameter("includes", nodeDescriptors);
+        final Product mergedProduct = mergeOp.getTargetProduct();
+
+        assertNotNull(mergedProduct);
+        assertTrue(mergedProduct.containsBand("Alvin"));
+        assertTrue(mergedProduct.containsBand("BigBird"));
+        assertTrue(mergedProduct.containsBand("Charlie"));
+        assertTrue(mergedProduct.containsBand("Jane"));
     }
 
     @Test
@@ -103,6 +139,69 @@ public class MergeOpTest {
     }
 
     @Test
+    public void testMergeOp_excludeBandsFromMaster() throws Exception {
+        final Product productA = new Product("dummy1", "mergeOpTest", 10, 10);
+
+        productA.addBand("A_rem_0", ProductData.TYPE_FLOAT32);
+        productA.addBand("A_rem_1", ProductData.TYPE_FLOAT32);
+        productA.addBand("A_rem_2", ProductData.TYPE_FLOAT32);
+        productA.addBand("B", ProductData.TYPE_FLOAT32);
+        productA.addBand("C", ProductData.TYPE_FLOAT32);
+
+        final MergeOp mergeOp = new MergeOp();
+        mergeOp.setSourceProduct("masterProduct", productA);
+        final MergeOp.NodeDescriptor remDescriptor = new MergeOp.NodeDescriptor();
+        remDescriptor.setNamePattern(".*rem.*");
+        remDescriptor.setProductId("masterProduct");
+        final MergeOp.NodeDescriptor bDescriptor = new MergeOp.NodeDescriptor();
+        bDescriptor.setNamePattern("B");
+        bDescriptor.setProductId("masterProduct");
+        final MergeOp.NodeDescriptor[] excludeDescriptor = new MergeOp.NodeDescriptor[]{remDescriptor, bDescriptor};
+        mergeOp.setParameter("excludes", excludeDescriptor);
+        final Product mergedProduct = mergeOp.getTargetProduct();
+        assertNotNull(mergedProduct);
+        assertTrue(!mergedProduct.containsBand("A_rem_0"));
+        assertTrue(!mergedProduct.containsBand("A_rem_1"));
+        assertTrue(!mergedProduct.containsBand("A_rem_2"));
+        assertTrue(!mergedProduct.containsBand("B"));
+        assertTrue(mergedProduct.containsBand("C"));
+    }
+
+    @Test
+    public void testMergeOp_includeAndExclude() throws Exception {
+        final Product productA = new Product("dummy1", "mergeOpTest", 10, 10);
+
+        productA.addBand("master", ProductData.TYPE_FLOAT32);
+        productA.addBand("A", ProductData.TYPE_FLOAT32);
+        productA.addBand("A_rem_1", ProductData.TYPE_FLOAT32);
+        productA.addBand("A_rem_2", ProductData.TYPE_FLOAT32);
+        productA.addBand("B", ProductData.TYPE_FLOAT32);
+        productA.addBand("C", ProductData.TYPE_FLOAT32);
+
+        final MergeOp mergeOp = new MergeOp();
+        mergeOp.setSourceProduct("masterProduct", productA);
+        final MergeOp.NodeDescriptor inclRemDescriptor = new MergeOp.NodeDescriptor();
+        inclRemDescriptor.setNamePattern(".*rem.*");
+        inclRemDescriptor.setProductId("masterProduct");
+
+        final MergeOp.NodeDescriptor exclRem2Descriptor = new MergeOp.NodeDescriptor();
+        exclRem2Descriptor.setNamePattern("A_rem_2");
+        exclRem2Descriptor.setProductId("masterProduct");
+
+        final MergeOp.NodeDescriptor[] includeDescriptor = new MergeOp.NodeDescriptor[]{inclRemDescriptor};
+        final MergeOp.NodeDescriptor[] excludeDescriptor = new MergeOp.NodeDescriptor[]{exclRem2Descriptor};
+        mergeOp.setParameter("includes", includeDescriptor);
+        mergeOp.setParameter("excludes", excludeDescriptor);
+        final Product mergedProduct = mergeOp.getTargetProduct();
+        assertNotNull(mergedProduct);
+        assertTrue(mergedProduct.containsBand("A_rem_1")); // included  by include descriptor
+        assertTrue(!mergedProduct.containsBand("A_rem_2")); // excluded by exclude descriptor
+        assertTrue(!mergedProduct.containsBand("A")); // excluded by not being included
+        assertTrue(!mergedProduct.containsBand("B")); // excluded by not being included
+        assertTrue(!mergedProduct.containsBand("C")); // excluded by not being included
+    }
+
+    @Test
     public void testValidateSourceProducts_Failing() throws Exception {
         final MergeOp mergeOp = new MergeOp();
 
@@ -111,7 +210,7 @@ public class MergeOpTest {
 
         mergeOp.setSourceProduct("masterProduct", productA);
         mergeOp.setSourceProduct("dummy2", productB);
-        try{
+        try {
             mergeOp.getTargetProduct();
             fail();
         } catch (OperatorException e) {
