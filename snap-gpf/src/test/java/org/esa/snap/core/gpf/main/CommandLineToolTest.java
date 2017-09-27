3f4e7114f69f9dcc4c268a6fe4f5d794f64d12f7
Marco Peters <marco.peters@brockmann-consult.de>
2017-09-27 12:51:07 +0200

    added little test

--- snap-gpf/src/test/java/org/esa/snap/core/gpf/main/CommandLineToolTest.java
+++ snap-gpf/src/test/java/org/esa/snap/core/gpf/main/CommandLineToolTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.Locale;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CommandLineToolTest {
     @Test
@@ -12,6 +12,7 @@ public class CommandLineToolTest {
 
         Locale.setDefault(Locale.ENGLISH);
         
+        assertEquals("43.0 GB", CommandLineTool.fromBytes(43L*(1024*1024*1024L)));
         assertEquals("3.2 GB", CommandLineTool.fromBytes(3421000000L));
         assertEquals("1.2 GB", CommandLineTool.fromBytes(1297000000L));
         assertEquals("7.5 MB", CommandLineTool.fromBytes(7845000));
