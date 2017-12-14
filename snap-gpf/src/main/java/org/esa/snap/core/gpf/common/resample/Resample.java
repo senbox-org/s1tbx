208af6b9c0c42f66c2d95a49aabdf70a35cd457a
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-12-14 10:49:38 +0100

    fix

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/common/resample/Resample.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/common/resample/Resample.java
@@ -6,6 +6,7 @@ import com.bc.ceres.jai.GeneralFilterFunction;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.Kernel;
 import org.esa.snap.core.datamodel.RasterDataNode;
+import org.esa.snap.core.util.jai.JAIUtils;
 import org.geotools.resources.XArray;
 
 import javax.media.jai.BorderExtender;
@@ -33,9 +34,19 @@ public class Resample {
     public static MultiLevelImage createInterpolatedMultiLevelImage(MultiLevelImage sourceImage, double noDataValue,
                                                                     AffineTransform sourceImageToModelTransform,
                                                                     final int referenceWidth, int referenceHeight,
-                                                                    Dimension tileSize,
                                                                     MultiLevelModel referenceMultiLevelModel,
                                                                     Interpolation interpolation) {
+        final Dimension tileSize = JAIUtils.computePreferredTileSize(referenceWidth, referenceHeight, 1);
+        return createInterpolatedMultiLevelImage(sourceImage, noDataValue, sourceImageToModelTransform, referenceWidth,
+                                                 referenceHeight, tileSize, referenceMultiLevelModel, interpolation);
+    }
+
+    static MultiLevelImage createInterpolatedMultiLevelImage(MultiLevelImage sourceImage, double noDataValue,
+                                                             AffineTransform sourceImageToModelTransform,
+                                                             final int referenceWidth, int referenceHeight,
+                                                             Dimension tileSize,
+                                                             MultiLevelModel referenceMultiLevelModel,
+                                                             Interpolation interpolation) {
         final RenderingHints targetHints = getRenderingHints(noDataValue);
         final float[] scalings = getScalings(sourceImageToModelTransform, referenceMultiLevelModel);
         return InterpolationScaler.scaleMultiLevelImage(referenceWidth, referenceHeight, tileSize,
