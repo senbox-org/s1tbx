bfef8113fb77c9c39f3635afe9bbe82d2dca6f51
spatial <luis@skywatch.co>
2018-01-17 12:06:04 -0500

    don't seek forward searching for images

--- s1tbx-io/src/main/java/org/esa/s1tbx/io/imageio/ImageIOFile.java
+++ s1tbx-io/src/main/java/org/esa/s1tbx/io/imageio/ImageIOFile.java
@@ -113,7 +113,7 @@ public class ImageIOFile {
         reader = iioReader;
         initReader();
 
-        numImages = reader.getNumImages(!reader.isSeekForwardOnly());
+        numImages = reader.getNumImages(false);
         if(numImages < 0)
             numImages = 1;
         numBands = 3;
