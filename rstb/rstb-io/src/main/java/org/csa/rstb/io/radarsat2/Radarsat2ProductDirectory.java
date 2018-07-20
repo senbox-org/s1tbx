971341c6f0de6debb7e7120f18379db7c5dbfa16
Luis Veci <luis@skywatch.co>
2018-07-20 16:22:58 -0400

    fix missing lat lon tpg
    # Conflicts:
    #	rstb/rstb-io/src/main/java/org/csa/rstb/io/radarsat2/Radarsat2ProductDirectory.java

--- rstb/rstb-io/src/main/java/org/csa/rstb/io/radarsat2/Radarsat2ProductDirectory.java
+++ rstb/rstb-io/src/main/java/org/csa/rstb/io/radarsat2/Radarsat2ProductDirectory.java
@@ -522,7 +522,7 @@ public class Radarsat2ProductDirectory extends XMLProductDirectory {
     @Override
     protected void addGeoCoding(final Product product) {
 
-        MetadataElement absRoot = AbstractMetadata.getAbstractedMetadata(product);
+        final MetadataElement absRoot = AbstractMetadata.getAbstractedMetadata(product);
         final boolean isAscending = absRoot.getAttributeString(AbstractMetadata.PASS).equals("ASCENDING");
         final boolean isAntennaPointingRight = absRoot.getAttributeString(AbstractMetadata.antenna_pointing).equals("right");
 
@@ -620,9 +620,11 @@ public class Radarsat2ProductDirectory extends XMLProductDirectory {
             product.addTiePointGrid(latGrid);
             product.addTiePointGrid(lonGrid);
         }
-        product.setSceneGeoCoding(tpGeoCoding);
-
         setLatLongMetadata(product, latGrid, lonGrid);
+
+        if (product.getSceneGeoCoding() == null) {
+            product.setSceneGeoCoding(tpGeoCoding);
+        }
     }
 
     private static void setLatLongMetadata(Product product, TiePointGrid latGrid, TiePointGrid lonGrid) {
