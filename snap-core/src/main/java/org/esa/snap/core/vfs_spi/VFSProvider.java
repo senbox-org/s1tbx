d91654c0be19de11375e37bcac02e489b4476411
Adrian DRAGHICI <adraghici@c-s.ro>
2019-02-18 18:18:41 +0200

    Add VFS Service Provider Interface.

---
+++ snap-core/src/main/java/org/esa/snap/core/vfs_spi/VFSProvider.java
@@ -0,0 +1,26 @@
+package org.esa.snap.core.vfs_spi;
+
+import java.util.ServiceLoader;
+
+/**
+ * Service Provider Interface for VFS.
+ * Provide access to VFS modules internal methods using SPI via VFS Service Providers for prevent cyclic graphs caused by direct dependency to VFS modules from SNAP modules.
+ *
+ * @author Adrian Drăghici
+ */
+public interface VFSProvider {
+
+    <R> R runVFSService(String serviceMethodName, Class<? extends R> returnType, Object... params);
+
+    static VFSProvider getVFSProviderInstance(String vfsProviderClassName) throws ClassNotFoundException {
+        VFSProvider nioFileSystemViewInstance = null;
+        for (VFSProvider instance : ServiceLoader.load(VFSProvider.class)) {
+            nioFileSystemViewInstance = instance.getClass().getName().contentEquals(vfsProviderClassName) ? instance : nioFileSystemViewInstance;
+        }
+        if (nioFileSystemViewInstance == null) {
+            throw new ClassNotFoundException("Class: " + vfsProviderClassName + " is not a provider of this service!");
+        }
+        return nioFileSystemViewInstance;
+    }
+
+}
