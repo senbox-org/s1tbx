532f0496775213bf54887c86a76abb2078b44379
Omar Barrilero <omar.barrilero@c-s.fr>
2019-07-12 17:45:26 +0200

    Fix test

--- s1tbx-commons/src/main/java/org/esa/s1tbx/commons/test/TestData.java
+++ s1tbx-commons/src/main/java/org/esa/s1tbx/commons/test/TestData.java
@@ -48,7 +48,7 @@ public class TestData {
     public final static File inputStackIMS = new File(inputSAR + "Stack" + sep + "coregistered_stack.dim");
 
     //ERS
-    public final static File inputERS_IMP = new File(inputSAR + "ERS" + sep + "subset_0_of_ERS-1_SAR_PRI-ORBIT_32506_DATE__02-OCT-1997_14_53_43.data");
+    public final static File inputERS_IMP = new File(inputSAR + "ERS" + sep + "subset_0_of_ERS-1_SAR_PRI-ORBIT_32506_DATE__02-OCT-1997_14_53_43.dim");
 //    public final static File inputERS_IMP = new File(inputSAR + "ERS" + sep + "subset_0_of_ERS-1_SAR_PRI-ORBIT_32506_DATE__02-OCT-1997_14_53_43.dim");
     public final static File inputERS_IMS = new File(inputSAR + "ERS" + sep + "subset_0_of_ERS-2_SAR_SLC-ORBIT_10249_DATE__06-APR-1997_03_09_34.dim");
 
