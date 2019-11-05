f8c5034ef69108e78d3ae0595fd67155f02b33f7
Luis Veci <luis@skywatch.co>
2019-11-05 15:33:03 -0500

    update RS1 test

--- rstb/rstb-io/src/test/java/org/csa/rstb/io/radarsat1/TestRadarsatProductReader.java
+++ rstb/rstb-io/src/test/java/org/csa/rstb/io/radarsat1/TestRadarsatProductReader.java
@@ -1,25 +1,9 @@
-/*
- * Copyright (C) 2015 by Array Systems Computing Inc. http://www.array.ca
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see http://www.gnu.org/licenses/
- */
+
 package org.csa.rstb.io.radarsat1;
 
 import org.esa.s1tbx.commons.test.ReaderTest;
 import org.esa.s1tbx.commons.test.S1TBXTests;
-import org.esa.snap.engine_utilities.gpf.TestProcessor;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -33,20 +17,16 @@ import static org.junit.Assume.assumeTrue;
  */
 public class TestRadarsatProductReader extends ReaderTest  {
 
-    //private static final File zipFile = new File("E:\\data\\RS1\\Toronto\\RS1_m0700843_S7_20070802_232037_HH_SGF.zip");
-    private static final File zipFile = new File(S1TBXTests.inputPathProperty + S1TBXTests.sep +"SAR" + S1TBXTests.sep + "RS1" + S1TBXTests.sep + "RS1_m0700843_S7_20070802_232037_HH_SGF.zip");
-
-    private final static String inputRS1 = S1TBXTests.inputPathProperty + S1TBXTests.sep + "SAR" + S1TBXTests.sep  + "RS1" + S1TBXTests.sep ;
-    private final static File[] rootPathsRadarsat1 = S1TBXTests.loadFilePath(inputRS1);
+    private static final File zipFile = new File(S1TBXTests.inputPathProperty + S1TBXTests.sep +"SAR" + S1TBXTests.sep + "RS1\\RS1_m0700850_S7_20121103_232202_HH_SGF.zip");
+    private static final File folder = new File(S1TBXTests.inputPathProperty + S1TBXTests.sep +"SAR" + S1TBXTests.sep + "RS1\\RS1_m0700850_S7_20121103_232202_HH_SGF");
+    private static final File metaFile = new File(S1TBXTests.inputPathProperty + S1TBXTests.sep +"SAR" + S1TBXTests.sep + "RS1\\RS1_m0700850_S7_20121103_232202_HH_SGF\\RS1_m0700850_S7_20121103_232202_HH_SGF.vol");
 
     @Before
     public void setUp() {
         // If the file does not exist: the test will be ignored
         assumeTrue(zipFile + " not found", zipFile.exists());
-
-        for (File file : rootPathsRadarsat1) {
-            assumeTrue(file + " not found", file.exists());
-        }
+        assumeTrue(folder + " not found", folder.exists());
+        assumeTrue(metaFile + " not found", metaFile.exists());
     }
 
     public TestRadarsatProductReader() {
@@ -58,15 +38,13 @@ public class TestRadarsatProductReader extends ReaderTest  {
         testReader(zipFile);
     }
 
-    /**
-     * Open all files in a folder recursively
-     *
-     * @throws Exception anything
-     */
-    @Test
-    public void testOpenAll() throws Exception {
-        TestProcessor testProcessor = S1TBXTests.createS1TBXTestProcessor();
-        testProcessor.recurseReadFolder(this, rootPathsRadarsat1, readerPlugIn, reader, null, null);
-    }
+//    @Test
+//    public void testOpeningFolder() throws Exception {
+//        testReader(folder);
+//    }
 
+    @Test
+    public void testOpeningVolFile() throws Exception {
+        testReader(metaFile);
+    }
 }
