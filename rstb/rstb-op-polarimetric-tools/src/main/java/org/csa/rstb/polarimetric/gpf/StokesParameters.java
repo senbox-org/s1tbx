291e9b678f94c49f834674fab0c7ec69b3efb897
Luis Veci <luis@skywatch.co>
2019-11-08 15:25:56 -0500

    fix copyright
    # Conflicts:
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/gpf/ui/IEMHybridInverOpUI.java
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/gpf/ui/IEMInverOpUI.java
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/gpf/ui/IEMMultiAngleInverOpUI.java
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/gpf/ui/IEMMultiPolInverOpUI.java
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/gpf/ui/SMDielectricModelInverOpUI.java
    #	rstb/rstb-op-soil-moisture-ui/src/main/java/org/csa/rstb/soilmoisture/rstbSoilMoistureModule.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/SoilMoistureActivator.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/BaseDielectricModel.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/DielectricModel.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/DielectricModelFactory.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/HallikainenDielectricModel.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/IEMHybridInverOp.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/IEMInverBase.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/IEMMultiAngleInverOp.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/IEMMultiPolInverOp.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/MironovDielectricModel.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/SMDielectricModelInverOp.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/SVFMinimizer.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/gpf/SingleVarFunc.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/io/matlab/MatlabReader.java
    #	rstb/rstb-op-soil-moisture/src/main/java/org/csa/rstb/soilmoisture/io/matlab/MatlabReaderPlugIn.java

--- rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/StokesParameters.java
+++ rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/StokesParameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Skywatch Space Applications Inc. https://www.skywatch.co
+ * Copyright (C) 2019 by SkyWatch Space Applications Inc. http://www.skywatch.com
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
