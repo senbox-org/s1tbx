09ea0f126d59f7442c25f764f17bffb643360201
Luis Veci <luis@skywatch.co>
2019-11-08 15:25:56 -0500

    fix copyright

--- rstb/rstb-io/src/test/java/org/csa/rstb/io/rcm/TestRCMProductReader.java
+++ rstb/rstb-io/src/test/java/org/csa/rstb/io/rcm/TestRCMProductReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Skywatch. https://www.skywatch.co
+ * Copyright (C) 2018 Skywatch. https://www.skywatch.com
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
