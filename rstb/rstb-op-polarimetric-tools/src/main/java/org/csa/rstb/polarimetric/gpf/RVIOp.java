321cfc2089cccecc4082dc51aac6a2898c6cdd4a
Dipankar Mandal <dipankar.agengg@gmail.com>
2021-02-02 07:22:36 +0530

    copyright update
    Updated copyright header, license.

--- rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/RVIOp.java
+++ rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/RVIOp.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 by Microwave Remote Sensing Lab, IITBombay http://www.mrslab.in
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
 
 package org.csa.rstb.polarimetric.gpf;
 
@@ -45,7 +60,7 @@ import java.util.Map;
 
 @OperatorMetadata(alias = "Radar-Vegetation-Index",
         category = "Radar/Polarimetric",
-        authors = "Dipankar Mandal",
+        authors = "Dipankar Mandal et al.",
         version = "1.0",
         copyright = "Copyright (C) 2020 by Microwave Remote Sensing Lab, IITBombay",
         description = "Radar Vegetation Indices generation")
@@ -80,7 +95,7 @@ public final class RVIOp extends Operator implements DualPolProcessor {
 
             sourceProductType = PolBandUtils.getSourceProductType(sourceProduct);
             if (sourceProductType != PolBandUtils.MATRIX.C2) {
-                throw new OperatorException("Dual-pol source product is expected.");
+                throw new OperatorException("Dual-pol C2 matrix source product is expected.");
             }
 
             srcBandList = PolBandUtils.getSourceBands(sourceProduct, sourceProductType);
