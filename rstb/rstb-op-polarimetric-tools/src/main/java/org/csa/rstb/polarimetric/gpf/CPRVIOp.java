9fcd17aa6cfb087ed5c3fc1cc5515e77f3066fa8
Dipankar Mandal <dipankar.agengg@gmail.com>
2021-02-02 07:22:36 +0530

    copyright update
    Updated copyright header, license.

--- rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/CPRVIOp.java
+++ rstb/rstb-op-polarimetric-tools/src/main/java/org/csa/rstb/polarimetric/gpf/CPRVIOp.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright (C) 2020 by Microwave Remote Sensing Lab, IITBombay http://www.mrslab.in
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
 package org.csa.rstb.polarimetric.gpf;
 
 import org.apache.commons.math3.util.FastMath;
@@ -49,7 +63,7 @@ import java.util.Map;
 
 @OperatorMetadata(alias = "Compactpol-Radar-Vegetation-Index",
         category = "Radar/Polarimetric",
-        authors = "Dipankar Mandal",
+        authors = "Dipankar Mandal et al.",
         version = "1.0",
         copyright = "Copyright (C) 2020 by Microwave Remote Sensing Lab, IITBombay",
         description = "Compact-pol Radar Vegetation Indices generation")
