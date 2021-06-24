940f97028e89ef5866121e5d9ea3588e30508e83
Luis Veci <luis@skywatch.co>
2021-06-24 15:33:09 -0400

    move ApplyOrbitFileOp into Ephemeris

---
+++ s1tbx-io-ephemeris/src/test/java/org/esa/s1tbx/orbits/io/delft/TestOrbitalDataRecordReader.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.s1tbx.orbits.io.delft;
+
+import org.esa.s1tbx.orbits.io.delft.OrbitalDataRecordReader;
+import org.esa.snap.core.util.ResourceInstaller;
+import org.esa.snap.engine_utilities.util.TestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Path;
+
+/**
+ * OrbitalDataRecordReader Tester.
+ *
+ * @author lveci
+ */
+public class TestOrbitalDataRecordReader {
+
+    private final static String envisatOrbitFilePath = "org/esa/s1tbx/orbits/envisat_ODR.051";
+    private final static String ers1OrbitFilePath = "org/esa/s1tbx/orbits/ers1_ODR.079";
+    private final static String ers2OrbitFilePath = "org/esa/s1tbx/orbits/ers2_ODR.015";
+    private final Path basePath = ResourceInstaller.findModuleCodeBasePath(this.getClass());
+
+    @Test
+    public void testOpenFile() {
+
+        final OrbitalDataRecordReader reader = new OrbitalDataRecordReader();
+
+        Assert.assertTrue(reader.OpenOrbitFile(basePath.resolve(envisatOrbitFilePath)));
+    }
+
+    @Test
+    public void testReadHeader() {
+
+        final OrbitalDataRecordReader reader = new OrbitalDataRecordReader();
+
+        if (reader.OpenOrbitFile(basePath.resolve(envisatOrbitFilePath))) {
+
+            reader.parseHeader1();
+            reader.parseHeader2();
+        } else {
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testReadERS1OrbitFiles() throws Exception {
+        readOrbitFile("ERS1 ORD", basePath.resolve(ers1OrbitFilePath));
+    }
+
+    @Test
+    public void testReadERS2OrbitFile() throws Exception {
+        readOrbitFile("ERS2 ORD", basePath.resolve(ers2OrbitFilePath));
+    }
+
+    @Test
+    public void testReadEnvisatOrbitFile() throws Exception {
+        readOrbitFile("Envisat ORD", basePath.resolve(envisatOrbitFilePath));
+    }
+
+    private static void readOrbitFile(final String name, final Path path) throws Exception {
+        final OrbitalDataRecordReader reader = new OrbitalDataRecordReader();
+        final boolean res = reader.readOrbitFile(path);
+        assert(res);
+
+        final OrbitalDataRecordReader.OrbitDataRecord[] orbits = reader.getDataRecords();
+        final StringBuilder str = new StringBuilder(name+ " Num Orbits " + orbits.length);
+        for (int i = 0; i < 2; ++i) {
+            str.append(" Orbit time " + orbits[i].time);
+            str.append(" lat " + orbits[i].latitude);
+            str.append(" lng " + orbits[i].longitude);
+            str.append(" hgt " + orbits[i].heightOfCenterOfMass);
+        }
+        TestUtils.log.info(str.toString());
+    }
+
+}
---
+++ s1tbx-io-ephemeris/src/test/java/org/esa/s1tbx/orbits/io/envisat/TestEnvisatReader.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2015 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.s1tbx.orbits.io.envisat;
+
+import org.esa.s1tbx.commons.test.S1TBXTests;
+import org.esa.snap.core.dataio.ProductReader;
+import org.esa.snap.dataio.envisat.EnvisatProductReaderPlugIn;
+import org.esa.snap.engine_utilities.gpf.TestProcessor;
+import org.esa.snap.engine_utilities.util.TestUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assume.assumeTrue;
+
+/**
+ * Test Product Reader.
+ *
+ * @author lveci
+ */
+public class TestEnvisatReader {
+
+    static {
+        TestUtils.initTestEnvironment();
+    }
+
+    private EnvisatProductReaderPlugIn readerPlugin;
+    private ProductReader reader;
+
+    private String[] productTypeExemptions = {"WVW", "WVI", "WVS", "WSS"};
+
+    public TestEnvisatReader() {
+        readerPlugin = new EnvisatProductReaderPlugIn();
+        reader = readerPlugin.createReaderInstance();
+    }
+
+    private final static String inputASAR = S1TBXTests.inputPathProperty + S1TBXTests.sep + "SAR" + S1TBXTests.sep  + "ASAR" + S1TBXTests.sep ;
+    private final static File[] rootPathsASAR = S1TBXTests.loadFilePath(inputASAR);
+
+    @Before
+    public void setUp() {
+        // If any of the file does not exist: the test will be ignored
+        for(File file : rootPathsASAR) {
+            assumeTrue(file + " not found", file.exists());
+        }
+    }
+
+    /**
+     * Open all files in a folder recursively
+     *
+     * @throws Exception anything
+     */
+    @Test
+    public void testOpenAll() throws Exception {
+        TestProcessor testProcessor = S1TBXTests.createS1TBXTestProcessor();
+        testProcessor.recurseReadFolder(this, rootPathsASAR, readerPlugin, reader, productTypeExemptions, null);
+    }
+}
