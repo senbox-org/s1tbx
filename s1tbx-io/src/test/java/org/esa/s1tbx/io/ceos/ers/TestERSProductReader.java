1a2882076c5fb34dd1e02b557eba3ef1b0ae0751
Luis Veci <luis@skywatch.co>
2021-09-26 15:32:54 -0400

    update ERS tests

--- s1tbx-io/src/test/java/org/esa/s1tbx/io/ceos/ers/TestERSProductReader.java
+++ s1tbx-io/src/test/java/org/esa/s1tbx/io/ceos/ers/TestERSProductReader.java
@@ -15,9 +15,10 @@
  */
 package org.esa.s1tbx.io.ceos.ers;
 
+import org.esa.s1tbx.commons.test.ProductValidator;
 import org.esa.s1tbx.commons.test.ReaderTest;
 import org.esa.s1tbx.commons.test.S1TBXTests;
-import org.esa.snap.engine_utilities.gpf.TestProcessor;
+import org.esa.snap.core.datamodel.Product;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,30 +33,39 @@ import static org.junit.Assume.assumeTrue;
  */
 public class TestERSProductReader extends ReaderTest {
 
-    public final static String inputERS = S1TBXTests.inputPathProperty + S1TBXTests.sep + "SAR" + S1TBXTests.sep  + "ERS" + S1TBXTests.sep ;
-    public final static File[] rootPathsERS = S1TBXTests.loadFilePath(inputERS);
-
+    final static File ers1_ceos_vmp_slc = new File(S1TBXTests.inputPathProperty +
+            "/SAR/ERS/ERS_Tandem_Etna/ERS1_SLCI_VMP_CEOS_01081995_orbit 21159 frame 0747_IPAF/SCENE1/VDF_DAT.001");
+    final static File ers2_ceos_pri = new File(S1TBXTests.inputPathProperty +
+            "/SAR/ERS/ERS2.SAR.PRI_29JAN1996_orbit04055_frame2714/SCENE1/VDF_DAT.001");
 
     @Before
     public void setUp() {
         // If the file does not exist: the test will be ignored
-        for (File file : rootPathsERS) {
-            assumeTrue(file + " not found", file.exists());
-        }
+        assumeTrue(ers1_ceos_vmp_slc + " not found", ers1_ceos_vmp_slc.exists());
+        assumeTrue(ers2_ceos_pri + " not found", ers2_ceos_pri.exists());
     }
 
     public TestERSProductReader() {
         super(new ERSProductReaderPlugIn());
     }
 
-    /**
-     * Open all files in a folder recursively
-     *
-     * @throws Exception anything
-     */
     @Test
-    public void testOpenAll() throws Exception {
-        TestProcessor testProcessor = S1TBXTests.createS1TBXTestProcessor();
-        testProcessor.recurseReadFolder(this, rootPathsERS, readerPlugIn, reader, null, null);
+    public void testERS1_CEOS_VMP_SLC() throws Exception {
+        Product prod = testReader(ers1_ceos_vmp_slc.toPath());
+
+        final ProductValidator validator = new ProductValidator(prod);
+        validator.validateProduct();
+        validator.validateMetadata();
+        validator.validateBands(new String[] {"i","q","Intensity"});
+    }
+
+    @Test
+    public void testERS2_CEOS_PRI() throws Exception {
+        Product prod = testReader(ers2_ceos_pri.toPath());
+
+        final ProductValidator validator = new ProductValidator(prod);
+        validator.validateProduct();
+        validator.validateMetadata();
+        validator.validateBands(new String[] {"Amplitude","Intensity"});
     }
 }
