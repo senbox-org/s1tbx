720109fcd25cbbbb5151e1a72647b4ec3ec63076
Luis Veci <luis@skywatch.co>
2022-02-07 09:11:04 -0500

    S1TBX-902 fix RCM SLC Zip reading

--- rstb/rstb-io/src/test/java/org/csa/rstb/io/rcm/TestEODMSRCMProductReader.java
+++ rstb/rstb-io/src/test/java/org/csa/rstb/io/rcm/TestEODMSRCMProductReader.java
@@ -33,12 +33,17 @@ import static org.junit.Assume.assumeTrue;
  */
 public class TestEODMSRCMProductReader extends ReaderTest {
 
-    private final static File inputGRDZip = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD.zip");
-    private final static File inputGRDFolder = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD");
-    private final static File inputGRDManifest = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD/manifest.safe");
+    private final static File eodmsGRDZip = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD.zip");
+    private final static File eodmsGRDFolder = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD");
+    private final static File eodmsGRDManifest = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GRD/RCM1_OK1024567_PK1025865_1_SC50MA_20191205_225938_HH_HV_GRD/manifest.safe");
 
-    private final static File inputGRCFolder = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GCD/RCM2_OK1028884_PK1029284_2_16M12_20191214_111155_HH_HV_GCD");
-    private final static File inputGRCManifest = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GCD/RCM2_OK1028884_PK1029284_2_16M12_20191214_111155_HH_HV_GCD/manifest.safe");
+    private final static File eodmsGRCZip = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GCD/RCM2_OK1028884_PK1029284_2_16M12_20191214_111155_HH_HV_GCD.zip");
+    private final static File eodmsGRCFolder = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GCD/RCM2_OK1028884_PK1029284_2_16M12_20191214_111155_HH_HV_GCD");
+    private final static File eodmsGRCManifest = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/GCD/RCM2_OK1028884_PK1029284_2_16M12_20191214_111155_HH_HV_GCD/manifest.safe");
+
+    private final static File eodmsScanSAR_CX_SLCZip = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/SLC/RCM1_OK1683816_PK1836418_1_SC30MCPB_20211026_112824_CH_CV_SLC.zip");
+
+    private final static File eodmsQP_SLCZip = new File(S1TBXTests.TEST_ROOT +"RCM/EODMS/SLC/RCM1_OK1925486_PK1946630_1_QP8_20220103_110328_HH_VV_HV_VH_SLC.zip");
 
     private final static File inputQP_SLC = new File(S1TBXTests.TEST_ROOT + "RCM/OpenData/Quebec City/RCM3_OK1050546_PK1050547_1_QP8_20191229_110339_HH_VV_HV_VH_SLC");
 
@@ -54,12 +59,15 @@ public class TestEODMSRCMProductReader extends ReaderTest {
     @Before
     public void setUp() {
         // If any of the file does not exist: the test will be ignored
-        assumeTrue(inputGRDZip + " not found", inputGRDZip.exists());
-        assumeTrue(inputGRDFolder + " not found", inputGRDFolder.exists());
-        assumeTrue(inputGRDManifest + " not found", inputGRDManifest.exists());
+        assumeTrue(eodmsGRDZip + " not found", eodmsGRDZip.exists());
+        assumeTrue(eodmsGRDFolder + " not found", eodmsGRDFolder.exists());
+        assumeTrue(eodmsGRDManifest + " not found", eodmsGRDManifest.exists());
 
-        assumeTrue(inputGRCFolder + " not found", inputGRCFolder.exists());
-        assumeTrue(inputGRCManifest + " not found", inputGRCManifest.exists());
+        assumeTrue(eodmsGRCFolder + " not found", eodmsGRCFolder.exists());
+        assumeTrue(eodmsGRCManifest + " not found", eodmsGRCManifest.exists());
+
+        //assumeTrue(eodmsScanSAR_CX_SLCZip + " not found", eodmsScanSAR_CX_SLCZip.exists());
+        assumeTrue(eodmsQP_SLCZip + " not found", eodmsQP_SLCZip.exists());
 
         assumeTrue(inputQP_SLC + " not found", inputQP_SLC.exists());
         assumeTrue(inputCP_SLC + " not found", inputCP_SLC.exists());
@@ -70,7 +78,7 @@ public class TestEODMSRCMProductReader extends ReaderTest {
 
     @Test
     public void testOpeningGRDManifest() throws Exception {
-        Product prod = testReader(inputGRDManifest.toPath());
+        Product prod = testReader(eodmsGRDManifest.toPath());
 
         final ProductValidator validator = new ProductValidator(prod);
         validator.validateProduct();
@@ -80,7 +88,7 @@ public class TestEODMSRCMProductReader extends ReaderTest {
 
     @Test
     public void testOpeningGRDFolder() throws Exception {
-        Product prod = testReader(inputGRDFolder.toPath());
+        Product prod = testReader(eodmsGRDFolder.toPath());
 
         final ProductValidator validator = new ProductValidator(prod);
         validator.validateProduct();
@@ -90,7 +98,7 @@ public class TestEODMSRCMProductReader extends ReaderTest {
 
     @Test
     public void testOpeningGRCManifest() throws Exception {
-        Product prod = testReader(inputGRCManifest.toPath());
+        Product prod = testReader(eodmsGRCManifest.toPath());
 
         final ProductValidator validator = new ProductValidator(prod);
         validator.validateProduct();
@@ -100,12 +108,42 @@ public class TestEODMSRCMProductReader extends ReaderTest {
 
     @Test
     public void testOpeningGRCFolder() throws Exception {
-        Product prod = testReader(inputGRCFolder.toPath());
+        Product prod = testReader(eodmsGRCFolder.toPath());
 
         final ProductValidator validator = new ProductValidator(prod);
         validator.validateProduct();
         validator.validateMetadata();
         validator.validateBands(new String[] {"Amplitude_HH","Intensity_HH", "Amplitude_HV","Intensity_HV"});
+    }
+
+    @Test
+    public void testOpeningGRCZip() throws Exception {
+        Product prod = testReader(eodmsGRCZip.toPath());
+
+        final ProductValidator validator = new ProductValidator(prod);
+        validator.validateProduct();
+        validator.validateMetadata();
+        validator.validateBands(new String[] {"Amplitude_HH","Intensity_HH", "Amplitude_HV","Intensity_HV"});
+    }
+
+//    @Test
+//    public void testOpeningScanSAR_CX_SLCZip() throws Exception {
+//        Product prod = testReader(eodmsScanSAR_CX_SLCZip.toPath());
+//
+//        final ProductValidator validator = new ProductValidator(prod);
+//        validator.validateProduct();
+//        validator.validateMetadata();
+//        validator.validateBands(new String[] {"i_RCH","q_RCH","Intensity_RCH","i_RCV","q_RCV","Intensity_RCV"});
+//    }
+
+    @Test
+    public void testOpeningQP_SLCZip() throws Exception {
+        Product prod = testReader(eodmsQP_SLCZip.toPath());
+
+        final ProductValidator validator = new ProductValidator(prod);
+        validator.validateProduct();
+        validator.validateMetadata();
+        validator.validateBands(new String[] {"i_HH","q_HH","Intensity_HH", "i_HV","q_HV","Intensity_HV","i_VV","q_VV","Intensity_VV", "i_VH","q_VH","Intensity_VH"});
     }
 
     @Test
