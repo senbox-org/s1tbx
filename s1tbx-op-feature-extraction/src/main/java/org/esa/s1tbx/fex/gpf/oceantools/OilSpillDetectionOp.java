f10a52ddf49691e4def490908423286d3b5a1774
Jun Lu <jun@skywatch.com>
2022-04-12 21:20:05 -0400

    Modified computeBackgroundMean to speed up processing

--- s1tbx-op-feature-extraction/src/main/java/org/esa/s1tbx/fex/gpf/oceantools/OilSpillDetectionOp.java
+++ s1tbx-op-feature-extraction/src/main/java/org/esa/s1tbx/fex/gpf/oceantools/OilSpillDetectionOp.java
@@ -277,9 +277,15 @@ public class OilSpillDetectionOp extends Operator {
             final int maxy = ty0 + th;
             final int maxx = tx0 + tw;
 
+            double previousMean;
+            List<Double> colSumList = new ArrayList<>(w);
+            List<Integer> colNumPixelList = new ArrayList<>(w);
+
             for (int ty = ty0, y = dminY; ty < maxy && y < dmaxY; ty++, y++) {
                 int ds = y * w;
                 trgIndex.calculateStride(ty);
+                previousMean = -1.0;
+
                 for (int tx = tx0, x = dminX; tx < maxx && x < dmaxX; tx++, x++) {
                     double v = data[x + ds];
                     if (noDataValue.equals(v)) {
@@ -287,12 +293,15 @@ public class OilSpillDetectionOp extends Operator {
                         continue;
                     }
 
-                    final double backgroundMean = computeBackgroundMean(data, x, y, dmaxX, dmaxY, w, noDataValue);
+                    final double backgroundMean = computeBackgroundMean(data, x, y, dmaxX, dmaxY, w, noDataValue,
+                            previousMean, colSumList, colNumPixelList);
+
                     if (backgroundMean != noDataValue && v < backgroundMean / kInLinearScale) {
                         trgData.setElemIntAt(trgIndex.getIndex(tx), 1);
                     } else {
                         trgData.setElemIntAt(trgIndex.getIndex(tx), 0);
                     }
+                    previousMean = backgroundMean;
                 }
             }
         } catch (Throwable e) {
@@ -306,33 +315,67 @@ public class OilSpillDetectionOp extends Operator {
      * @param x          The x coordinate of the central point of the sliding window.
      * @param y          The y coordinate of the central point of the sliding window.
      * @param maxX       max x of data
-     * @param maxX       max y of data
+     * @param maxY       max y of data
      * @param width      width of data rect
      * @param noDataValue the place holder for no data
-     * @return The mena value.
+     * @param previousMean Mean value computed for previous window
+     * @param colSumList Column sum of pixels in the previous window
+     * @param colNumPixelList The number of valid pixels for each column in the previous window
+     * @return The mean value
      */
-    private double computeBackgroundMean(float[] data, int x, int y, int maxX, int maxY, int width, final double noDataValue) {
+    private double computeBackgroundMean(float[] data, int x, int y, int maxX, int maxY, int width,
+                                         final double noDataValue, final double previousMean,
+                                         List<Double> colSumList, List<Integer> colNumPixelList) {
 
         final int x0 = Math.max(x - halfBackgroundWindowSize, 0);
         final int y0 = Math.max(y - halfBackgroundWindowSize, 0);
         final int xMax = Math.min(x + halfBackgroundWindowSize, maxX-1);
         final int yMax = Math.min(y + halfBackgroundWindowSize, maxY-1);
 
-        double mean = 0.0;
-        int numPixels = 0;
-        for (int yy = y0; yy < yMax; yy++) {
-            int stride = yy * width;
-            for (int xx = x0; xx < xMax; xx++) {
-                double v = data[xx + stride];
+        if (previousMean != -1.0) {
+            double colSum = 0.0;
+            int colNumPixels = 0;
+            for (int yy = y0; yy < yMax; yy++) {
+                double v = data[yy * width + xMax - 1];
                 if (v != noDataValue) {
-                    mean += v;
-                    numPixels++;
+                    colSum += v;
+                    colNumPixels++;
                 }
             }
+
+            int previousValidPixels = colNumPixelList.stream().reduce(0, Integer::sum);
+            final double previousSum = previousMean * previousValidPixels;
+            final double mean = (previousSum - colSumList.get(0) + colSum) /
+                    (previousValidPixels - colNumPixelList.get(0) + colNumPixels);
+            colSumList.remove(0);
+            colSumList.add(colSum);
+            colNumPixelList.remove(0);
+            colNumPixelList.add(colNumPixels);
+            return mean;
         }
 
-        if (numPixels > 0) {
-            return mean / numPixels;
+        colSumList.clear();
+        colNumPixelList.clear();
+        double totalSum = 0.0;
+        int totalNumPixels = 0;
+        for (int xx = x0; xx < xMax; xx++) {
+            double colSum = 0.0;
+            int colNumPixels = 0;
+            for (int yy = y0; yy < yMax; yy++) {
+                double v = data[yy * width + xx];
+                if (v != noDataValue) {
+                    colSum += v;
+                    colNumPixels++;
+                }
+            }
+            colSumList.add(colSum);
+            colNumPixelList.add(colNumPixels);
+            totalSum += colSum;
+            totalNumPixels += colNumPixels;
+        }
+
+        if (totalNumPixels > 0) {
+            return totalSum / totalNumPixels;
         } else {
             return noDataValue;
         }
