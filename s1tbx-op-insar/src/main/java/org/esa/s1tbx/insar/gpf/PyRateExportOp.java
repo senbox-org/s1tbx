54f7b1b4faf12388a07fcc32f5352a997fb298f7
Alex McVittie <alexander.mcvittie@gmail.com>
2023-03-30 16:41:24 -0400

    Basic params

--- s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/PyRateExportOp.java
+++ s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/PyRateExportOp.java
@@ -1,6 +1,14 @@
 package org.esa.s1tbx.insar.gpf;
 
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.Operator;
+import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+import org.esa.snap.core.gpf.annotations.SourceProducts;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+
+import java.io.File;
 
 /**
  * Export products into format suitable for import to PyRate
@@ -13,5 +21,24 @@ import org.esa.snap.core.gpf.annotations.OperatorMetadata;
         autoWriteDisabled = true,
         description = "Export data for PyRate processing")
 
-public class PyRateExportOp {
+public class PyRateExportOp extends Operator {
+
+    @SourceProducts
+    private Product[] sourceProduct;
+
+    @TargetProduct
+    private Product targetProduct;
+
+    @Parameter(description = "The output folder to which the data product is written.")
+    private File targetFolder;
+
+    @Parameter(description = "Include coherence", defaultValue = "true")
+    private Boolean includeCoherenceBands = true;
+
+
+
+    @Override
+    public void initialize() throws OperatorException {
+
+    }
 }
