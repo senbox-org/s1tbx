079659d2203ed8a11cd2f93ec5821dadb62e9edc
Alex McVittie <alexander.mcvittie@gmail.com>
2023-03-30 16:18:22 -0400

    Set name

--- s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/PyRateExportOp.java
+++ s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/PyRateExportOp.java
@@ -5,13 +5,13 @@ import org.esa.snap.core.gpf.annotations.OperatorMetadata;
 /**
  * Export products into format suitable for import to PyRate
  */
-@OperatorMetadata(alias = "StampsExport",
+@OperatorMetadata(alias = "PyrateExport",
         category = "Radar/Interferometric/PSI \\ SBAS",
         authors = "Alex McVittie",
         version = "1.0",
         copyright = "Copyright (C) 2023 SkyWatch Space Applications Inc.",
         autoWriteDisabled = true,
-        description = "Export data for StaMPS processing")
+        description = "Export data for PyRate processing")
 
 public class PyRateExportOp {
 }
