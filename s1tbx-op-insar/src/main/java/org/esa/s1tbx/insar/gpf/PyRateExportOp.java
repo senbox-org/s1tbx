868b6991ed339d4ca0bd336e4c4f04c59dbc17be
Alex McVittie <alexander.mcvittie@gmail.com>
2023-03-30 16:17:27 -0400

    Initial creation of operator class

---
+++ s1tbx-op-insar/src/main/java/org/esa/s1tbx/insar/gpf/PyRateExportOp.java
@@ -0,0 +1,17 @@
+package org.esa.s1tbx.insar.gpf;
+
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+
+/**
+ * Export products into format suitable for import to PyRate
+ */
+@OperatorMetadata(alias = "StampsExport",
+        category = "Radar/Interferometric/PSI \\ SBAS",
+        authors = "Alex McVittie",
+        version = "1.0",
+        copyright = "Copyright (C) 2023 SkyWatch Space Applications Inc.",
+        autoWriteDisabled = true,
+        description = "Export data for StaMPS processing")
+
+public class PyRateExportOp {
+}
